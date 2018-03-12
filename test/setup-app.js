a0fbfe13ac91dcc54dc421bee814c9f4c0a00340
Alan Verbner <alverbner@gmail.com>
2018-03-12 08:38:37 -0300

    Initial commit

---
+++ test/setup-app.js
@@ -0,0 +1,10 @@
+import { jsdom } from 'jsdom';
+import hook from 'css-modules-require-hook';
+
+global.document = jsdom('<!doctype html><html><body></body></html>');
+global.window = document.defaultView;
+global.navigator = global.window.navigator;
+
+hook({
+  generateScopedName: '[name]__[local]___[hash:base64:5]'
+});
---
+++ webpack/prod.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+
+const customPath = path.join(__dirname, './customPublicPath');
+
+module.exports = {
+  entry: {
+    todoapp: [customPath, path.join(__dirname, '../chrome/extension/todoapp')],
+    background: [customPath, path.join(__dirname, '../chrome/extension/background')],
+    inject: [customPath, path.join(__dirname, '../chrome/extension/inject')]
+  },
+  output: {
+    path: path.join(__dirname, '../build/js'),
+    filename: '[name].bundle.js',
+    chunkFilename: '[id].chunk.js'
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.IgnorePlugin(/[^/]+\/[\S]+.dev$/),
+    new webpack.optimize.UglifyJsPlugin({
+      comments: false,
+      compressor: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+  ],
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['react-optimize']
+      }
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+        {
+          loader: 'postcss-loader',
+          options: {
+            plugins: () => [autoprefixer]
+          }
+        }
+      ]
+    }]
+  }
+};
