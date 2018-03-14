98cd29051288019980c25e7f1b554e5f0d04b6a0
github_iohk.com <mirko.alic@iohk.io>
2018-03-14 17:07:31 -0300

    [PI-19] Fix Uint8array decoding from storage

--- app/state/WalletStorage.js
+++ app/state/WalletStorage.js
@@ -2,9 +2,24 @@
 const WalletStorage = {};
 const WALLET_STORAGE_SK_KEY = 'WALLET_STORAGE_SK_KEY';
 
-// Private method.
+// [Private method]
+// Prevent change wallet information
 const freezeWallet = function () {
   if (WalletStorage.sk) Object.freeze(WalletStorage);
+};
+
+// [Private method]
+// encodeUint8Array :: String with JSON format => Uint8Array -> String
+const encodeUint8Array = function (x) {
+  return JSON.stringify(x);
+};
+
+// [Private method]
+// decodeUint8Array :: String with JSON format => String -> Uint8Array
+const decodeUint8Array = function (x) {
+  const xObj = JSON.parse(x);
+  const xAsArray = Object.values(xObj);
+  return Uint8Array.from(xAsArray);
 };
 
 WalletStorage.initWallet = function () {
@@ -13,7 +28,7 @@ WalletStorage.initWallet = function () {
     if (window.localStorage) {
       try {
         const walletJson = localStorage.getItem(WALLET_STORAGE_SK_KEY);
-        this.sk = Uint8Array.from(walletJson);
+        this.sk = decodeUint8Array(walletJson);
         freezeWallet();
       } catch (e) {
         console.warn('[WalletStorage - initWallet] Unable to return Wallet info');
@@ -36,7 +51,7 @@ WalletStorage.setWallet = function (wallet) {
   this.sk = wallet;
   if (window.localStorage) {
     console.log('[WalletStorage - SetWallet] saving wallet..');
-    const walletJson = JSON.stringify(this.sk);
+    const walletJson = encodeUint8Array(this.sk);
     localStorage.setItem(WALLET_STORAGE_SK_KEY, walletJson);
   } else {
     console.warn('[WalletStorage - SetWallet] The browser doesn\'t support local storage');
