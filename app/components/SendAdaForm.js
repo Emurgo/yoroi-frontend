55af1b2be5f082a1526ae1b793f8a536115d082d
Alan Verbner <alan.verbner@iohk.io>
2018-03-19 14:52:52 -0300

    [PI-23] Basic form definition

---
+++ app/components/SendAdaForm.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+class SendAdaForm extends Component {
+  
+  handleChange = field => (event) => {
+    this.setState({ [field]: event.target.value });
+  };
+
+  handleSubmit = submitCb => () => {
+    submitCb(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        <TextField
+          hintText=""
+          floatingLabelText="To"
+          onChange={this.handleChange('to')}
+        />
+        <TextField
+          hintText=""
+          floatingLabelText="Amount"
+          onChange={this.handleChange('amount')}
+        />
+        <RaisedButton label="Import" primary onClick={this.handleSubmit(this.props.onSubmit)} />
+      </div>
+    );
+  }
+}
+
+SendAdaForm.propTypes = {
+  onSubmit: PropTypes.func
+};
+
+export default SendAdaForm;
