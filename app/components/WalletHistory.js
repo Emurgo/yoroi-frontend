9f3bd70c0f8e5f4754f1b610d69ffc36b43efa3d
Alan Verbner <alan.verbner@iohk.io>
2018-03-25 11:17:34 -0300

    Update card status

--- app/components/WalletHistory.js
+++ app/components/WalletHistory.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import List, {
   ListItem,
@@ -8,6 +8,12 @@ import Card, { CardContent } from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
 import Avatar from 'material-ui/Avatar';
 import NumberFormat from 'react-number-format';
+
+import ExpansionPanel, {
+  ExpansionPanelDetails,
+  ExpansionPanelSummary,
+} from 'material-ui/ExpansionPanel';
+
 // import Button from 'material-ui/Button';
 // import { openTx } from '../utils/explorerLinks';
 import {
@@ -16,63 +22,88 @@ import {
 } from '../utils/formatter';
 import style from './WalletHistory.css';
 
-const WalletHistory = (props) => {
-  const getAmount = ({ ctbOutputSum: { getCoin } }) => {
+class WalletHistory extends Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  getAmount = ({ ctbOutputSum: { getCoin } }) => {
     return Number(getCoin) / 1000000;
   };
+  
   // TODO: Choose between send or receive
-  const getSendReceivIconPath = (tx) => {
+  getSendReceivIconPath = (tx) => {
     console.log("tc", tx);
     if (Math.random() > 0.5) {
       return 'img/send-ic.svg';
     }
     return 'img/receive-ic.svg';
   };
-  const getTransactionItem = (tx) => {
+  getTransactionItem = (tx, expandedId, onExpand) => {
+    const txId = tx.ctbId;
     return (
-      <Card>
-        <CardContent className={style.cardContent}>
-          <Avatar src={getSendReceivIconPath(tx)} />
+      <ExpansionPanel key={txId} expanded={expandedId === txId} onChange={() => onExpand(txId)}>
+        <ExpansionPanelSummary classes={{ content: 'expansion-panel-summary-content' }}>
+          <Avatar src={this.getSendReceivIconPath(tx)} />
           <div className={style.itemBody}>
-            <Typography variant="title">Ada received</Typography>
-            <Typography variant="body2" color="textSecondary">{formatTimestamp(tx.ctbTimeIssued)}</Typography>
-            <Typography variant="subheading">Transaction ID</Typography>
-            <Typography variant="body2" color="textSecondary">{formatTransactionID(tx.ctbId)}</Typography>
+            <Typography variant="body1">Received</Typography>
           </div>
           <div className={style.amount}>
-            <Typography variant="subheading">
-              <NumberFormat thousandSeparator value={getAmount(tx)} displayType="text" suffix=" ADA" />
-            </Typography>
+            <div>
+              <Typography variant="subheading">
+                <NumberFormat thousandSeparator value={this.getAmount(tx)} displayType="text" suffix=" ADA" />
+              </Typography>
+            </div>
+            <div>
+              <Typography variant="body2" color="textSecondary">{formatTimestamp(tx.ctbTimeIssued)}</Typography>
+            </div>
           </div>
-        </CardContent>
-      </Card>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography variant="subheading">Transaction ID</Typography>
+          <Typography variant="body2" color="textSecondary">{formatTransactionID(tx.ctbId)}</Typography>
+          <Typography variant="body2" color="textSecondary">{formatTimestamp(tx.ctbTimeIssued)}
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
     );
   };
 
-  const getTransactionsItems = (txs) => {
-    return txs.map((tx, index) => {
-      return getTransactionItem(tx, index);
+  handleItemClick = currExpanded => (clickedId) => {
+    const expandedId = currExpanded === clickedId ? '' : clickedId;
+    this.setState({ expandedId })
+  }
+
+  getTransactionsItems = (txs) => {
+    const currExpanded = this.state.expandedId;
+    return txs.map((tx) => {
+      return this.getTransactionItem(tx, currExpanded, this.handleItemClick(currExpanded));
     });
   };
 
-  const getTransactionHistoryComponent = () => {
-    return getTransactionsItems(props.txs);
+  getTransactionHistoryComponent = () => {
+    return this.getTransactionsItems(this.props.txs, this.state.expanded);
   };
 
-  const getNoTransactionHistoryComponent = () => (
+  getNoTransactionHistoryComponent = () => (
     <ListItem>
       <ListItemText primary="No Transactions History" />
     </ListItem>
   );
 
-  return (
-    <List className={style.listContainer}>
-      {
-        (props.txs && props.txs.length !== 0) ?
-        getTransactionHistoryComponent() : getNoTransactionHistoryComponent()
-      }
-    </List>
-  );
+  render() {
+    const txs = this.props;
+    return (
+      <List className={style.listContainer}>
+        {
+          (txs && txs.length !== 0) ?
+          this.getTransactionHistoryComponent() : this.getNoTransactionHistoryComponent()
+        }
+      </List>
+    );
+  }
 };
 
 WalletHistory.propTypes = {
