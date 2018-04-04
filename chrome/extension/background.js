95bd00996f1c31196d57835766844c0b40be0129
Alan Verbner <alan.verbner@iohk.io>
2018-04-04 10:59:49 -0300

    Load the extension in a new tab

--- chrome/extension/background.js
+++ chrome/extension/background.js
@@ -6,7 +6,7 @@ function promisifier(method) {
   // return a function
   return function promisified(...args) {
     // which returns a promise
-    return new Promise((resolve) => {
+    return new Promise(resolve => {
       args.push(resolve);
       method.apply(this, args);
     });
@@ -18,14 +18,11 @@ function promisifyAll(obj, list) {
 }
 
 // let chrome extension api support Promise
-promisifyAll(chrome, [
-  'tabs',
-  'windows',
-  'browserAction',
-  'contextMenus'
-]);
-promisifyAll(chrome.storage, [
-  'local',
-]);
+promisifyAll(chrome, ['tabs', 'windows', 'browserAction', 'contextMenus']);
+promisifyAll(chrome.storage, ['local']);
+
+chrome.browserAction.onClicked.addListener(() => {
+  chrome.tabs.create({ url: 'main_window.html' });
+});
 
 require('./background/badge');
