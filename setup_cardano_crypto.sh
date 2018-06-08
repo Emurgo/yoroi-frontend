a6fd9cc5be7a3701f530a2cbd32d7929cf002fc1
Alan Verbner <alan.verbner@iohk.io>
2018-06-08 10:46:01 -0300

    [PI-157] Update rust update instructions in README

--- setup_cardano_crypto.sh
+++ setup_cardano_crypto.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # https://stackoverflow.com/a/10168693/3329806
-git submodule update --init --remote --merge && \
+git submodule update --init --remote && \
 cd js-cardano-wasm && \
 npm install && \
 ./build && \
