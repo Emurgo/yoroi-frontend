afc45421e7ccc00bdfde91018c0ae4a00a624757
github_iohk.com <mirko.alic@iohk.io>
2018-07-04 10:36:18 -0300

    [PI-237] Fix create wallet page

--- app/containers/wallet/WalletAddPage.js
+++ app/containers/wallet/WalletAddPage.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import WalletAdd from '../../components/wallet/WalletAdd';
 import WalletAddDialog from '../../components/wallet/WalletAddDialog';
-import WalletCreateDialog from '../../components/wallet/WalletCreateDialog';
 import WalletRestoreDialog from '../../components/wallet/WalletRestoreDialog';
+import WalletCreateDialog from '../../components/wallet/WalletCreateDialog';
+import WalletBackupDialog from '../../components/wallet/WalletBackupDialog';
 import WalletRestoreDialogContainer from '../wallet/dialogs/WalletRestoreDialogContainer';
 import WalletCreateDialogContainer from '../wallet/dialogs/WalletCreateDialogContainer';
+import WalletBackupDialogContainer from '../wallet/dialogs/WalletBackupDialogContainer';
 import environment from '../../environment';
 import resolver from '../../utils/imports';
 import type { InjectedProps } from '../../types/injectedPropsType';
@@ -41,6 +43,8 @@ export default class WalletAddPage extends Component<Props> {
       content = <WalletCreateDialogContainer onClose={this.onClose} />;
     } else if (uiDialogs.isOpen(WalletRestoreDialog)) {
       content = <WalletRestoreDialogContainer onClose={this.onClose} />;
+    } else if (uiDialogs.isOpen(WalletBackupDialog)) {
+      content = <WalletBackupDialogContainer onClose={this.onClose} />;
     } else {
       content = (
         <WalletAdd
