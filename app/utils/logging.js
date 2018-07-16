f7ed72805213d7ea059b298e402edfb30565e788
Alan Verbner <alan.verbner@iohk.io>
2018-07-16 18:11:19 -0300

    [PI-283] In memory log tracking and download link

--- app/utils/logging.js
+++ app/utils/logging.js
@@ -1,30 +1,45 @@
 // @flow
+import RingBuffer from 'ringbufferjs';
+import moment from 'moment';
+import FileSaver from 'file-saver';
+
+import type { ConfigType } from '../../config/config-types';
+
 const logger = console;
+declare var CONFIG: ConfigType;
+const { logsBufferSize, logsFileSuffix } = CONFIG.app;
+const logs = new RingBuffer(logsBufferSize);
 
 export const Logger = {
 
-  debug: (data: string) => {
+  debug: (data : string) => {
     logger.debug(data);
   },
 
-  info: (data: string) => {
+  info: (data : string) => {
     logger.info(data);
   },
 
-  error: (data: string) => {
+  error: (data : string) => {
     logger.error(data);
+    logs.enq(`[${moment().format()}] ${data}\n`);
   },
 
-  warn: (data: string) => {
+  warn: (data : string) => {
     logger.info(data);
-  },
+  }
+};
 
+export const downloadLogs = () => {
+  const toDownload = logs.peekN(logs.size() - 1);
+  const blob = new Blob(toDownload, { type: 'text/plain;charset=utf-8' });
+  FileSaver.saveAs(blob, `${moment().format()}${logsFileSuffix}`);
 };
 
 // ========== STRINGIFY =========
 
-export const stringifyData = (data: any) => JSON.stringify(data, null, 2);
+export const stringifyData = (data : any) => JSON.stringify(data, null, 2);
 
-export const stringifyError = (error: any) => (
+export const stringifyError = (error : any) => (
   JSON.stringify(error, Object.getOwnPropertyNames(error), 2)
 );
