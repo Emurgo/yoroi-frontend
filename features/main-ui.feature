95fd3c85225f6134aee2268d7172d12a00a57293
AlexGavrilas <alexgavrilas92@gmail.com>
2018-09-11 16:13:45 +0300

    add it-25 test

--- features/main-ui.feature
+++ features/main-ui.feature
@@ -20,12 +20,23 @@ Feature: Main UI
     Then I should see the balance number "9,007,199,254.720698 ADA"
 
   @it-15
-  Scenario: Main Screen Tabs Switching
+  Scenario: Main Screen Tabs Switching (IT-15)
     Given I am testing "Main UI"
-    Given There is a wallet stored named complex-wallet
-    When I go to the send transaction screen
+    When There is a wallet stored named complex-wallet
+    And I go to the send transaction screen
     Then I should see send transaction screen
     When I go to the receive screen
     Then I should see the Receive screen
     When I go to the transaction history screen
     Then I should see the transactions screen
+
+  @it-25
+  Scenario: Ensure user can copy Wallet address to Windows clipboard via "Copy address" buttons (IT-25)
+    Given I am testing "Main UI"
+    When There is a wallet stored named complex-wallet
+    When I go to the receive screen
+    Then I should see the Receive screen
+    When I click on "copy to clipboard" button
+    Then I should see "You have successfully copied wallet address" pop up:
+    | message                                            |
+    | wallet.receive.page.addressCopyNotificationMessage |
--- features/step_definitions/main-ui-steps.js
+++ features/step_definitions/main-ui-steps.js
@@ -1,6 +1,15 @@
 import { When, Given, Then } from 'cucumber';
-import { By } from 'selenium-webdriver';
+import { By, Key } from 'selenium-webdriver'; 
 import { expect } from 'chai';
+import i18n from '../support/helpers/i18n-helpers';
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function checkErrorByTranslationId(client, errorSelector, error) {
+  await client.waitUntilText(errorSelector, await client.intl(error.message));
+}
 
 Then(/^I should see the balance number "([^"]*)"$/, async function (number) {
   await this.waitUntilText('.TopBar_walletAmount', number);
@@ -17,4 +26,14 @@ Then(/^I go to the transaction history screen$/, async function () {
 
 Then(/^I should see the transactions screen$/, async function () {
   await this.waitForElement("//div[@class='WalletSummary_numberOfTransactions']", By.xpath);
+});
+//
+Then(/^I click on "copy to clipboard" button$/, async function () {
+  await this.click("//div[@class = 'WalletReceive_hash']//following-sibling::span", By.xpath);
+});
+
+Then(/^I should see "You have successfully copied wallet address" pop up:$/, async function (data) {
+  const error = data.hashes()[0];
+  const errorMessage = await this.intl(error.message);
+  await this.waitForElement(`//div[@class='VerticalFlexContainer_component']//span[contains(text(), '${errorMessage}')]`, By.xpath);
 });
