b17c595548dacf188b4df2d5ef1e536799b85cef
AlexGavrilas <alexgavrilas92@gmail.com>
2018-09-20 14:01:33 +0300

    add it-34 test

---
+++ features/support/helpers/helpers.js
@@ -0,0 +1,11 @@
+export const checkIfElementsInArrayAreUnique = async function (arr) {
+    arr.sort();
+     for (let i = 0; i < arr.length; i++) {
+         if (arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])) { 
+             return false; 
+         } 
+     } 
+     return true;
+ }
+
+  
