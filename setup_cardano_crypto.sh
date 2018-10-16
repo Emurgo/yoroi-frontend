34f8e8b5a7784fa322c32b42a3fb2016398f6f69
Nicolas Arqueros <nykholas@gmail.com>
2018-10-16 22:38:22 +0200

    updates cardano library

--- setup_cardano_crypto.sh
+++ setup_cardano_crypto.sh
@@ -4,7 +4,7 @@ git submodule update --init --recursive && \
 cd js-cardano-wasm && \
 git checkout e099e7e666805b5efb4eac14103b5dafdb1b73de && \
 cd rust && \
-git checkout fe3f0870638ae41bb80afdc0cea59febe502d687 && \
+git checkout 13e342a3b7cd99676e330d3f36368c063f76544b && \
 cd .. && \
 npm install && \
 ../js-cardano-wasm-build && \
