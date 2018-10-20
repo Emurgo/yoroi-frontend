0ddf9c07ebdcd36075ce7e7df3ed3e58ed9d084a
sebastiengllmt <sebastiengllmt@gmail.com>
2018-10-21 04:17:12 +0900

    use hack to suppress whitelist generation

--- translations/translation-runner.js
+++ translations/translation-runner.js
@@ -1,8 +1,19 @@
-const manageTranslations = require('react-intl-translations-manager').default;
+const manageTranslations = require('react-intl-translations-manager');
 
-manageTranslations({
+function disableWhitelistFile(langResults) {
+  // we hijack this hook to modify an otherwise unrelated property
+  // this causes a downstream function that writes whitelists to write to dev/null
+  // kind of hacky but this suprpesses generation of pointless whitelist files
+  langResults.whitelistFilepath = "/dev/null";
+  return undefined;
+}
+
+manageTranslations.default({
   messagesDirectory: 'translations/messages',
   translationsDirectory: 'app/i18n/locales',
   singleMessagesFile: true,
-  languages: ['en-US', 'zh-Hans', 'zh-Hant', 'ko-KR', 'ja-JP']
+  languages: ['en-US', 'zh-Hans', 'zh-Hant', 'ko-KR', 'ja-JP'],
+  overrideCoreMethods: {
+    provideWhitelistFile: (langResults) => { disableWhitelistFile(langResults); }
+  }
 });
