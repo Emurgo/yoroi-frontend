5acd662ec4e3d15b209429dd175528bb82acbc1c
sebastiengllmt <sebastiengllmt@gmail.com>
2018-10-21 04:23:49 +0900

    small cleanup

--- translations/translation-runner.js
+++ translations/translation-runner.js
@@ -1,14 +1,16 @@
-const manageTranslations = require('react-intl-translations-manager');
+const manageTranslations = require('react-intl-translations-manager').default;
 
 function disableWhitelistFile(langResults) {
   // we hijack this hook to modify an otherwise unrelated property
   // this causes a downstream function that writes whitelists to write to dev/null
   // kind of hacky but this suprpesses generation of pointless whitelist files
+
+  // TBD: does this work on Windows? Maybe there is a more platform-independent way to do this
   langResults.whitelistFilepath = "/dev/null";
   return undefined;
 }
 
-manageTranslations.default({
+manageTranslations({
   messagesDirectory: 'translations/messages',
   translationsDirectory: 'app/i18n/locales',
   singleMessagesFile: true,
