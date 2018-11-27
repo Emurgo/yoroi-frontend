4c9956e252564a90eeec07da476a1c8c0909e93b
sebastiengllmt <sebastiengllmt@gmail.com>
2018-11-27 20:51:19 +0900

    PR feedback

--- features/support/helpers/helpers.js
+++ features/support/helpers/helpers.js
@@ -3,11 +3,5 @@
 export const checkIfElementsInArrayAreUnique = async function (
   arr: Array<string>
 ): Promise<boolean> {
-  arr.sort();
-  for (let i = 0; i < arr.length; i++) {
-    if (arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])) {
-      return false;
-    }
-  }
-  return true;
+  return (new Set(arr)).size === arr.length;
 };
