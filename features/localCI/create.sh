7a5b0261018915ed393788531c448058d98f95f8
Sebastien Guillemot <SebastienGllmt@gmail.com>
2018-12-06 13:59:47 +0900

    Add all steps for to run CI locally (#132)
    Hopefully this should be a pretty easy setup.
    
    This nice since you can run the CI in the background and it won't interrupt you by having Chrome popup to run the tests if you ran them in the regular way (although it takes a bit longer since you have to copy your files to the docker container)
    Also, since you're reusing the same docker container, it is much faster than pushing many small changes to Github hoping the CI will eventually pass.

---
+++ features/localCI/create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get update; apt-get clean
+
+# Install x11vnc.
+sudo apt-get install -y x11vnc
+
+# Install xvfb.
+sudo apt-get install -y xvfb
+
+# Install fluxbox.
+sudo apt-get install -y fluxbox
+
+# Install wget.
+sudo apt-get install -y wget
+
+# Install wmctrl.
+sudo apt-get install -y wmctrl
