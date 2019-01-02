b7fd73a7e2af0ad65ca0338c0aeca6ac62882ef5
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2019-01-02 21:02:17 +0300

    Created export-api

---
+++ app/api/export/index.js
@@ -0,0 +1,137 @@
+import moment from 'moment';
+import { sendFileToUser } from './utils';
+
+export type TransactionExportRow = {
+  type: 'in' | 'out',
+  amount: string,
+  fee: string,
+  date: Date
+}
+
+export type CsvData = {
+  headers: Array<string>,
+  rows: Array<Array<string>>
+}
+
+export type TransactionExportFileType = 'csv';
+export type TransactionExportDataFormat = 'CoinTracking';
+
+export type ExportFileResponse = {
+  data: Blob,
+  fileType: TransactionExportFileType
+}
+
+/**
+ * This api layer provides access to the electron local storage
+ * for user settings that are not synced with any coin backend.
+ */
+
+export default class ExportApi {
+
+  /**
+   * Request object MUST contains:
+   * - rows: array of export-data
+   * - fileName: only name without extension
+   *
+   * No result will be returned. File is sent to user as side-effect.
+   */
+  exportTransactions(request : {
+    rows: Array<TransactionExportRow>,
+    format?: TransactionExportDataFormat,
+    fileType?: TransactionExportFileType,
+    fileName: string
+  }) {
+    const { rows, format, fileType, fileName } = request;
+    const data = this.convertExportRowsToCsv(rows, format);
+    const fileResponse = this.convertCsvDataToFile(data, fileType);
+    this.sendFileToUser(fileResponse.data, `${fileName}.${fileResponse.fileType}`);
+  }
+
+  // noinspection JSMethodCanBeStatic
+  /**
+   * Convert specified abstract rows to a specific data-format.
+   */
+  convertExportRowsToCsv(
+    rows: Array<TransactionExportRow>,
+    format?: TransactionExportDataFormat
+  ): CsvData {
+    switch (format || 'CoinTracking') {
+      case 'CoinTracking': return _formatExportRowsIntoCoinTrackingFormat(rows);
+      default: throw new Error('Unexpected export data format: ' + format);
+    }
+  }
+
+  // noinspection JSMethodCanBeStatic
+  /**
+   * Convert specified abstract CsvData to a byte-blob of a specific file-type.
+   */
+  convertCsvDataToFile(
+    data: CsvData,
+    fileType?: TransactionExportFileType
+  ): ExportFileResponse {
+    fileType = fileType || 'csv';
+    switch (fileType) {
+      case 'csv': return {
+        data: new Blob([_writeCsvDataIntoCsvFile(data)], {
+          type: 'text/csv;charset=utf-8'
+        }),
+        fileType
+      };
+      default: throw new Error('Unexpected file type: ' + fileType);
+    }
+  }
+
+  // noinspection JSMethodCanBeStatic
+  /**
+   * Make browser to download the specified blob of bytes as a file with the specified name
+   */
+  sendFileToUser(data: Blob, fileName: string) {
+    sendFileToUser(data, fileName);
+  }
+
+}
+
+function _formatExportRowsIntoCoinTrackingFormat(rows: Array<TransactionExportRow>): CsvData {
+  return {
+    headers: [
+      'Type (Trade, IN or OUT)',
+      'Buy Amount',
+      'Buy Cur.',
+      'Sell Amount',
+      'Sell Cur.',
+      'Fee Amount (optional)',
+      'Fee Cur. (optional)',
+      'Exchange (optional)',
+      'Trade Group (optional)',
+      'Comment (optional)',
+      'Date'
+    ],
+    rows: rows.map(r => [
+      _formatExportRowTypeForCoinTracking(r.type),
+      r.type === 'in' ? r.amount : '',
+      r.type === 'in' ? 'ADA' : '',
+      r.type === 'out' ? r.amount : '',
+      r.type === 'out' ? 'ADA' : '',
+      r.type === 'out' ? r.fee : '',
+      r.type === 'out' ? 'ADA' : '',
+      '',
+      '',
+      '',
+      moment(r.date).format('YYYY-MM-DD HH:mm:ss')
+    ])
+  };
+}
+
+function _formatExportRowTypeForCoinTracking(type: string): string {
+  switch (type) {
+    case 'in': return 'Deposit';
+    case 'out': return 'Withdrawal';
+    default: throw new Error('Unexpected export row type: ' + type);
+  }
+}
+
+function _writeCsvDataIntoCsvFile(data: CsvData): string {
+  return [data.headers, ...data.rows]
+    .map(x => x.map(s => `"${s.replace(/"/g, '""')}"`).join(','))
+    .join('\n');
+}
--- app/api/index.js
+++ app/api/index.js
@@ -1,13 +1,16 @@
 // @flow
 import AdaApi from './ada/index';
 import LocalStorageApi from './localStorage/index';
+import ExportApi from './export/index';
 
 export type Api = {
   ada: AdaApi,
   localStorage: LocalStorageApi,
+  export: ExportApi,
 };
 
 export const setupApi = (): Api => ({
   ada: new AdaApi(),
   localStorage: new LocalStorageApi(),
+  export: new ExportApi(),
 });
