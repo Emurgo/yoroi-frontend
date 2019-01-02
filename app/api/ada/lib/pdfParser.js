ce56b74b7c4511bc9031d9e8b67a1299215df2ac
Nicole Tobal <ntobal@atixlabs.com>
2019-01-02 15:19:04 -0300

    [PI-7] File decryption method added

--- app/api/ada/lib/pdfParser.js
+++ app/api/ada/lib/pdfParser.js
@@ -1,6 +1,7 @@
 // @flow
 import pdfjsLib from 'pdfjs-dist';
 import type { FileEvent, PDF } from '../adaTypes';
+import { decryptForceVend, decryptRecoveryRegularVend, decryptRecoveryForceVend, decryptRegularVend } from './decrypt';
 
 export const getSelectedFile = (event: FileEvent): Blob => event.target.files[0];
 
@@ -18,8 +19,34 @@ export const readFile = (file: Blob): Promise<Uint8Array> =>
     } catch (error) {
       reject(error);
     }
-  })
-;
+  });
+
+export const decryptFile = (
+  decryptionKey: string | Array<string>,
+  redemptionType: string,
+  file: Uint8Array
+): Uint8Array => {
+  // If pass phrase is given assume that it's an encrypted certificate
+  if (decryptionKey) {
+    // Decrypt the file
+    let decryptedFile;
+    switch (redemptionType) {
+      case 'forceVended':
+        decryptedFile = decryptForceVend(decryptionKey, file);
+        break;
+      case 'recoveryRegular':
+        decryptedFile = decryptRecoveryRegularVend(decryptionKey, file);
+        break;
+      case 'recoveryForceVended':
+        decryptedFile = decryptRecoveryForceVend(decryptionKey, file);
+        break;
+      default: // regular
+        decryptedFile = decryptRegularVend(decryptionKey, file);
+    }
+    return decryptedFile;
+  }
+  return file;
+};
 
 // It was based in the following example: https://ourcodeworld.com/articles/read/405/how-to-convert-pdf-to-text-extract-text-from-pdf-with-javascript
 export const parsePDFFile = (file: Uint8Array): Promise<string> => (
--- tests/unit/test/pdfParser_test.js
+++ tests/unit/test/pdfParser_test.js
@@ -6,10 +6,10 @@ const pdfParser = require('../../../app/api/ada/lib/pdfParser');
 
 describe('PDF parse test', () => {
   it('should parse PDF content', async () => {
-    const fileBuffer = getMockedFileBuffer('test.pdf');
+    const fileBuffer = getMockedFileBuffer('regular.pdf');
     try {
       const result = await pdfParser.parsePDFFile(fileBuffer);
-      assert.equal(result, mockData.PDFContent, 'PDF parser result does not equal content');
+      assert.equal(result, mockData.parsePDF.PDFContent, 'PDF parser result does not equal content');
     } catch (error) {
       assert.fail(error.message);
     }
@@ -20,17 +20,60 @@ describe('PDF parse test', () => {
       await pdfParser.parsePDFFile();
       assert.fail();
     } catch (error) {
-      assert(true);
+      assert.notEqual(error.message, 'assert.fail()', 'Test failed');
     }
   });
 
   it('should fail parsing PDF content when an invalid file is passed', async () => {
-    const fileBuffer = getMockedFileBuffer('test.txt');
+    const fileBuffer = getMockedFileBuffer('regular.txt');
     try {
       await pdfParser.parsePDFFile(Buffer.from(fileBuffer));
       assert.fail();
     } catch (error) {
-      assert(true);
+      assert.notEqual(error.message, 'assert.fail()', 'Test failed');
+    }
+  });
+});
+
+describe('PDF decrypt test', () => {
+  it('should decrypt and read encrypted regular PDF content', async() => {
+    const fileBuffer = getMockedFileBuffer('regular.pdf.enc');
+    try {
+      const decryptedFile = pdfParser.decryptFile(mockData.decryptPDF.passphrase,
+        mockData.decryptPDF.regularTitle, fileBuffer);
+      const result = await pdfParser.parsePDFFile(Buffer.from(decryptedFile));
+      assert.equal(result, mockData.decryptPDF.regularPDFContent, 'PDF parser result does not equal content');
+    } catch (error) {
+      assert.fail(error.message);
+    }
+  });
+
+  it('should decrypt and read encrypted force vended PDF content', async() => {
+    const fileBuffer = getMockedFileBuffer('force-vended.pdf.enc');
+    try {
+      const decryptedFile = pdfParser.decryptFile(mockData.decryptPDF.data, 'forceVended', fileBuffer);
+      const result = await pdfParser.parsePDFFile(Buffer.from(decryptedFile));
+      assert.equal(result, mockData.decryptPDF.forceVendedPDFContent, 'PDF parser result does not equal content');
+    } catch (error) {
+      assert.fail(error.message);
+    }
+  });
+
+  it('should fail at decrypting with no data', async() => {
+    try {
+      const decryptedFile = pdfParser.decryptFile();
+      assert.equal(decryptedFile, undefined);
+    } catch (error) {
+      assert.notEqual(error.message, 'assert.fail()', 'Test failed');
+    }
+  });
+
+  it('should fail at decrypting when only redemtion key is passed', async() => {
+    try {
+      pdfParser.decryptFile(mockData.decryptPDF.passphrase);
+      assert.fail();
+    } catch (error) {
+      assert.notEqual(error.message, 'assert.fail()', 'Test failed');
     }
   });
 });
