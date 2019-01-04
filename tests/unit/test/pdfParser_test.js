863cb28ece080550c8f68fd20e5919e583cf2778
Nicole Tobal <ntobal@atixlabs.com>
2019-01-04 11:09:42 -0300

    [PI-7] Try-catch taken out from tests

--- tests/unit/test/pdfParser_test.js
+++ tests/unit/test/pdfParser_test.js
@@ -11,19 +11,15 @@ const should = chai.should(); // eslint-disable-line
 describe('PDF parse test', () => {
   it('should parse PDF content', async () => {
     const fileBuffer = getMockedFileBuffer('regular.pdf');
-    try {
-      const result = await pdfParser.parsePDFFile(fileBuffer);
-      assert.equal(result, mockData.parsePDF.PDFContent, 'PDF parser result should equal content');
-    } catch (error) {
-      assert.fail(error.message);
-    }
+    const result = await pdfParser.parsePDFFile(fileBuffer);
+    assert.equal(result, mockData.parsePDF.PDFContent, 'PDF parser result should equal content');
   });
 
-  it('should fail parsing PDF content when no file is passed', () => {
-    return pdfParser.parsePDFFile().should.be.rejectedWith(mockData.parsePDF.parametersError);
-  });
+  it('should fail parsing PDF content when no file is passed', () => (
+    pdfParser.parsePDFFile().should.be.rejectedWith(mockData.parsePDF.parametersError)
+  ));
 
-  it('should fail parsing PDF content when an invalid file is passed', async () => {
+  it('should fail parsing PDF content when an invalid file is passed', () => {
     const fileBuffer = getMockedFileBuffer('regular.txt');
     return pdfParser.parsePDFFile(Buffer.from(fileBuffer))
       .should.be.rejectedWith(mockData.parsePDF.structureError);
@@ -31,27 +27,19 @@ describe('PDF parse test', () => {
 });
 
 describe('PDF decrypt test', () => {
-  it('should decrypt and read encrypted regular PDF content', async() => {
+  it('should decrypt and read encrypted regular PDF content', () => {
     const fileBuffer = getMockedFileBuffer('regular.pdf.enc');
     const decryptedFileBuffer = getMockedFileBuffer('regular-decrypted.txt');
-    try {
-      const decryptedFile = pdfParser.decryptFile(mockData.decryptPDF.passphrase,
-        mockData.decryptPDF.regularTitle, fileBuffer);
-      assert(Buffer.from(decryptedFile).equals(decryptedFileBuffer), 'PDF decrypted content should equal specific content');
-    } catch (error) {
-      assert.fail(error.message);
-    }
+    const decryptedFile = pdfParser.decryptFile(mockData.decryptPDF.passphrase,
+      mockData.decryptPDF.regularTitle, fileBuffer);
+    assert(Buffer.from(decryptedFile).equals(decryptedFileBuffer), 'PDF decrypted content should equal specific content');
   });
 
-  it('should decrypt and read encrypted force vended PDF content', async() => {
+  it('should decrypt and read encrypted force vended PDF content', () => {
     const fileBuffer = getMockedFileBuffer('force-vended.pdf.enc');
     const decryptedFileBuffer = getMockedFileBuffer('force-vended-decrypted.txt');
-    try {
-      const decryptedFile = pdfParser.decryptFile(mockData.decryptPDF.data, 'forceVended', fileBuffer);
-      assert(Buffer.from(decryptedFile).equals(decryptedFileBuffer), 'PDF decrypted content should equal specific content');
-    } catch (error) {
-      assert.fail(error.message);
-    }
+    const decryptedFile = pdfParser.decryptFile(mockData.decryptPDF.data, 'forceVended', fileBuffer);
+    assert(Buffer.from(decryptedFile).equals(decryptedFileBuffer), 'PDF decrypted content should equal specific content');
   });
 
   it('should fail at decrypting with no data', () => {
