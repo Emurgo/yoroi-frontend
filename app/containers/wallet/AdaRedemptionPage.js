2425140c400a5f174c3d2d725ef24033180a0ae6
Juani <jicostanzo@atixlabs.com>
2019-01-08 12:46:12 -0300

    [PI-27] Create AdaRedemptionNoWallets component.
    If the user has no wallets and tries to redeem adas this component will show instead.

--- app/containers/wallet/AdaRedemptionPage.js
+++ app/containers/wallet/AdaRedemptionPage.js
@@ -1,8 +1,38 @@
 // @flow
 import React, { Component } from 'react';
+import { observer, inject } from 'mobx-react';
+import type { InjectedProps } from '../../types/injectedPropsType';
+import AdaRedemptionNoWallets from '../../components/wallet/ada-redemption/AdaRedemptionNoWallets';
+import { ROUTES } from '../../routes-config';
 
-export default class AdaRedemptionPage extends Component {
+type Props = InjectedProps;
+
+@inject('stores', 'actions') @observer
+export default class AdaRedemptionPage extends Component<Props> {
+
+  static defaultProps = { actions: null, stores: null };
+
+  handleGoToCreateWalletClick = () => {
+    this.props.actions.router.goToRoute.trigger({ route: ROUTES.WALLETS.ADD });
+  };
+
   render() {
+    const { wallets } = this.props.stores.ada;
+
+    const selectableWallets = wallets.all.map((w) => ({
+      value: w.id, label: w.name
+    }));
+
+    if (!wallets.all.length) {
+      return (
+        <div>
+          <AdaRedemptionNoWallets
+            onGoToCreateWalletClick={this.handleGoToCreateWalletClick}
+          />
+        </div>
+      );
+    }
+
     return (
       <div>
         Ada Redeem page
