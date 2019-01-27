b14f6b8308720a8b2115a3c896139105a296d88d
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2019-01-28 02:47:16 +0300

    Changed configs for CI

--- setup_cardano_crypto.sh
+++ setup_cardano_crypto.sh
@@ -4,8 +4,8 @@ git submodule update --init --recursive && \
 cd js-cardano-wasm && \
 git checkout c7d99c64d7af833b8990a95037755db3cb6d37d4 && \
 git submodule update
-../js-cardano-wasm-build && \
 npm install && \
+../js-cardano-wasm-build && \
 npm link && \
 cd .. && \
 npm link rust-cardano-crypto
