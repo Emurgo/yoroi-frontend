b1331f6262b3ddd9e7ef389b29b013a39030814c
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2019-01-28 14:41:42 +0300

    Updated `js-cardano-wasm` revision

--- setup_cardano_crypto.sh
+++ setup_cardano_crypto.sh
@@ -2,7 +2,7 @@
 
 git submodule update --init --recursive && \
 cd js-cardano-wasm && \
-git checkout c7d99c64d7af833b8990a95037755db3cb6d37d4 && \
+git checkout 3dcd62d6f10d7281e08a236f3ca5dff92e04e90a && \
 git submodule update
 npm install && \
 ../js-cardano-wasm-build && \
