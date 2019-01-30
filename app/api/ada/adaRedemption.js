a1265c3fc0b5b302798872f44c409d524bd4ec60
Juani <jicostanzo@atixlabs.com>
2019-01-30 14:29:43 -0300

    [PI-52] WIP testing to see if we can get the key pair from the seed for paper vended

--- app/api/ada/adaRedemption.js
+++ app/api/ada/adaRedemption.js
@@ -2,10 +2,13 @@
 
 import { getAddressFromRedemptionKey } from './lib/cardanoCrypto/cryptoRedemption';
 import bs58 from 'bs58';
+import supercop from 'supercop.js';
 import { getUTXOsForAddresses } from './lib/yoroi-backend-api';
+import { decryptRegularVend } from './lib/decrypt';
 
 export type RedeemAdaParams = {
   redemptionCode: string,
+  mnemonic: ?Array<string>,
   spendingPassword: string,
   walletId: string,
   accountIndex: number
@@ -36,6 +39,20 @@ export async function redeemAda(
 export async function redeemPaperVendedAda(
   redemptionParams: RedeemPaperVendedAdaParams
 ) : Promise<Object> {
+  // Crear buffer a partir del redemptionCode (seed encriptada con AES)
+  const redemptionCodeBuffer = bs58.decode(redemptionParams.redemptionCode);
+  // A partir del mnemonic obtener la key de AES
+    // convertir mnemonic a bytes
+    // hacer un hash de los bytes para tener la key de AES
+  const seed = decryptRegularVend(transformMnemonic(redemptionParams.mnemonics), redemptionCodeBuffer);
+
+  const uint8ArrayAddress2 = getAddressFromRedemptionKey(Buffer.from(seed, 'base64'));
+  // en base a la key de AES y el buffer del redemptionCode -> desencryptar la seed (redemptionCode)
+  // Ahora tenemos la seed
+  // usar el paquete para obtener las keys  a partir del seed
+  // secret del key = redemptionKey
+
+
   const redemptionKey = Buffer.from(redemptionParams.redemptionCode, 'base64');
   const uint8ArrayAddress = getAddressFromRedemptionKey(redemptionKey);
   const address = bs58.encode(Buffer.from(uint8ArrayAddress));
