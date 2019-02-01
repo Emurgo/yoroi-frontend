c0d67cf1317db7c797e8727d197f25b184a1d5ee
Juani <jicostanzo@atixlabs.com>
2019-02-01 12:26:01 -0300

    [PI-52] Fix how we get uint8ArrayAddress for redeemPaperVendedAda

--- app/api/ada/adaRedemption.js
+++ app/api/ada/adaRedemption.js
@@ -35,26 +35,24 @@ export async function redeemAda(
   return {};
 }
 
+function transformMnemonicToString(mnemonic) {
+  let aux = '';
+
+  mnemonic.forEach(word => {
+    aux += ` ${word}`;
+  });
+
+  return aux.trim();
+}
+
 // TODO: return a Promise of AdaTransaction instead of Object, https://trello.com/c/0FOFzcfy/12-broadcast-redeem-tx
 export async function redeemPaperVendedAda(
   redemptionParams: RedeemPaperVendedAdaParams
 ) : Promise<Object> {
-  // Crear buffer a partir del redemptionCode (seed encriptada con AES)
   const redemptionCodeBuffer = bs58.decode(redemptionParams.redemptionCode);
-  // A partir del mnemonic obtener la key de AES
-    // convertir mnemonic a bytes
-    // hacer un hash de los bytes para tener la key de AES
-  const seed = decryptRegularVend(transformMnemonic(redemptionParams.mnemonics), redemptionCodeBuffer);
-
-  const uint8ArrayAddress2 = getAddressFromRedemptionKey(Buffer.from(seed, 'base64'));
-  // en base a la key de AES y el buffer del redemptionCode -> desencryptar la seed (redemptionCode)
-  // Ahora tenemos la seed
-  // usar el paquete para obtener las keys  a partir del seed
-  // secret del key = redemptionKey
-
-
-  const redemptionKey = Buffer.from(redemptionParams.redemptionCode, 'base64');
-  const uint8ArrayAddress = getAddressFromRedemptionKey(redemptionKey);
+  const mnemonicAsString = transformMnemonicToString(redemptionParams.mnemonics);
+  const seed = decryptRegularVend(mnemonicAsString, redemptionCodeBuffer);
+  const uint8ArrayAddress = getAddressFromRedemptionKey(Buffer.from(seed, 'base64'));
   const address = bs58.encode(Buffer.from(uint8ArrayAddress));
   const utxos = await getUTXOsForAddresses({ addresses: [address] });
   // TODO: generate ada redeem tx with createRedemptionTransaction function from cardanoCrypto, https://trello.com/c/iAmeNvGk/11-create-a-redeem-tx
