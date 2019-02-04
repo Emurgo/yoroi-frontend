6fbe6b259d4416d9c092af683a04b77fbf5beb9a
Juani <jicostanzo@atixlabs.com>
2019-02-04 08:47:35 -0300

    [PI-52] Remove transformMnemonicToString for a join

--- app/api/ada/adaRedemption.js
+++ app/api/ada/adaRedemption.js
@@ -42,22 +42,12 @@ export async function redeemAda(
   return {};
 }
 
-function transformMnemonicToString(mnemonic) {
-  let aux = '';
-
-  mnemonic.forEach(word => {
-    aux += ` ${word}`;
-  });
-
-  return aux.trim();
-}
-
 // TODO: return a Promise of AdaTransaction instead of Object, https://trello.com/c/0FOFzcfy/12-broadcast-redeem-tx
 export async function redeemPaperVendedAda(
   redemptionParams: RedeemPaperVendedAdaParams
 ) : Promise<Object> {
   const redemptionCodeBuffer = bs58.decode(redemptionParams.redemptionCode);
-  const mnemonicAsString = transformMnemonicToString(redemptionParams.mnemonics);
+  const mnemonicAsString = redemptionParams.mnemonics.join(' ');
   const seed = decryptRegularVend(mnemonicAsString, redemptionCodeBuffer);
   const uint8ArrayAddress = getAddressFromRedemptionKey(Buffer.from(seed, 'base64'));
   const senderAddress = bs58.encode(Buffer.from(uint8ArrayAddress));
