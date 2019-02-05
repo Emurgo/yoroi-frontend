0d6dd6f8040f111e5752301675e2c13f675994ca
ASUKA1006 <asuka106@outlook.jp>
2019-02-06 02:07:38 +0900

    refactoring the footer

---
+++ app/containers/footer/AddWalletFooter.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import FooterItem from '../../components/footer/FooterItem';
+import HorizontalFlexContainer from '../../components/layout/HorizontalFlexContainer';
+
+import buyTrezorSvg from '../../assets/images/footer/buy-trezor.inline.svg';
+import whatIsHardwareWalletSvg from '../../assets/images/footer/what-is-hardware-wallet.inline.svg';
+import howCreateWalletSvg from '../../assets/images/footer/how-to-create-wallet.inline.svg';
+import howConnetTrezorSvg from '../../assets/images/footer/how-to-connect-trezor.inline.svg';
+import howRestoreWalletSvg from '../../assets/images/footer/how-to-restore-wallet.inline.svg';
+
+import { defineMessages } from 'react-intl';
+
+
+const messages = defineMessages({
+  buyTrezorHardwareWallet: {
+    id: 'wallet.footer.buyTrezorHardwareWallet.text',
+    defaultMessage: '!!!Buy a Trezor hardware wallet.',
+    description: 'Footer Buy a Trezor hardware wallet link text.'
+  },
+  whatIsHardwareWallet: {
+    id: 'wallet.footer.whatIsHardwareWallet.text',
+    defaultMessage: '!!!What is a hardware wallet?',
+    description: 'Footer What is a hardware wallet? link text.'
+  },
+  howToConnectTrezor: {
+    id: 'wallet.footer.howToConnectTrezor.text',
+    defaultMessage: '!!!How to connect a Trezor.',
+    description: 'Footer How to connect a Trezor link text.'
+  },
+  howToCreateWallet: {
+    id: 'wallet.footer.howToCreateWallet.text',
+    defaultMessage: '!!!How to create a wallet.',
+    description: 'Footer How to create a wallet link text.'
+  },
+  howToRestoreWallet: {
+    id: 'wallet.footer.howToRestoreWallet.text',
+    defaultMessage: '!!!How to restore a wallet.',
+    description: 'Footer How to restore a wallet link text.'
+  },
+});
+
+
+export default class AddWalletFooter extends Component {
+
+  render() {
+    return (
+      <HorizontalFlexContainer>
+        <FooterItem
+          url="https://yoroi-wallet.com/get-trezor"
+          svg={buyTrezorSvg}
+          message={messages.buyTrezorHardwareWallet}
+        />
+        <FooterItem
+          url="https://medium.com/@emurgo_io/whats-a-hardware-wallet-b3605a026008"
+          svg={whatIsHardwareWalletSvg}
+          message={messages.whatIsHardwareWallet}
+        />
+        <FooterItem
+          url="https://youtu.be/Dp0wXwtToX0"
+          svg={howConnetTrezorSvg}
+          message={messages.howToConnectTrezor}
+        />
+        <FooterItem
+          url="https://youtu.be/9jg8lsreIQ8?t=56"
+          svg={howCreateWalletSvg}
+          message={messages.howToCreateWallet}
+        />
+        <FooterItem
+          url="https://youtu.be/PKKWgTNKSks?t=14"
+          svg={howRestoreWalletSvg}
+          message={messages.howToRestoreWallet}
+        />
+      </HorizontalFlexContainer>
+    );
+  }
+}
--- app/containers/wallet/WalletAddPage.js
+++ app/containers/wallet/WalletAddPage.js
@@ -18,13 +18,13 @@ import resolver from '../../utils/imports';
 import type { InjectedProps } from '../../types/injectedPropsType';
 import AdaWalletsStore from '../../stores/ada/AdaWalletsStore';
 import TrezorConnectStore from '../../stores/ada/TrezorConnectStore';
-import HelpLinkFooter from '../../components/footer/HelpLinkFooter';
+import AddWalletFooter from '../footer/AddWalletFooter';
 
 type Props = InjectedProps;
 const MainLayout = resolver('containers/MainLayout');
 
 const messages = defineMessages({
-  title: {
+  title: { 
     id: 'wallet.add.page.title',
     defaultMessage: '!!!Add Wallet',
     description: 'Add Wallet Title.'
@@ -101,18 +101,10 @@ export default class WalletAddPage extends Component<Props> {
       );
     }
 
-    const footer = (
-      <HelpLinkFooter
-        showBuyTrezorHardwareWallet
-        showHowToConnectTrezor
-        showHowToCreateWallet
-        showHowToRestoreWallet
-      />);
-
     return (
       <MainLayout
         topbar={topBar}
-        footer={footer}
+        footer={<AddWalletFooter />}
       >
         {content}
       </MainLayout>
