b0694650ec9a34750ac3d0114f7b511104436839
Asuka Oomae <31379986+ASUKA1006@users.noreply.github.com>
2019-02-08 17:27:06 +0900

    Refactoring the footer (#282)
    Fix #205 
    
    Use HorizontalFlexContainer so it is much cleaner.
    
    I also add "What is a hardware wallet?" which was previously disabled.
    
    # Big screen
    
    <img width="1572" alt="screen shot 2019-02-06 at 1 38 51 am" src="https://user-images.githubusercontent.com/31379986/52346196-6a4b6b80-2a62-11e9-96d8-1d6d51de3dce.png">
    
    # Small screen
    
    <img width="918" alt="screen shot 2019-02-06 at 1 39 30 am" src="https://user-images.githubusercontent.com/31379986/52346203-6cadc580-2a62-11e9-9618-9c02fc6fc7dd.png">

---
+++ app/components/footer/FooterItem.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import SvgInline from 'react-svg-inline';
+import { intlShape } from 'react-intl';
+import styles from './FooterItem.scss';
+
+type Props = {
+  url: string,
+  svg: string,
+  message: any
+};
+
+export default class FooterItem extends Component<Props> {
+
+  static contextTypes = {
+    intl: intlShape.isRequired,
+  };
+
+  render() {
+    const { intl } = this.context;
+    const { url, svg, message } = this.props;
+
+    return (
+      <div className={styles.component}>
+        {/* Buy a Trezor Hardware wallet */}
+        {
+          <a
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.block}
+          >
+            <div className={styles.icon}>
+              <SvgInline svg={svg} cleanup={['title']} />
+            </div>
+            <div className={styles.text}>
+              {intl.formatMessage(message)}
+            </div>
+          </a>
+        }
+      </div>
+    );
+  }
+}
