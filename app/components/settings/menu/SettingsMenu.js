8ad700bfce05b894401affdb4c3a6e5fc14e04d9
Asuka Oomae <31379986+ASUKA1006@users.noreply.github.com>
2019-02-14 04:12:41 +0900

    About Yoroi block (#291)
    1) Removed the scrollbar for the footer (size was too small to fit SVG)
    2) Adds About Us to the settings page
    
    - [ ] Need SVG logo for EMURGO
    - [ ] Yoroi logo is plain white so it is hard to see
    - [ ] Review these are the links we want on the About Us
    - [ ] Maybe left-align instead of center?
    - [ ] #289 Added a media query with a size specifically for the add wallet page (5-element rows and full width of app). This means the text for our icons disappears even when there is a lot of space left. Probably we need to replace this media query with something smarter.
    
    <img width="1462" alt="screen shot 2019-02-10 at 3 13 12 am" src="https://user-images.githubusercontent.com/31379986/52524590-e56e8500-2ce1-11e9-8b3f-54fc9c771d33.png">

--- app/components/settings/menu/SettingsMenu.js
+++ app/components/settings/menu/SettingsMenu.js
@@ -27,6 +27,11 @@ const messages = defineMessages({
     defaultMessage: '!!!Terms of use',
     description: 'Label for the "Terms of use" link in the settings menu.',
   },
+  AnoutYoroi: {
+    id: 'settings.menu.aboutYroi.link.label',
+    defaultMessage: '!!!About Yoroi',
+    description: 'Label for the "About Yoroi" link in the settings menu.',
+  },
 });
 
 type Props = {
@@ -80,6 +85,13 @@ export default class SettingsMenu extends Component<Props> {
             onClick={() => onItemClick(ROUTES.SETTINGS.SUPPORT)}
             active={isActiveItem(ROUTES.SETTINGS.SUPPORT)}
             className="support"
+          />
+
+          <SettingsMenuItem
+            label={intl.formatMessage(messages.AnoutYoroi)}
+            onClick={() => onItemClick(ROUTES.SETTINGS.ABOUT_YOROI)}
+            active={isActiveItem(ROUTES.SETTINGS.ABOUT_YOROI)}
+            className="AboutYoroi"
           />
         </div>
       </div>
