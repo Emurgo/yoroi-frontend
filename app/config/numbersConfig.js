a3decc9c69d53bd315667ae202b1c22fd327a43c
sebastiengllmt <sebastiengllmt@gmail.com>
2019-02-25 15:42:23 +0900

    more small stuff

--- app/config/numbersConfig.js
+++ app/config/numbersConfig.js
@@ -1,5 +1,8 @@
-export const LOVELACES_PER_ADA = 1000000;
+// @flow
+import BigNumber from 'bignumber.js';
+
+export const LOVELACES_PER_ADA = new BigNumber('1 000 000'.replace(/ /g, ''), 10);
+export const TOTAL_SUPPLY = new BigNumber('45 000 000 000'.replace(/ /g, ''), 10).times(LOVELACES_PER_ADA);
 export const MAX_INTEGER_PLACES_IN_ADA = 11;
 export const DECIMAL_PLACES_IN_ADA = 6;
 export const MAX_ADA_WALLETS_COUNT = 1;
-export const TOTAL_SUPPLY = 45000000000 * LOVELACES_PER_ADA;
