d5fc2b1124301eb10f297d209e9f7aabc86ca202
SARVESHWAR SINGH <sarveshwar@emurgo.io>
2019-02-27 14:08:59 +0900

    topbar yoroi with ledger impl without actual ledger icon

--- app/components/topbar/TopBarCategory.js
+++ app/components/topbar/TopBarCategory.js
@@ -22,9 +22,10 @@ export default class TopBarCategory extends Component<Props> {
       className
     ]);
 
+    const isWithHW = (className === 'with-trezor-t' || className === 'with-ledger-nano-s');
     const iconStyles = classNames([
       className === 'wallets' ? styles.walletsIcon : null,
-      className === 'with-trezor-t' ? styles.withTrezorTIcon : null,
+      isWithHW ? styles.withHardwareWalletIcon : null,
       styles.icon
     ]);
 
