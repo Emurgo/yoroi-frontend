3166dcf6af6619a4559d688c334f994932a2451a
Cody Butz <codyjbutz@gmail.com>
2019-04-02 18:15:46 -0400

    adds flags to language dropdown

--- app/components/settings/categories/GeneralSettings.js
+++ app/components/settings/categories/GeneralSettings.js
@@ -9,6 +9,7 @@ import ReactToolboxMobxForm from '../../../utils/ReactToolboxMobxForm';
 import LocalizableError from '../../../i18n/LocalizableError';
 import styles from './GeneralSettings.scss';
 import type { ReactIntlMessage } from '../../../types/i18nTypes';
+import SvgInline from 'react-svg-inline';
 
 const messages = defineMessages({
   languageSelectLabel: {
@@ -60,7 +61,8 @@ export default class GeneralSettings extends Component<Props> {
     const languageId = form.$('languageId');
     const languageOptions = languages.map(language => ({
       value: language.value,
-      label: intl.formatMessage(language.label)
+      label: intl.formatMessage(language.label),
+      svg: language.svg
     }));
     const componentClassNames = classNames([styles.component, 'general']);
     const languageSelectClassNames = classNames([
@@ -76,6 +78,12 @@ export default class GeneralSettings extends Component<Props> {
           {...languageId.bind()}
           onChange={this.selectLanguage}
           skin={SelectSkin}
+          optionRenderer={option => (
+            <div>
+              <SvgInline svg={option.svg} className={styles.flag} width="30" height="27" />
+              <span className={styles.language}>{option.label}</span>
+            </div>
+          )}
         />
         {error && <p className={styles.error}>{error}</p>}
 
