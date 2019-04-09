507f21a8707e22755bac958982ac20145ee67287
sebastiengllmt <sebastiengllmt@gmail.com>
2019-04-09 12:29:32 +0900

    remove unused functions

--- app/api/ada/lib/decrypt.js
+++ app/api/ada/lib/decrypt.js
@@ -1,5 +1,5 @@
 import aesjs from 'aes-js';
-import { mnemonicToEntropy, validateMnemonic } from 'bip39';
+import { validateMnemonic } from 'bip39';
 import blakejs from 'blakejs';
 import crypto from 'crypto';
 import validWords from 'bip39/src/wordlists/english.json';
@@ -17,24 +17,6 @@ const iv = Buffer.alloc(16); // it's iv = 0 simply
 function decryptWithAES(aesKey, bytes) {
   return new aesjs.ModeOfOperation.ctr(aesKey, new aesjs.Counter(iv)).decrypt(bytes); // eslint-disable-line
 }
-
-const hexChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-
-const hexToBytes = (s) => {
-  const arr = [];
-  if (s.length & 1 === 1) { // eslint-disable-line
-    throw new Error(`Wrong hex: ${s}`);
-  }
-  for (let i = 0; i < s.length / 2; ++i) {
-    const c1 = s[2 * i];
-    const c2 = s[(2 * i) + 1];
-    const i1 = hexChar.indexOf(c1);
-    const i2 = hexChar.indexOf(c2);
-    if (i1 === -1 || i2 === -1) throw new Error(`Wrong hex: ${s}`);
-    arr[i] = (i1 << 4) + i2;
-  }
-  return new Uint8Array(arr);
-};
 
 const blake2b = (data) => blakejs.blake2b(data, null, 32);
 
