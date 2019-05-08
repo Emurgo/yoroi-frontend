700d86287f28ebe61d6e4bdc18230c96d72426ea
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2019-05-08 17:52:47 +0300

    Changed top-bar to balance in the center. And created 5 color-schemes for identicons.

--- app/components/topbar/WalletTopbarTitle.js
+++ app/components/topbar/WalletTopbarTitle.js
@@ -14,6 +14,15 @@ type Props = {
   formattedWalletAmount?: Function
 };
 
+const mkcolor = (primary, secondary, spots) => ({ primary, secondary, spots });
+const COLORS = [
+  mkcolor('#E1F2FF', '#17D1AA', '#A80B32'),
+  mkcolor('#E1F2FF', '#FA5380', '#0833B2'),
+  mkcolor('#E1F2FF', '#f06ef5', '#0804f7'),
+  mkcolor('#E1F2FF', '#ebb687', '#852d62'),
+  mkcolor('#E1F2FF', '#eda9a5', '#43327a'),
+];
+
 /** Dynamically generated title for the topbar when a wallet is selected */
 @observer
 export default class WalletTopbarTitle extends Component<Props> {
@@ -29,28 +38,29 @@ export default class WalletTopbarTitle extends Component<Props> {
     // If we are looking at a wallet, show its name and balance
     const walletRoutesMatch = matchRoute(`${ROUTES.WALLETS.ROOT}/:id(*page)`, currentRoute);
     const showWalletInfo = walletRoutesMatch && wallet;
+    const color = COLORS[Math.floor(Math.random()*5)];
     const topbarTitle = showWalletInfo && formattedWalletAmount ? (
       <div className={styles.walletInfo}>
         <div className={styles.divIcon}>
           <Blockies
             seed={`${Math.random()}`}
             size={7}
-            scale={7}
-            bgColor='#55ff00'
-            color='#dd01fa'
-            spotColor='#000'
+            scale={6}
+            bgColor={color.primary}
+            color={color.secondary}
+            spotColor={color.spots}
             className={styles.walletIcon}
           />
         </div>
-        <div className={styles.divPlate}>
-          <div className={styles.walletPlate}>ABCD</div>
-          <div className={styles.walletPlate}>1234</div>
-        </div>
         <div className={styles.divWalletInfo}>
           <div className={styles.walletName}>{wallet && wallet.name}</div>
+          <div className={styles.walletPlate}>[ABCD-1234]</div>
+        </div>
+        <div className={styles.divAmount}>
           <div className={styles.walletAmount}>
             { wallet && formattedWalletAmount(wallet.amount) + ' ADA' }
           </div>
+          <div className={styles.walletAmountLabel}>Total balance</div>
         </div>
       </div>
     ) : null;
