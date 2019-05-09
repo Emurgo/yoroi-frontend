ec3fe5058e8e5c94c2c2eebd9745c6681b77a8f0
YankoFelipe <yankofelipe.af@gmail.com>
2019-05-08 22:03:55 -0400

    Fixing the chrome test fails

--- app/containers/settings/Settings.js
+++ app/containers/settings/Settings.js
@@ -64,7 +64,7 @@ export default class Settings extends Component<InjectedContainerProps> {
         )}
         classicTheme={profile.isClassicTheme}
       >
-        <SettingsLayout menu={menu} classicTheme={profile.currentTheme}>
+        <SettingsLayout menu={menu} currentTheme={profile.currentTheme}>
           {children}
         </SettingsLayout>
       </Layout>
