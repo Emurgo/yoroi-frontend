a1ca518198bac73b53c986ad7237283f17d2efd8
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2019-05-15 01:35:25 +0300

    Replaced paper images with the new design

--- app/api/ada/paperWallet/paperWalletPdf.js
+++ app/api/ada/paperWallet/paperWalletPdf.js
@@ -1,9 +1,9 @@
 // @flow
 import Pdf from 'jspdf';
 import qr from 'qr-image';
-import paperWalletPage1Path from '../../../assets/images/paper-wallet/paper-wallet-certificate-page-1.png';
-import paperWalletPage2PassPath from '../../../assets/images/paper-wallet/paper-wallet-certificate-page-2.png';
-import paperWalletCertificateBgPath from '../../../assets/images/paper-wallet/paper-wallet-certificate-background.png';
+import paperWalletPage1Path from '../../../assets/images/paper-wallet/paper-wallet-certificate.front.png';
+import paperWalletPage2Path from '../../../assets/images/paper-wallet/paper-wallet-certificate.back.png';
+// import paperWalletCertificateBgPath from '../../../assets/images/paper-wallet/paper-wallet-certificate-background.png';
 import { Logger, stringifyError } from '../../../utils/logging';
 import type { Network } from '../../../../config/config-types';
 import { NetworkType } from '../../../../config/config-types';
@@ -52,26 +52,26 @@ export const generateAdaPaperPdf = async (
     updateStatus(PdfGenSteps.background);
 
     // background images
-    const bgUrl = paperWalletCertificateBgPath;
-    await addImage(doc, bgUrl, pageSize);
+    await addImage(doc, paperWalletPage1Path, pageSize);
     if (network !== NetworkType.MAINNET) {
-      printTestnetLabel(doc, network, 178, 20, 15);
+      printTestnetLabel(doc, network, 172);
     }
 
     if (accountPlate) {
       // print account plate ID bottom-left corner of main front section
       doc.setFontSize(12);
-      doc.text(40, 175, accountPlate.id);
+      doc.text(145, 180, accountPlate.id);
     }
 
     updateStatus(PdfGenSteps.frontpage);
 
     // first page
     if (network !== NetworkType.MAINNET) {
-      printTestnetLabel(doc, network, 85);
+      printTestnetLabel(doc, network, 105);
     }
-    const page1Uri = paperWalletPage1Path;
-    await addImage(doc, page1Uri, pageSize);
+
+    // const page1Uri = paperWalletPage1Path;
+    // await addImage(doc, page1Uri, pageSize);
 
     updateStatus(PdfGenSteps.addresses);
     if (!printAddresses(doc, addresses)) {
@@ -113,8 +113,7 @@ export const generateAdaPaperPdf = async (
       textCenter(doc, 130, accountPlate.id, null, 180, true);
     }
 
-    const page2Uri = paperWalletPage2PassPath;
-    await addImage(doc, page2Uri, pageSize);
+    await addImage(doc, paperWalletPage2Path, pageSize);
     updateStatus(PdfGenSteps.mnemonic);
     printMnemonics(doc, words);
 
@@ -135,7 +134,7 @@ function printTestnetLabel(
   r?: number,
   xShift?: number
 ) {
-  doc.setFontSize(60);
+  doc.setFontSize(50);
   doc.setFontType('bold');
   doc.setTextColor(255, 180, 164);
   textCenter(doc, y, network.toUpperCase(), null, r, (r || 0) > 90, xShift);
