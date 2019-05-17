a439428f9c189c3b49f9c13f3e3424c0e861bc35
Evgenij Beloded <belodedevgenij@gmail.com>
2019-05-17 12:10:02 +0700

    Eliminate dead code

--- chrome/extension/background.js
+++ chrome/extension/background.js
@@ -1,27 +1,4 @@
-const bluebird = require('bluebird');
 const _ = require('lodash');
-
-global.Promise = bluebird;
-
-function promisifier(method) {
-  // return a function
-  return function promisified(...args) {
-    // which returns a promise
-    return new Promise(resolve => {
-      args.push(resolve);
-      method.apply(this, args);
-    });
-  };
-}
-
-function promisifyAll(obj, list) {
-  list.forEach(api => bluebird.promisifyAll(obj[api], { promisifier }));
-}
-
-// let chrome extension api support Promise
-promisifyAll(chrome, ['tabs', 'windows', 'browserAction']);
-promisifyAll(chrome.storage, ['local']);
-
 const onIconClicked = () => {
   chrome.tabs.create({ url: 'main_window.html' });
 };
