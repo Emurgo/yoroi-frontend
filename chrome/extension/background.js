f14238caae61cf8a0145764f42d65385afca66a2
Evgenij Beloded <belodedevgenij@gmail.com>
2019-05-17 12:10:25 +0700

    Remove debounce and lodash

--- chrome/extension/background.js
+++ chrome/extension/background.js
@@ -1,6 +1,5 @@
-const _ = require('lodash');
 const onIconClicked = () => {
   chrome.tabs.create({ url: 'main_window.html' });
 };
 
-chrome.browserAction.onClicked.addListener(_.debounce(onIconClicked, 500));
+chrome.browserAction.onClicked.addListener(onIconClicked);
