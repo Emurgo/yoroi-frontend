2bd65c2fd6779bf2e03a2a5e3e87750631c45c23
Valerii Ladomyriak <valerii.ladomyriak@vacuumlabs.com>
2019-05-22 16:33:19 +0200

    reverted step back

--- app/containers/wallet/WalletAddPage.js
+++ app/containers/wallet/WalletAddPage.js
@@ -92,8 +92,8 @@ export default class WalletAddPage extends Component<Props> {
         <WalletCreateDialogContainer
           actions={actions}
           stores={stores}
+          onClose={this.onClose}
           classicTheme={profile.isClassicTheme}
-          onClose={() => actions.dialogs.open.trigger({ dialog: WalletCreateListDialog })}
         />
       );
     } else if (uiDialogs.isOpen(WalletCreateListDialog)) {
@@ -142,8 +142,8 @@ export default class WalletAddPage extends Component<Props> {
         <WalletTrezorConnectDialogContainer
           actions={actions}
           stores={stores}
+          onClose={this.onClose}
           classicTheme={profile.isClassicTheme}
-          onClose={() => actions.dialogs.open.trigger({ dialog: WalletConnectHardwareDialog })}
         />
       );
     } else if (uiDialogs.isOpen(WalletLedgerConnectDialogContainer)) {
@@ -151,8 +151,8 @@ export default class WalletAddPage extends Component<Props> {
         <WalletLedgerConnectDialogContainer
           actions={actions}
           stores={stores}
+          onClose={this.onClose}
           classicTheme={profile.isClassicTheme}
-          onClose={() => actions.dialogs.open.trigger({ dialog: WalletConnectHardwareDialog })}
         />
       );
     } else {
