18acb40198ce4a88571d2d2197f7e422a2357a8b
leivaburto <cristobal.leiva@usach.cl>
2019-05-24 21:29:43 -0400

    CSS Animations, required inputs, valid+label classes and others

--- app/themes/skins/FormFieldOwnSkin.js
+++ app/themes/skins/FormFieldOwnSkin.js
@@ -67,6 +67,19 @@ export const FormFieldOwnSkin = class extends React.Component<Props, State> {
             this.props.error ? styles.error : '',
           ])}
         >
+
+          <div className={styles.iconsWrapper}>
+            {this.props.done && <SvgInline svg={SuccessSvg} />}
+            {this.props.type === 'password' && !this.props.error ? (
+              <button tabIndex="-1" type="button" onClick={this.showPassword}>
+                {isPasswordShown
+                  ? <SvgInline svg={PasswordSvg} />
+                  : <SvgInline svg={PasswordHiddenSvg} />}
+              </button>
+            ) : null}
+            {this.props.error && <SvgInline svg={ErrorSvg} />}
+          </div>
+          {this.props.render(omit(renderProps, ['themeId']))}
           {this.props.label && (
             // eslint-disable-next-line
             <legend
@@ -82,19 +95,6 @@ export const FormFieldOwnSkin = class extends React.Component<Props, State> {
               {this.props.label}
             </legend>
           )}
-
-          <div className={styles.iconsWrapper}>
-            {this.props.done && <SvgInline svg={SuccessSvg} />}
-            {this.props.type === 'password' && !this.props.error ? (
-              <button tabIndex="-1" type="button" onClick={this.showPassword}>
-                {isPasswordShown
-                  ? <SvgInline svg={PasswordSvg} />
-                  : <SvgInline svg={PasswordHiddenSvg} />}
-              </button>
-            ) : null}
-            {this.props.error && <SvgInline svg={ErrorSvg} />}
-          </div>
-          {this.props.render(omit(renderProps, ['themeId']))}
         </fieldset>
       </div>
     );
