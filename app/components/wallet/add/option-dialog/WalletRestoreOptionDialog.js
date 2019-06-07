e2c34e8bb2db0eabdceee631167f1b5fb6b5ee4b
SARVESHWAR SINGH <sarveshwar@emurgo.io>
2019-06-07 14:57:45 +0900

    i18n fix

--- app/components/wallet/add/option-dialog/WalletRestoreOptionDialog.js
+++ app/components/wallet/add/option-dialog/WalletRestoreOptionDialog.js
@@ -9,23 +9,23 @@ import OptionBlock from './OptionBlock';
 
 const messages = defineMessages({
   dialogTitle: {
-    id: 'wallet.restore.dialog.title.label',
+    id: 'wallet.add.optionDialog.restore.dialogTitle',
     defaultMessage: '!!!Restore wallet',
   },
-  walletMnemonicTitle: {
-    id: 'wallet.create.type.mnemonic.title',
+  restoreNormalTitle: {
+    id: 'wallet.add.optionDialog.restore.normalWallet.title',
     defaultMessage: '!!!Wallet from 15 mnemonic words',
   },
-  walletMnemonicDescription: {
-    id: 'wallet.create.type.mnemonic.description',
+  restoreNormalDescription: {
+    id: 'wallet.add.optionDialog.restore.normalWallet.description',
     defaultMessage: '!!!The simplest and most common way to create a Wallet. Yoroi will generate 15 mnemonic words that you will have to store in a safe place in order to restore the wallet.',
   },
-  walletPaperTitle: {
-    id: 'wallet.create.type.paper.title',
+  restorePaperWalletTitle: {
+    id: 'wallet.add.optionDialog.restore.paperWallet.title',
     defaultMessage: '!!!Paper Wallet',
   },
-  walletPaperDescription: {
-    id: 'wallet.create.type.paper.description',
+  restorePaperWalletDescription: {
+    id: 'wallet.add.optionDialog.restore.paperWallet.description',
     defaultMessage: '!!!Allows the generation of an offline wallet that should be printed. The process involves setting up an optional password for extra security and then printing a pdf that contains the mnemonics (for later recovery) and some Cardano addresses onto a paper.',
   },
 });
@@ -60,14 +60,14 @@ export default class WalletRestoreOptionDialog extends Component<Props> {
             <OptionBlock
               onSubmit={onRestore}
               type="restoreNormalWallet"
-              title={intl.formatMessage(messages.walletMnemonicTitle)}
-              learnMoreText={intl.formatMessage(messages.walletMnemonicDescription)}
+              title={intl.formatMessage(messages.restoreNormalTitle)}
+              learnMoreText={intl.formatMessage(messages.restoreNormalDescription)}
             />
             <OptionBlock
               onSubmit={onPaperRestore}
               type="restorePaperWallet"
-              title={intl.formatMessage(messages.walletPaperTitle)}
-              learnMoreText={intl.formatMessage(messages.walletPaperDescription)}
+              title={intl.formatMessage(messages.restorePaperWalletTitle)}
+              learnMoreText={intl.formatMessage(messages.restorePaperWalletDescription)}
             />
           </ul>
         </div>
