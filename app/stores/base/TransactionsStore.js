2c3da44c583f5288e729aadaed7f505efb2c6e8f
Sebastien <sebastiengllmt@gmail.com>
2019-06-20 10:43:17 +0900

    fix BigNumber types (2/2)

--- app/stores/base/TransactionsStore.js
+++ app/stores/base/TransactionsStore.js
@@ -1,7 +1,6 @@
 // @flow
 import { observable, computed, action, extendObservable } from 'mobx';
 import _ from 'lodash';
-import BigNumber from 'bignumber.js';
 import Store from './Store';
 import CachedRequest from '../lib/LocalizedCachedRequest';
 import WalletTransaction from '../../domain/WalletTransaction';
