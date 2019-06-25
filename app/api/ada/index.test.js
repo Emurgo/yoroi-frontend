9af8e0b973bfb0ba206af06329835d8d753f6a2f
yushi <yushihuang@gmail.com>
2019-06-25 13:08:25 +0800

    chore: add test for AdaApi#restoreWallet

---
+++ app/api/ada/index.test.js
@@ -0,0 +1,33 @@
+// @flow
+import './lib/test-config';
+import AdaApi from './index';
+import { RustModule } from './lib/cardanoCrypto/rustLoader';
+import {
+  silenceLogsForTesting,
+} from '../../utils/logging';
+
+import type {
+  FilterUsedRequest,
+  FilterUsedResponse,
+} from './lib/state-fetch/types';
+
+beforeAll(async () => {
+  await RustModule.load();
+  //silenceLogsForTesting();
+});
+
+test('Restore wallet', async () => {
+  async function checkAddressesInUse(_body: FilterUsedRequest): Promise<FilterUsedResponse> {
+    return [];
+  }
+
+  const restoreRequest = {
+    recoveryPhrase: 'prevent company field green slot measure chief hero apple task eagle sunset endorse dress seed',
+    walletName: 'mywallet',
+    walletPassword: '123',
+    checkAddressesInUse
+  };
+
+  const response : any = await AdaApi.prototype.restoreWallet(restoreRequest);
+  expect(response.accounts[0].plate.id).toEqual('DBJL-9530');
+});
