70aa4ba936e5dd4ca3f82d9d7245b0817fdf45e9
YankoFelipe <yankofelipe.af@gmail.com>
2019-06-25 23:26:21 -0400

    Formatting the banner text correctly

--- app/components/topbar/banners/ServerErrorBanner.js
+++ app/components/topbar/banners/ServerErrorBanner.js
@@ -1,23 +1,22 @@
 // @flow
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
-import { intlShape, defineMessages, FormattedMessage } from 'react-intl';
+import { intlShape, defineMessages, FormattedHTMLMessage } from 'react-intl';
 import SvgInline from 'react-svg-inline';
 import globalMessages from '../../../i18n/global-messages';
 import { handleExternalLinkClick } from '../../../utils/routing';
 import styles from './ServerErrorBanner.scss';
-import environment from '../../../environment';
 import warningSvg from '../../../assets/images/warning.inline.svg';
 import type { ServerStatusErrorType } from '../../../types/serverStatusErrorType';
 
 const messages = defineMessages({
   serverErrorLabel: {
     id: 'serverError.label.message',
-    defaultMessage: '!!!WARNING: Connection to the server failed. Please check our Twitter account {twitterLink}',
+    defaultMessage: '!!!WARNING: Connection to the server failed. Please check our Twitter account {twitterLink}. Your funds are safe (SAFU), you can check them in any block explorer.',
   },
   networkErrorLabel: {
     id: 'networkError.label.message',
-    defaultMessage: '!!!WARNING: Connection to the server failed. Please check your internet connection or our Twitter account {twitterLink}',
+    defaultMessage: '!!!WARNING: Connection to the server failed. Please check your internet connection or our Twitter account {twitterLink}. Your funds are safe (SAFU), you can check them in any block explorer.',
   },
 });
 
@@ -64,11 +63,7 @@ export default class ServerErrorBanner extends Component<Props> {
         {displayMessage === null ? null : (
           <div className={styles.serverError}>
             <SvgInline key="0" svg={warningSvg} className={styles.warningIcon} />
-            <FormattedMessage
-              {...displayMessage}
-              values={{ twitterLink, network: environment.NETWORK }}
-              key="1"
-            />
+            <FormattedHTMLMessage {...displayMessage} values={{ twitterLink }} key="1" />
           </div>)
         }
       </div>
