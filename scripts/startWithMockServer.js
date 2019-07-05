e75e5e80a7c6da294ae287a200832bc4febc93a5
yushi <yushihuang@gmail.com>
2019-07-05 17:51:35 +0800

    feature: add script for developing against the mock backend server

---
+++ scripts/startWithMockServer.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const { getMockServer, closeMockServer } = require('../features/mock-chain/mockServer');
+const { testWallets } = require('../features/mock-chain/TestWallets');
+const  { resetChain } = require('../features/mock-chain/mockImporter');
+
+getMockServer({});
+resetChain();
+
+const tasks = require('./tasks');
+
+const createWebpackServer = require('webpack-httpolyglot-server');
+
+const ENV = 'test';
+
+const config = require(`../webpack/devConfig`);
+process.env.NODE_CONFIG_ENV = ENV;
+
+tasks.replaceWebpack();
+console.log('[Copy assets]');
+console.log('-'.repeat(80));
+tasks.copyAssets('dev', ENV);
+
+console.log('[Webpack Dev]');
+console.log('-'.repeat(80));
+console.log('If you\'re developing Inject page,');
+console.log('please allow `https://localhost:3000` connections in Google Chrome,');
+console.log('and load unpacked extensions with `./dev` folder. (see https://developer.chrome.com/extensions/getstarted#unpacked)\n');
+
+createWebpackServer(config.baseDevConfig(ENV), {
+  host: 'localhost',
+  port: 3000
+});
