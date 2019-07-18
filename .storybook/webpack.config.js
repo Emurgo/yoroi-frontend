9397d7d2531720f5c32886deebb70a50e294d90b
SARVESHWAR SINGH <sarveshwar@emurgo.io>
2019-07-18 17:51:29 +0900

    add storysource

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,5 +4,16 @@ const custom = require('../webpack/devConfig');
 
 module.exports = async ({ config, mode }) => {
   const customConfig = custom.baseDevConfig();
-  return { ...config, module: { ...config.module, rules: customConfig.module.rules } };
+  const finalConfig = {
+    ...config,
+    module: { ...config.module, rules: customConfig.module.rules }
+  };
+
+  finalConfig.module.rules.push({
+    test: /\.stories\.jsx?$/,
+    loaders: [require.resolve('@storybook/addon-storysource/loader')],
+    enforce: 'pre',
+  });
+
+  return finalConfig;
 };
