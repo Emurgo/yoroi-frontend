d663e97d109309d78440491546ed11aaa465f817
Sebastien <sebastiengllmt@gmail.com>
2019-08-20 02:06:48 +0900

    fix environment usage

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,11 +1,24 @@
+// @flow
+
 const path = require('path');
+const webpack = require('webpack');
+const ConfigWebpackPlugin = require('config-webpack');
+
 // your app's webpack.config.js
 const custom = require('../webpack/devConfig');
+const commonConfig = require('../webpack/commonConfig');
+
+const ENV = 'test';
 
 module.exports = async ({ config, mode }) => {
-  const customConfig = custom.baseDevConfig();
+  const customConfig = custom.baseDevConfig(ENV);
   const finalConfig = {
     ...config,
+    plugins: [
+      ...config.plugins,
+      new ConfigWebpackPlugin(),
+      new webpack.DefinePlugin(commonConfig.definePlugin(ENV, false))
+    ],
     module: { ...config.module, rules: customConfig.module.rules }
   };
 
