f4109b84e99dfb15ec87960132974dbb2a3c8109
Sebastien <sebastiengllmt@gmail.com>
2019-09-19 23:19:03 +0900

    comment

--- chrome/manifestEnvs.js
+++ chrome/manifestEnvs.js
@@ -1,8 +1,14 @@
 // @flow
 
-// URL of full featured Seiza
-export const SEIZA_URL = process.env.SEIZA_URL || 'http://localhost:3001';
-
 // URL of Seiza for Yoroi
-// export const SEIZA_FOR_YOROI_URL = process.env.SEIZA_FOR_YOROI_URL || 'https://seiza.com';
-export const SEIZA_FOR_YOROI_URL = process.env.SEIZA_FOR_YOROI_URL || 'http://localhost:3001';
+export const SEIZA_FOR_YOROI_URL = process.env.SEIZA_FOR_YOROI_URL || 'https://seiza.com';
+//export const SEIZA_FOR_YOROI_URL = process.env.SEIZA_FOR_YOROI_URL || 'http://localhost:3001';
+
+/**
+ * SEIZA_FOR_YOROI_URL is the URL of the yoroi version of Seiza, that's in an <iframe>
+ * But the Yoroi version of Seiza has an "Open in Seiza" feature
+ * that opens a new tab with original Seiza URL, which is a different URL than Seiza for Yoroi.
+ * Therefore we also need to have SEIZA_URL in manifest.json (frame-src)
+ */
+export const SEIZA_URL = process.env.SEIZA_URL || 'https://seiza.com';
+//export const SEIZA_URL = process.env.SEIZA_URL || 'http://localhost:3001';
