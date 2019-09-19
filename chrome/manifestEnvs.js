7d1db4dd3be71cdb576e0b83a08aaae1e113bc81
Sebastien <sebastiengllmt@gmail.com>
2019-09-20 00:08:25 +0900

    fix export for commonjs

--- chrome/manifestEnvs.js
+++ chrome/manifestEnvs.js
@@ -1,8 +1,8 @@
 // @flow
 
 // URL of Seiza for Yoroi
-export const SEIZA_FOR_YOROI_URL = process.env.SEIZA_FOR_YOROI_URL || 'https://seiza.com';
-//export const SEIZA_FOR_YOROI_URL = process.env.SEIZA_FOR_YOROI_URL || 'http://localhost:3001';
+const SEIZA_FOR_YOROI_URL = process.env.SEIZA_FOR_YOROI_URL || 'https://seiza.com';
+// export const SEIZA_FOR_YOROI_URL = process.env.SEIZA_FOR_YOROI_URL || 'http://localhost:3001';
 
 /**
  * SEIZA_FOR_YOROI_URL is the URL of the yoroi version of Seiza, that's in an <iframe>
@@ -10,5 +10,10 @@ export const SEIZA_FOR_YOROI_URL = process.env.SEIZA_FOR_YOROI_URL || 'https://s
  * that opens a new tab with original Seiza URL, which is a different URL than Seiza for Yoroi.
  * Therefore we also need to have SEIZA_URL in manifest.json (frame-src)
  */
-export const SEIZA_URL = process.env.SEIZA_URL || 'https://seiza.com';
-//export const SEIZA_URL = process.env.SEIZA_URL || 'http://localhost:3001';
+const SEIZA_URL = process.env.SEIZA_URL || 'https://seiza.com';
+// export const SEIZA_URL = process.env.SEIZA_URL || 'http://localhost:3001';
+
+module.exports = {
+  SEIZA_FOR_YOROI_URL,
+  SEIZA_URL,
+};
