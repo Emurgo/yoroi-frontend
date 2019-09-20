f869db8ff0a90fd02b7f602e1c6d6aded97b2e7f
Sebastien <sebastiengllmt@gmail.com>
2019-09-20 15:38:59 +0900

    small fix

--- scripts/compress.js
+++ scripts/compress.js
@@ -4,6 +4,9 @@ const path = require('path');
 const ChromeExtension = require('crx');
 const argv = require('minimist')(process.argv.slice(2));
 /* eslint import/no-unresolved: 0 */
+
+// Note: do NOT delete this $FlowFixMe. CI needs it to avoid failing to look up the generated file
+// $FlowFixMe this file is generated at build time so Flow fails to find it
 const name = require('../build/manifest.json').name;
 
 const keyPath = argv.key;
