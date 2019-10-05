f137bba435408480f90aec3a1aac1a93e8699aca
Sarveshwar Singh <sarveshwar@emurgo.io>
2019-10-06 00:19:56 +0900

    Close WebPage window when port is closed

--- chrome/hw-wallet/ledger/ledger-content-script.js
+++ chrome/hw-wallet/ledger/ledger-content-script.js
@@ -1,26 +1,36 @@
-
-let portName = {
-  name: 'YOROI-LEDGER-CONNECT'
-};
-
-// Make Extension and WebPage port to communicate over this channel
-let browserPort = chrome.runtime.connect(portName);
-
-// Passing messages from Extension ==> WebPage
-browserPort.onMessage.addListener(msg => {
-  window.postMessage(msg, window.location.origin);
-});
-
-// De-refer and close window when port is closed
-browserPort.onDisconnect.addListener(d => {
-  window.close();
-  browserPort = null;
-});
-
-// Passing messages from WebPage ==> Extension
-window.addEventListener('message', event => {
-  console.log(JSON.stringify(event.data, null, 2));
-  if (browserPort) {
-    browserPort.postMessage(event.data)
+(function init () {
+  const YOROI_LEDGER_CONNECT_TARGET_NAME = 'YOROI-LEDGER-CONNECT';
+  const closeWindowMsg = {
+    target: YOROI_LEDGER_CONNECT_TARGET_NAME,
+    action: 'close-window'
   }
-});
+  const portName = {
+    name: YOROI_LEDGER_CONNECT_TARGET_NAME
+  };
+  
+  // Make Extension and WebPage port to communicate over this channel
+  let browserPort = chrome.runtime.connect(portName);
+  
+  // Passing messages from Extension ==> WebPage
+  browserPort.onMessage.addListener(msg => {
+    window.postMessage(msg, window.location.origin);
+  });
+  
+  // Close WebPage window when port is closed
+  browserPort.onDisconnect.addListener(d => {
+    console.debug(`Closing WebPage window!!`);
+    window.postMessage(closeWindowMsg, window.location.origin);
+  });
+  
+  // Passing messages from WebPage ==> Extension
+  window.addEventListener('message', event => {
+    const { data } = event; 
+    if (data) {
+      console.debug(`${data.action}::${data.success}`);
+    }
+  
+    if (browserPort) {
+      browserPort.postMessage(event.data)
+    }
+  });
+}());
