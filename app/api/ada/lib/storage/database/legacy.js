acae295431a4f3a1be34f4f78420352d4bc3f0a6
Sebastien <sebastiengllmt@gmail.com>
2019-10-09 11:17:27 +0900

    checkpoint move stuff over

--- app/api/ada/lib/storage/database/legacy.js
+++ app/api/ada/lib/storage/database/legacy.js
@@ -1,6 +1,7 @@
 // @flow
 
 import { dumpByVersion } from './index';
+import { RustModule } from '../../cardanoCrypto/rustLoader';
 
 /**
  * This file contains methods used to extract information
@@ -22,6 +23,26 @@ export type LegacyAdaAddress = {
   cadId: string,
   cadIsUsed: boolean,
 } & LegacyAddressingInfo;
+
+type LegacyLocalStorageWallet = {
+  adaWallet: AdaWallet,
+  masterKey?: string, // unused in hardware wallets
+  // this is a per-account setting but we only have 1 account per wallet in Yoroi
+  lastReceiveAddressIndex: number
+}
+
+export type LegacyCryptoAccount = {
+  account: number,
+  // master public key
+  root_cached_key: RustModule.Wallet.Bip44AccountPublic,
+  derivation_scheme: string
+}
+
+type LegacyLocalStorageCryptoAccount = {
+  account: number,
+  root_cached_key: string, // MasterPublicKey
+  derivation_scheme: string
+}
 
 export const getLegacyAddressesList = (): Array<LegacyAdaAddress> => {
   if (dumpByVersion.Addresses) {
