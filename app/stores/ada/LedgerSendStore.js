8c758036908b4d738308f448dcf241ceeef88dfe
SARVESHWAR SINGH <sarveshwar@emurgo.io>
2019-10-21 15:41:28 +0900

    Ledger Senc Tx UI was getting blocked for few seconds

--- app/stores/ada/LedgerSendStore.js
+++ app/stores/ada/LedgerSendStore.js
@@ -126,6 +126,12 @@ export default class LedgerSendStore extends Store {
           ledgerSignTxDataResp.ledgerSignTxPayload.outputs,
         );
 
+      // There is no need of ledgerConnect after this line.
+      // UI was getting blocked for few seconds
+      // because _prepareAndBroadcastSignedTx takes time.
+      // Disposing here will fix the UI issue.
+      ledgerConnect && ledgerConnect.dispose();
+
       await this._prepareAndBroadcastSignedTx(
         ledgerSignTxResp,
         params.signRequest.unsignedTx,
