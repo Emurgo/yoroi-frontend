dc4f176a5a310801a519f7595fcf203b656ed769
Sebastien <sebastiengllmt@gmail.com>
2019-11-05 19:07:37 +0900

    remove some unused i18n strings

--- app/i18n/errors.js
+++ app/i18n/errors.js
@@ -1,11 +1,19 @@
 // @flow
 import LocalizableError from './LocalizableError';
+import { defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  unableToLoad: {
+    id: 'app.errors.unableToLoad',
+    defaultMessage: '!!!Unable to load!',
+  },
+});
 
 export class UnableToLoadError extends LocalizableError {
   constructor() {
     super({
-      id: 'app.errors.unableToLoad',
-      defaultMessage: '!!!Unable to load!',
+      id: messages.unableToLoad.id,
+      defaultMessage: messages.unableToLoad.defaultMessage || '',
     });
   }
 }
