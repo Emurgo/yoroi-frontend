e2f53fbfb74d6eb4028886eda3d5fff1247b46e5
SARVESHWAR SINGH <sarveshwar@emurgo.io>
2019-11-08 17:11:41 +0900

    add new theme: YoroiShelleyTestnet

---
+++ app/themes/prebuilt/YoroiShelleyTestnet.js
@@ -0,0 +1,12 @@
+// @flow
+//  ==== Theme: Yoroi Shelley Testnet === //
+
+// Using YoroiModern as base for YoroiShelleyTestnet theme
+import YoroiShelleyTestnet from './YoroiModern';
+
+// Here you can override or add new key-value pair
+const overrides = {
+
+};
+
+export default Object.assign(YoroiShelleyTestnet, overrides);
