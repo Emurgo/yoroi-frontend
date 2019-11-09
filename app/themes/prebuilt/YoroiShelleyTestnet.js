36fe5d4e1012fb95a547231e72f04a111862a0f5
Sarveshwar Singh <sarveshwar@emurgo.io>
2019-11-09 13:23:37 +0900

    add topbar color

--- app/themes/prebuilt/YoroiShelleyTestnet.js
+++ app/themes/prebuilt/YoroiShelleyTestnet.js
@@ -6,7 +6,7 @@ import YoroiShelleyTestnet from './YoroiModern';
 
 // Here you can override or add new key-value pair
 const overrides = {
-
+  '--theme-topbar-background-color': 'linear-gradient(225deg, #F14D78 0%, #1A44B7 100%)'
 };
 
 export default Object.assign(YoroiShelleyTestnet, overrides);
