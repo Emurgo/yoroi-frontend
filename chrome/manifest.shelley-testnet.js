836d43ca77359ef51b77a912895019b890988ba5
Nicolas Arqueros <nykholas@gmail.com>
2019-11-13 19:07:08 -0500

    update config

--- chrome/manifest.shelley-testnet.js
+++ chrome/manifest.shelley-testnet.js
@@ -8,7 +8,7 @@ export default buildManifest({
   description: 'A simple, secure and fast Cardano ADA wallet.',
   defaultTitle: 'Yoroi Shelley Testnet',
   // TODO: change backend URLs
-  contentSecurityPolicy: `default-src 'self'; frame-src ${SEIZA_FOR_YOROI_URL} ${SEIZA_URL} https://connect.trezor.io/ https://emurgo.github.io/yoroi-extension-ledger-bridge; script-src 'self' 'unsafe-eval' blob:; connect-src https://iohk-mainnet.yoroiwallet.com wss://iohk-mainnet.yoroiwallet.com:443; style-src * 'unsafe-inline' 'self' blob:; img-src 'self' data:;`,
+  contentSecurityPolicy: `default-src 'self'; frame-src ${SEIZA_FOR_YOROI_URL} ${SEIZA_URL} https://connect.trezor.io/ https://emurgo.github.io/yoroi-extension-ledger-bridge; script-src 'self' 'unsafe-eval' blob:; connect-src https://shelley-testnet-yoroi-backend.yoroiwallet.com wss://shelley-testnet-yoroi-backend.yoroiwallet.com:443; style-src * 'unsafe-inline' 'self' blob:; img-src 'self' data:;`,
   iconOverride: {
     /* eslint-disable quote-props */
     '16': 'img/shelley-16.png',
