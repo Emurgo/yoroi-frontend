59323cad907d60933091f7ba6e23f40953957e09
Sebastien <sebastiengllmt@gmail.com>
2019-11-22 14:20:20 +0900

    Refactor transfer tx generation

---
+++ app/api/ada/transactions/byron/yoroiTransfer.js
@@ -0,0 +1,130 @@
+// @flow
+
+import { isEmpty } from 'lodash';
+import BigNumber from 'bignumber.js';
+import { coinToBigNumber, v3SecretToV2, } from '../utils';
+import {
+  Logger,
+  stringifyError,
+} from '../../../../utils/logging';
+import { LOVELACES_PER_ADA } from '../../../../config/numbersConfig';
+import {
+  NoInputsError,
+  GenerateTransferTxError
+} from '../../errors';
+import {
+  sendAllUnsignedTx,
+  signTransaction,
+} from './transactionsV2';
+import type { AddressedUtxo } from '../types';
+import type {
+  AddressUtxoFunc,
+} from '../../lib/state-fetch/types';
+import type {
+  TransferTx
+} from '../../../../types/TransferTypes';
+import { RustModule } from '../../lib/cardanoCrypto/rustLoader';
+import type {
+  Address, Addressing
+} from '../../lib/storage/models/PublicDeriver/interfaces';
+
+/**
+ * Generate transaction including all addresses with no change.
+*/
+export async function generateYoroiTransferTx(payload: {
+  addresses: Array<{| ...Address, ...Addressing |}>,
+  outputAddr: string,
+  keyLevel: number,
+  signingKey: RustModule.WalletV3.Bip32PrivateKey,
+  getUTXOsForAddresses: AddressUtxoFunc,
+}): Promise<TransferTx> {
+  // fetch UTXO
+  const utxos = await payload.getUTXOsForAddresses({
+    addresses: payload.addresses.map(addr => addr.address)
+  });
+
+  // add addressing info to the UTXO
+  const addressingMap = new Map<string, Addressing>(
+    payload.addresses.map(entry => [
+      entry.address,
+      { addressing: entry.addressing }
+    ])
+  );
+  const senderUtxos = utxos.map(utxo => {
+    const addressing = addressingMap.get(utxo.receiver);
+    if (addressing == null) {
+      throw new Error('should never happen');
+    }
+    return {
+      ...utxo,
+      addressing: addressing.addressing
+    };
+  });
+
+  if (isEmpty(utxos)) {
+    const error = new NoInputsError();
+    Logger.error(`yoroiTransfer::generateYoroiTransferTx ${stringifyError(error)}`);
+    throw error;
+  }
+
+  return buildYoroiTransferTx({
+    senderUtxos,
+    outputAddr: payload.outputAddr,
+    keyLevel: payload.keyLevel,
+    signingKey: payload.signingKey,
+  });
+}
+
+/**
+ * Generate transaction including all addresses with no change.
+*/
+export async function buildYoroiTransferTx(payload: {
+  senderUtxos: Array<AddressedUtxo>,
+  outputAddr: string,
+  keyLevel: number,
+  signingKey: RustModule.WalletV3.Bip32PrivateKey,
+}): Promise<TransferTx> {
+  try {
+    const { senderUtxos, outputAddr, } = payload;
+
+    const totalBalance = senderUtxos
+      .map(utxo => new BigNumber(utxo.amount))
+      .reduce(
+        (acc, amount) => acc.plus(amount),
+        new BigNumber(0)
+      );
+
+    // first build a transaction to see what the fee will be
+    const unsignedTxResponse = sendAllUnsignedTx(
+      outputAddr,
+      senderUtxos
+    );
+    const fee = coinToBigNumber(
+      unsignedTxResponse.txBuilder.get_balance_without_fees().value()
+    );
+
+    // sign inputs
+    const signedTx = signTransaction(
+      {
+        senderUtxos: unsignedTxResponse.senderUtxos,
+        unsignedTx: unsignedTxResponse.txBuilder.make_transaction(),
+        changeAddr: unsignedTxResponse.changeAddr,
+      },
+      payload.keyLevel,
+      v3SecretToV2(payload.signingKey)
+      ,
+    );
+
+    // return summary of transaction
+    return {
+      recoveredBalance: totalBalance.dividedBy(LOVELACES_PER_ADA),
+      fee: fee.dividedBy(LOVELACES_PER_ADA),
+      signedTx,
+      senders: senderUtxos.map(utxo => utxo.receiver),
+      receiver: outputAddr,
+    };
+  } catch (error) {
+    Logger.error(`transfer::buildTransferTx ${stringifyError(error)}`);
+    throw new GenerateTransferTxError();
+  }
+}
--- app/api/ada/transactions/utils.js
+++ app/api/ada/transactions/utils.js
@@ -256,19 +256,46 @@ export function copySignRequest(req: BaseSignRequest): BaseSignRequest {
   };
 }
 
-export function v2SkKeyToV3Key(
-  v2Key: RustModule.WalletV2.PrivateKey,
-): RustModule.WalletV3.PrivateKey {
-  return RustModule.WalletV3.PrivateKey.from_extended_bytes(
-    // need to slice out the chain code from the private key
-    Buffer.from(v2Key.to_hex().slice(0, 128), 'hex')
+export function v3SecretToV2(
+  v3Key: RustModule.WalletV3.Bip32PrivateKey,
+): RustModule.WalletV2.PrivateKey {
+  return RustModule.WalletV2.PrivateKey.from_hex(
+    Buffer.from(v3Key.as_bytes()).toString('hex')
   );
 }
-export function v2PkKeyToV3Key(
-  v2Key: RustModule.WalletV2.PublicKey,
-): RustModule.WalletV3.PublicKey {
-  return RustModule.WalletV3.PublicKey.from_bytes(
-    // need to slice out the chain code from the public key
-    Buffer.from(v2Key.to_hex().slice(0, 64), 'hex')
+export function v3PublicToV2(
+  v3Key: RustModule.WalletV3.Bip32PublicKey,
+): RustModule.WalletV2.PublicKey {
+  return RustModule.WalletV2.PublicKey.from_hex(
+    Buffer.from(v3Key.as_bytes()).toString('hex')
   );
+}
+
+/**
+ * Will return undefined for Daedalus addresses (can't be represented by v3 WASM)
+ */
+export function v2SecretToV3(
+  v2Key: RustModule.WalletV2.PrivateKey,
+): RustModule.WalletV3.Bip32PrivateKey | void {
+  try {
+    return RustModule.WalletV3.Bip32PrivateKey.from_bytes(
+      Buffer.from(v2Key.to_hex(), 'hex')
+    );
+  } catch (_e) {
+    return undefined;
+  }
+}
+/**
+ * Will return undefined for Daedalus addresses (can't be represented by v3 WASM)
+ */
+export function v2PublicToV3(
+  v2Key: RustModule.WalletV2.PublicKey,
+): RustModule.WalletV3.Bip32PublicKey | void {
+  try {
+    return RustModule.WalletV3.Bip32PublicKey.from_bytes(
+      Buffer.from(v2Key.to_hex(), 'hex')
+    );
+  } catch (_e) {
+    return undefined;
+  }
 }
