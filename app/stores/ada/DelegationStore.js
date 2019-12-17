242045b119728d2bd26d3d96eed9f913adcd7ba6
Sebastien <sebastiengllmt@gmail.com>
2019-12-17 11:21:37 +0900

    reload account state on network status

--- app/stores/ada/DelegationStore.js
+++ app/stores/ada/DelegationStore.js
@@ -54,6 +54,8 @@ export default class DelegationStore extends Store {
         this.stores.substores.ada.transactions.totalAvailable,
         // need to recalculate when there are no more pending transactions
         this.stores.substores.ada.transactions.hasAnyPending,
+        // if query failed due to server issue, need to re-query when it comes back online
+        this.stores.substores.ada.serverConnectionStore.checkAdaServerStatus,
       ],
       // $FlowFixMe error in mobx types
       async () => {
