3c119e479a4225dae5830f7d338b6beb3c27079a
David Novak <david.novak@vacuumlabs.com>
2019-12-19 10:16:44 +0100

    Added styles for wallet amount after decimal place

--- app/components/wallet/my-wallets/WalletDetails.js
+++ app/components/wallet/my-wallets/WalletDetails.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import BigNumber from 'bignumber.js';
 import PublicDeriverWithCachedMeta from '../../../domain/PublicDeriverWithCachedMeta';
+import { DECIMAL_PLACES_IN_ADA } from '../../../config/numbersConfig';
 
 import styles from './WalletDetails.scss';
 import IconEyeOpen from '../../../assets/images/my-wallets/icon_eye_open.inline.svg';
@@ -14,6 +15,17 @@ type Props = {|
     +onUpdateHideBalance: void => void,
     +shouldHideBalance: boolean
 |};
+
+function splitAmount(
+  value: string | null,
+  index: number,
+): ([string | null, string | null]) {
+  if (value !== null) {
+    const startIndex = value.length - index;
+    return [value.substring(0, startIndex), value.substring(startIndex)];
+  }
+  return [null, null];
+}
 
 export default class WalletDetails extends Component<Props> {
   static defaultProps = {
@@ -29,9 +41,13 @@ export default class WalletDetails extends Component<Props> {
       onUpdateHideBalance
     } = this.props;
 
-    const walletAmount = formattedWalletAmount ?
+    const walletAmount = formattedWalletAmount ? (
       publicDeriver && formattedWalletAmount(publicDeriver.amount)
-      : null;
+    ) : null;
+
+    const [
+      beforeDecimal, afterDecimal
+    ]:([string | null, string | null])  = splitAmount(walletAmount, DECIMAL_PLACES_IN_ADA);
 
     const currency = ' ADA';
 
@@ -41,7 +57,10 @@ export default class WalletDetails extends Component<Props> {
           <div className={styles.amount}>
             { publicDeriver && shouldHideBalance ?
               <span className={styles.hiddenAmount}>******</span> :
-              walletAmount
+              <>
+                {beforeDecimal}
+                <span className={styles.afterDecimal}>{afterDecimal}</span>
+              </>
             }
             {currency}
           </div>
