51320b08995db9308aab8a634a014d45d1dbaa7d
David Novak <david.novak@vacuumlabs.com>
2020-01-17 10:51:23 +0100

    Added NavBarBack component

--- app/containers/NavBarContainer.js
+++ app/containers/NavBarContainer.js
@@ -3,25 +3,43 @@ import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import type { InjectedProps } from '../types/injectedPropsType';
 import environment from '../environment';
-
+import { intlShape, defineMessages } from 'react-intl';
 import { formattedWalletAmount } from '../utils/formatters';
 import NavBar from '../components/topbar/NavBar';
 import NavPlate from '../components/topbar/NavPlate';
-import NavBarTitle from '../components/topbar/NavBarTitle';
 import NavWalletDetails from '../components/topbar/NavWalletDetails';
 import NavDropdown from '../components/topbar/NavDropdown';
 import NavDropdownRow from '../components/topbar/NavDropdownRow';
+import NavBarBack from '../components/topbar/NavBarBack';
+import { ROUTES } from '../routes-config';
+
+const messages = defineMessages({
+  backButton: {
+    id: 'wallet.nav.backButton',
+    defaultMessage: '!!!Back to my wallets',
+  },
+});
 
 type Props = InjectedProps;
 
 @observer
 export default class NavBarContainer extends Component<Props> {
 
+  static contextTypes = {
+    intl: intlShape.isRequired,
+  };
+
+
   updateHideBalance = () => {
     this.props.actions.profile.updateHideBalance.trigger();
   }
 
+  navigateToWallets = (destination: string) => {
+    this.props.actions.router.goToRoute.trigger({ route: destination });
+  }
+
   render() {
+    const { intl } = this.context;
     const { stores } = this.props;
     const { profile } = stores;
 
@@ -29,8 +47,14 @@ export default class NavBarContainer extends Component<Props> {
     const publicDeriver = walletsStore.selected;
     const walletName = publicDeriver ? publicDeriver.conceptualWalletName : '';
 
-    // TODO: Change to i18n
-    const title = (<NavBarTitle title="My Wallets" />);
+    // TODO: Replace route with ROUTES.WALLETS.ROOT after merging MyWallets screen
+    const title = (
+      <NavBarBack
+        route={ROUTES.SETTINGS.ROOT}
+        onBackClick={this.navigateToWallets}
+        title={intl.formatMessage(messages.backButton)}
+      />
+    );
 
     const plateComponent = (
       <NavPlate
