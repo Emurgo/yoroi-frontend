c90ee7f4f561de7f33ee00467c5152d029e9ee64
Sebastien Guillemot <SebastienGllmt@gmail.com>
2020-01-17 20:30:01 +0900

    Add support for undelegating (#1310)
    * Add support for undelegating
    
    * fix import order issue

--- app/api/ada/lib/storage/bridge/tests/mockNetwork.js
+++ app/api/ada/lib/storage/bridge/tests/mockNetwork.js
@@ -11,12 +11,12 @@ import type {
   RemoteTransaction, RemoteUnspentOutput,
   AccountStateRequest, AccountStateResponse, AccountStateFunc,
   PoolInfoRequest, PoolInfoResponse, PoolInfoFunc,
-  AccountStateSuccess, AccountStateFailure, AccountStateDelegation, PoolTuples,
+  AccountStateSuccess, AccountStateFailure, AccountStateDelegation,
   SignedRequestInternal, RemoteCertificate,
   RemoteTransactionInput, RemoteTransactionOutput,
 } from '../../../state-fetch/types';
 import { RollbackApiError, } from '../../../../errors';
-import { addressToKind, groupToSingle, } from '../utils';
+import { addressToKind, groupToSingle, delegationTypeToResponse, } from '../utils';
 import { CoreAddressTypes } from '../../database/primitives/enums';
 import type { CoreAddressT } from '../../database/primitives/enums';
 import {
@@ -543,40 +543,6 @@ export function toRemoteTx(
     epoch: null,
     slot: null,
   };
-}
-
-function delegationTypeToResponse(
-  type: RustModule.WalletV3.DelegationType,
-): AccountStateDelegation {
-  const kind = type.get_kind();
-  switch (kind) {
-    case RustModule.WalletV3.DelegationKind.NonDelegated: return { pools: [], };
-    case RustModule.WalletV3.DelegationKind.Full: {
-      const poolId = type.get_full();
-      if (poolId == null) {
-        throw new Error(`${nameof(delegationTypeToResponse)} Should never happen`);
-      }
-      return {
-        pools: [[poolId.to_string(), 1]]
-      };
-    }
-    case RustModule.WalletV3.DelegationKind.Ratio: {
-      const ratios = type.get_ratios();
-      if (ratios == null) {
-        throw new Error(`${nameof(delegationTypeToResponse)} Should never happen`);
-      }
-      const poolTuples: Array<PoolTuples> = [];
-      const pools = ratios.pools();
-      for (let i = 0; i < pools.size(); i++) {
-        const pool = pools.get(i);
-        poolTuples.push([pool.pool().to_string(), pool.parts()]);
-      }
-      return {
-        pools: poolTuples,
-      };
-    }
-    default: throw new Error(`${nameof(delegationTypeToResponse)} unexpected kind ${kind}`);
-  }
 }
 
 function getPoolInfoIfMatch(
