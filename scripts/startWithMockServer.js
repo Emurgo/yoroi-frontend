849823b5ca688e0b42cd757c36d071fc2af9d2aa
Sebastien Guillemot <sebastiengllmt@gmail.com>
2020-02-08 06:15:03 +0900

    fix flow issues

--- scripts/startWithMockServer.js
+++ scripts/startWithMockServer.js
@@ -14,7 +14,7 @@ tasks.replaceWebpack();
 
 console.log('[Build manifest]');
 console.log('-'.repeat(80));
-tasks.buildManifests();
+tasks.buildManifests(true);
 
 console.log('[Copy assets]');
 console.log('-'.repeat(80));
@@ -26,7 +26,7 @@ console.log('If you\'re developing Inject page,');
 console.log(`please allow 'https://localhost:${connections.Ports.WebpackDev}' connections in Google Chrome,`);
 console.log('and load unpacked extensions with `./dev` folder. (see https://developer.chrome.com/extensions/getstarted#unpacked)\n');
 
-// mock backend script runus the same babel for compiling the server as for compiling the website
+// mock backend script runs the same babel for compiling the server as for compiling the website
 // this is problematic because we need the nodejs version of the WASM bindings for the server
 // we hack it by just loading the nodejs versions here and swapping it into the variables
 const { RustModule } = require('../app/api/ada/lib/cardanoCrypto/rustLoader');
