1232827b98c273a9037ffcedbcea5738dfff9694
Sebastien Guillemot <sebastiengllmt@gmail.com>
2020-03-10 08:58:57 +0900

    URI scheme storybook

--- app/containers/uri/URILandingDialogContainer.js
+++ app/containers/uri/URILandingDialogContainer.js
@@ -1,26 +1,26 @@
 // @flow
 import React, { Component } from 'react';
-import { action, observable } from 'mobx';
+import { computed, action, observable } from 'mobx';
 import { observer } from 'mobx-react';
 
-import type { InjectedDialogContainerProps } from '../../types/injectedPropsType';
+import type { InjectedOrGenerated } from '../../types/injectedPropsType';
 
 import URILandingDialog from '../../components/uri/URILandingDialog';
 import URIVerifyDialog from '../../components/uri/URIVerifyDialog';
 import URIInvalidDialog from '../../components/uri/URIInvalidDialog';
 
-import type { UriParams } from '../../utils/URIHandling';
+export type GeneratedData = typeof URILandingDialogContainer.prototype.generated;
 
 type Props = {|
-  ...InjectedDialogContainerProps,
+  ...InjectedOrGenerated<GeneratedData>,
+  +onClose: void => void,
   +onConfirm: void => void,
-  +uriParams: ?UriParams,
 |};
 
 @observer
 export default class URILandingDialogContainer extends Component<Props> {
 
-  @observable showDisclaimer = true;
+  @observable showDisclaimer: boolean = true;
 
   @action
   toggleShowDisclaimer: void => void = () => {
@@ -40,7 +40,7 @@ export default class URILandingDialogContainer extends Component<Props> {
   }
 
   render() {
-    if (!this.props.uriParams) {
+    if (!this.generated.stores.loading.uriParams) {
       return (
         <URIInvalidDialog
           onClose={this.onCancel}
@@ -49,29 +49,47 @@ export default class URILandingDialogContainer extends Component<Props> {
       );
     }
     // assert not null
-    const uriParams = this.props.uriParams;
+    const uriParams = this.generated.stores.loading.uriParams;
 
     if (!this.showDisclaimer) {
       return (
         <URIVerifyDialog
           onSubmit={this.onVerifiedSubmit}
-          onBack={() => this.toggleShowDisclaimer()}
+          onBack={this.toggleShowDisclaimer}
           onCancel={this.onCancel}
           uriParams={uriParams}
-          selectedExplorer={this.props.stores.profile.selectedExplorer}
+          selectedExplorer={this.generated.stores.profile.selectedExplorer}
         />
       );
     }
 
     return (
       <URILandingDialog
-        onSubmit={() => this.toggleShowDisclaimer()}
+        onSubmit={this.toggleShowDisclaimer}
         onClose={this.onCancel}
-        classicTheme={this.props.classicTheme}
+        classicTheme={this.generated.stores.profile.isClassicTheme}
       />
     );
-
-
   }
 
+  @computed get generated() {
+    if (this.props.generated !== undefined) {
+      return this.props.generated;
+    }
+    if (this.props.stores == null || this.props.actions == null) {
+      throw new Error(`${nameof(URILandingDialogContainer)} no way to generated props`);
+    }
+    const { stores, } = this.props;
+    return Object.freeze({
+      stores: {
+        profile: {
+          selectedExplorer: stores.profile.selectedExplorer,
+          isClassicTheme: stores.profile.isClassicTheme,
+        },
+        loading: {
+          uriParams: stores.loading.uriParams,
+        },
+      },
+    });
+  }
 }
