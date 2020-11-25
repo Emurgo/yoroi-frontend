5c18c2b048341cd2498fc44d4322aed6005056f0
rooooooooob <raftias@gmail.com>
2020-11-24 21:28:53 -0500

    Fix Connector-Yoroi RPC flow errors

--- chrome/extension/background.js
+++ chrome/extension/background.js
@@ -3,12 +3,24 @@ import debounce from 'lodash/debounce';
 
 import { setupApi } from '../../app/api/index';
 import { schema } from 'lovefield';
+import type {
+  lf$Database,
+} from 'lovefield';
 import {
   loadLovefieldDB,
 } from '../../app/api/ada/lib/storage/database/index';
 import {
   getWallets
 } from '../../app/api/common/index';
+import {
+  PublicDeriver,
+} from '../../app/api/ada/lib/storage/models/PublicDeriver/index';
+import {
+  asGetBalance,
+} from '../../app/api/ada/lib/storage/models/PublicDeriver/traits';
+import type {
+  IPublicDeriver,
+} from '../../app/api/ada/lib/storage/models/PublicDeriver/interfaces';
 
 /*::
 declare var chrome;
@@ -20,25 +32,22 @@ const onYoroiIconClicked = () => {
   chrome.tabs.create({ url: 'main_window.html' });
 };
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
 chrome.browserAction.onClicked.addListener(debounce(onYoroiIconClicked, 500, { leading: true }));
 
-let db = undefined
+let db: ?lf$Database = null;
 
 async function connectHandler(message, sender, sendResponse) {
-  // TODO: make this only loaded when needed
-  async function firstWallet() {
-    //const api = await setupApi();
-    const wallets = await getWallets({db: db});
-    return wallets[0];
+  async function firstWallet(): Promise<PublicDeriver<>> {
+    if (db != null) {
+      const wallets = await getWallets({db: db});
+      return new Promise.resolve(wallets[0]);
+    } else {
+      throw Promise.reject("Database not loaded");
+    }
   }
   if (sender.id == connectorId) {
     console.log("REAL(background.js)-yoroi received: " + JSON.stringify(message))
     if (message.type == "yoroi_connect_request") {
-      //chrome.tabs.create({ url: 'main_window.html', active: true });
       db = await loadLovefieldDB(schema.DataStoreType.INDEXED_DB);
       chrome.runtime.sendMessage(
 		    connectorId,
@@ -51,11 +60,14 @@ async function connectHandler(message, sender, sendResponse) {
       case "get_balance":
         if (message.params[0] == "ERG") {
           const wallet = await firstWallet();
-          wallet.getBalance().then(x => {
-            sendResponse({
-              ok: x
+          const canGetBalance = asGetBalance(wallet);
+          if (canGetBalance != null) {
+            canGetBalance.getBalance().then(x => {
+              sendResponse({
+                ok: x
+              });
             });
-          });
+          }
         } else {
           sendResponse({
               ok: 5
@@ -88,7 +100,7 @@ async function connectHandler(message, sender, sendResponse) {
         break;
     }
   } else {
-      console.log("received message \"" + message + "\" from other sender: " + sender.id);
+    console.log("received message \"" + message + "\" from other sender: " + sender.id);
   }
 }
 
