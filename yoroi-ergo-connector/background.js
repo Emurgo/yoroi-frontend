dcafd1e4614f96c5a4e214d75c10b275426829da
rooooooooob <raftias@gmail.com>
2020-12-04 00:47:58 -0500

    TX sign confirmation attempt using notifications
    As the page action popup can only be opened from a user action I tried
    to see if notifications would work. This is just a WIP attempt and we
    should not use them due to several issues:
    
    1) they're easily dismissed by clicking on any other option or just on
    the notification itself
    2) they require an additional permission
    3) we can't customize the GUI to display the tx information in a nice
    way
    
    More investigation will have to be done into creating a better popup for
    asking for user confirmation on TX signing

--- yoroi-ergo-connector/background.js
+++ yoroi-ergo-connector/background.js
@@ -1,10 +1,39 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+var signRpcRequest = null;
+var contentSignTabId = null;
+
+chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
+    if (notifId === "sign-tx") {
+        if (btnIdx === 0) {
+            chrome.tabs.sendMessage(contentSignTabId, { type: "sign_tx_confirm", rpcRequest: signRpcRequest });
+        } else if (btnIdx === 1) {
+            // handle error
+        }
+    }
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
     //alert("background message: " + JSON.stringify(sender));
-    if (request.type == "init_page_action") {
+    if (request.type === "init_page_action") {
         chrome.pageAction.setPopup({
             tabId: sender.tab.id,
             popup: "main.html"
         });
         chrome.pageAction.show(sender.tab.id);
+    } else if (request.type === "sign_tx_notification") {
+        signRpcRequest = request.rpcRequest;
+        contentSignTabId = sender.tab.id;
+        chrome.notifications.create(
+            "sign-tx",
+            {
+                type: "basic",
+                iconUrl: "yoroi.svg",
+                title: "Sign TX Request",
+                message: JSON.stringify(request.rpcRequest.params[0]),
+                buttons: [
+                    { title: "Sign" },
+                    { title: "Cancel" }
+                ]
+                //requireInteraction: true
+            });
     }
 });
