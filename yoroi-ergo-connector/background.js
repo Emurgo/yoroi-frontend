e6888b6b7acdcb3bb37c5edf5f0ffeb41fc93c69
rooooooooob <raftias@gmail.com>
2020-12-09 00:17:37 -0500

    Switch tx sign UI to custom popup page
    Notifications as mentioned before are an awful UX so we'll try a normal
    webpage.

--- yoroi-ergo-connector/background.js
+++ yoroi-ergo-connector/background.js
@@ -1,39 +1,25 @@
 var signRpcRequest = null;
-var contentSignTabId = null;
 
-chrome.notifications.onButtonClicked.addListener(function(notifId, btnIdx) {
-    if (notifId === "sign-tx") {
-        if (btnIdx === 0) {
-            chrome.tabs.sendMessage(contentSignTabId, { type: "sign_tx_confirm", rpcRequest: signRpcRequest });
-        } else if (btnIdx === 1) {
-            // handle error
-        }
-    }
-});
-
-chrome.runtime.onMessage.addListener(function(request, sender) {
-    //alert("background message: " + JSON.stringify(sender));
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    //console.log("background.js message: " + JSON.stringify(request));
     if (request.type === "init_page_action") {
         chrome.pageAction.setPopup({
             tabId: sender.tab.id,
             popup: "main.html"
         });
         chrome.pageAction.show(sender.tab.id);
+    } else if (request.type === "tx_sign_window_retrieve_data") {
+        const ret = signRpcRequest;
+        signRpcRequest = null;
+        sendResponse(ret);
     } else if (request.type === "sign_tx_notification") {
+        chrome.windows.create({
+            url: "sign.html",
+            width: 240,
+            height: 400,
+            focused: true,
+            type: "popup"
+        });
         signRpcRequest = request.rpcRequest;
-        contentSignTabId = sender.tab.id;
-        chrome.notifications.create(
-            "sign-tx",
-            {
-                type: "basic",
-                iconUrl: "yoroi.svg",
-                title: "Sign TX Request",
-                message: JSON.stringify(request.rpcRequest.params[0]),
-                buttons: [
-                    { title: "Sign" },
-                    { title: "Cancel" }
-                ]
-                //requireInteraction: true
-            });
     }
 });
