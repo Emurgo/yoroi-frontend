8b98f1c7f0719ca58f723a9462b709ce8f3d188b
Paul Ccari <paulclindo@gmail.com>
2021-01-31 01:40:08 -0500

    Work on Connect Page UI and connect to functionality

---
+++ app/ergo-connector/components/connect/ConnectPage.js
@@ -0,0 +1,114 @@
+// @flow
+import React, { Component } from 'react';
+import type { Node } from 'react';
+import { intlShape, defineMessages } from 'react-intl';
+import type { $npm$ReactIntl$IntlFormat } from 'react-intl';
+import styles from './ConnectPage.scss';
+import { Button } from 'react-polymorph/lib/components/Button';
+import { ButtonSkin } from 'react-polymorph/lib/skins/simple/ButtonSkin';
+import { Checkbox } from 'react-polymorph/lib/components/Checkbox';
+import { CheckboxSkin } from 'react-polymorph/lib/skins/simple/CheckboxSkin';
+import WalletCard from './WalletCard';
+import globalMessages from '../../../i18n/global-messages';
+import { observer } from 'mobx-react';
+
+const messages = defineMessages({
+  subtitle: {
+    id: 'ergo-connector.label.connect',
+    defaultMessage: '!!!Connect to',
+  },
+  selectAllWallets: {
+    id: 'ergo-connector.label.selectAllWallets',
+    defaultMessage: '!!!Select all wallets',
+  },
+  labelConnect: {
+    id: 'global.labels.connect',
+    defaultMessage: '!!!Connect',
+  },
+  connectInfo: {
+    id: 'ergo-connector.connect.info',
+    defaultMessage: '!!!Your connection preferences will be saved to your Yoroi dApp list',
+  },
+  noWalletsFound: {
+    id: 'ergo-connector.connect.noWalletsFound',
+    defaultMessage: '!!! Not found any wallet, Try again',
+  },
+});
+
+type Props = {|
+  accounts?: Array<Object>,
+  message?: {| tabId: number, url: string |},
+  onToggleCheckbox: number => void,
+  onCancel: () => void,
+  onConnect: number => void,
+  handleSubmit: () => void,
+|};
+
+@observer
+class ConnectPage extends Component<Props> {
+  static contextTypes: {| intl: $npm$ReactIntl$IntlFormat |} = {
+    intl: intlShape.isRequired,
+  };
+
+  render(): Node {
+    const { intl } = this.context;
+    const { accounts, message, onCancel, onToggleCheckbox, handleSubmit } = this.props;
+
+    const isCheckedWallet = accounts
+      ? Boolean(accounts.findIndex(item => item.checked === true))
+      : [];
+
+    return (
+      <>
+        <div className={styles.connectWrapper}>
+          <div className={styles.image}>
+            <img src="" alt="" />
+          </div>
+          <div className={styles.title}>
+            <h2>{intl.formatMessage(messages.subtitle)}</h2>
+            <p>{message?.url ?? ''}</p>
+          </div>
+        </div>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>
+            {/* TODO: Check multiple wallets */}
+            <Checkbox skin={CheckboxSkin} label={intl.formatMessage(messages.selectAllWallets)} />
+          </li>
+          {accounts && accounts.length ? (
+            accounts.map((item, idx) => (
+              <li key={item.name} className={styles.listItem}>
+                <Checkbox
+                  skin={CheckboxSkin}
+                  label={<WalletCard name={item.name} balance={item.balance} />}
+                  onChange={() => onToggleCheckbox(idx)}
+                  checked={item.checked || false}
+                />
+              </li>
+            ))
+          ) : (
+            <div>{intl.formatMessage(messages.noWalletsFound)}</div>
+          )}
+        </ul>
+        <div className={styles.bottom}>
+          <p>{intl.formatMessage(messages.connectInfo)} </p>
+          <div className={styles.wrapperBtn}>
+            <Button
+              className="secondary"
+              label={intl.formatMessage(globalMessages.cancel)}
+              skin={ButtonSkin}
+              onClick={onCancel}
+            />
+            <Button
+              label={intl.formatMessage(messages.labelConnect)}
+              skin={ButtonSkin}
+              disabled={isCheckedWallet}
+              onClick={handleSubmit}
+            />
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default ConnectPage;
---
+++ app/ergo-connector/containers/connect/ConnectContainer.js
@@ -0,0 +1,112 @@
+// @flow
+import type { Node } from 'react';
+import React, { Component } from 'react';
+import Layout from '../../components/layout/Layout';
+import ConnectPage from '../../components/connect/ConnectPage';
+import { getWalletsInfo } from '../../../../chrome/extension/background';
+import { observer } from 'mobx-react';
+
+type Props = {||};
+declare var chrome;
+
+type State = {|
+  accounts?: Array<any>,
+|};
+
+let chromeMessage;
+
+chrome.runtime.sendMessage({ type: 'connect_retrieve_data' }, function (response) {
+  console.log(response);
+  if (response) {
+    chromeMessage = response;
+  }
+});
+
+@observer
+export default class ConnectContainer extends Component<Props, State> {
+  static defaultProps: {| accounts: void |} = {
+    accounts: undefined,
+  };
+
+  state: State = {
+    accounts: [],
+  };
+
+  async componentDidMount() {
+    const accounts = await getWalletsInfo();
+    if (accounts) {
+      this.setState({ accounts });
+    }
+  }
+  onToggleCheckbox: (index: number) => void = index => {
+    console.log(index);
+    const { accounts } = this.state;
+    if (accounts) {
+      let newItems = accounts.slice();
+      newItems[index].checked = !newItems[index].checked;
+      this.setState({ accounts: newItems });
+    }
+  };
+
+  handleAllChecked: () => void = event => {
+    const { accounts } = this.state;
+    if (accounts) {
+      // eslint-disable-next-line no-return-assign
+      accounts.forEach(item => (item.isChecked = event));
+      this.setState({ accounts });
+    }
+  };
+
+  onConnect(walletIndex: number) {
+    chrome.storage.local.get('connector_whitelist', async result => {
+      const whitelist = Object.keys(result).length === 0 ? [] : result.connector_whitelist;
+      whitelist.push({ url: chromeMessage.url, walletIndex });
+      chrome.storage.local.set({ connector_whitelist: whitelist });
+      console.log('connecting now!', whitelist);
+    });
+    chrome.runtime.sendMessage({
+      type: 'connect_response',
+      accepted: true,
+      account: walletIndex,
+      tabId: chromeMessage.tabId,
+    });
+    console.log('connecting now!', chromeMessage);
+  }
+
+  onCancel() {
+    chrome.runtime.sendMessage({
+      type: 'connect_response',
+      accepted: false,
+      tabId: chromeMessage.tabId,
+    });
+    console.log('close now!', chromeMessage);
+  }
+
+  handleSubmit: () => void = () => {
+    const { accounts } = this.state;
+    if (accounts) {
+      const walletIndex = accounts.findIndex(item => item.checked === true);
+      if (walletIndex >= 0) {
+        this.onConnect(walletIndex);
+      }
+    }
+  };
+
+  render(): Node {
+    const { accounts } = this.state;
+    console.log(this.props);
+
+    return (
+      <>
+        <ConnectPage
+          message={chromeMessage}
+          accounts={accounts}
+          onConnect={this.onConnect}
+          onToggleCheckbox={this.onToggleCheckbox}
+          onCancel={this.onCancel}
+          handleSubmit={this.handleSubmit}
+        />
+      </>
+    );
+  }
+}
