40fbcf39a29e9db986e2a51f369a27c3727d33e5
Paul Ccari <paulclindo@gmail.com>
2021-03-11 21:09:51 -0500

    fix CSP connector for img src

--- packages/yoroi-ergo-connector/manifest/manifest.template.js
+++ packages/yoroi-ergo-connector/manifest/manifest.template.js
@@ -30,7 +30,7 @@ module.exports = ({
     "default_title": displayName,
     default_icon: icons,
   },
-  "content_security_policy": "default-src 'none'; script-src 'self';",
+  "content_security_policy": "default-src 'none'; script-src 'self'; img-src https://* ;",
   "permissions": [
     "activeTab",
   ]
