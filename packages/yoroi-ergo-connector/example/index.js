9a7f32c4e1813a98c4911af4436d016326e73eae
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2021-07-18 22:11:06 +0300

    Using json-bigint in the ergo-connector example project

--- packages/yoroi-ergo-connector/example/index.js
+++ packages/yoroi-ergo-connector/example/index.js
@@ -1,4 +1,14 @@
 import * as wasm from "ergo-lib-wasm-browser";
+import JSONBigInt from 'json-bigint';
+
+function convertUnsignedTxOutputsToBigInt(tx) {
+    tx.outputs.forEach(o => {
+        o.value = BigInt(o.value);
+        o.assets.forEach(a => {
+            a.amount = BigInt(a.amount);
+        })
+    });
+}
 
 function initDapp() {
     ergo_request_read_access().then(function(access_granted) {
@@ -14,17 +24,17 @@ function initDapp() {
             //     //console.log(`get_unused_addresses() = {`);
             //     // for (const address of addresses) {
             //     //     const addr = wasm.NetworkAddress.from_bytes(Buffer.from(address, 'hex'));
-            //     //     console.log(`${JSON.stringify(address)} -> ${addr.to_base58()}`);
+            //     //     console.log(`${JSONBigInt.stringify(address)} -> ${addr.to_base58()}`);
             //     // }
             //     // console.log('}');
-            //     console.log(`get_unused_addresses() = ${JSON.stringify(addresses)}`);
+            //     console.log(`get_unused_addresses() = ${JSONBigInt.stringify(addresses)}`);
             // });
             /*function pagedUsedAddresses(page, limit) {
                 ergo.get_used_addresses({ page, limit }).then(usedAddresses => {
-                    console.log(`usedAddresses[${page * limit} - ${page * limit + usedAddresses.length - 1}] = ${JSON.stringify(usedAddresses)}`);
+                    console.log(`usedAddresses[${page * limit} - ${page * limit + usedAddresses.length - 1}] = ${JSONBigInt.stringify(usedAddresses)}`);
                     pagedUsedAddresses(page + 1, limit);
                 }).catch(e => {
-                    console.log(`paginateError = ${JSON.stringify(e)}`);
+                    console.log(`paginateError = ${JSONBigInt.stringify(e)}`);
                 });
             }
             pagedUsedAddresses(0, 3);
@@ -32,18 +42,18 @@ function initDapp() {
             //     console.log(`get_used_addresses() = {`);
             //     for (const address of addresses) {
             //     //     const addr = wasm.NetworkAddress.from_bytes(Buffer.from(address, 'hex'));
-            //     //     console.log(`${JSON.stringify(address)} -> ${addr.to_base58()}`);
+            //     //     console.log(`${JSONBigInt.stringify(address)} -> ${addr.to_base58()}`);
             //         console.log(`    ${address}`);
             //     }
             //     console.log('}');
-            //     //console.log(`get_used_addresses() = ${JSON.stringify(addresses)}`);
+            //     //console.log(`get_used_addresses() = ${JSONBigInt.stringify(addresses)}`);
             // });
             const assetId = "12c83f696c0731cfa6f70d1dea4c438c29ba3a193bf651b0c74d101ad62cf8b9";
             ergo.get_balance(assetId).then(result => {
                 console.log(`get_balance(custom asset) = ${result}`);
             });
             ergo.get_utxos(5000, assetId).then(result => {
-                console.log(`get_utxos(5000, custom asset) = ${JSON.stringify(result)}`);
+                console.log(`get_utxos(5000, custom asset) = ${JSONBigInt.stringify(result)}`);
             });*/
             ergo.get_balance().then(async function(result) {
                 let tx = {};
@@ -56,7 +66,7 @@ function initDapp() {
                 button.textContent = "Send";
                 button.onclick = async function() {
                     status.innerText = "Creating transaction";
-                    const donationAddr = "9hD2Cw6yQL6zzrw3TFgKdwFkBdDdU3ro1xRFmjouDw4NYS2S5RD";
+                    const donationAddr = "9huf3zpvZpFzmyG1srwHWvwEYKCUvsuGpMKbXgcKjgHv1YiUKcw";
                     const creationHeight = 398959;
                     const amountToSend = BigInt(valueEntry.value);
                     const amountToSendBoxValue = wasm.BoxValue.from_i64(wasm.I64.from_str(amountToSend.toString()));
@@ -78,7 +88,7 @@ function initDapp() {
                     console.log(`${changeValue} | cv.ts() = ${changeValue.toString()}`);
                     const changeValueBoxValue = wasm.BoxValue.from_i64(wasm.I64.from_str(changeValue.toString()));
                     const changeAddr = await ergo.get_change_address();
-                    console.log(`changeAddr = ${JSON.stringify(changeAddr)}`);
+                    console.log(`changeAddr = ${JSONBigInt.stringify(changeAddr)}`);
                     const selector = new wasm.SimpleBoxSelector();
                     const boxSelection = selector.select(
                         wasm.ErgoBoxes.from_boxes_json(utxos),
@@ -86,7 +96,7 @@ function initDapp() {
                         new wasm.Tokens());
                     console.log(`boxes selected: ${boxSelection.boxes().len()}`);
                     const outputCandidates = wasm.ErgoBoxCandidates.empty();
-                    const token = new wasm.Token(wasm.TokenId.from_box_id(wasm.BoxId.from_str(utxos[2].boxId)), wasm.TokenAmount.from_i64(wasm.I64.from_str("1234567890123456789")));
+                    const token = new wasm.Token(wasm.TokenId.from_box_id(wasm.BoxId.from_str(utxos[0].boxId)), wasm.TokenAmount.from_i64(wasm.I64.from_str("1234567890123456789")));
                     const donationBoxBuilder = new wasm.ErgoBoxCandidateBuilder(
                         amountToSendBoxValue,
                         wasm.Contract.pay_to_address(wasm.Address.from_base58(donationAddr)),
@@ -114,7 +124,8 @@ function initDapp() {
                     //dataInputs.add(new wasm.DataInput(wasm.BoxId.from_str("0f0e4c71ccfbe7e749591ef2a906607b415deadee8c23a8d822517c4cd55374e")));
                     txBuilder.set_data_inputs(dataInputs);
                     const tx = txBuilder.build().to_json();
-                    console.log(`tx: ${JSON.stringify(tx)}`);
+                    convertUnsignedTxOutputsToBigInt(tx);
+                    console.log(`tx: ${JSONBigInt.stringify(tx)}`);
                     console.log(`original id: ${tx.id}`);
                     // sigma-rust doesn't support most compilation so manually insert it here
                     // this is HEIGHT > 1337 but in hex and without the checksum/etc for the address of the contract
@@ -122,13 +133,15 @@ function initDapp() {
                     // and this is a register-using one
                     //tx.outputs[0].ergoTree = "1002040004f2c001d193e4c6b2a573000004047301";
                     // and we rebuild it using
-                    const correctTx = wasm.UnsignedTransaction.from_json(JSON.stringify(tx)).to_json();
+                    const correctTx = wasm.UnsignedTransaction.from_json(JSONBigInt.stringify(tx)).to_json();
+                    convertUnsignedTxOutputsToBigInt(correctTx);
+                    console.log(`correctTx:`, correctTx);
                     console.log(`new id: ${correctTx.id}`);
                     // we must use the exact order chosen as after 0.4.3 in sigma-rust
                     // this can change and might not use all the utxos as the coin selection
                     // might choose a more optimal amount
                     correctTx.inputs = correctTx.inputs.map(box => {
-console.log(`box: ${JSON.stringify(box)}`);
+console.log(`box: ${JSONBigInt.stringify(box)}`);
                         const fullBoxInfo = utxos.find(utxo => utxo.boxId === box.boxId);
                         return {
                             ...fullBoxInfo,
@@ -136,7 +149,7 @@ console.log(`box: ${JSON.stringify(box)}`);
                         };
                     });
                     status.innerText = "Awaiting transaction signing";
-                    console.log(`${JSON.stringify(correctTx)}`);
+                    console.log(`${JSONBigInt.stringify(correctTx)}`);
                     ergo
                         .sign_tx(correctTx)
                         .then(async signedTx => {
@@ -149,12 +162,12 @@ console.log(`box: ${JSON.stringify(box)}`);
                                 txTracker.href = `https://explorer.ergoplatform.com/en/transactions/${sentTxId}`;
                                 status.appendChild(txTracker);
                             } catch (e) {
-                                status.innerText = `Transaction could not be sent: ${JSON.stringify(e)}`;
+                                status.innerText = `Transaction could not be sent: ${JSONBigInt.stringify(e)}`;
                             }
                         })
                         .catch(err => {
-                            console.log(`Error: ${JSON.stringify(err)}`);
-                            status.innerText = `Error: ${JSON.stringify(err)}`
+                            console.log(`Error: ${JSONBigInt.stringify(err)}`);
+                            status.innerText = `Error: ${JSONBigInt.stringify(err)}`
                         });
                 }
                 div.appendChild(valueEntry);
