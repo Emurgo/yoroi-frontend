bd90b2cd264b54666a4e4e0e074eaf934935ad1d
Paul Ccari <paulclindo@gmail.com>
2021-08-11 17:29:56 -0500

    add sibebarCategory card

---
+++ packages/yoroi-extension/app/components/topbar/SideBarCategoryRevamp.js
@@ -0,0 +1,41 @@
+// @flow
+import React, { Component } from 'react';
+import type { Node } from 'react';
+import type { MessageDescriptor, $npm$ReactIntl$IntlFormat } from 'react-intl';
+import { intlShape } from 'react-intl';
+import { observer } from 'mobx-react';
+import classNames from 'classnames';
+import styles from './SideBarCategoryRevamp.scss';
+
+type Props = {|
+  +icon: string,
+  +active: boolean,
+  +onClick: void => void,
+  +label: ?MessageDescriptor,
+|};
+
+@observer
+export default class SideBarCategoryRevamp extends Component<Props> {
+  static contextTypes: {| intl: $npm$ReactIntl$IntlFormat |} = {
+    intl: intlShape.isRequired,
+  };
+
+  render(): Node {
+    const { intl } = this.context;
+
+    const { icon, active, onClick, label } = this.props;
+
+    const componentStyles = classNames([styles.component, active ? styles.active : null]);
+
+    const SvgElem = icon;
+
+    return (
+      <button type="button" className={componentStyles} onClick={onClick} disabled={active}>
+        <span className={styles.icon}>
+          <SvgElem />
+        </span>
+        {label ? <span className={styles.label}>{intl.formatMessage(label)}</span> : null}
+      </button>
+    );
+  }
+}
