596e6067d104fab43dbb6b8e7f33a4337a76d667
Paul Ccari <paulclindo@gmail.com>
2021-08-21 18:50:26 -0500

    add buyAdaButton and dialog styles for Revamp

--- packages/yoroi-extension/app/components/topbar/BuySellAdaButton.js
+++ packages/yoroi-extension/app/components/topbar/BuySellAdaButton.js
@@ -1,31 +1,35 @@
 // @flow
 import React, { Component } from 'react';
-import type { Node } from 'react';
+import type { Node, ComponentType } from 'react';
 import { observer } from 'mobx-react';
-import { intlShape, } from 'react-intl';
+import { intlShape } from 'react-intl';
 import styles from './BuySellAdaButton.scss';
 import globalMessages from '../../i18n/global-messages';
 import type { $npm$ReactIntl$IntlFormat } from 'react-intl';
-import classnames  from 'classnames';
+import classnames from 'classnames';
 import { Button } from 'react-polymorph/lib/components/Button';
 import { ButtonSkin } from 'react-polymorph/lib/skins/simple/ButtonSkin';
+import { withLayout } from '../../themes/context/layout';
+import type { LayoutComponentMap } from '../../themes/context/layout';
 
 type Props = {|
   +onBuySellClick: void => void,
 |};
 
-@observer
-export default class BuySellAdaButton extends Component<Props> {
+type InjectedProps = {|
+  +renderLayoutComponent: LayoutComponentMap => Node,
+|};
 
-  static contextTypes: {|intl: $npm$ReactIntl$IntlFormat|} = {
+@observer
+class BuySellAdaButton extends Component<Props & InjectedProps> {
+  static contextTypes: {| intl: $npm$ReactIntl$IntlFormat |} = {
     intl: intlShape.isRequired,
   };
 
   render(): Node {
-
     const { intl } = this.context;
 
-    return (
+    const BuyAdaButtonClassic = (
       <Button
         label={intl.formatMessage(globalMessages.buySellAda)}
         className={classnames([styles.button, 'secondary'])}
@@ -33,5 +37,21 @@ export default class BuySellAdaButton extends Component<Props> {
         skin={ButtonSkin}
       />
     );
+
+    const BuyAdaButtonRevamp = (
+      <Button
+        label={intl.formatMessage(globalMessages.buySellAda)}
+        className={styles.buttonRevamp}
+        onClick={() => this.props.onBuySellClick()}
+        skin={ButtonSkin}
+      />
+    );
+
+    return this.props.renderLayoutComponent({
+      CLASSIC: BuyAdaButtonClassic,
+      REVAMP: BuyAdaButtonRevamp,
+    });
   }
 }
+
+export default (withLayout(BuySellAdaButton): ComponentType<Props>);
