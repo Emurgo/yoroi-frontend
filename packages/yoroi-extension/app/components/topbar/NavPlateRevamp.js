4d888e48a524466fdd69c6e4039657299db44157
Paul Ccari <paulclindo@gmail.com>
2021-08-21 19:48:28 -0500

    add new components for navbar revamp version

---
+++ packages/yoroi-extension/app/components/topbar/NavPlateRevamp.js
@@ -0,0 +1,201 @@
+// @flow
+import React, { Component } from 'react';
+import type { Node } from 'react';
+import { observer } from 'mobx-react';
+import { intlShape, defineMessages } from 'react-intl';
+import styles from './NavPlateRevamp.scss';
+import WalletAccountIcon from './WalletAccountIcon';
+import ConceptualIcon from '../../assets/images/wallet-nav/conceptual-wallet.inline.svg';
+import TrezorIcon from '../../assets/images/wallet-nav/trezor-wallet.inline.svg';
+import LedgerIcon from '../../assets/images/wallet-nav/ledger-wallet.inline.svg';
+import { MultiToken } from '../../api/common/lib/MultiToken';
+
+import { truncateToken, splitAmount } from '../../utils/formatters';
+import type { WalletChecksum } from '@emurgo/cip4-js';
+import type { $npm$ReactIntl$IntlFormat, $npm$ReactIntl$MessageDescriptor } from 'react-intl';
+import type { ConceptualWallet } from '../../api/ada/lib/storage/models/ConceptualWallet/index';
+import { isCardanoHaskell } from '../../api/ada/lib/storage/database/prepackaged/networks';
+import { Bip44Wallet } from '../../api/ada/lib/storage/models/Bip44Wallet/wrapper';
+import globalMessages from '../../i18n/global-messages';
+import {
+  isLedgerNanoWallet,
+  isTrezorTWallet,
+} from '../../api/ada/lib/storage/models/ConceptualWallet/index';
+import { getTokenName } from '../../stores/stateless/tokenHelpers';
+import { hiddenAmount } from '../../utils/strings';
+import type { TokenLookupKey } from '../../api/common/lib/MultiToken';
+import type { TokenRow } from '../../api/ada/lib/storage/database/primitives/tables';
+
+const messages = defineMessages({
+  standardWallet: {
+    id: 'wallet.nav.type.standard',
+    defaultMessage: '!!!Standard wallet',
+  },
+  paperWallet: {
+    id: 'wallet.nav.type.paper',
+    defaultMessage: '!!!Paper wallet',
+  },
+  trezorWallet: {
+    id: 'wallet.nav.type.trezor',
+    defaultMessage: '!!!Trezor wallet',
+  },
+  ledgerWallet: {
+    id: 'wallet.nav.type.ledger',
+    defaultMessage: '!!!Ledger wallet',
+  },
+});
+
+type Props = {|
+  +plate: null | WalletChecksum,
+  +wallet: {|
+    conceptualWallet: ConceptualWallet,
+    conceptualWalletName: string,
+  |},
+  +rewards: null | void | MultiToken,
+
+  +walletAmount: null | MultiToken,
+  +getTokenInfo: ($ReadOnly<Inexact<TokenLookupKey>>) => $ReadOnly<TokenRow>,
+|};
+
+function constructPlate(
+  plate: WalletChecksum,
+  saturationFactor: number,
+  divClass: string
+): [string, React$Element<'div'>] {
+  return [
+    plate.TextPart,
+    <div className={divClass}>
+      <WalletAccountIcon
+        iconSeed={plate.ImagePart}
+        saturationFactor={saturationFactor}
+        scalePx={6}
+      />
+    </div>,
+  ];
+}
+
+@observer
+export default class NavPlateRevamp extends Component<Props> {
+  static contextTypes: {| intl: $npm$ReactIntl$IntlFormat |} = {
+    intl: intlShape.isRequired,
+  };
+
+  getEra: ConceptualWallet => void | $Exact<$npm$ReactIntl$MessageDescriptor> = wallet => {
+    if (!isCardanoHaskell(wallet.getNetworkInfo())) {
+      return undefined;
+    }
+    if (wallet instanceof Bip44Wallet) {
+      return globalMessages.byronLabel;
+    }
+    return globalMessages.shelleyLabel;
+  };
+
+  getType: ConceptualWallet => $Exact<$npm$ReactIntl$MessageDescriptor> = wallet => {
+    if (isLedgerNanoWallet(wallet)) {
+      return messages.ledgerWallet;
+    }
+    if (isTrezorTWallet(wallet)) {
+      return messages.trezorWallet;
+    }
+    return messages.standardWallet;
+  };
+
+  getIcon: ConceptualWallet => string = wallet => {
+    if (isLedgerNanoWallet(wallet)) {
+      return LedgerIcon;
+    }
+    if (isTrezorTWallet(wallet)) {
+      return TrezorIcon;
+    }
+    return ConceptualIcon;
+  };
+
+  render(): Node {
+    const { intl, shouldHideBalance } = this.context;
+
+    const [, iconComponent] = this.props.plate
+      ? constructPlate(this.props.plate, 0, styles.icon)
+      : [];
+
+    const typeText = [
+      // this.getEra(this.props.wallet.conceptualWallet),
+      this.getType(this.props.wallet.conceptualWallet),
+    ]
+      .filter(text => text != null)
+      .map(text => intl.formatMessage(text))
+      .join(' - ');
+    const totalAmount = this.getTotalAmount();
+
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
+          <h5 className={styles.name}>{this.props.wallet.conceptualWalletName}</h5>
+          {' ·  '}
+          <div className={styles.type}>{typeText}</div>
+        </div>
+        <div className={styles.card}>
+          <div>{iconComponent}</div>
+          <div className={styles.content}>
+            <div className={styles.amount}>
+              {this.renderAmountDisplay({
+                shouldHideBalance,
+                amount: totalAmount,
+              })}
+            </div>
+            <div className={styles.fixedAmount}>
+              <p>
+                {/* TODO: fix value */}
+                !!2,000,00 USD
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  renderAmountDisplay: ({|
+    shouldHideBalance: boolean,
+    amount: ?MultiToken,
+  |}) => Node = request => {
+    if (request.amount == null) {
+      return <div className={styles.isLoading} />;
+    }
+
+    const defaultEntry = request.amount.getDefaultEntry();
+    const tokenInfo = this.props.getTokenInfo(defaultEntry);
+    const shiftedAmount = defaultEntry.amount.shiftedBy(-tokenInfo.Metadata.numberOfDecimals);
+
+    let balanceDisplay;
+    if (request.shouldHideBalance) {
+      balanceDisplay = <span>{hiddenAmount}</span>;
+    } else {
+      const [beforeDecimalRewards, afterDecimalRewards] = splitAmount(
+        shiftedAmount,
+        tokenInfo.Metadata.numberOfDecimals
+      );
+
+      balanceDisplay = (
+        <>
+          {beforeDecimalRewards}
+          <span className={styles.afterDecimal}>{afterDecimalRewards}</span>
+        </>
+      );
+    }
+
+    return (
+      <>
+        {balanceDisplay} {truncateToken(getTokenName(tokenInfo))}
+      </>
+    );
+  };
+
+  getTotalAmount: void => null | MultiToken = () => {
+    if (this.props.rewards === undefined) {
+      return this.props.walletAmount;
+    }
+    if (this.props.rewards === null || this.props.walletAmount === null) {
+      return null;
+    }
+    return this.props.rewards.joinAddCopy(this.props.walletAmount);
+  };
+}
