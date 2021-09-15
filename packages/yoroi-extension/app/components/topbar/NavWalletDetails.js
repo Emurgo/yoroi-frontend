40f8f790675e58eaa8504fa354c1f03b04d823e9
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2021-09-15 13:32:07 +0300

    <TODO:RWRD2109>

--- packages/yoroi-extension/app/components/topbar/NavWalletDetails.js
+++ packages/yoroi-extension/app/components/topbar/NavWalletDetails.js
@@ -110,12 +110,13 @@ export default class NavWalletDetails extends Component<Props> {
                 </p>
                 {this.renderAmountDisplay({ shouldHideBalance, amount: walletAmount })}
               </div>
-              <div>
-                <p className={styles.label}>
-                  {intl.formatMessage(globalMessages.rewardsLabel)}&nbsp;
-                </p>
-                {this.renderAmountDisplay({ shouldHideBalance, amount: rewards })}
-              </div>
+              {/* <TODO:RWRD2109> */}
+              {/*<div>*/}
+              {/*  <p className={styles.label}>*/}
+              {/*    {intl.formatMessage(globalMessages.rewardsLabel)}&nbsp;*/}
+              {/*  </p>*/}
+              {/*  {this.renderAmountDisplay({ shouldHideBalance, amount: rewards })}*/}
+              {/*</div>*/}
             </div>
             }
             {this.props.rewards === undefined && (
@@ -150,7 +151,8 @@ export default class NavWalletDetails extends Component<Props> {
     if (this.props.rewards === null || this.props.walletAmount === null) {
       return null;
     }
-    return this.props.rewards.joinAddCopy(this.props.walletAmount);
+    // <TODO:RWRD2109>
+    return this.props.walletAmount; // .joinAddCopy(this.props.rewards);
   }
 
   renderAmountDisplay: {|
