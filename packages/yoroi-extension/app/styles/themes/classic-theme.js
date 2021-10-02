9b079551b3baca2c9d3284aa1f6c96f9058c9f1e
Paul Ccari <paulclindo@gmail.com>
2021-10-02 00:54:39 -0500

    add common settings and merge with themes

--- packages/yoroi-extension/app/styles/themes/classic-theme.js
+++ packages/yoroi-extension/app/styles/themes/classic-theme.js
@@ -14,70 +14,55 @@ import {
   ClassicTooltip,
   ClassicSelect,
 } from '../overrides';
+import { commonTheme } from './common-theme';
 
-export const classicTheme: Object = createTheme({
-  name: 'classic',
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: `
+export const classicTheme: Object = createTheme(
+  {
+    name: 'classic',
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: `
         ${SFUIDisplayFonts}
         ${RobotoMonoFonts}
       `,
+      },
+      MuiButton: ClassicButton,
+      MuiCheckbox: ClassicCheckbox,
+      MuiInputLabel: ClassicInputLabel,
+      MuiTextField: ClassicTextField,
+      MuiSelect: ClassicSelect,
+      MuiMenuItem: ClassicMenuItem,
+      MuiMenu: ClassicMenu,
+      MuiTabs: ClassicTabs,
+      MuiTabPanel: ClassicTabPanel,
+      MuiChip: ClassicChip,
+      MuiTooltip: ClassicTooltip,
     },
-    MuiButton: ClassicButton,
-    MuiCheckbox: ClassicCheckbox,
-    MuiInputLabel: ClassicInputLabel,
-    MuiTextField: ClassicTextField,
-    MuiSelect: ClassicSelect,
-    MuiMenuItem: ClassicMenuItem,
-    MuiMenu: ClassicMenu,
-    MuiTabs: ClassicTabs,
-    MuiTabPanel: ClassicTabPanel,
-    MuiChip: ClassicChip,
-    MuiTooltip: ClassicTooltip,
-  },
-  palette: {
-    primary: {
-      main: 'hsl(223, 20%, 27%)',
+    palette: {
+      /* `main` is added since MUI required it but we don't use it at all */
+      primary: {
+        main: '#3154CB',
+        '50': '#E1E7F2',
+        '100': '#164FD6',
+        '200': '#3154CB',
+        '300': '#1A44B7',
+        contrastText: '#FFF',
+      },
+      secondary: {
+        main: '#daa49a',
+        '50': '#F4FDFA',
+        '100': '#C9ECE5',
+        '200': '#EDB2A6',
+        '300': '#DAA49A',
+        contrastText: '#FFF',
+      },
     },
-    secondary: {
-      main: 'hsl(9, 46%, 73%)',
-      light: 'hsl(9 46% 73% / 20%)',
-      dark: 'hsl(10 66% 79%)',
-      disabled: 'hsl(9 46% 73% / 30%)',
-      contrastText: 'hsl(210 25% 98%)',
+    shape: {
+      borderRadius: 0,
     },
-    error: {
-      main: 'hsl(354, 79%, 61%)',
-    },
-    warning: {
-      main: 'hsl(4, 79%, 58%)',
-    },
-    input: {
-      main: 'hsl(214 16% 81%)',
-      dark: 'hsl(0 0% 29%)',
-    },
-    grey: {
-      '50': 'hsl(201, 22%, 95%)',
-      '100': 'hsl(216, 26%, 93%)',
-      '200': 'hsl(219, 23%, 89%)',
-      '300': 'hsl(222, 19%, 81%)',
-      '400': 'hsl(221, 17%, 70%)',
-      '500': 'hsl(221, 12%, 59%)',
-      '600': 'hsl(221, 10%, 47%)',
-      '700': 'hsl(228, 15%, 34%)',
-      '800': 'hsl(229, 20%, 28%)',
-      '900': 'hsl(236, 37%, 11%)',
+    typography: {
+      fontFamily: '"SFUIDisplay", sans-serif',
     },
   },
-  shape: {
-    borderRadius: 0,
-  },
-  typography: {
-    fontFamily: '"SFUIDisplay", sans-serif',
-    tooltip: {
-      fontWeight: 400,
-      fontSize: '0.75rem',
-    },
-  },
-});
+  commonTheme
+);
--- packages/yoroi-extension/app/styles/themes/modern-theme.js
+++ packages/yoroi-extension/app/styles/themes/modern-theme.js
@@ -14,110 +14,55 @@ import {
   ModernInputLabel,
   ModernSelect,
 } from '../overrides';
+import { commonTheme } from './common-theme';
 
-export const modernTheme: Object = createTheme({
-  name: 'modern',
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: `
+export const modernTheme: Object = createTheme(
+  {
+    name: 'modern',
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: `
         ${RubikFonts}
         ${RobotoMonoFonts}
       `,
+      },
+      MuiButton: ModernButton,
+      MuiCheckbox: ModernCheckbox,
+      MuiTextField: ModernTextField,
+      MuiSelect: ModernSelect,
+      MuiInputLabel: ModernInputLabel,
+      MuiMenu: ModernMenu,
+      MuiMenuItem: ModernMenuItem,
+      MuiTabs: ModernTabs,
+      MuiTabPanel: ModernTabPanel,
+      MuiChip: ModernChip,
+      MuiTooltip: ModernTooltip,
     },
-    MuiButton: ModernButton,
-    MuiCheckbox: ModernCheckbox,
-    MuiTextField: ModernTextField,
-    MuiSelect: ModernSelect,
-    MuiInputLabel: ModernInputLabel,
-    MuiMenu: ModernMenu,
-    MuiMenuItem: ModernMenuItem,
-    MuiTabs: ModernTabs,
-    MuiTabPanel: ModernTabPanel,
-    MuiChip: ModernChip,
-    MuiTooltip: ModernTooltip,
-  },
-  palette: {
-    primary: {
-      light: '#164FD6',
-      main: '#3154CB',
-      dark: '#1A44B7',
+    palette: {
+      /* `main` is added since MUI required it but we don't use it at all */
+      primary: {
+        main: '#3154CB',
+        '50': '#F0F2FB',
+        '100': '#164FD6',
+        '200': '#3154CB',
+        '300': '#1A44B7',
+        contrastText: '#FFF',
+      },
+      secondary: {
+        main: '#16E3BA',
+        '50': '#F4FDFA', // ?
+        '100': '#C9ECE5', // light
+        '200': '#16E3BA', // main - hover btn color -
+        '300': '#17D1AA', // dark
+        contrastText: '#FFF',
+      },
     },
-    secondary: {
-      main: '#17D1AA',
-      light: 'hsl(168deg 82% 49% / 10%)',
-      dark: '#16e3ba',
-      disabled: '#c9ede5',
-      contrastText: '#fff',
+    shape: {
+      borderRadius: 8,
     },
-    error: {
-      main: '#FF1351',
-    },
-    warning: {
-      main: '#f6a823',
-    },
-    input: {
-      main: '#9c9c9c',
-      dark: '#4a4a4a',
-    },
-    grey: {
-      '50': '#F0F3F5',
-      '100': '#EAEDF2',
-      '200': '#DCE0E9',
-      '300': '#C4CAD7',
-      '400': '#A7AFC0',
-      '500': '#8A92A3',
-      '600': '#6B7384',
-      '700': '#4A5065',
-      '800': '#383E54',
-      '900': '#242838',
+    typography: {
+      fontFamily: 'Rubik, sans-serif',
     },
   },
-  shape: {
-    borderRadius: 8,
-  },
-  typography: {
-    fontFamily: 'Rubik, sans-serif',
-    h1: {
-      fontWeight: 500,
-      fontSize: '1.875rem',
-    },
-    h2: {
-      fontWeight: 500,
-      fontSize: '1.75rem',
-    },
-    h3: {
-      fontWeight: 500,
-      fontSize: '1.5rem',
-    },
-    h4: {
-      fontWeight: 500,
-      fontSize: '1.25rem',
-    },
-    h5: {
-      fontWeight: 500,
-      fontSize: '1.125rem',
-    },
-    h6: {
-      fontWeight: 500,
-      fontSize: '1rem',
-      textTransform: 'uppercase',
-    },
-    h7: {
-      fontWeight: 500,
-      fontSize: '0.875rem',
-      textTransform: 'uppercase',
-    },
-    body1: {
-      fontWeight: 400,
-      fontSize: '1rem',
-    },
-    body2: {
-      fontWeight: 400,
-      fontSize: '0.875rem',
-    },
-    body3: {
-      fontWeight: 400,
-      fontSize: '0.75rem',
-    },
-  },
-});
+  commonTheme
+);
