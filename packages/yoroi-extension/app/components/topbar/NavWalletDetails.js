93fe0e98e4c5a19a32f4f5e907e06ce2a1cce77b
Ahmed Ibrahim <ib153507@gmail.com>
2021-10-07 10:33:06 +0200

    Update balance display

--- packages/yoroi-extension/app/components/topbar/NavWalletDetails.js
+++ packages/yoroi-extension/app/components/topbar/NavWalletDetails.js
@@ -4,7 +4,7 @@ import { observer } from 'mobx-react';
 import type { Node } from 'react';
 import classnames from 'classnames';
 import { intlShape, } from 'react-intl';
-import { splitAmount, truncateToken } from '../../utils/formatters';
+import { amountWithoutZeros, splitAmount, truncateToken } from '../../utils/formatters';
 
 import globalMessages from '../../i18n/global-messages';
 import styles from './NavWalletDetails.scss';
@@ -181,14 +181,9 @@ export default class NavWalletDetails extends Component<Props> {
     if (request.shouldHideBalance) {
       balanceDisplay = (<span>{hiddenAmount}</span>);
     } else {
-      const [beforeDecimalRewards, afterDecimalRewards] = splitAmount(
-        shiftedAmount,
-        tokenInfo.Metadata.numberOfDecimals,
-      );
       balanceDisplay = (
         <>
-          {beforeDecimalRewards}
-          <span>{afterDecimalRewards}</span>
+          {amountWithoutZeros(shiftedAmount.toFormat(tokenInfo.Metadata.numberOfDecimals))}
         </>
       );
     }
