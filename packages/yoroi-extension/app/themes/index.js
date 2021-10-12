7b5fece105b09eab837d1d4f41df9643ac7b0dbc
Paul Ccari <paulclindo@gmail.com>
2021-10-11 23:35:41 -0500

    small fix for revamp theme

--- packages/yoroi-extension/app/themes/index.js
+++ packages/yoroi-extension/app/themes/index.js
@@ -13,6 +13,7 @@ export type Theme = $Values<typeof THEMES>;
 export const MuiThemes: {| [Theme]: Object |} = Object.freeze({
   [THEMES.YOROI_CLASSIC]: classicTheme,
   [THEMES.YOROI_MODERN]: modernTheme,
+  [THEMES.YOROI_REVAMP]: modernTheme,
 });
 
 // Refer: https://github.com/Emurgo/yoroi-frontend/pull/497
