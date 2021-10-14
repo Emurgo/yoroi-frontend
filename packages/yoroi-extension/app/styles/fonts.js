f86a9653bdf316c23bcc82a042135e2d534039bc
Paul Ccari <paulclindo@gmail.com>
2021-10-13 21:01:37 -0500

    fix fonts

--- packages/yoroi-extension/app/styles/fonts.js
+++ packages/yoroi-extension/app/styles/fonts.js
@@ -52,7 +52,6 @@ const RubikFonts = `
     font-display: swap;
     font-weight: 300;
     src: local('Rubik'), local('Rubik-Light'), url(${RubikLightTtf}) format('truetype');
-    unicodeRange: 'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
   }
   @font-face {
     font-family: 'Rubik';
@@ -60,7 +59,6 @@ const RubikFonts = `
     font-display: swap;
     font-weight: 400;
     src: local('Rubik'), local('Rubik-Regular'), url(${RubikRegularTtf}) format('truetype');
-    unicodeRange: 'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
   }
   @font-face {
     font-family: 'Rubik';
@@ -68,7 +66,6 @@ const RubikFonts = `
     font-display: swap;
     font-weight: 500;
     src: local('Rubik'), local('Rubik-Medium'), url(${RubikMediumTtf}) format('truetype');
-    unicodeRange: 'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
   }
   @font-face {
     font-family: 'Rubik';
@@ -76,7 +73,6 @@ const RubikFonts = `
     font-display: swap;
     font-weight: 700;
     src: local('Rubik'), local('Rubik-Bold'), url(${RubikBoldTtf}) format('truetype');
-    unicodeRange: 'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
   }
 `;
 
@@ -87,7 +83,6 @@ const SFUIDisplayFonts = `
     font-display: swap;
     font-weight: 300;
     src: local('SFUIDisplay'), local('SFUIDisplay-Light'), url(${SFUIDisplayLightTtf}) format('woff');
-    unicodeRange: 'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
   }
   @font-face {
     font-family: 'SFUIDisplay';
@@ -95,7 +90,6 @@ const SFUIDisplayFonts = `
     font-display: swap;
     font-weight: 400;
     src: local('SFUIDisplay'), local('SFUIDisplay-Regular'), url(${SFUIDisplayRegularTtf}) format('woff');
-    unicodeRange: 'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
   }
   @font-face {
     font-family: 'SFUIDisplay';
@@ -103,7 +97,6 @@ const SFUIDisplayFonts = `
     font-display: swap;
     font-weight: 500;
     src: local('SFUIDisplay'), local('SFUIDisplay-Medium'), url(${SFUIDisplayMediumTtf}) format('woff');
-    unicodeRange: 'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
   }
   @font-face {
     font-family: 'SFUIDisplay';
@@ -111,7 +104,6 @@ const SFUIDisplayFonts = `
     font-display: swap;
     font-weight: 700;
     src: local('SFUIDisplay'), local('SFUIDisplay-Bold'), url(${SFUIDisplayBoldTtf}) format('woff');
-    unicodeRange: 'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF',
   }
 `;
 export { RobotoMonoFonts, RubikFonts, SFUIDisplayFonts };
