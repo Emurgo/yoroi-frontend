c9a3ee82a9c9ef2e5f068d8243ec115ec8a4150e
twwu123 <tszwaiwu.96@gmail.com>
2021-10-27 19:57:10 +0800

    Add minting NFT example

--- packages/yoroi-ergo-connector/example-cardano/index.js
+++ packages/yoroi-ergo-connector/example-cardano/index.js
@@ -1,3 +1,4 @@
+import * as Blakejs from 'blakejs'
 import * as CardanoWasm from "@emurgo/cardano-serialization-lib-browser"
 import { getTtl } from './utils'
 const cardanoAccessBtn = document.querySelector('#request-access')
@@ -10,6 +11,7 @@ const submitTx = document.querySelector('#submit-tx')
 const signTx = document.querySelector('#sign-tx')
 const alertEl = document.querySelector('#alert')
 const spinner = document.querySelector('#spinner')
+const mintAssets = document.querySelector('#mint-assets')
 const mintNFT = document.querySelector('#mint-NFT')
 
 let accessGranted = false
@@ -169,7 +171,7 @@ signTx.addEventListener('click', () => {
   )
 
   // add a keyhash input - for ADA held in a Shelley-era normal address (Base, Enterprise, Pointer)
-  const utxo = utxos[1]
+  const utxo = utxos[0]
   const addr = CardanoWasm.Address.from_bytes(
     Buffer.from(utxo.receiver, 'hex')
   )
@@ -228,7 +230,7 @@ signTx.addEventListener('click', () => {
   })
 })
 
-mintNFT.addEventListener('click', () => {
+mintAssets.addEventListener('click', () => {
   toggleSpinner('show');
 
   if (!accessGranted) {
@@ -258,7 +260,7 @@ mintNFT.addEventListener('click', () => {
   )
 
   // add a keyhash input - for ADA held in a Shelley-era normal address (Base, Enterprise, Pointer)
-  const utxo = utxos[1]
+  const utxo = utxos[0]
 
   const addr = CardanoWasm.Address.from_bytes(
     Buffer.from(utxo.receiver, 'hex')
@@ -280,17 +282,11 @@ mintNFT.addEventListener('click', () => {
     Buffer.from(changeAddress, 'hex')
   )
 
-  // the script string has a short prefix to represent some CBOR properties
-  // 82   array
-  // 00   unsigned
-  // 58 1C   28 bytes
-  const scriptString = '8200581C' + Buffer.from(keyHash.to_bytes()).toString('hex')
-  const policyScript = CardanoWasm.NativeScript.from_bytes(Buffer.from(scriptString, 'hex'))
+  const policyScript = CardanoWasm.NativeScript.new_script_pubkey(CardanoWasm.ScriptPubkey.new(keyHash))
 
-  // the policy id is derived from the policy script through a blake2b-224 hash
-  // this can be done programmatically, but I decided to do it manually through the cardano-cli to make sure there are no mistakes
-  // I manually put the policy script into the cardano-cli and generated the policy id.
-  const policyID = CardanoWasm.ScriptHash.from_bytes(Buffer.from("46aa846842567770593994c2509ab01a5c77440827c5e2702b370e18", 'hex'))
+  // Append a 0 byte onto the front of the encoded policy script and then hash the corresponding result using Blake2b-224
+  var input = "00" + Buffer.from(policyScript.to_bytes()).toString('hex')
+  const policyID = CardanoWasm.ScriptHash.from_bytes(Blakejs.blake2b(Buffer.from(input, "hex"), null, 28))
 
   const fee = CardanoWasm.BigNum.from_str('3000000')
   txBuilder.set_fee(fee)
@@ -344,6 +340,158 @@ mintNFT.addEventListener('click', () => {
       txBody,
       witnessSet,
       undefined,
+    )
+    transactionHex = Buffer.from(transaction.to_bytes()).toString('hex')
+    console.log(transactionHex)
+  }).catch(error => {
+    console.error(error)
+    toggleSpinner('hide')
+    alertWarrning('Signing tx fails')
+  })
+})
+
+mintNFT.addEventListener('click', () => {
+  toggleSpinner('show');
+
+  if (!accessGranted) {
+    alertError('Should request access first');
+    return;
+  }
+
+  if (!utxos) {
+    alertError('Should request utxos first');
+    return
+  }
+
+  if (!changeAddress) {
+    alertError('Should request change address first')
+  }
+
+  const txBuilder = CardanoWasm.TransactionBuilder.new(
+    // all of these are taken from the mainnet genesis settings
+    // linear fee parameters (a*size + b)
+    CardanoWasm.LinearFee.new(CardanoWasm.BigNum.from_str('44'), CardanoWasm.BigNum.from_str('155381')),
+    // minimum utxo value
+    CardanoWasm.BigNum.from_str('1000000'),
+    // pool deposit
+    CardanoWasm.BigNum.from_str('500000000'),
+    // key deposit
+    CardanoWasm.BigNum.from_str('2000000')
+  )
+
+  const metadataObj = {
+    "ff7b74142cd8185bef17008ef05664bb9ee7991cb6f6eac9f28a7144": {
+      "NFT1": {
+        "description": "This is my first NFT thanks to the Cardano foundation",
+        "name": "Cardano foundation NFT guide token",
+        "id": "1",
+        "image": "ipfs://QmRhTTbUrPYEw3mJGGhQqQST9k86v1DPBiTTWJGKDJsVFw"
+      }
+    }
+  }
+
+  const metadatum = CardanoWasm.encode_json_str_to_metadatum(JSON.stringify(metadataObj), CardanoWasm.MetadataJsonSchema.NoConversions)
+  const gMetadatum = CardanoWasm.GeneralTransactionMetadata.new()
+  gMetadatum.insert(CardanoWasm.BigNum.from_str('721'), metadatum)
+  const metadata = CardanoWasm.TransactionMetadata.new(gMetadatum)
+  txBuilder.set_metadata(metadata)
+
+  // add a keyhash input - for ADA held in a Shelley-era normal address (Base, Enterprise, Pointer)
+  const utxo = utxos[0]
+
+  const addr = CardanoWasm.Address.from_bytes(
+    Buffer.from(utxo.receiver, 'hex')
+  )
+  const baseAddr = CardanoWasm.BaseAddress.from_address(addr);
+  const keyHash = baseAddr.payment_cred().to_keyhash();
+  txBuilder.add_key_input(
+    keyHash,
+    CardanoWasm.TransactionInput.new(
+      CardanoWasm.TransactionHash.from_bytes(
+        Buffer.from(utxo.tx_hash, "hex")
+      ), // tx hash
+      utxo.tx_index, // index
+    ),
+    CardanoWasm.Value.new(CardanoWasm.BigNum.from_str(utxo.amount))
+  )
+
+  const shelleyChangeAddress = CardanoWasm.Address.from_bytes(
+    Buffer.from(changeAddress, 'hex')
+  )
+
+  const ttl = getTtl()
+
+  // Add the keyhash script to ensure the NFT can only be minted by the corresponding wallet
+  const keyHashScript = CardanoWasm.NativeScript.new_script_pubkey(CardanoWasm.ScriptPubkey.new(keyHash))
+
+  // We then need to add a timelock to ensure the NFT won't be minted again after the given expiry slot
+  const timelock = CardanoWasm.TimelockExpiry.new(41800000)
+  const timelockScript = CardanoWasm.NativeScript.new_timelock_expiry(timelock)
+
+  // Then the policy script is an "all" script of these two scripts
+  const scripts = CardanoWasm.NativeScripts.new()
+  scripts.add(timelockScript)
+  scripts.add(keyHashScript)
+
+  const policyScript = CardanoWasm.NativeScript.new_script_all(CardanoWasm.ScriptAll.new(scripts))
+
+  // According to the cardano Alonzo cddl, and extra "0" byte needs to be appended to the CBOR encoded policy script
+  var input = "00" + Buffer.from(policyScript.to_bytes()).toString('hex')
+
+  const policyID = CardanoWasm.ScriptHash.from_bytes(Blakejs.blake2b(Buffer.from(input, "hex"), null, 28))
+
+  const fee = CardanoWasm.BigNum.from_str('3000000')
+  txBuilder.set_fee(fee)
+
+  // since the fee has been set manually, the change cannot be calculated automatically
+  // we will manually create a transaction output that gives utxo amount - fee
+  // and also some amount of our test token
+  const value = CardanoWasm.Value.new(CardanoWasm.BigNum.from_str(utxo.amount).checked_sub(fee))
+  const testTokenAsset = CardanoWasm.Assets.new()
+  testTokenAsset.insert(CardanoWasm.AssetName.new(Buffer.from("4E465431", 'hex')), CardanoWasm.BigNum.from_str('1'))
+  const testTokenMultiAsset = CardanoWasm.MultiAsset.new()
+  testTokenMultiAsset.insert(policyID, testTokenAsset)
+  value.set_multiasset(testTokenMultiAsset)
+
+  txBuilder.add_output(
+    CardanoWasm.TransactionOutput.new(
+      shelleyChangeAddress,
+      value
+    ),
+  )
+
+  txBuilder.set_ttl(41800000)
+
+
+  // calculate the min fee required and send any change to an address
+  // txBuilder.add_change_if_needed(shelleyChangeAddress)
+
+  const txBody = txBuilder.build()
+
+  const testToken = CardanoWasm.MintAssets.new()
+  testToken.insert(CardanoWasm.AssetName.new(Buffer.from("4E465431", 'hex')), CardanoWasm.Int.new(CardanoWasm.BigNum.from_str('1')))
+  const mint = CardanoWasm.Mint.new()
+  mint.insert(policyID, testToken)
+
+  txBody.set_mint(mint)
+  txBody.set
+
+  const txHex = Buffer.from(txBody.to_bytes()).toString('hex')
+
+  cardano.sign_tx(txHex, true).then(witnessSetHex => {
+    toggleSpinner('hide')
+    alertSuccess('Signing tx succeeds: ')
+    const witnessSet = CardanoWasm.TransactionWitnessSet.from_bytes(
+      Buffer.from(witnessSetHex, 'hex')
+    )
+    // we then add the token's policy script to the transaction witness set
+    const witnessScripts = CardanoWasm.NativeScripts.new()
+    witnessScripts.add(policyScript)
+    witnessSet.set_scripts(witnessScripts)
+    const transaction = CardanoWasm.Transaction.new(
+      txBody,
+      witnessSet,
+      metadata,
     )
     transactionHex = Buffer.from(transaction.to_bytes()).toString('hex')
     console.log(transactionHex)
