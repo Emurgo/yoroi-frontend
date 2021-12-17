ca8fae99c5edf593b2a24ea59c82bc077cf34acf
Paul Ccari <paulclindo@gmail.com>
2021-12-16 20:36:43 -0500

    replace sass to mui styling

--- packages/yoroi-extension/app/components/dapp-connector/Layout/DappConnectorNavbar.js
+++ packages/yoroi-extension/app/components/dapp-connector/Layout/DappConnectorNavbar.js
@@ -1,21 +1,24 @@
 // @flow
-import { Component } from 'react';
 import { observer } from 'mobx-react';
-import styles from './DappConnectorNavbar.scss'
-import type { Node } from 'react';
+import type { ComponentType, Node } from 'react';
+import { Typography } from '@mui/material';
+import { Box } from '@mui/system';
 
-type Props = {|
+type Props = {||}
 
-|}
-
-@observer
-export default class DappConnectorNavbar extends Component<Props> {
-
-    render(): Node {
-        return (
-          <div className={styles.component}>
-            <h1 className={styles.header}>Dapp connector</h1>
-          </div>
-        )
-    }
-}
+const DappConnectorNavbar = (): Node => {
+  return (
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      bgcolor="var(--yoroi-palette-common-white)"
+      boxShadow="0 2px 5px 3px rgba(0, 0, 0, 0.06)"
+    >
+      <Typography color="var(--yoroi-palette-gray-800)" variant="h5" padding="32px 40px">
+        Dapp connector
+      </Typography>
+    </Box>
+  );
+};
+export default (observer(DappConnectorNavbar): ComponentType<Props>);
