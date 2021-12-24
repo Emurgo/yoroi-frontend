5f8acdc7b6388242ece7ef5afbfe6b4667a0703e
Ahmed Ibrahim <ib153507@gmail.com>
2021-12-24 11:02:54 +0200

    Design a switcher

--- packages/yoroi-extension/app/components/dapp-connector/Layout/DappConnectorNavbar.js
+++ packages/yoroi-extension/app/components/dapp-connector/Layout/DappConnectorNavbar.js
@@ -5,8 +5,14 @@ import styles from './DappConnectorNavbar.scss'
 import type { Node } from 'react';
 import { defineMessages, intlShape } from 'react-intl';
 import type { $npm$ReactIntl$IntlFormat } from 'react-intl';
+import classnames from 'classnames'
+
 
 type Props = {||}
+type State = {|
+  isChecked: boolean,
+  isFocused: boolean,
+|}
 
 const messages = defineMessages({
   switcherLabelOn: {
@@ -21,25 +27,50 @@ const messages = defineMessages({
 
 
 @observer
-export default class DappConnectorNavbar extends Component<Props> {
+export default class DappConnectorNavbar extends Component<Props, State> {
 
   static contextTypes: {|intl: $npm$ReactIntl$IntlFormat|} = {
     intl: intlShape.isRequired,
   };
 
+  state: State = {
+    isFocused: false,
+    isChecked: false,
+  }
+
+  onFocus: void => void = () => {
+    this.setState({ isFocused: true })
+  }
+
+  onChange: void => void = () => {
+    const { isChecked } = this.state
+    this.setState({ isChecked: !isChecked })
+  }
+
   render(): Node {
     const { intl } = this.context;
-    const isDappConnectorActive = false
+    const { isFocused, isChecked } = this.state
+
     return (
       <div className={styles.component}>
         <h1 className={styles.header}>Dapp connector</h1>
         <div>
           <p>
             <span>{intl.formatMessage(
-              isDappConnectorActive ? messages.switcherLabelOn : messages.switcherLabelOff
+              isChecked ? messages.switcherLabelOn : messages.switcherLabelOff
               )}
             </span>
           </p>
+          <label
+            htmlFor='switcher'
+            className={styles.switch}
+          >
+            <input onChange={this.onChange} type="checkbox" id='switcher' />
+            <span className={classnames([
+              styles.slider, isFocused && styles.sliderFocus, isChecked && styles.sliderChecked
+            ])}
+            />
+          </label>
         </div>
       </div>
     )
