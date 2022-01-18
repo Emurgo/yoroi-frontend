00c663f967d309670400c62a0356dece0dc87eb7
RAHUL NAIR <rahulnair@RAHULs-MacBook-Pro.local>
2022-01-18 13:11:30 +0530

    added gitignore for reports

--- packages/yoroi-extension/reportGenerator.js
+++ packages/yoroi-extension/reportGenerator.js
@@ -1,6 +1,8 @@
-var reporter = require('cucumber-html-reporter');
 
-var options = {
+// @flow
+const reporter = require('cucumber-html-reporter');
+
+const options = {
         theme: 'bootstrap',
         jsonFile: 'reports/cucumberReport.json',
         output: 'reports/cucumberReport.html',
@@ -10,12 +12,12 @@ var options = {
        // screenshotsDirectory: './testScreenshots/',
         storeScreenshots: false,
         metadata: {
-            "App Version":"0.3.2",
-            "Test Environment": "Dev",
-            "Browser": "Chrome  97",
-            "Platform": "MacPro",
-            "Parallel": "Scenarios",
-            "Executed": "Local"
+            'App Version': '0.3.2',
+            'Test Environment': 'Dev',
+            'Browser': 'Chrome  97',
+            'Platform': 'MacPro',
+            'Parallel': 'Scenarios',
+            'Executed': 'Local'
         }
     };
 
