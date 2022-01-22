e7c06c364c856d57e174502521da1bace47383ce
Ahmed Ibrahim <ib153507@gmail.com>
2022-01-22 13:20:08 +0200

    render colors in a table

--- packages/yoroi-extension/app/components/experimental/YoroiPalette/YoroiPalette.js
+++ packages/yoroi-extension/app/components/experimental/YoroiPalette/YoroiPalette.js
@@ -4,6 +4,8 @@ import { observer } from 'mobx-react';
 import styles from './YoroiPalette.scss'
 import type { Node } from 'react';
 import { classicTheme } from '../../../styles/themes/classic-theme'
+import {  modernTheme } from '../../../styles/themes/modern-theme'
+import classNames from 'classnames';
 import { getPalette } from './palette';
 
 type Props = {|
@@ -11,16 +13,45 @@ type Props = {|
 |}
 
 /**
- * Display shadows
+ * @todos
+ * 1. Display shadows
+ * 2. Display All colors
+ * 3. Toggle according to the theme
+ *
  */
+
+const themes = {
+    classic: classicTheme,
+    modern: modernTheme
+}
 
 @observer
 export default class YoroiPalettePage extends Component<Props> {
 
+    state = {
+        currentTheme: 'classic',
+    }
+
+
     render(): Node {
-        const palette = getPalette(classicTheme)
+        const { currentTheme } = this.state
+        const palette = getPalette(themes[currentTheme])
+
         return (
           <div className={styles.component}>
+            <div className={styles.themes}>
+              <button className={classNames([(currentTheme === 'classic') && styles.active])} type='button'>Classic Theme</button>
+              <button className={classNames([(currentTheme === 'modern') && styles.active])} type='button'>Modern Theme</button>
+            </div>
+
+            <div className={styles.row}>
+              <p>Color</p>
+              <p className={styles.colorHex}>
+                Color Value
+              </p>
+              <p className={styles.colorName}>Name in code</p>
+
+            </div>
             {Object.entries(palette).map((row) => (
               <div className={styles.row} key={row[1]}>
                 <span
