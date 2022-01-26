24aceaa0f512c6e579831fde7455ec1a3ccc8d2a
Ahmed Ibrahim <ib153507@gmail.com>
2022-01-26 12:57:39 +0200

    Add the permission status to the UI

--- packages/yoroi-extension/app/components/dapp-connector/Layout/DappConnectorNavbar.js
+++ packages/yoroi-extension/app/components/dapp-connector/Layout/DappConnectorNavbar.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import { observer } from 'mobx-react';
 import styles from './DappConnectorNavbar.scss'
 import type { Node } from 'react';
+import classnames from 'classnames';
 
 type Props = {|
 
@@ -16,13 +17,16 @@ export default class DappConnectorNavbar extends Component<Props> {
   }
 
   componentDidMount() {
+    this.checkAccess()
+  }
+
+  checkAccess() {
     chrome.permissions.contains({
       permissions: ['tabs', 'activeTab'],
       origins: [
         '*://*/*'
       ]
     }, (result) => {
-      console.log({result})
       this.setState({ hasPermission: result })
     });
   }
@@ -34,6 +38,7 @@ export default class DappConnectorNavbar extends Component<Props> {
         '*://*/*'
       ]
     }, (granted) => {
+      this.setState({ hasPermission: granted })
       if (granted) {
         alert('you have the permissions')
       } else {
@@ -49,6 +54,7 @@ export default class DappConnectorNavbar extends Component<Props> {
         '*://*/*'
       ]
     }, (removed) => {
+      this.setState({ hasPermission: removed })
       if (removed) {
         alert('permissions removed')
       } else {
@@ -62,14 +68,11 @@ export default class DappConnectorNavbar extends Component<Props> {
     return (
       <div className={styles.component}>
         <h1 className={styles.header}>Dapp connector</h1>
-        <p>{this.state.hasPermission? 'Has permission' : 'Has no permission'}</p>
-        <br />
-        <br />
-        <button onClick={this.getPermission} type='button'>Get Permission</button>
-        <br />
-        <br />
-        <br />
-        <button onClick={this.removePermission} type='button'>Remove Permission</button>
+        <div className={styles.permissionsContainer}>
+          <p className={styles.hasPermission}>{this.state.hasPermission? 'Has permission' : 'Has no permission'}</p>
+          <button className={classnames([styles.permission, styles.getPermission])} onClick={this.getPermission.bind(this)} type='button'>Get Permission</button>
+          <button className={classnames([styles.permission, styles.removePermission])} onClick={this.removePermission.bind(this)} type='button'>Remove Permission</button>
+        </div>
       </div>
     )
   }
