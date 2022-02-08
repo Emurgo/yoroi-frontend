5f2d916d57a8749c1160b435f0f9232e5ecab1be
Ahmed Ibrahim <ib153507@gmail.com>
2022-02-08 11:48:24 +0200

    Remove unused code

--- packages/yoroi-extension/app/containers/settings/categories/PermissionsSettingsPage.js
+++ packages/yoroi-extension/app/containers/settings/categories/PermissionsSettingsPage.js
@@ -4,59 +4,10 @@ import { Component } from 'react';
 import { observer } from 'mobx-react';
 import { computed } from 'mobx';
 import { defineMessages, intlShape } from 'react-intl';
-import ExplorerSettings from '../../../components/settings/categories/general-setting/ExplorerSettings';
-import UnitOfAccountSettings from '../../../components/settings/categories/general-setting/UnitOfAccountSettings';
 import type { InjectedOrGenerated } from '../../../types/injectedPropsType';
-import UriSettingsBlock from '../../../components/settings/categories/general-setting/UriSettingsBlock';
-import registerProtocols from '../../../uri-protocols';
-import environment from '../../../environment';
-import { unitOfAccountDisabledValue } from '../../../types/unitOfAccountType';
-import AdaCurrency from '../../../assets/images/currencies/ADA.inline.svg';
 import type { $npm$ReactIntl$IntlFormat } from 'react-intl';
-import type { UnitOfAccountSettingType } from '../../../types/unitOfAccountType';
-import LocalizableError from '../../../i18n/LocalizableError';
-import {
-  PublicDeriver,
-} from '../../../api/ada/lib/storage/models/PublicDeriver/index';
-import NoWalletMessage from '../../wallet/NoWalletMessage';
-import { isCardanoHaskell } from '../../../api/ada/lib/storage/database/prepackaged/networks';
-import type {
-  ExplorerRow,
-} from '../../../api/ada/lib/storage/database/explorers/tables';
-import { SelectedExplorer } from '../../../domain/SelectedExplorer';
-import type {
-  GetAllExplorersResponse,
-} from '../../../api/ada/lib/storage/bridge/explorers';
 
 const currencyLabels = defineMessages({
-  USD: {
-    id: 'settings.unitOfAccount.currency.usd',
-    defaultMessage: '!!!US dollar',
-  },
-  JPY: {
-    id: 'settings.unitOfAccount.currency.jpy',
-    defaultMessage: '!!!Japanese yen',
-  },
-  EUR: {
-    id: 'settings.unitOfAccount.currency.eur',
-    defaultMessage: '!!!Euro',
-  },
-  CNY: {
-    id: 'settings.unitOfAccount.currency.cny',
-    defaultMessage: '!!!Chinese Renminbi yuan',
-  },
-  KRW: {
-    id: 'settings.unitOfAccount.currency.krw',
-    defaultMessage: '!!!South Korean won',
-  },
-  BTC: {
-    id: 'settings.unitOfAccount.currency.btc',
-    defaultMessage: '!!!Bitcoin',
-  },
-  ETH: {
-    id: 'settings.unitOfAccount.currency.eth',
-    defaultMessage: '!!!Ethereum',
-  },
 });
 
 type GeneratedData = typeof PermissionsSettingsPage.prototype.generated;
@@ -66,13 +17,6 @@ export default class PermissionsSettingsPage extends Component<InjectedOrGenerat
 
   static contextTypes: {|intl: $npm$ReactIntl$IntlFormat|} = {
     intl: intlShape.isRequired,
-  };
-
-  onSelectUnitOfAccount: string => Promise<void> = async (value) => {
-    const unitOfAccount = (value === 'ADA')
-      ? unitOfAccountDisabledValue
-      : { enabled: true, currency: value };
-    await this.generated.actions.profile.updateUnitOfAccount.trigger(unitOfAccount);
   };
 
   render(): Node {
@@ -86,58 +30,13 @@ export default class PermissionsSettingsPage extends Component<InjectedOrGenerat
 
   @computed get generated(): {|
     actions: {|
-      explorers: {|
-        updateSelectedExplorer: {|
-          trigger: (params: {|
-            explorer: $ReadOnly<ExplorerRow>,
-          |}) => Promise<void>
-        |},
-      |},
-      profile: {|
-        updateUnitOfAccount: {|
-          trigger: (
-            params: UnitOfAccountSettingType
-          ) => Promise<void>
-        |}
-      |},
       walletSettings: {|
         requestTabPermission: {|
           trigger: (params: void) => void,
         |}
       |},
     |},
-    canRegisterProtocol: () => boolean,
     stores: {|
-      coinPriceStore: {|
-        getCurrentPrice: (
-          from: string,
-          to: string
-        ) => ?number,
-        lastUpdateTimestamp: null | number,
-        refreshCurrentUnit: {| isExecuting: boolean |}
-      |},
-      explorers: {|
-        selectedExplorer: Map<number, SelectedExplorer>,
-        setSelectedExplorerRequest: {|
-          error: ?LocalizableError,
-          isExecuting: boolean
-        |},
-        allExplorers: GetAllExplorersResponse,
-      |},
-      profile: {|
-        UNIT_OF_ACCOUNT_OPTIONS: Array<{|
-          svg: string,
-          symbol: string
-        |}>,
-        setUnitOfAccountRequest: {|
-          error: ?LocalizableError,
-          isExecuting: boolean
-        |},
-        unitOfAccount: UnitOfAccountSettingType
-      |},
-      wallets: {|
-        selected: null | PublicDeriver<>
-      |},
       walletSettings: {|
         isDappEnabled: boolean,
       |},
@@ -150,51 +49,17 @@ export default class PermissionsSettingsPage extends Component<InjectedOrGenerat
       throw new Error(`${nameof(PermissionsSettingsPage)} no way to generated props`);
     }
     const { stores, actions } = this.props;
-    const profileStore = stores.profile;
     return Object.freeze({
       stores: {
-        explorers: {
-          selectedExplorer: stores.explorers.selectedExplorer,
-          allExplorers: stores.explorers.allExplorers,
-          setSelectedExplorerRequest: {
-            isExecuting: stores.explorers.setSelectedExplorerRequest.isExecuting,
-            error: stores.explorers.setSelectedExplorerRequest.error,
-          },
-        },
-        profile: {
-          UNIT_OF_ACCOUNT_OPTIONS: profileStore.UNIT_OF_ACCOUNT_OPTIONS,
-          unitOfAccount: profileStore.unitOfAccount,
-          setUnitOfAccountRequest: {
-            error: profileStore.setUnitOfAccountRequest.error,
-            isExecuting: profileStore.setUnitOfAccountRequest.isExecuting,
-          },
-        },
-        coinPriceStore: {
-          getCurrentPrice: stores.coinPriceStore.getCurrentPrice,
-          lastUpdateTimestamp: stores.coinPriceStore.lastUpdateTimestamp,
-          refreshCurrentUnit: {
-            isExecuting: stores.coinPriceStore.refreshCurrentUnit.isExecuting,
-          },
-        },
-        wallets: {
-          selected: stores.wallets.selected,
-        },
         walletSettings: {
             isDappEnabled: stores.walletSettings.isDappEnabled,
         },
       },
       actions: {
-        explorers: {
-          updateSelectedExplorer: { trigger: actions.explorers.updateSelectedExplorer.trigger },
-        },
-        profile: {
-          updateUnitOfAccount: { trigger: actions.profile.updateUnitOfAccount.trigger },
-        },
         walletSettings: {
             requestTabPermission: { trigger: actions.walletSettings.requestTabPermission.trigger },
         },
       },
-      canRegisterProtocol: environment.userAgentInfo.canRegisterProtocol,
     });
   }
 }
