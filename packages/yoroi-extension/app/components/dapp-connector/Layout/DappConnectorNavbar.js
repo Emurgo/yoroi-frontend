fb75a90696cb0c5829bd4c9b197a5f2d21385b06
Ahmed Ibrahim <ib153507@gmail.com>
2022-02-09 16:12:40 +0200

    Remove old demo buttons

--- packages/yoroi-extension/app/components/dapp-connector/Layout/DappConnectorNavbar.js
+++ packages/yoroi-extension/app/components/dapp-connector/Layout/DappConnectorNavbar.js
@@ -3,7 +3,6 @@ import { Component } from 'react';
 import { observer } from 'mobx-react';
 import styles from './DappConnectorNavbar.scss'
 import type { Node } from 'react';
-import classnames from 'classnames';
 import { intlShape } from 'react-intl';
 import type { $npm$ReactIntl$IntlFormat } from 'react-intl';
 import { connectorMessages } from '../../../i18n/global-messages';
@@ -23,57 +22,11 @@ export default class DappConnectorNavbar extends Component<Props, State> {
     intl: intlShape.isRequired,
   };
 
-  state: State = {
-    hasPermission: false,
-  }
-
-  componentDidMount() {
-    this.checkAccess()
-  }
-
-  checkAccess() {
-    chrome.permissions.contains({
-      permissions: ['tabs', 'activeTab'],
-      origins: [
-        '*://*/*'
-      ]
-    }, (result) => {
-      this.setState({ hasPermission: result })
-    });
-  }
-
-  getPermission() {
-    chrome.permissions.request({
-      permissions: ['tabs', 'activeTab'],
-      origins: [
-        '*://*/*'
-      ]
-    }, (granted) => {
-      this.setState({ hasPermission: granted })
-    });
-  }
-
-  removePermission() {
-    chrome.permissions.remove({
-      permissions: ['tabs', 'activeTab'],
-      origins: [
-        '*://*/*'
-      ]
-    }, (removed) => {
-      this.setState({ hasPermission: !removed })
-    });
-  }
-
   render(): Node {
     const { intl } = this.context
     return (
       <div className={styles.component}>
         <h1 className={styles.header}>{intl.formatMessage(connectorMessages.dappConnector)}</h1>
-        <div className={styles.permissionsContainer}>
-          <p className={styles.hasPermission}>{this.state.hasPermission? 'Has permission' : 'Has no permission'}</p>
-          <button className={classnames([styles.permission, styles.getPermission])} onClick={this.getPermission.bind(this)} type='button'>Get Permission</button>
-          <button className={classnames([styles.permission, styles.removePermission])} onClick={this.removePermission.bind(this)} type='button'>Remove Permission</button>
-        </div>
       </div>
     )
   }
