fd44cb94e04dec7d97874fa406a4919275f83112
yushi <yushihuang@gmail.com>
2022-02-10 01:12:29 +0800

    add CIP8 sign and verify function

--- packages/yoroi-extension/app/ergo-connector/api/index.js
+++ packages/yoroi-extension/app/ergo-connector/api/index.js
@@ -5,7 +5,7 @@ import type { WalletChecksum } from '@emurgo/cip4-js';
 import type { WalletAuthEntry } from '../../../chrome/extension/ergo-connector/types';
 import { RustModule } from '../../api/ada/lib/cardanoCrypto/rustLoader';
 import blake2b from 'blake2b';
-
+import { asGetSigningKey } from '../../api/ada/lib/storage/models/PublicDeriver/traits';
 
 export const createAuthEntry: ({|
   appAuthID: ?string,
@@ -54,4 +54,48 @@ export const authSignHexPayload: ({|
     ),
   ).to_raw_key();
   return appPrivKey.sign(Buffer.from(payloadHex, 'hex')).to_hex();
+}
+
+// return the hex string representation of the COSESign1
+const cip8Sign = async (
+  deriver: PublicDeriver<>,
+  password: string,
+  payload: Buffer,
+): Promise<string> => {
+  const withSigningKey = asGetSigningKey(deriver);
+  if (!withSigningKey) {
+    throw new Error(`[createAuthEntry] no signing key`);
+  }
+  const signingKeyFromStorage = await withSigningKey.getSigningKey();
+  const normalizedKey = await withSigningKey.normalizeKey({
+    ...signingKeyFromStorage,
+    password,
+  });
+  const signingKey = RustModule.WalletV4.Bip32PrivateKey.from_bytes(
+    Buffer.from(normalizedKey.prvKeyHex, 'hex')
+  );
+  const derivedSignKey = signingKey.derive(0).derive(0).to_raw_key();
+
+  const protectedHeader = RustModule.MessageSigning.HeaderMap.new();
+  const protectedSerialized = RustModule.MessageSigning.ProtectedHeaderMap.new(protectedHeader);
+  const unprotected = RustModule.MessageSigning.HeaderMap.new();
+  const headers = RustModule.MessageSigning.Headers.new(protectedSerialized, unprotected);
+  const builder = RustModule.MessageSigning.COSESign1Builder.new(headers, payload, false);
+  const toSign = builder.make_data_to_sign().to_bytes();
+  const signedSigStruct = derivedSignKey.sign(toSign).to_bytes();
+  const coseSign1 = builder.build(signedSigStruct);
+  return Buffer.from(coseSign1.to_bytes()).toString('hex');
+}
+
+const cip8Verify = async (
+  publicKey: RustModule.WalletV4.PublicKey,
+  coseSign1Hex: string,
+): Promise<boolean> => {
+  const coseSign1 = RustModule.MessageSigning.COSESign1.from_bytes(
+    Buffer.from(coseSign1Hex, 'hex')
+  );
+  const sigStructReconstructed = coseSign1.signed_data().to_bytes();
+  const signature = RustModule.WalletV4.Ed25519Signature.from_bytes(coseSign1.signature());
+
+  return publicKey.verify(sigStructReconstructed, signature);
 }
