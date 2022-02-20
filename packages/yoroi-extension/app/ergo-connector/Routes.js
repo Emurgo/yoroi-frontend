b10a337ea6596340ae2eac9468a94acec94a6bf8
Ahmed Ibrahim <ib153507@gmail.com>
2022-02-20 14:56:36 +0200

    Remove page routes

--- packages/yoroi-extension/app/ergo-connector/Routes.js
+++ packages/yoroi-extension/app/ergo-connector/Routes.js
@@ -1,19 +1,14 @@
 // @flow
 import type { Node } from 'react';
-import { Route, Redirect, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import type { StoresMap } from './stores/index';
 import type { ActionsMap } from './actions/index';
 import { ROUTES } from './routes-config';
 
 // PAGES
 import ConnectContainer from './containers/ConnectContainer';
-import ConnectWebsitesContainer from './containers/ConnectWebsitesContainer';
 import Layout from './components/layout/Layout';
 import SignTxContainer from './containers/SignTxContainer';
-import SettingsContainer from './containers/SettingsContainer';
-import TermsOfUseContainer from './containers/TermsOfUseContainer';
-import SupportContainer from './containers/SupportContainer';
-import AboutContainer from './containers/AboutContainer';
 import LoadingPage from '../containers/LoadingPage';
 
 export const Routes = (stores: StoresMap, actions: ActionsMap): Node => {
@@ -26,32 +21,11 @@ export const Routes = (stores: StoresMap, actions: ActionsMap): Node => {
 const ToplevelPages = (stores: StoresMap, actions: ActionsMap): Node => (
   <Switch>
     <Route
-      exact
-      path={ROUTES.SETTINGS.GENERAL}
-      component={props => <SettingsContainer {...props} stores={stores} actions={actions} />}
-    />
-    <Route
-      exact
-      path={ROUTES.SETTINGS.TERMS_OF_USE}
-      component={props => <TermsOfUseContainer {...props} stores={stores} actions={actions} />}
-    />
-    <Route
-      exact
-      path={ROUTES.SETTINGS.SUPPORT}
-      component={props => <SupportContainer {...props} stores={stores} actions={actions} />}
-    />
-    <Route
-      exact
-      path={ROUTES.SETTINGS.ABOUT}
-      component={props => <AboutContainer {...props} stores={stores} actions={actions} />}
-    />
-    <Route
       path={ROUTES.ROOT}
       component={_props =>
         wrapGeneralPages(GeneralSubpages(stores, actions))
       }
     />
-    <Redirect to={ROUTES.SETTINGS.GENERAL} />
   </Switch>
 );
 
@@ -64,15 +38,9 @@ const GeneralSubpages = (stores, actions) => (
     />
     <Route
       exact
-      path={ROUTES.CONNECTED_WEBSITES}
-      component={props => <ConnectWebsitesContainer {...props} stores={stores} actions={actions} />}
-    />
-    <Route
-      exact
       path={ROUTES.SIGNIN_TRANSACTION}
       component={props => <SignTxContainer {...props} stores={stores} actions={actions} />}
     />
-    <Redirect to={ROUTES.SETTINGS.GENERAL} />
   </Switch>
 );
 
