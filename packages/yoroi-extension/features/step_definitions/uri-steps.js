58e47f518846ef29cfe89efc565d0b3763ae0253
Denis Nebytov <denis@emurgo.io>
2022-02-22 14:14:33 +0300

    Fixed IT-108

--- packages/yoroi-extension/features/step_definitions/uri-steps.js
+++ packages/yoroi-extension/features/step_definitions/uri-steps.js
@@ -33,7 +33,7 @@ When(/^I open a cardano URI for address (([^"]*)) and ([0-9]+) ADA$/, async func
 
 Then(/^I should see and accept a warning dialog$/, async function () {
   await this.waitForElement('.URILandingDialog');
-  await this.click('.URILandingDialog .primary');
+  await this.click('.URILandingDialog .MuiButton-primary');
 });
 
 Then(/^I should see a dialog with the transaction details$/, async function (table) {
