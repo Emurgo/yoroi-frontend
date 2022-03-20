7e358505b9f4c2eb6b9677f9a3a0363da349f0e5
Ahmed Ibrahim <ib153507@gmail.com>
2022-03-20 18:39:52 +0200

    Move all table text to intl

--- packages/yoroi-extension/app/components/dapp-connector/ConnectedWebsites/ConnectedWebsitesPage.js
+++ packages/yoroi-extension/app/components/dapp-connector/ConnectedWebsites/ConnectedWebsitesPage.js
@@ -15,6 +15,8 @@ import NoDApp from './NoDApp';
 import { Box } from '@mui/system';
 import { BRANDED_DAPPS } from './dapps';
 import DApp from './DApp';
+import type { $npm$ReactIntl$IntlFormat } from 'react-intl';
+import { defineMessages, intlShape } from 'react-intl';
 
 type WalletInfo = {| balance: null | MultiToken, plate: WalletChecksum |}
 type Props = {|
@@ -28,91 +30,116 @@ type Props = {|
     +getWalletInfo: (PublicDeriver<>) => WalletInfo
 |};
 
+const messages = defineMessages({
+  wallets: {
+    id: 'connector.connectedwebsites.table.wallets',
+    defaultMessage: `!!!Wallets`,
+  },
+  dApps: {
+    id: 'connector.connectedwebsites.table.dapps',
+    defaultMessage: `!!!DApps`,
+  },
+  cardano: {
+    id: 'connector.connectedwebsites.table.cardano',
+    defaultMessage: 'Cardano, ADA'
+  },
+  ergo: {
+    id: 'connector.connectedwebsites.table.ergo',
+    defaultMessage: 'Ergo, ERG'
+  }
+});
+
+
 @observer
 export default class ConnectedWebsitesPage extends Component<Props> {
+  static contextTypes: {| intl: $npm$ReactIntl$IntlFormat |} = {
+    intl: intlShape.isRequired,
+  };
+
   render(): Node {
-      const { whitelistEntries, wallets } = this.props;
-      if (whitelistEntries == null
-        || whitelistEntries.length === 0
-        || wallets == null
-        || wallets.length === 0
-      ) {
-          return <NoDApp />
-      }
+    const { intl } = this.context
+    const { whitelistEntries, wallets } = this.props;
+    if (whitelistEntries == null
+      || whitelistEntries.length === 0
+      || wallets == null
+      || wallets.length === 0
+    ) {
+        return <NoDApp />
+    }
 
-      const { ergoNodes, cardanoNodes } = whitelistEntries.map((
-        { url, protocol, publicDeriverId, image }
-      ) => {
-        const wallet = wallets.find( cacheEntry =>
-          cacheEntry.getPublicDeriverId() === publicDeriverId
-        )
-        if (wallet == null) {
-          return [null, null]
-        }
-        const { balance, plate } = this.props.getWalletInfo(wallet)
-        return [isErgo(wallet.getParent().getNetworkInfo()), (
-          <WalletRow
-            key={url}
-            url={url}
-            protocol={protocol}
-            websiteIcon={image}
-            isActiveSite={this.props.activeSites.includes(url)}
-            onRemoveWallet={this.props.onRemoveWallet}
-            balance={balance}
-            plate={plate}
-            shouldHideBalance={this.props.shouldHideBalance}
-            getTokenInfo={this.props.getTokenInfo}
-            settingsCache={this.props.getConceptualWallet(wallet)}
-          />
-        )]
-      }).reduce((acc, [isWalletErgo, node]) => {
-        if (node != null) {
-          acc[isWalletErgo === true ? 'ergoNodes' : 'cardanoNodes'].push(node);
-        }
-        return acc;
-      }, { ergoNodes: [], cardanoNodes: [] });
-
-
-
-      return (
-        <div className={styles.component}>
-          <div className={styles.container}>
-            <div className={styles.header}>
-              <p>Wallets</p>
-              <p>Dapps</p>
-            </div>
-            <div>
-              {cardanoNodes.length > 0 &&
-              <div className={styles.chain}>
-                <h1>Cardano, ADA</h1>
-                {cardanoNodes}
-                <div className={styles.line}>
-                  <div />
-                </div>
-              </div>
-              }
-              {ergoNodes.length > 0 &&
-              <div className={styles.chain}>
-                <h1>Ergo, ERG</h1>
-                {ergoNodes}
-              </div>}
-            </div>
-          </div>
-          <Box
-            sx={{
-              backgroundColor: 'var(--yoroi-palette-common-white)',
-              display: 'flex',
-              flexDirection: 'column',
-              borderRadius: '8px',
-              height: '600px',
-              overflowY: 'auto',
-              marginTop: '40px',
-              marginLeft: '40px',
-            }}
-          >
-            {BRANDED_DAPPS.map(dapp => <DApp key={dapp.id} dapp={dapp} />)}
-          </Box>
-        </div>
+    const { ergoNodes, cardanoNodes } = whitelistEntries.map((
+      { url, protocol, publicDeriverId, image }
+    ) => {
+      const wallet = wallets.find( cacheEntry =>
+        cacheEntry.getPublicDeriverId() === publicDeriverId
       )
+      if (wallet == null) {
+        return [null, null]
+      }
+      const { balance, plate } = this.props.getWalletInfo(wallet)
+      return [isErgo(wallet.getParent().getNetworkInfo()), (
+        <WalletRow
+          key={url}
+          url={url}
+          protocol={protocol}
+          websiteIcon={image}
+          isActiveSite={this.props.activeSites.includes(url)}
+          onRemoveWallet={this.props.onRemoveWallet}
+          balance={balance}
+          plate={plate}
+          shouldHideBalance={this.props.shouldHideBalance}
+          getTokenInfo={this.props.getTokenInfo}
+          settingsCache={this.props.getConceptualWallet(wallet)}
+        />
+      )]
+    }).reduce((acc, [isWalletErgo, node]) => {
+      if (node != null) {
+        acc[isWalletErgo === true ? 'ergoNodes' : 'cardanoNodes'].push(node);
+      }
+      return acc;
+    }, { ergoNodes: [], cardanoNodes: [] });
+
+
+
+    return (
+      <div className={styles.component}>
+        <div className={styles.container}>
+          <div className={styles.header}>
+            <p>{intl.formatMessage(messages.wallets)}</p>
+            <p>{intl.formatMessage(messages.dapps)}</p>
+          </div>
+          <div>
+            {cardanoNodes.length > 0 &&
+            <div className={styles.chain}>
+              <h1>{intl.formatMessage(messages.cardano)}</h1>
+              {cardanoNodes}
+              <div className={styles.line}>
+                <div />
+              </div>
+            </div>
+            }
+            {ergoNodes.length > 0 &&
+            <div className={styles.chain}>
+              <h1>{intl.formatMessage(messages.ergo)}</h1>
+              {ergoNodes}
+            </div>}
+          </div>
+        </div>
+        <Box
+          sx={{
+            backgroundColor: 'var(--yoroi-palette-common-white)',
+            display: 'flex',
+            flexDirection: 'column',
+            borderRadius: '8px',
+            height: '600px',
+            overflowY: 'auto',
+            marginTop: '40px',
+            marginLeft: '40px',
+          }}
+        >
+          {BRANDED_DAPPS.map(dapp => <DApp key={dapp.id} dapp={dapp} />)}
+        </Box>
+      </div>
+    )
   }
 }
