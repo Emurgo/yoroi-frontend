4eb8b103818c24e305aba290c74fcafcabbcba9a
Denis Nebytov <denis@emurgo.io>
2022-03-27 15:58:57 +0300

    flow fix

--- packages/yoroi-extension/features/pages/walletReceivePage.js
+++ packages/yoroi-extension/features/pages/walletReceivePage.js
@@ -1,13 +1,15 @@
 // @flow
 
-export const getGeneratedAddressLocator = (rowIndex: number) => {
+import type { LocatorObject } from '../support/webdriver';
+
+export const getGeneratedAddressLocator = (rowIndex: number): LocatorObject => {
   return {
     locator: `.generatedAddress-${rowIndex + 1} .RawHash_hash`,
     method: 'css'
   };
 };
 
-export const getAddressLocator = (address: string) => {
+export const getAddressLocator = (address: string): LocatorObject => {
   return {
     locator: `//div[contains(text(), "${address}")]`,
     method: 'xpath'
