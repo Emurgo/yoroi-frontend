8c3d80ae4c1ed79af342fa5e26053c7c3e10aef2
Ahmed Ibrahim <ib153507@gmail.com>
2022-05-20 16:57:49 +0200

    Add quick animation

--- packages/yoroi-extension/app/components/widgets/Support.js
+++ packages/yoroi-extension/app/components/widgets/Support.js
@@ -6,44 +6,54 @@ import { IconButton } from '@mui/material';
 
 export default class Support extends Component <{||}> {
 
-    loadScript(src: string, id: string): void {
-        const script = document.createElement('script')
-        script.src = src
-        script.id = id
-        document.body.appendChild(script)
-    }
+  state = {
+    isOpened: false
+  }
 
-    componentDidMount() {
-        this.loadScript('https://static.zdassets.com/ekr/snippet.js?key=68b95d72-6354-4343-8a64-427979a6f5d6', 'ze-snippet');
-        this.interval = setInterval(()=>{
-          if (typeof window.zE !== 'undefined' && typeof window.zE.hide === 'function') {
-            window.zE.hide()
-            clearInterval(this.interval)
-          }
-        }, 500);
-    }
+  loadScript(src: string, id: string): void {
+      const script = document.createElement('script')
+      script.src = src
+      script.id = id
+      document.body.appendChild(script)
+  }
 
-    openChatBoxSupport(){
-        if (typeof window.zE !== 'undefined') {
-          window.zE.activate()
+  componentDidMount() {
+      this.loadScript('https://static.zdassets.com/ekr/snippet.js?key=68b95d72-6354-4343-8a64-427979a6f5d6', 'ze-snippet');
+      this.interval = setInterval(()=>{
+        if (typeof window.zE !== 'undefined' && typeof window.zE.hide === 'function') {
+          window.zE.hide()
+          clearInterval(this.interval)
+
+          window.zE('webWidget:on', 'close', () => {
+            this.setState({ isOpened: false })
+          })
         }
-    }
+      }, 500);
+  }
 
-    render(): Node {
-        return (
-          <IconButton
-            sx={{
-              position: 'absolute',
-              bottom: '24px',
-              right: '30px',
-              zIndex: '1',
-              padding: '0px'
-            }}
-            className="main-btn"
-            onClick={this.openChatBoxSupport}
-          >
-            <SupportIcon />
-          </IconButton>
-        )
-    }
+  openChatBoxSupport(){
+    if (typeof window.zE !== 'undefined') {
+        this.setState({ isOpened: true })
+        window.zE.activate()
+      }
+  }
+
+  render(): Node {
+      return (
+        <IconButton
+          sx={{
+            position: 'absolute',
+            bottom: '24px',
+            right: '30px',
+            zIndex: '1',
+            padding: '3px',
+            transition: 'transform 400ms ease-in-out 50ms',
+            transform: this.state.isOpened ? 'translateY(100px)' : 'translateY(0px)'
+          }}
+          onClick={this.openChatBoxSupport.bind(this)}
+        >
+          <SupportIcon />
+        </IconButton>
+      )
+  }
 }
