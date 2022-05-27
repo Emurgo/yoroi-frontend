1e2c62fcb281371304e448eb4504fda1483772c5
Ahmed Ibrahim <ib153507@gmail.com>
2022-05-27 13:46:06 +0200

    Add dialog buttons

--- packages/yoroi-extension/app/components/wallet/staking/dashboard/RevampSwitchDialog.js
+++ packages/yoroi-extension/app/components/wallet/staking/dashboard/RevampSwitchDialog.js
@@ -12,45 +12,53 @@ import styles from './RevampSwitchDialog.scss'
 
 const messages = defineMessages({
     dialogTitle: {
-        id: 'wallet.dashboard.revampAnnouncement.dialog.dialogTitle',
-        defaultMessage: '!!!new yoroi version is available',
+      id: 'wallet.dashboard.revampAnnouncement.dialog.dialogTitle',
+      defaultMessage: '!!!new yoroi version is available',
     },
     header: {
-        id: 'wallet.dashboard.revampAnnouncement.dialog.header',
-        defaultMessage: '!!!New Yoroi version enables an exceptional experience for Cardano ADA holders. Apply it and take advantage of the new and improved existing features.'
+      id: 'wallet.dashboard.revampAnnouncement.dialog.header',
+      defaultMessage: '!!!New Yoroi version enables an exceptional experience for Cardano ADA holders. Apply it and take advantage of the new and improved existing features.'
     },
     feature1: {
-        id: 'wallet.dashboard.revampAnnouncement.dialog.feature1',
-        defaultMessage: '!!!Better performance and new UI',
+      id: 'wallet.dashboard.revampAnnouncement.dialog.feature1',
+      defaultMessage: '!!!Better performance and new UI',
     },
     feature2: {
-        id: 'wallet.dashboard.revampAnnouncement.dialog.feature2',
-        defaultMessage: '!!!Multiple asset transaction (NEW)',
+      id: 'wallet.dashboard.revampAnnouncement.dialog.feature2',
+      defaultMessage: '!!!Multiple asset transaction (NEW)',
     },
     feature3: {
-        id: 'wallet.dashboard.revampAnnouncement.dialog.feature3',
-        defaultMessage: '!!!NFT Gallery (NEW)',
+      id: 'wallet.dashboard.revampAnnouncement.dialog.feature3',
+      defaultMessage: '!!!NFT Gallery (NEW)',
     },
     feature4: {
-        id: 'wallet.dashboard.revampAnnouncement.dialog.feature4',
-        defaultMessage: '!!!FAQ (NEW)',
+      id: 'wallet.dashboard.revampAnnouncement.dialog.feature4',
+      defaultMessage: '!!!FAQ (NEW)',
     },
     feature5: {
-        id: 'wallet.dashboard.revampAnnouncement.dialog.feature5',
-        defaultMessage: '!!!Support center (NEW)',
+      id: 'wallet.dashboard.revampAnnouncement.dialog.feature5',
+      defaultMessage: '!!!Support center (NEW)',
     },
     feature6: {
-        id: 'wallet.dashboard.revampAnnouncement.dialog.feature6',
-        defaultMessage: '!!!Fiat pairing (NEW)',
+      id: 'wallet.dashboard.revampAnnouncement.dialog.feature6',
+      defaultMessage: '!!!Fiat pairing (NEW)',
     },
     feature7: {
-        id: 'wallet.dashboard.revampAnnouncement.dialog.feature7',
-        defaultMessage: '!!!Release notes (Coming soon)',
+      id: 'wallet.dashboard.revampAnnouncement.dialog.feature7',
+      defaultMessage: '!!!Release notes (Coming soon)',
     },
     footer: {
-        id: 'wallet.dashboard.revampAnnouncement.dialog.footer',
-        defaultMessage: '!!!P.S. You can always move back to the previous version.',
-    }
+      id: 'wallet.dashboard.revampAnnouncement.dialog.footer',
+      defaultMessage: '!!!P.S. You can always move back to the previous version.',
+    },
+    tryItNow: {
+      id: 'wallet.dashboard.revampAnnouncement.dialog.tryItNowBtn',
+      defaultMessage: '!!!try it now',
+    },
+    getStarted: {
+      id: 'wallet.dashboard.revampAnnouncement.dialog.getStartedBtn',
+      defaultMessage: '!!!get started',
+    },
 });
 
 @observer
@@ -111,7 +119,7 @@ export default class RevampSwitchDialog extends Component {
             </List>
 
             <Typography fontWeight='500' fontSize='16px' lineHeight='24px' color='var(--yoroi-palette-gray-900)' marginTop='16px'>
-              {intl.formatMessage(messages.footer)}
+              {intl.formatMessage(messages.tryItNow)}
             </Typography>
           </Dialog>
         )
