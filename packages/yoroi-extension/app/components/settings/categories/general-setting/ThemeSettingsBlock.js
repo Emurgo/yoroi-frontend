f6b44fc46e6765086cc3bcfb906b17648650d733
Ahmed Ibrahim <ib153507@gmail.com>
2022-06-13 12:49:26 +0200

    Remove render layout

--- packages/yoroi-extension/app/components/settings/categories/general-setting/ThemeSettingsBlock.js
+++ packages/yoroi-extension/app/components/settings/categories/general-setting/ThemeSettingsBlock.js
@@ -91,7 +91,7 @@ type AllProps = {| ...Props, ...InjectedProps |};
 const NEW_THEME = THEMES.YOROI_REVAMP
 const OLD_THEME = `${THEMES.YOROI_MODERN}-${THEMES.YOROI_CLASSIC}`
 @observer
-class ThemeSettingsBlock extends Component<AllProps> {
+export default class ThemeSettingsBlock extends Component<AllProps> {
   static contextTypes: {| intl: $npm$ReactIntl$IntlFormat |} = {
     intl: intlShape.isRequired,
   };
@@ -341,13 +341,171 @@ class ThemeSettingsBlock extends Component<AllProps> {
         )}
       </Box>
     );
-    return this.props.renderLayoutComponent({
-      CLASSIC: themeBlockClassicComponent,
-      REVAMP: themeBlockClassicComponent, // Todo: remove renderLayoutComponent
-    });
+    return (
+      <Box sx={{ borderTop: '1px solid var(--yoroi-palette-gray-200)', paddingY: '24px' }}>
+        <Typography color='var(--yoroi-palette-gray-900)' fontSize='18px' fontWeight='500' marginBottom='10px'>
+          {intl.formatMessage(messages.version)}
+        </Typography>
+        <Box>
+          <RadioGroup
+            aria-labelledby="theme-switch-buttons"
+            value={currentTheme === NEW_THEME ? NEW_THEME : OLD_THEME}
+            onChange={(e) => {
+              const theme = e.target.value === NEW_THEME ? NEW_THEME : THEMES.YOROI_MODERN
+              selectTheme({ theme })
+            }}
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+            }}
+          >
+            <FormControlLabel
+              value={OLD_THEME}
+              control={<Radio size='small' />}
+              label={intl.formatMessage(messages.currentVersion)}
+              sx={{
+                marginRight: '20px'
+              }}
+            />
+            <FormControlLabel
+              value={NEW_THEME}
+              control={<Radio size='small' />}
+              label={intl.formatMessage(messages.newVersion)}
+            />
+          </RadioGroup>
+        </Box>
+        {
+          currentTheme !== THEMES.YOROI_REVAMP && (
+            <Box>
+              <Box sx={{ marginTop: '20px' }}>
+                <Typography
+                  variant="h5"
+                  sx={{
+                    fontWeight: 500,
+                    marginBottom: '8px',
+                    color: 'var(--yoroi-support-settings-text)',
+                    fontSize: '18px',
+                  }}
+                >
+                  {intl.formatMessage(messages.selectColorTheme)}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="var(--yoroi-support-settings-text)"
+                  sx={{ marginBottom: '2px' }}
+                >
+                  <FormattedHTMLMessage {...messages.themeNote} />
+                </Typography>
+                <Typography variant="body2" color="var(--yoroi-support-settings-text)">
+                  <FormattedMessage {...messages.blog} values={{ blogLink }} />
+                </Typography>
+              </Box>
+
+              <Box sx={{ marginTop: '16px' }}>
+                <RadioGroup
+                  row
+                  value={currentTheme}
+                  onChange={(e) => {
+                    selectTheme({ theme: e.target.value })
+                  }}
+                >
+                  <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', marginRight: '24px' }}>
+                    <Box>
+                      <YoroiClassicTheme />
+                    </Box>
+                    <FormControlLabel value={THEMES.YOROI_MODERN} control={<Radio size='small' />} label='Modern' />
+                  </Box>
+                  <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start' }}>
+                    <YoroiClassicTheme />
+                    <FormControlLabel value={THEMES.YOROI_CLASSIC} control={<Radio size='small' />} label='classic' />
+                  </Box>
+                </RadioGroup>
+              </Box>
+            </Box>
+          )
+        }
+        {/* {commonHeader}
+        <Box sx={{ maxWidth: '1300px', textAlign: 'center', marginTop: '20px' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
+            <ThemeButton
+              variant={null}
+              onClick={selectTheme.bind(this, { theme: THEMES.YOROI_MODERN })}
+              isActive={currentTheme === THEMES.YOROI_MODERN}
+            >
+              {currentTheme === THEMES.YOROI_MODERN && hasCustomTheme() && (
+                <WarningMessage variant="body2">
+                  {intl.formatMessage(messages.themeWarning)}
+                </WarningMessage>
+              )}
+              <ThemeThumbnail theme={THEMES.YOROI_MODERN} themeKey="modern" />
+              <Typography
+                variant="body2"
+                sx={{ textalign: 'center', color: 'var(--yoroi-support-settings-text)' }}
+              >
+                {intl.formatMessage(messages.themeYoroiModern)}
+              </Typography>
+            </ThemeButton>
+            <ThemeButton
+              variant={null}
+              isActive={currentTheme === THEMES.YOROI_CLASSIC}
+              onClick={selectTheme.bind(this, { theme: THEMES.YOROI_CLASSIC })}
+            >
+              {currentTheme === THEMES.YOROI_CLASSIC && hasCustomTheme() && (
+                <WarningMessage variant="body2">
+                  {intl.formatMessage(messages.themeWarning)}
+                </WarningMessage>
+              )}
+              <ThemeThumbnail theme={THEMES.YOROI_CLASSIC} themeKey="classic" />
+              <Typography sx={{ textalign: 'center', color: 'var(--yoroi-support-settings-text)' }}>
+                {intl.formatMessage(messages.themeYoroiClassic)}
+              </Typography>
+            </ThemeButton>
+          </Box>
+
+          <Button variant="primary" onClick={exportTheme.bind(this)} sx={{ width: '400px' }}>
+            {intl.formatMessage(messages.themeExportButton)}
+          </Button>
+        </Box>
+
+        {shouldDisplayRevampButton && (
+          <Box sx={{ margin: '20px 0', display: 'flex', justifyContent: 'center' }}>
+            <Button
+              sx={{
+                width: '400px',
+                background: 'white',
+                color: '#6b7384',
+                border: '1px solid #6b7384',
+                '&:hover': {
+                  color: '#383838',
+                  background: 'white',
+                },
+                position: 'relative',
+                '&::after': {
+                  content: '"new"',
+                  top: '50%',
+                  right: '30px',
+                  transform: 'translateY(-50%)',
+                  position: 'absolute',
+                  color: 'var(--yoroi-comp-button-primary-text)',
+                  backgroundColor: 'var(--yoroi-comp-button-primary-background)',
+                  padding: '4px 10px',
+                  borderRadius: '777px',
+                },
+              }}
+              onClick={() => {
+                selectTheme({ theme: THEMES.YOROI_REVAMP });
+                this.props.switchToFirstWallet();
+              }}
+              id='switchToRevampButton'
+            >
+              {intl.formatMessage(messages.tryYoroiRevamp)}
+            </Button>
+          </Box>
+        )} */}
+      </Box>
+    )
   }
 }
-export default (withLayout(ThemeSettingsBlock): ComponentType<Props>);
 
 const WarningMessage = styled(Typography)({
   position: 'absolute',
