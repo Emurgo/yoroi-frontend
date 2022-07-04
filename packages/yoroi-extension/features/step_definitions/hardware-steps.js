3ddca13b7e7c14d3837789faa323f920d2f02591
Denis Nebytov <denis@emurgo.io>
2022-07-05 00:18:49 +0300

    Prettified hardware-steps.js

--- packages/yoroi-extension/features/step_definitions/hardware-steps.js
+++ packages/yoroi-extension/features/step_definitions/hardware-steps.js
@@ -1,8 +1,10 @@
 // @flow
 
-import { When, Then, } from 'cucumber';
+import { When, Then } from 'cucumber';
 import { testWallets } from '../mock-chain/TestWallets';
-import { truncateAddress, } from '../../app/utils/formatters';
+// eslint-disable-next-line import/named
+import { truncateAddress } from '../../app/utils/formatters';
+import { addressField, derivationField, verifyButton } from '../pages/verifyAddressPage';
 
 When(/^I select a Byron-era Ledger device$/, async function () {
   await this.click({ locator: '.WalletAdd_btnConnectHW', method: 'css' });
@@ -15,6 +17,7 @@ When(/^I select a Byron-era Ledger device$/, async function () {
   await this.click({ locator: '.WalletConnectHWOptionDialog_connectLedger', method: 'css' });
   await this.click({ locator: '.WalletEraOptionDialog_bgByronMainnet', method: 'css' });
 });
+
 When(/^I select a Shelley-era Ledger device$/, async function () {
   await this.click({ locator: '.WalletAdd_btnConnectHW', method: 'css' });
 
@@ -26,6 +29,7 @@ When(/^I select a Shelley-era Ledger device$/, async function () {
   await this.click({ locator: '.WalletConnectHWOptionDialog_connectLedger', method: 'css' });
   await this.click({ locator: '.WalletEraOptionDialog_bgShelleyMainnet', method: 'css' });
 });
+
 When(/^I restore the Ledger device$/, async function () {
   await this.waitForElement({ locator: '.CheckDialog_component', method: 'css' });
   await this.click({ locator: '.primary', method: 'css' });
@@ -36,7 +40,6 @@ When(/^I restore the Ledger device$/, async function () {
   await this.waitForElement({ locator: '.SaveDialog', method: 'css' });
   await this.click({ locator: '.primary', method: 'css' });
 });
-
 
 When(/^I select a Byron-era Trezor device$/, async function () {
   await this.click({ locator: '.WalletAdd_btnConnectHW', method: 'css' });
@@ -49,6 +52,7 @@ When(/^I select a Byron-era Trezor device$/, async function () {
   await this.click({ locator: '.WalletConnectHWOptionDialog_connectTrezor', method: 'css' });
   await this.click({ locator: '.WalletEraOptionDialog_bgByronMainnet', method: 'css' });
 });
+
 When(/^I select a Shelley-era Trezor device$/, async function () {
   await this.click({ locator: '.WalletAdd_btnConnectHW', method: 'css' });
 
@@ -69,10 +73,12 @@ When(/^I restore the Trezor device$/, async function () {
   // between these is where the tab & iframe gets opened
 
   await this.waitForElement({ locator: '.SaveDialog', method: 'css' });
-  await this.input({ locator: "input[name='walletName']", method: 'css' }, testWallets['trezor-wallet'].name);
+  await this.input(
+    { locator: "input[name='walletName']", method: 'css' },
+    testWallets['trezor-wallet'].name
+  );
   await this.click({ locator: '.primary', method: 'css' });
 });
-
 
 When(/^I see the hardware send money confirmation dialog$/, async function () {
   await this.waitForElement({ locator: '.HWSendConfirmationDialog_dialog', method: 'css' });
@@ -83,11 +89,11 @@ When(/^I click on the verify address button$/, async function () {
 });
 
 When(/^I see the verification address "([^"]*)"$/, async function (address) {
-  await this.waitUntilText({ locator: '.verificationAddress', method: 'css' }, truncateAddress(address));
+  await this.waitUntilText(addressField, truncateAddress(address));
 });
 
 When(/^I see the derivation path "([^"]*)"$/, async function (path) {
-  await this.waitUntilText({ locator: '.VerifyAddressDialog_derivation', method: 'css' }, path);
+  await this.waitUntilText(derivationField, path);
 });
 
 Then(/^I verify the address on my ledger device$/, async function () {
@@ -99,7 +105,7 @@ Then(/^I verify the address on my ledger device$/, async function () {
 });
 
 Then(/^I verify the address on my trezor device$/, async function () {
-  await this.click({ locator: '.Dialog_actions .primary', method: 'css' });
+  await this.click(verifyButton);
   // we should have this disable while the action is processing, but we don't show a spinner on this
   await this.waitForElementNotPresent({ locator: '.ErrorBlock_component', method: 'css' });
 });
