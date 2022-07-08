274a8cf118f21321f6160a0382f6124501e5f762
Cristian Merlo <cristian.merlo@globant.com>
2022-07-08 16:18:14 -0300

    Tests for FAQ

---
+++ packages/yoroi-extension/features/step_definitions/sidebar-steps.js
@@ -0,0 +1,23 @@
+// @flow
+
+import { When, Given, Then } from 'cucumber';
+import { expect } from 'chai';
+import { faqButton } from '../pages/sidebarPage';
+import { faqTabName } from '../support/windowManager';
+
+When(/^I click on FAQ button$/, async function () {
+  this.webDriverLogger.info(`Step: I click on FAQ button`);
+  await this.click(faqButton);
+});
+
+Then(/^I should see the FAQ button$/, async function () {
+  this.webDriverLogger.info(`Step: I should see FAQ button`);
+  await this.waitForElement(faqButton);
+});
+
+Then(/^I should see a new tab opened with address (.+)$/, async function (address) {
+  this.webDriverLogger.info(`Step: I should see a new tab opened with address ${address}`);
+  await this.windowManager.findNewWindowAndSwitchTo(faqTabName);
+  const actualAddresses = await this.driver.getCurrentUrl();
+  expect(actualAddresses).to.equal(address);
+});
