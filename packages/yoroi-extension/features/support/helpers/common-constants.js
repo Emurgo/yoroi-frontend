dbfeb3a185deac2fecfbe3425427ffbced2232b0
Cristian Merlo <cristian.merlo@globant.com>
2022-07-20 11:32:18 -0300

    Added sugested changes

--- packages/yoroi-extension/features/support/helpers/common-constants.js
+++ packages/yoroi-extension/features/support/helpers/common-constants.js
@@ -5,3 +5,11 @@ export const snapshotsDir = './features/yoroi_snapshots/';
 export const testRunsLogsDir = `${testRunsDataDir}Logs/`;
 export const mockDAppLogsDir = `${testRunsLogsDir}mockDApp/`;
 export const windowManagerLogsDir = `${testRunsLogsDir}windowManager/`;
+
+export const mailsacAPIKey = process.env.MAILSAC_API_KEY;
+export const mailsacEmail = 'emurgoqa@mailsac.com';
+export const emailOptions = {
+    method: 'get',
+    url: `https://mailsac.com/api/addresses/${mailsacEmail}/messages`,
+    headers: { 'Mailsac-Key': mailsacAPIKey },
+  };
