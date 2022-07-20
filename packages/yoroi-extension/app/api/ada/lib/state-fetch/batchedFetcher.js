ee6928e5972e646afe4de2a0cb89ed4de63cbd4c
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2022-07-20 15:00:57 +0300

    eslint

--- packages/yoroi-extension/app/api/ada/lib/state-fetch/batchedFetcher.js
+++ packages/yoroi-extension/app/api/ada/lib/state-fetch/batchedFetcher.js
@@ -108,7 +108,7 @@ export class BatchedFetcher implements IFetcher {
       const { network, assets } = body;
       const assetChunks = chunk(assets, MINT_METADATA_REQUEST_PAGE_SIZE);
       const responses = await Promise.all(assetChunks.map(
-        chunk => this.baseFetcher.getMultiAssetMintMetadata({ network, assets: chunk })
+        assets => this.baseFetcher.getMultiAssetMintMetadata({ network, assets })
       ));
       const result = {};
       for (const response of responses) {
