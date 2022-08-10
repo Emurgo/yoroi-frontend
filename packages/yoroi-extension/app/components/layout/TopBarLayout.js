48addb0fd11d9af74fab941df759f47b4b2f2789
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-10 13:40:32 +0200

    Fix page overflow on firfox

--- packages/yoroi-extension/app/components/layout/TopBarLayout.js
+++ packages/yoroi-extension/app/components/layout/TopBarLayout.js
@@ -79,7 +79,7 @@ function TopBarLayout({
                     minHeight: '200px',
                     padding: '40px',
                     backgroundColor: 'var(--yoroi-palette-gray-50)',
-                    overflow: 'overlay',
+                    overflow: 'scroll',
                   }}
                 >
                   {children}
