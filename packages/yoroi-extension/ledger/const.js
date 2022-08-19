52ee7fcfb02328ef5306008772be55e7a663caf0
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2022-08-19 17:41:18 +0300

    ff check fix

--- packages/yoroi-extension/ledger/const.js
+++ packages/yoroi-extension/ledger/const.js
@@ -7,11 +7,12 @@ export const DEFAULT_LOCALE = 'en-US';
 export const ENV = {
   isProduction: process.env.NODE_ENV === 'production',
   isDevelopment: process.env.NODE_ENV === 'development',
-  isFirefox: !!window.InstallTrigger
+  isFirefox: String(navigator.userAgent).includes('Firefox'),
+  isHidSupported: navigator.hid != null,
 };
-export const DEFAULT_TRANSPORT_PROTOCOL: TransportIdType = ENV.isFirefox
-  ? TRANSPORT_ID.WEB_AUTHN
-  : TRANSPORT_ID.WEB_HID;
+export const DEFAULT_TRANSPORT_PROTOCOL: TransportIdType = ENV.isHidSupported
+  ? TRANSPORT_ID.WEB_HID
+  : TRANSPORT_ID.WEB_AUTHN;
 export const DEVICE_LOCK_CHECK_TIMEOUT_MS = 500; // In milli-seconds
 export const TRANSPORT_EXCHANGE_TIMEOUT_MS = 120000; // In milli-seconds
 export const SUPPORTED_VERSION = `>=2.1.0`; // supported version of the Cardano app
