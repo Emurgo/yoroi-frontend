2baf80111fa519cf59416e9aa4353146dbd132ea
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-16 13:28:59 +0200

    Add props to control the qr code

--- packages/yoroi-extension/app/components/widgets/QrCodeWrapper.js
+++ packages/yoroi-extension/app/components/widgets/QrCodeWrapper.js
@@ -8,22 +8,38 @@ import { readCssVar } from '../../styles/utils';
 type Props = {|
   +value: string,
   +size: number,
+  +id?: string,
+  +includeMargin?: boolean,
+  +addBg?: boolean
 |};
 
 @observer
 export default class QrCodeWrapper extends Component<Props> {
+
+  static defaultProps: {|
+    id: string,
+    includeMargin: boolean,
+    addBg: boolean,
+  |} = {
+    id: 'qr-code',
+    includeMargin: false,
+    addBg: false,
+  }
+
   render(): Node {
     // Get QRCode color value from active theme's CSS variable
     const qrCodeBackgroundColor = readCssVar('--yoroi-qr-code-background');
     const qrCodeForegroundColor = readCssVar('--yoroi-qr-code-foreground');
+    const { id, includeMargin, addBg } = this.props;
 
     return (
       <QRCode
         value={this.props.value}
-        bgColor={qrCodeBackgroundColor}
-        fgColor={qrCodeForegroundColor}
+        bgColor={addBg && qrCodeBackgroundColor}
+        fgColor={addBg && qrCodeForegroundColor}
         size={this.props.size}
-        id="qr-code"
+        includeMargin={includeMargin === true}
+        id={id}
       />
     );
   }
