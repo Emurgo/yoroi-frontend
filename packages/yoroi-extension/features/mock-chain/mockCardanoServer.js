d2623305d164fa6efa8b583fa4460e5f0a7c0bc4
Denis <nebytov.d@gmail.com>
2022-10-14 16:32:57 +0300

    using new loggers

--- packages/yoroi-extension/features/mock-chain/mockCardanoServer.js
+++ packages/yoroi-extension/features/mock-chain/mockCardanoServer.js
@@ -40,12 +40,14 @@ const fs = require('fs');
 const addressesLimit = 50;
 const txsLimit = 20;
 
-let logger;
-
-function _validateAddressesReq({ addresses }: { addresses: Array<string>, ... } = {}): boolean {
-  logger.info(`mockCardanoServer: Validate Addresses request`);
+function _validateAddressesReq(
+  { addresses }: { addresses: Array<string>, ... } = {},
+  localLogger: LocalLogger)
+  : boolean {
+  localLogger.logInfo(`Validate Addresses request`);
   if (!addresses || addresses.length > addressesLimit || addresses.length === 0) {
-    throw new Error('Addresses request length should be (0, ' + addressesLimit + ']');
+    localLogger.logError(`Addresses request length should be (0, ${addressesLimit})`);
+    throw new Error(`Addresses request length should be (0, ${addressesLimit})`);
   }
   // TODO: Add address validation
   return true;
@@ -53,21 +55,19 @@ function _validateAddressesReq({ addresses }: { addresses: Array<string>, ... } 
 
 function _defaultSignedTransaction(
   req: { body: SignedRequestInternal, ... },
-  res: { send(arg: SignedResponse): any, ... }
+  res: { send(arg: SignedResponse): any, ... },
+  localLogger: LocalLogger
 ): void {
-  logger.info(`mockCardanoServer: Default Signed Transaction`);
-  logger.info(`mockCardanoServer: mockImporter.sendTx -> request`);
-  logger.info(JSON.stringify(req.body));
+  localLogger.logInfo(`Default Signed Transaction`);
+  localLogger.logInfo(`mockImporter.sendTx <- request\n    ${JSON.stringify(req.body)}`);
   const response = mockImporter.sendTx(req.body);
-  logger.info(`mockCardanoServer: mockImporter.sendTx -> response`);
-  logger.info(JSON.stringify(response));
+  localLogger.logInfo(`mockImporter.sendTx -> response\n    ${JSON.stringify(response)}`);
   res.send(response);
 }
 
 const expectedTxBase64 = [];
 
 export function setExpectedTx(signedTx: void | string): void {
-  logger.info(`mockCardanoServer: Set expected transaction`);
   if (signedTx == null) {
     // remove all elements from the array
     expectedTxBase64.splice(0, expectedTxBase64.length);
@@ -137,14 +137,14 @@ export function getMockServer(settings: {
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir, { recursive: true });
   }
-  const loggerPath = `${dir}/cardanoMockServerLog_${getLogDate()}.log`;
+  const logPath = `${dir}/cardanoMockServerLog_${getLogDate()}.log`;
+  const localLogger = new LocalLogger('mockCardanoServer', logPath);
 
-  logger = simpleNodeLogger.createSimpleFileLogger(loggerPath);
   if (!MockServer) {
     const middlewares = [...defaults({ logger: !!settings.outputLog }), bodyParser];
 
     const server = create();
-    logger.info(`mockCardanoServer: JSON Server Created`);
+    localLogger.logInfo(`JSON Server Created`);
 
     server.use(middlewares);
 
@@ -154,12 +154,11 @@ export function getMockServer(settings: {
         req: { body: AddressUtxoRequest, ... },
         res: { send(arg: AddressUtxoResponse): any, ... }
       ): Promise<void> => {
-        chai.assert.isTrue(_validateAddressesReq(req.body));
-        logger.info(`mockCardanoServer: /api/txs/utxoForAddresses -> request`);
-        logger.info(JSON.stringify(req.body));
+        const methodLogger = localLogger.getMethodLogger('POST', '/api/txs/utxoForAddresses');
+        chai.assert.isTrue(_validateAddressesReq(req.body, localLogger));
+        methodLogger.logRequest(JSON.stringify(req.body));
         const utxoForAddresses = await mockImporter.utxoForAddresses(req.body);
-        logger.info(`mockCardanoServer: /api/txs/utxoForAddresses -> response`);
-        logger.info(JSON.stringify(utxoForAddresses));
+        methodLogger.logResponseSuccess(JSON.stringify(utxoForAddresses));
         res.send(utxoForAddresses);
       }
     );
@@ -170,12 +169,11 @@ export function getMockServer(settings: {
         req: { body: UtxoSumRequest, ... },
         res: { send(arg: UtxoSumResponse): any, ... }
       ): Promise<void> => {
-        chai.assert.isTrue(_validateAddressesReq(req.body));
-        logger.info(`mockCardanoServer: /api/txs/utxoSumForAddresses -> request`);
-        logger.info(JSON.stringify(req.body));
+        const methodLogger = localLogger.getMethodLogger('POST', '/api/txs/utxoSumForAddresses');
+        chai.assert.isTrue(_validateAddressesReq(req.body, localLogger));
+        methodLogger.logRequest(JSON.stringify(req.body));
         const utxoSumForAddresses = await mockImporter.utxoSumForAddresses(req.body);
-        logger.info(`mockCardanoServer: /api/txs/utxoSumForAddresses -> response`);
-        logger.info(JSON.stringify(utxoSumForAddresses));
+        methodLogger.logResponseSuccess(JSON.stringify(utxoSumForAddresses));
         res.send(utxoSumForAddresses);
       }
     );
@@ -186,13 +184,11 @@ export function getMockServer(settings: {
         req: { body: HistoryRequest, ... },
         res: { send(arg: HistoryResponse): any, ... }
       ): Promise<void> => {
-        chai.assert.isTrue(_validateAddressesReq(req.body));
-        logger.info(`mockCardanoServer: /api/v2/txs/history -> request`);
-        logger.info(JSON.stringify(req.body));
-
+        const methodLogger = localLogger.getMethodLogger('POST', '/api/v2/txs/history');
+        chai.assert.isTrue(_validateAddressesReq(req.body, localLogger));
+        methodLogger.logRequest(JSON.stringify(req.body));
         const history = await mockImporter.history(req.body);
-        logger.info(`mockCardanoServer: /api/v2/txs/history -> response`);
-        logger.info(JSON.stringify(history));
+        methodLogger.logResponseSuccess(JSON.stringify(history));
         // Returns a chunk of txs
         res.send(history.slice(0, txsLimit));
       }
@@ -204,11 +200,10 @@ export function getMockServer(settings: {
         req: { body: BestBlockRequest, ... },
         res: { send(arg: BestBlockResponse): any, ... }
       ): Promise<void> => {
-        logger.info(`mockCardanoServer: /api/v2/getblock-> request`);
-        logger.info(JSON.stringify(req.body));
+        const methodLogger = localLogger.getMethodLogger('POST', '/api/v2/bestblock');
+        methodLogger.logRequest(JSON.stringify(req.body));
         const bestBlock = await mockImporter.getBestBlock(req.body);
-        logger.info(`mockCardanoServer: /api/v2/getblock -> response`);
-        logger.info(JSON.stringify(bestBlock));
+        methodLogger.logResponseSuccess(JSON.stringify(bestBlock));
         res.send(bestBlock);
       }
     );
@@ -223,26 +218,25 @@ export function getMockServer(settings: {
           ...
         }
       ): void => {
-        logger.info(`mockCardanoServer: /api/txs/signed-> request`);
+        const methodLogger = localLogger.getMethodLogger('POST', '/api/txs/signed');
         // note: don't use this in practice because ttl makes the tx hash computer-time-sensitive
         if (expectedTxBase64.length !== 0 && expectedTxBase64[0] !== req.body.signedTx) {
-          logger.error(
-            `mockCardanoServer: Wrong transaction payload. Expected ${expectedTxBase64[0]} and found ${req.body.signedTx}`
-          );
-          /*throw new Error(
+          localLogger.logError(
             `Wrong transaction payload. Expected ${expectedTxBase64[0]} and found ${req.body.signedTx}`
-          );*/
+          );
+          // throw new Error(
+          //   `Wrong transaction payload. Expected ${expectedTxBase64[0]} and found ${req.body.signedTx}`
+          // );
         }
-        logger.info(JSON.stringify(req.body));
+        methodLogger.logRequest(JSON.stringify(req.body));
 
         if (settings.signedTransaction) {
           settings.signedTransaction(req, res);
         } else {
-          _defaultSignedTransaction(req, res);
+          _defaultSignedTransaction(req, res, localLogger);
         }
 
-        logger.info(`mockCardanoServer: /api/txs/signed-> response`);
-        logger.info(JSON.stringify(res, getCircularReplacer()));
+        methodLogger.logResponseSuccess(JSON.stringify(res, getCircularReplacer()));
       }
     );
 
@@ -252,12 +246,11 @@ export function getMockServer(settings: {
         req: { body: PoolInfoRequest, ... },
         res: { send(arg: PoolInfoResponse): any, ... }
       ): Promise<void> => {
-        logger.info(`mockCardanoServer: /api/pool/info -> request`);
-        logger.info(JSON.stringify(req.body));
+        const methodLogger = localLogger.getMethodLogger('POST', '/api/pool/info');
+        methodLogger.logRequest(JSON.stringify(req.body));
 
         const poolsInfo = await mockImporter.getPoolInfo(req.body);
-        logger.info(`mockCardanoServer: /api/pool/info -> response`);
-        logger.info(JSON.stringify(poolsInfo));
+        methodLogger.logResponseSuccess(JSON.stringify(poolsInfo));
         res.send(poolsInfo);
       }
     );
@@ -268,12 +261,11 @@ export function getMockServer(settings: {
         req: { body: RewardHistoryRequest, ... },
         res: { send(arg: RewardHistoryResponse): any, ... }
       ): Promise<void> => {
-        logger.info(`mockCardanoServer: /api/account/rewardHistory -> request`);
-        logger.info(JSON.stringify(req.body));
+        const methodLogger = localLogger.getMethodLogger('POST', '/api/account/rewardHistory');
+        methodLogger.logRequest(JSON.stringify(req.body));
 
         const poolsInfo = await mockImporter.getRewardHistory(req.body);
-        logger.info(`mockCardanoServer: /api/account/rewardHistory -> response`);
-        logger.info(JSON.stringify(poolsInfo));
+        methodLogger.logResponseSuccess(JSON.stringify(poolsInfo));
         res.send(poolsInfo);
       }
     );
@@ -284,13 +276,12 @@ export function getMockServer(settings: {
         req: { body: AccountStateRequest, ... },
         res: { send(arg: AccountStateResponse): any, ... }
       ): Promise<void> => {
-        chai.assert.isTrue(_validateAddressesReq(req.body));
-        logger.info(`mockCardanoServer: /api/account/state -> request`);
-        logger.info(JSON.stringify(req.body));
+        const methodLogger = localLogger.getMethodLogger('POST', '/api/account/rewardHistory');
+        chai.assert.isTrue(_validateAddressesReq(req.body, localLogger));
+        methodLogger.logRequest(JSON.stringify(req.body));
 
         const accountState = await mockImporter.getAccountState(req.body);
-        logger.info(`mockCardanoServer: /api/account/state -> response`);
-        logger.info(JSON.stringify(accountState));
+        methodLogger.logResponseSuccess(JSON.stringify(accountState));
         res.send(accountState);
       }
     );
@@ -301,27 +292,30 @@ export function getMockServer(settings: {
         req: { body: FilterUsedRequest, ... },
         res: { send(arg: FilterUsedResponse): any, ... }
       ): Promise<void> => {
-        logger.info(`mockCardanoServer: /api/v2/addresses/filterUsed -> request`);
-        logger.info(JSON.stringify(req.body));
+        const methodLogger = localLogger.getMethodLogger('POST', '/api/v2/addresses/filterUsed');
+        methodLogger.logRequest(JSON.stringify(req.body));
         const response = await mockImporter.usedAddresses(req.body);
 
-        logger.info(`mockCardanoServer: /api/v2/addresses/filterUsed -> response`);
-        logger.info(JSON.stringify(response));
+        methodLogger.logResponseSuccess(JSON.stringify(response));
         res.send(response);
       }
     );
 
     server.get('/api/status', (req, res: { send(arg: ServerStatusResponse): any, ... }): void => {
+      const methodLogger = localLogger.getMethodLogger('GET', '/api/status');
+      methodLogger.logRequest();
       const status = mockImporter.getApiStatus();
-      logger.info(`mockCardanoServer: GET: /api/status`);
-      logger.info(JSON.stringify(status));
+      methodLogger.logResponseSuccess(JSON.stringify(status));
       res.send(status);
     });
 
     // To test the dApp connector, we need a no-op mock dApp.
     server.get('/mock-dapp', (req, res: { send(arg: ServerStatusResponse): any, ... }): void => {
+      const methodLogger = localLogger.getMethodLogger('GET', '/mock-dapp');
+      methodLogger.logRequest();
       // $FlowFixMe[prop-missing]
       res.header('content-type', 'text/html');
+      methodLogger.logResponseSuccess();
       // $FlowFixMe[incompatible-call]
       res.send(`
                <!doctype html>
@@ -336,86 +330,103 @@ export function getMockServer(settings: {
     });
 
     server.get('/api/txs/io/:txHash/o/:txIndex', (req, res) => {
-      logger.info(`mockCardanoServer: /api/txs/io/:txHash/o/:txIndex -> request`);
-      logger.info(JSON.stringify(req.body));
+      const methodLogger = localLogger.getMethodLogger('GET', '/api/txs/io/:txHash/o/:txIndex');
+      methodLogger.logRequest(JSON.stringify(req.body));
 
       const result = mockImporter.getUtxoData(req.params.txHash, Number(req.params.txIndex));
-      logger.info(`mockCardanoServer: /api/txs/io/:txHash/o/:txIndex -> response`);
-      logger.info(JSON.stringify(result));
       if (result) {
         res.send(result);
+        methodLogger.logResponseSuccess(JSON.stringify(result));
         return;
       }
+      methodLogger.logResponseError(`404 Transaction not found`)
       res.status(404);
       res.send('Transaction not found');
     });
 
     server.get('/api/v2/tipStatus', async (req, res) => {
+      const methodLogger = localLogger.getMethodLogger('GET', '/api/v2/tipStatus');
+      methodLogger.logRequest();
       const bestBlockHash = await mockImporter.mockUtxoApi.getBestBlock();
       const safeBlockHash = await mockImporter.mockUtxoApi.getSafeBlock();
-      res.send({
+      const response = {
         safeBlocK: { hash: safeBlockHash },
         bestBlock: { hash: bestBlockHash },
-      });
+      };
+      methodLogger.logResponseSuccess(JSON.stringify(response));
+      res.send(response);
     });
 
     server.post('/api/v2/tipStatus', async (req, res) => {
+      const methodLogger = localLogger.getMethodLogger('POST', '/api/v2/tipStatus');
+      methodLogger.logRequest(JSON.stringify(req.body));
       const { bestBlocks } = req.body.reference;
       const tipStatus = await mockImporter.mockUtxoApi.getTipStatusWithReference(
         bestBlocks
       );
       if (tipStatus.result !== 'SUCCESS') {
+        methodLogger.logResponseError('500 REFERENCE_POINT_BLOCK_NOT_FOUND');
         res.status(500);
         res.send({ error: { response: 'REFERENCE_POINT_BLOCK_NOT_FOUND' } });
       } else {
         const value = tipStatus.value;
         if (!value) {
-          throw new Error('unpected null value');
+          methodLogger.logResponseError('unexpected null value');
+          throw new Error('unexpected null value');
         }
         const bestBlockHash = await mockImporter.mockUtxoApi.getBestBlock();
         const safeBlockHash = await mockImporter.mockUtxoApi.getSafeBlock();
-        res.send({
+        const response = {
           safeBlock: safeBlockHash,
           bestBlock: bestBlockHash,
           reference: value.reference,
-        });
+        };
+        methodLogger.logResponseSuccess(JSON.stringify(response));
+        res.send(response);
       }
     });
 
     server.post('/api/v2/txs/utxoAtPoint', async (req, res) => {
+      const methodLogger = localLogger.getMethodLogger('POST', '/api/v2/txs/utxoAtPoint');
+      methodLogger.logRequest(JSON.stringify(req.body));
       const { addresses, referenceBlockHash } = req.body;
       const { value } = await mockImporter.mockUtxoApi.getUtxoAtPoint(
         { addresses, referenceBlockHash }
       );
       if (!value) {
-        throw new Error('unpected null value');
+        methodLogger.logResponseError('unexpected null value');
+        throw new Error('unexpected null value');
       }
-      res.send(
-        value.map(v => (
-          {
-            utxo_id: v.utxoId,
-            tx_hash: v.txHash,
-            tx_index: v.txIndex,
-            block_num: v.blockNum,
-            receiver: v.receiver,
-            amount: v.amount,
-            assets: v.assets,
-          }
-        ))
-      );
+      const response = value.map(v => (
+        {
+          utxo_id: v.utxoId,
+          tx_hash: v.txHash,
+          tx_index: v.txIndex,
+          block_num: v.blockNum,
+          receiver: v.receiver,
+          amount: v.amount,
+          assets: v.assets,
+        }
+      ));
+      methodLogger.logResponseSuccess(JSON.stringify(response));
+      res.send(response);
     });
 
     server.post('/api/v2/txs/utxoDiffSincePoint', async (req, res) => {
+      const methodLogger = localLogger.getMethodLogger('POST', '/api/v2/txs/utxoDiffSincePoint');
+      methodLogger.logRequest(req.body);
       const { addresses, untilBlockHash, afterBestBlock } = req.body;
       const { result, value } = await mockImporter.mockUtxoApi.getUtxoDiffSincePoint(
         { addresses, untilBlockHash, afterBestBlock }
       );
       if (result !== 'SUCCESS') {
+        methodLogger.logResponseError('500 REFERENCE_POINT_BLOCK_NOT_FOUND');
         res.status(500);
         res.send({ error: { response: 'REFERENCE_POINT_BLOCK_NOT_FOUND' } });
       } else {
         if (!value) {
-          throw new Error('unpected null value');
+          methodLogger.logResponseError('unexpected null value');
+          throw new Error('unexpected null value');
         }
         // casting `value` to `any` is the only way to pass flow check
         const diffItems = (value: any).diffItems.map(item => {
@@ -433,6 +444,7 @@ export function getMockServer(settings: {
             tx_index: item.utxo.txIndex,
           };
         });
+        methodLogger.logResponseSuccess({ diffItems });
         res.send({ diffItems });
       }
     });
@@ -442,7 +454,7 @@ export function getMockServer(settings: {
     MockServer = server.listen(Ports.DevBackendServe, () => {
       // eslint-disable-next-line no-console
       console.log(`JSON Server is running at ${Ports.DevBackendServe}`);
-      logger.info(`mockCardanoServer: JSON Server is running at ${Ports.DevBackendServe}`);
+      localLogger.logInfo(`JSON Server is running at ${Ports.DevBackendServe}`);
     });
   }
   return MockServer;
@@ -452,6 +464,5 @@ export function closeMockServer() {
   if (MockServer) {
     MockServer.close();
     MockServer = null;
-    logger.info(`mockCardanoServer: JSON Server closed`);
   }
 }
