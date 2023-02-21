55b0d8dc656f06ae19a2be54cf491e9d84e4fb05
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-02-21 16:07:26 +0200

    feat: add dialog action & title

--- packages/yoroi-extension/app/components/wallet/create-wallet/CreateWalletPage.js
+++ packages/yoroi-extension/app/components/wallet/create-wallet/CreateWalletPage.js
@@ -1,11 +1,17 @@
 // @flow
 import { Component } from 'react';
 import type { Node } from 'react';
+import InfoDialog from '../../widgets/infoDialog';
 
 export default class CreateWalletPage extends Component<{||}> {
     render(): Node {
         return (
+          <div>
             <h1>Create new wallet!!</h1>
+            <InfoDialog open>
+                <h1>Hello, World</h1>
+            </InfoDialog>
+          </div>
         )
     }
 }
