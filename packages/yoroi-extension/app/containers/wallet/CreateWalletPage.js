8d7da4ab134e7d86680282bbbb6471fbf3a12e01
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-02-21 14:18:53 +0200

    feat: add create new wallet route

---
+++ packages/yoroi-extension/app/containers/wallet/CreateWalletPage.js
@@ -0,0 +1,11 @@
+// @flow
+import { Component } from 'react';
+import type { Node } from 'react';
+
+export default class CreateWalletPage extends Component<{||}> {
+    render(): Node {
+        return (
+            <h1>Create new wallet!!</h1>
+        )
+    }
+}
