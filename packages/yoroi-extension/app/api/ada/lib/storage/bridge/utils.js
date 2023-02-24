009466a3565514847ab8ff01400e8ebac1195a44
yushi <yushihuang@gmail.com>
2023-02-24 15:01:44 +0800

    fix address type function

--- packages/yoroi-extension/app/api/ada/lib/storage/bridge/utils.js
+++ packages/yoroi-extension/app/api/ada/lib/storage/bridge/utils.js
@@ -331,14 +331,11 @@ export function isJormungandrAddress(
   kind: CoreAddressT
 ): boolean {
   // note: excluding legacy byron addresses
-  const types = [
-    CoreAddressTypes.JORMUNGANDR_SINGLE,
-    CoreAddressTypes.JORMUNGANDR_GROUP,
-    CoreAddressTypes.JORMUNGANDR_ACCOUNT,
-    CoreAddressTypes.JORMUNGANDR_MULTISIG,
-  ];
-
-  return kind in types;
+  if (kind === CoreAddressTypes.JORMUNGANDR_SINGLE) return true;
+  if (kind === CoreAddressTypes.JORMUNGANDR_GROUP) return true;
+  if (kind === CoreAddressTypes.JORMUNGANDR_ACCOUNT) return true;
+  if (kind === CoreAddressTypes.JORMUNGANDR_MULTISIG) return true;
+  return false;
 }
 
 export function isErgoAddress(
