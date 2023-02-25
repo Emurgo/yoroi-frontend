d2cba8f53cd8eb60871319cd0c47179b074c9846
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-02-25 23:44:29 +0200

    Add StepController component

---
+++ packages/yoroi-extension/app/components/wallet/create-wallet/StepController.js
@@ -0,0 +1,55 @@
+// @flow
+import type { Node, ComponentType } from 'react';
+import { injectIntl } from 'react-intl';
+import { observer } from 'mobx-react';
+import type { $npm$ReactIntl$IntlShape } from 'react-intl';
+import { Button, Stack } from '@mui/material'
+import globalMessages from '../../../i18n/global-messages';
+
+type Intl = {|
+  intl: $npm$ReactIntl$IntlShape,
+|};
+
+type Props = {|
+    goNext(): void,
+    goBack(): void,
+|};
+
+function LearnAboutRecoveryPhrase(props: Props & Intl): Node {
+    const { intl, goBack, goNext } = props;
+
+    return (
+      <Stack direction='row' alignItems='center' justifyContent='center' mt='14px' py='24px' gap='24px'>
+        <Button
+          variant='outlined'
+          disableRipple={false}
+          onClick={goBack}
+          sx={{
+            width: '144px',
+            height: '40px',
+            minWidth: 'unset',
+            minHeight: 'unset',
+            fontSize: '14px',
+            lineHeight: '15px',
+          }}
+        >
+          {intl.formatMessage(globalMessages.backButtonLabel)}
+        </Button>
+        <Button
+          variant='rv-primary'
+          disableRipple={false}
+          onClick={goNext}
+          sx={{
+            width: '144px',
+            height: '40px',
+            fontSize: '14px',
+            lineHeight: '15px',
+          }}
+        >
+          {intl.formatMessage(globalMessages.nextButtonLabel)}
+        </Button>
+      </Stack>
+    )
+}
+
+export default (injectIntl(observer(LearnAboutRecoveryPhrase)) : ComponentType<Props>);
