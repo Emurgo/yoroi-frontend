eb0dc8f5a6942d5fd1d1fd83a23a96a7fd3fe03d
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-02-25 22:11:36 +0200

    Create page header

--- packages/yoroi-extension/app/components/wallet/create-wallet/CreateWalletPage.js
+++ packages/yoroi-extension/app/components/wallet/create-wallet/CreateWalletPage.js
@@ -1,15 +1,29 @@
 // @flow
 import { useEffect, useState } from 'react';
 import type { Node, ComponentType } from 'react';
-
+import type { $npm$ReactIntl$IntlShape } from 'react-intl';
+import { defineMessages, injectIntl } from 'react-intl';
 import SaveRecoveryPhraseTipsDialog from './SaveRecoveryPhraseTipsDialog';
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { observer } from 'mobx-react';
+import YoroiLogo from '../../../assets/images/yoroi-logo-shape-blue.inline.svg'
 
+const messages: * = defineMessages({
+  title: {
+    id: 'wallet.create.page.title',
+    defaultMessage: '!!!Create a Wallet',
+  },
+});
 
-function CreateWalletPage(): Node {
+type Intl = {|
+  intl: $npm$ReactIntl$IntlShape,
+|};
+
+type Props = {||};
+
+function CreateWalletPage(props: Props & Intl): Node {
+  const { intl } = props;
   const [open, setOpen] = useState(false);
-
 
   useEffect(() => {
     setOpen(true)
@@ -17,7 +31,21 @@ function CreateWalletPage(): Node {
 
   return (
     <Box>
-      <h1>Create new wallet!!</h1>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <Box sx={{ width: '56px', height: '48px', mb: '38px' }}>
+          <img src={YoroiLogo} alt="Yoroi" title="Yoroi" />
+        </Box>
+
+        <Typography variant='h3'>{intl.formatMessage(messages.title)}</Typography>
+      </Box>
+
       <SaveRecoveryPhraseTipsDialog
         open={open}
         onClose={() => setOpen(false)}
@@ -27,4 +55,4 @@ function CreateWalletPage(): Node {
 }
 
 
-export default (observer(CreateWalletPage) : ComponentType<Props> )
+export default (injectIntl(observer(CreateWalletPage)) : ComponentType<Props> )
