b323aa4cd5eab2e3d5bf8c8d5a209b97c2f8240b
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-02-28 15:02:29 +0200

    feat: add zendesk link to the tips dialog

--- packages/yoroi-extension/app/components/widgets/infoDialog.js
+++ packages/yoroi-extension/app/components/widgets/infoDialog.js
@@ -1,5 +1,5 @@
 // @flow
-import { Modal, Typography, Button, Stack } from '@mui/material';
+import { Modal, Typography, Button, Stack, Link } from '@mui/material';
 import type { Node, ComponentType } from 'react';
 import { Box } from '@mui/system';
 import type { $npm$ReactIntl$IntlShape } from 'react-intl';
@@ -24,13 +24,13 @@ type Intl = {|
 |};
 
 function InfoDialog(props: Props & Intl): Node {
-   const { open, onClose, children, intl } = props;
+  const { open, onClose, children, intl } = props;
 
-   return (
-     <Modal
-       open={open}
-       onClose={onClose}
-       sx={{
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      sx={{
         background: 'var(--yoroi-comp-dialog-overlay-background-color)',
         display: 'flex',
         justifyContent: 'center',
@@ -39,62 +39,75 @@ function InfoDialog(props: Props & Intl): Node {
           background: 'none',
         },
       }}
-     >
-       <Box
-         sx={{
-            width: '648px',
-            minHeight: '415px',
-            background: 'var(--yoroi-palette-common-white)',
-            borderRadius: '8px',
-            boxShadow: '0px 13px 20px -1px rgba(0, 0, 0, 0.15)',
-            borderWidth: '1px',
-            borderStyle: 'solid',
-            borderColor: 'grey.200',
-            padding: '24px',
-            display: 'flex',
-            alignitems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'column',
-         }}
-       >
-         <Typography
-           sx={{
-              display: 'block',
-              fontSize: '16px',
-              fontWeight: '500',
-              lineHeight: '22px',
-              textTransform: 'uppercase',
-              textAlign: 'center',
-              mb: '25px',
-            }}
-         >
-           {intl.formatMessage(messages.dialogTitle)}
-         </Typography>
-         <Box
-           sx={{
+    >
+      <Box
+        sx={{
+          width: '648px',
+          minHeight: '415px',
+          background: 'var(--yoroi-palette-common-white)',
+          borderRadius: '8px',
+          boxShadow: '0px 13px 20px -1px rgba(0, 0, 0, 0.15)',
+          borderWidth: '1px',
+          borderStyle: 'solid',
+          borderColor: 'grey.200',
+          padding: '24px',
+          display: 'flex',
+          alignitems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <Typography
+          sx={{
+            display: 'block',
+            fontSize: '16px',
+            fontWeight: '500',
+            lineHeight: '22px',
+            textTransform: 'uppercase',
+            textAlign: 'center',
+            mb: '25px',
+          }}
+        >
+          {intl.formatMessage(messages.dialogTitle)}
+        </Typography>
+        <Box
+          sx={{
             flex: 1,
-           }}
-         >
-           {children}
-         </Box>
-         <Stack alignItems='center' justifyContent='center'>
-           <Button
-             variant='rv-primary'
-             disableRipple={false}
-             onClick={onClose}
-             sx={{
-               width: '176px',
-               height: '40px',
-               fontSize: '14px',
-               lineHeight: '15px',
-             }}
-           >
-             {intl.formatMessage(globalMessages.continue)}
-           </Button>
-         </Stack>
-       </Box>
-     </Modal>
-   )
+          }}
+        >
+          {children}
+        </Box>
+        <Link
+          href="https://emurgohelpdesk.zendesk.com/hc/en-us/categories/4412619927695-Yoroi-"
+          target='_blank'
+          rel="noreferrer noopener"
+          sx={{
+            textDecoration: 'none',
+            display: 'block',
+            textAlign: 'center',
+            py: '24px',
+          }}
+        >
+          Learn more on Yoroi Zendesk
+        </Link>
+        <Stack alignItems='center' justifyContent='center'>
+          <Button
+            variant='rv-primary'
+            disableRipple={false}
+            onClick={onClose}
+            sx={{
+              width: '176px',
+              height: '40px',
+              fontSize: '14px',
+              lineHeight: '15px',
+            }}
+          >
+            {intl.formatMessage(globalMessages.continue)}
+          </Button>
+        </Stack>
+      </Box>
+    </Modal>
+  );
 }
 
 export default (injectIntl(InfoDialog): ComponentType<Props>)
