0322bf2fb898d47bc00def974a98484b7e7c84e0
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-02-28 14:54:23 +0200

    refactor: move the tips dialog state to the local state

--- packages/yoroi-extension/app/components/wallet/create-wallet/CreateWalletSteps.js
+++ packages/yoroi-extension/app/components/wallet/create-wallet/CreateWalletSteps.js
@@ -1,9 +1,11 @@
 // @flow
+import { useEffect, useState } from 'react';
 import type { Node, ComponentType } from 'react';
 import { defineMessages, injectIntl } from 'react-intl';
 import { observer } from 'mobx-react';
 import type { $npm$ReactIntl$IntlShape } from 'react-intl';
 import { Stack, Box, Typography } from '@mui/material'
+import SaveRecoveryPhraseTipsDialog from './SaveRecoveryPhraseTipsDialog';
 
 const messages: * = defineMessages({
   firstStep: {
@@ -33,50 +35,60 @@ type Props = {|
 |};
 
 function CreateWalletSteps(props: Props & Intl): Node {
-    const { intl, currentStep } = props;
-    // steps: [id, label]
-    const steps = [
-        [1, messages.firstStep],
-        [2, messages.secondStep],
-        [3, messages.thirdStep],
-        [4, messages.forthStep],
-    ];
+  const { intl, currentStep } = props;
+  // steps: [id, label]
+  const steps = [
+    [1, messages.firstStep],
+    [2, messages.secondStep],
+    [3, messages.thirdStep],
+    [4, messages.forthStep],
+  ];
 
-    return (
-      <Box>
-        <Stack direction='row' alignItems='center' justifyContent='center' gap='24px' mt='24px' mb='48px'>
-          {steps.map(([stepId, label]) => {
-            const stepColor = currentStep === stepId ? 'primary.200' : 'grey.400';
-            return (
-              <Stack direction='row' alignItems='center' justifyContent='center' key={stepId}>
-                <Box
-                  sx={{
-                    width: '24px',
-                    height: '24px',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    mr: '8px',
-                    borderWidth: '2.5px',
-                    borderStyle: 'solid',
-                    borderColor: stepColor,
-                    borderRadius: '50%',
-                    transition: 'color 300ms ease',
-                  }}
-                >
-                  <Typography variant='body2' fontWeight={500} color={stepColor}>
-                    {stepId}
-                  </Typography>
-                </Box>
-                <Typography variant='body1' color={stepColor} fontWeight={500}>
-                  {intl.formatMessage(label)}
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setOpen(true)
+  }, [])
+
+  return (
+    <Box>
+      <Stack direction='row' alignItems='center' justifyContent='center' gap='24px' mt='24px' mb='48px'>
+        {steps.map(([stepId, label]) => {
+          const stepColor = currentStep === stepId ? 'primary.200' : 'grey.400';
+          return (
+            <Stack direction='row' alignItems='center' justifyContent='center' key={stepId}>
+              <Box
+                sx={{
+                  width: '24px',
+                  height: '24px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  mr: '8px',
+                  borderWidth: '2.5px',
+                  borderStyle: 'solid',
+                  borderColor: stepColor,
+                  borderRadius: '50%',
+                  transition: 'color 300ms ease',
+                }}
+              >
+                <Typography variant='body2' fontWeight={500} color={stepColor}>
+                  {stepId}
                 </Typography>
-              </Stack>
-            )
-          })}
-        </Stack>
-      </Box>
-    )
+              </Box>
+              <Typography variant='body1' color={stepColor} fontWeight={500}>
+                {intl.formatMessage(label)}
+              </Typography>
+            </Stack>
+          )
+        })}
+      </Stack>
+      <SaveRecoveryPhraseTipsDialog
+        open={open}
+        onClose={() => setOpen(false)}
+      />
+    </Box>
+  );
 }
 
 export default (injectIntl(observer(CreateWalletSteps)) : ComponentType<Props>);
