4b4f211e6704fda72f7aeba92311ec1088d8b7c7
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-03-08 21:03:41 +0200

    Save dialog ids to local storage

--- packages/yoroi-extension/app/components/wallet/create-wallet/steps.js
+++ packages/yoroi-extension/app/components/wallet/create-wallet/steps.js
@@ -10,3 +10,13 @@ export const TIPS_DIALOGS = Object.freeze({
     LEARN_ABOUT_RECOVERY_PHRASE: 'LEARN_ABOUT_RECOVER_PHRASE',
     SAVE_RECOVERY_PHRASE: 'SAVE_RECOVERY_PHRASE',
 })
+
+const asDialogId: string => string = (dialogId: string) => `dialog__${dialogId}`;
+
+export function markDialogAsShown(dialogId: string) {
+    localStorage.setItem(asDialogId(dialogId), 'true');
+}
+
+export function isDialogShownBefore(dialogId: string) {
+    return localStorage.getItem(asDialogId(dialogId)) === 'true';
+}
