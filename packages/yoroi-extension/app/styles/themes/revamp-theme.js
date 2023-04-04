62ea30f464af97f2b6d3dd6e4150293bef90917d
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-04-04 15:29:57 +0200

    feat: add design system typography

--- packages/yoroi-extension/app/styles/themes/revamp-theme.js
+++ packages/yoroi-extension/app/styles/themes/revamp-theme.js
@@ -22,6 +22,24 @@ import { deepmerge } from '@mui/utils';
 
 const fontFamily = ['Rubik', 'sans-serif'].join(',');
 
+/**
+ *
+ * @param {*} token typography token: `h1`, `h2`
+ * @param {*} variant typography styles object
+ * @param {*} mediumWeight typography font weight on medium size
+ * @returns
+ */
+function withMedium(token, variant, mediumWeight: number = 500) {
+  return {
+    [token]: variant,
+    [`${token}-regular`]: variant,
+    [`${token}-medium`]: {
+      ...variant,
+      fontWeight: mediumWeight,
+    },
+  };
+}
+
 const theme = {
   name: 'revamp',
   components: {
@@ -74,6 +92,10 @@ const theme = {
       '900': '#17453C',
       contrastText: '#FFF',
     },
+    common: {
+      black: '#000000',
+      white: '#FFFFFF',
+    },
     background: {
       sidebar: {
         start: '#1e46bd',
@@ -94,43 +116,50 @@ const theme = {
   },
   typography: {
     fontFamily,
-    h1: {
-      fontWeight: 500,
-      fontSize: '1.875rem',
+    // token: heading-1-regular
+    ...withMedium('h1', {
+      fontWeight: 400,
+      fontSize: '1.875rem', // 30px
+      lineHeight: '38px',
       fontFamily,
-    },
-    h2: {
-      fontWeight: 500,
-      fontSize: '1.75rem',
+    }),
+    ...withMedium('h2', {
+      fontWeight: 400,
+      fontSize: '1.75rem', // 28px
+      lineHeight: '32px',
       fontFamily,
-    },
-    h3: {
-      fontWeight: 500,
-      fontSize: '1.5rem',
+    }),
+    ...withMedium('h3', {
+      fontWeight: 400,
+      fontSize: '1.5rem', // 24px
+      lineHeight: '32px',
       fontFamily,
-    },
-    h4: {
-      fontWeight: 500,
-      fontSize: '1.25rem',
+    }),
+    ...withMedium('h4', {
+      fontWeight: 400,
+      fontSize: '1.25rem', // 20px
+      lineHeight: '28px',
       fontFamily,
-    },
-    h5: {
-      fontWeight: 500,
-      fontSize: '1.125rem',
+    }),
+    ...withMedium('h5', {
+      fontWeight: 400,
+      fontSize: '1.125rem', // 18px
+      lineHeight: '26px',
       fontFamily,
-    },
-    h6: {
-      fontWeight: 500,
-      fontSize: '1rem',
-      fontFamily,
+    }),
+    ...withMedium('h6', {
+      fontWeight: 400,
+      fontSize: '1rem', // 16px
+      lineHeight: '26px',
       textTransform: 'uppercase',
-    },
-    h7: {
-      fontWeight: 500,
+      fontFamily,
+    }),
+    ...withMedium('h7', {
+      fontWeight: 400,
       fontSize: '0.875rem',
       fontFamily,
       textTransform: 'uppercase',
-    },
+    }),
     body1: {
       fontWeight: 400,
       fontSize: '1rem',
