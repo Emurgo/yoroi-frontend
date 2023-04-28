bb6542af7828bc88a9c8bc92153e33b348e042c1
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-04-28 17:47:40 +0300

    feat: update checkbox styles

--- packages/yoroi-extension/app/styles/overrides/Checkbox.js
+++ packages/yoroi-extension/app/styles/overrides/Checkbox.js
@@ -1,3 +1,5 @@
+import { revampBaseTheme } from '../themes/revamp-base-theme';
+
 // @flow
 const ClassicCheckbox = {
   styleOverrides: {
@@ -14,6 +16,7 @@ const ClassicCheckbox = {
     },
   },
 };
+
 const ModernCheckbox = {
   styleOverrides: {
     root: {
@@ -23,11 +26,29 @@ const ModernCheckbox = {
       '&.Mui-checked': {
         color: 'var(--yoroi-comp-checkbox-background-active)',
       },
-      '&.Mui-disabled	': {
+      '&.Mui-disabled': {
         color: 'var(--yoroi-comp-checkbox-border-disabled)',
       },
     },
   },
 };
 
-export { ClassicCheckbox, ModernCheckbox };
+const RevampCheckbox = {
+  styleOverrides: {
+    root: {
+      padding: 0,
+      marginRight: '18px',
+      borderRadius: '2px',
+      color: 'primary.500',
+      '&.Mui-checked': {
+        color: 'primary.500',
+      },
+      '&.Mui-disabled': {
+        color: revampBaseTheme.palette.gray[400],
+        backgroundColor: revampBaseTheme.palette.gray[400],
+      },
+    },
+  },
+};
+
+export { ClassicCheckbox, ModernCheckbox, RevampCheckbox };
