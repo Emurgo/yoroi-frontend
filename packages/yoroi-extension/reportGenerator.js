9e5f3240d97a2a93d4ee8fb9bfe350d94ccea211
Denis <nebytov.d@gmail.com>
2023-05-21 20:02:16 +0300

    prettified reportGenerator

--- packages/yoroi-extension/reportGenerator.js
+++ packages/yoroi-extension/reportGenerator.js
@@ -1,24 +1,24 @@
-
 // @flow
 const reporter = require('cucumber-html-reporter');
 
 const options = {
-        theme: 'bootstrap',
-        jsonFile: 'reports/cucumberReport.json',
-        output: 'reports/cucumberReport.html',
-        reportSuiteAsScenarios: true,
-        scenarioTimestamp: true,
-        launchReport: true,
-       // screenshotsDirectory: './testScreenshots/',
-        storeScreenshots: false,
-        metadata: {
-            'App Version': '0.3.2',
-            'Test Environment': 'Dev',
-            'Browser': 'Chrome  97',
-            'Platform': 'MacPro',
-            'Parallel': 'Scenarios',
-            'Executed': 'Local'
-        }
-    };
+  theme: 'bootstrap',
+  jsonFile: 'reports/cucumberReport.json',
+  output: 'reports/cucumberReport.html',
+  reportSuiteAsScenarios: true,
+  scenarioTimestamp: true,
+  launchReport: true,
+  storeScreenshots: true,
+  screenshotsDirectory: 'reports/testScreenshots/',
+  metadata: {
+    'App Version': '0.3.2',
+    'Test Environment': 'Dev',
+    Browser: 'Chrome  97',
+    Platform: 'MacPro',
+    Parallel: 'Scenarios',
+    Executed: 'Local',
+  },
+  failedSummaryReport: true,
+};
 
-    reporter.generate(options);
+reporter.generate(options);
