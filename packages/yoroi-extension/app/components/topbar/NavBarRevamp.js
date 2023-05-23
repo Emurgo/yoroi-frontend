b486158415313ec0da93b9e71ca96ab73b21ce58
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-05-23 16:06:31 +0300

    feat: update wallet header to follow the new grid layout

--- packages/yoroi-extension/app/components/topbar/NavBarRevamp.js
+++ packages/yoroi-extension/app/components/topbar/NavBarRevamp.js
@@ -2,7 +2,9 @@
 import { Component } from 'react';
 import type { Node } from 'react';
 import { observer } from 'mobx-react';
+// import { Box, Grid } from '@mui/material';
 import { Box } from '@mui/material';
+import { revampTheme } from '../../styles/themes/revamp-theme';
 
 type Props = {|
   +children?: ?Node,
@@ -34,28 +36,28 @@ class NavBarRevamp extends Component<Props> {
     return (
       <Box
         sx={{
-          backgroundColor: 'var(--yoroi-palette-common-white)',
+          backgroundColor: 'common.white',
+          // todo: box shadow should be removed
           boxShadow:
             '0 4px 6px 0 #dee2ea, 0 1px 2px 0 rgba(222, 226, 234, 0.82), 0 2px 4px 0 rgba(222, 226, 234, 0.74)',
         }}
       >
         <Box
           sx={{
-            maxWidth: 'calc(1366px - 90px)',
-            position: 'relative',
-            zIndex: 100,
-            height: menu != null ? '125px' : '90px',
-            margin: 'auto',
+            paddingX: '16px',
+            [revampTheme.breakpoints.up('lg')]: {
+              paddingX: '24px',
+            },
           }}
         >
           <Box
             as="header"
             sx={{
-              color: 'var(--yoroi-palette-gray-800)',
+              color: 'gray.800',
               display: 'flex',
               alignItems: 'center',
               height: 'inherit',
-              padding: menu != null ? '34px 40px 50px' : '32px 40px',
+              py: '20px',
             }}
           >
             <Box
@@ -79,18 +81,8 @@ class NavBarRevamp extends Component<Props> {
                 )}
               </Box>
             </Box>
-            {menu != null ? (
-              <Box
-                sx={{
-                  position: 'absolute',
-                  bottom: 0,
-                  left: 0,
-                }}
-              >
-                {menu}
-              </Box>
-            ) : null}
           </Box>
+          <Box>{menu}</Box>
         </Box>
       </Box>
     );
