63df39c314543745f41607425fcccd275f95599d
Nebytov Denis <nebytov.d@gmail.com>
2023-09-04 19:02:34 +0300

    updated locators

--- packages/yoroi-extension/features/pages/createWalletPage.js
+++ packages/yoroi-extension/features/pages/createWalletPage.js
@@ -2,6 +2,61 @@
 
 import type { LocatorObject } from '../support/webdriver';
 
+export const nextButton: LocatorObject = {
+  locator: 'primaryButton',
+  method: 'id',
+};
+export const backButton: LocatorObject = {
+  locator: 'secondaryButton',
+  method: 'id',
+};
+export const createWalletWarning: LocatorObject = { locator: 'learnAboutRecoveryPhraseComponent', method: 'id' };
+export const recoveryPhraseBox: LocatorObject = { locator: 'recoveryPhraseBox', method: 'id' };
+export const getRecoveryPhraseWord = (wordIndex: number): LocatorObject => {
+  return { locator: `recoveryPhraseWord${wordIndex}`, method: 'id' };
+};
+export const getFullRecoveryPhrase = async (customWorld: any): Promise<Array<string>> => {
+  const fullPhraseArray = [];
+  for (let index = 0; index < 15; index++) {
+    const wordLocator = getRecoveryPhraseWord(index);
+    const rawWord: string = await (await customWorld.findElement(wordLocator)).getText();
+    const onlyWord = rawWord.split(' ')[1]
+    fullPhraseArray.push(onlyWord);
+  }
+
+  return fullPhraseArray;
+}
+export const toggleRecoveryPhraseBlurButton: LocatorObject = {
+  locator: 'toggleRecoveryPhraseButton',
+  method: 'id',
+};
+export const verifyRecoveryPhraseBox: LocatorObject = { locator: 'verifyRecoveryPhraseStepComponent', method: 'id' };
+export const getVerifyRecoveryPhraseWordButton = (wordIndex: number): LocatorObject => {
+  return { locator: `verifyRecoveryPhraseWord${wordIndex}`, method: 'id' };
+};
+export const enterFullRecoveryPhrase = async (
+  customWorld: any,
+  recoveryPhraseArray: Array<string>
+): Promise<void> => {
+  for (const wordToFind of recoveryPhraseArray) {
+    for (let index = 0; index < 15; index++) {
+      const presentedWordLocator = getVerifyRecoveryPhraseWordButton(index);
+      const isAdded = (await customWorld.getCssValue(presentedWordLocator, 'cursor')) === 'not-allowed'
+      if (isAdded) {
+        continue;
+      }
+      const presentedWord = await (await customWorld.findElement(presentedWordLocator)).getText();
+      if (presentedWord === wordToFind) {
+        await customWorld.click(presentedWordLocator)
+        break;
+      }
+    }
+  }
+};
+export const isValidPhraseMessage: LocatorObject = { locator: 'isValidPhraseMessage', method: 'id' };
+export const addWalletDetailsBox: LocatorObject = { locator: 'addWalletDetailsStepComponent', method: 'id' };
+
+// !!! OLD LOCATORS !!!
 export const walletInfoDialog: LocatorObject = { locator: '.WalletCreateDialog', method: 'css' };
 export const creationConfirmButton: LocatorObject = {
   locator: '.WalletCreateDialog .primary',
---
+++ packages/yoroi-extension/features/pages/walletDetailsPage.js
@@ -0,0 +1,20 @@
+// @flow
+
+import type { LocatorObject } from '../support/webdriver';
+
+export const walletNameInput: LocatorObject = {
+  locator: 'walletNameInput',
+  method: 'id',
+};
+export const walletPasswordInput: LocatorObject = {
+  locator: 'walletPasswordInput',
+  method: 'id',
+};
+export const repeatPasswordInput: LocatorObject = {
+  locator: 'repeatPasswordInput',
+  method: 'id',
+};
+export const newWalletDialogPlate: LocatorObject = {
+  locator: 'walletPlateText',
+  method: 'id',
+};
