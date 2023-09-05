1c07ef47f014f89aedcede07b4da29bb343a0155
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-09-05 11:28:33 +0300

    feat(staking): render tx info (reward, fees, final rewards)

--- packages/yoroi-extension/app/containers/wallet/staking/WithdrawRewardsDialog.js
+++ packages/yoroi-extension/app/containers/wallet/staking/WithdrawRewardsDialog.js
@@ -8,7 +8,7 @@ import Dialog from '../../../components/widgets/Dialog';
 import { Box, Typography } from '@mui/material';
 import { defineMessages, intlShape } from 'react-intl';
 import DialogCloseButton from '../../../components/widgets/DialogCloseButton';
-import { computed } from 'mobx';
+import { action, computed, observable } from 'mobx';
 import { observer } from 'mobx-react';
 import globalMessages from '../../../i18n/global-messages';
 import { toSvg } from 'jdenticon';
@@ -16,6 +16,20 @@ import styles from './WithdrawRewardsDialog.scss';
 import { CopyAddress } from '../../../components/wallet/assets/NFTDetails';
 import { addressToDisplayString } from '../../../api/ada/lib/storage/bridge/utils';
 import { truncateAddress } from '../../../utils/formatters';
+import { MultiToken } from '../../../api/common/lib/MultiToken';
+import { getDefaultEntryToken } from '../../../stores/toplevel/TokenInfoStore';
+import {
+  genFormatTokenAmount,
+  genLookupOrFail,
+  getTokenName,
+} from '../../../stores/stateless/tokenHelpers';
+import ReactToolboxMobxForm from '../../../utils/ReactToolboxMobxForm';
+import {
+  isLedgerNanoWallet,
+  isTrezorTWallet,
+} from '../../../api/ada/lib/storage/models/ConceptualWallet';
+import { asGetSigningKey } from '../../../api/ada/lib/storage/models/PublicDeriver/traits';
+import SpendingPasswordInput from '../../../components/widgets/forms/SpendingPasswordInput';
 
 const messages = defineMessages({
   dialogTitle: {
@@ -25,6 +39,10 @@ const messages = defineMessages({
   withdrawalAddress: {
     id: 'wallet.withdrawRewards.transaction.address',
     defaultMessage: '!!!Withdrawal address',
+  },
+  finalRewards: {
+    id: 'wallet.withdrawRewards.transaction.finalRewardsLabel',
+    defaultMessage: '!!!Final Rewards',
   },
 });
 
@@ -39,6 +57,84 @@ type Props = {
 export default class WithdrawRewardsDialog extends Component<Props> {
   static contextTypes: {| intl: $npm$ReactIntl$IntlFormat |} = {
     intl: intlShape.isRequired,
+  };
+
+  @observable spendingPasswordForm: void | ReactToolboxMobxForm;
+
+  @action
+  setSpendingPasswordForm(form: ReactToolboxMobxForm) {
+    this.spendingPasswordForm = form;
+  }
+
+  componentWillUnmount() {
+    this.generated.stores.wallets.sendMoneyRequest.reset();
+    this.generated.stores.substores.ada.delegationTransaction.createWithdrawalTx.reset();
+    this.generated.actions.ada.ledgerSend.cancel.trigger();
+    this.generated.actions.ada.trezorSend.cancel.trigger();
+  }
+
+  submit: void => Promise<void> = async () => {
+    const selected = this.generated.stores.wallets.selected;
+    if (selected == null) throw new Error(`${nameof(WithdrawRewardsDialog)} no wallet selected`);
+
+    if (this.spendingPasswordForm == null) {
+      if (this.props.transactionRequest.result == null) return;
+      const signRequest = this.generated.stores.substores.ada.delegationTransaction
+        .createWithdrawalTx.result;
+
+      if (isTrezorTWallet(selected.getParent())) {
+        await this.generated.actions.ada.trezorSend.sendUsingTrezor.trigger({
+          params: {
+            signRequest,
+          },
+          publicDeriver: selected,
+        });
+      }
+      if (isLedgerNanoWallet(selected.getParent())) {
+        await this.generated.actions.ada.ledgerSend.sendUsingLedgerWallet.trigger({
+          params: {
+            signRequest,
+          },
+          publicDeriver: selected,
+        });
+      }
+      // if (this.generated.stores.wallets.sendMoneyRequest.error == null) {
+      //   this.props.onSubmit.trigger();
+      // }
+    } else {
+      this.spendingPasswordForm.submit({
+        onSuccess: async form => {
+          const { walletPassword } = form.values();
+
+          if (this.props.transactionRequest.result == null) return;
+          await this.generated.actions.wallets.sendMoney.trigger({
+            signRequest: this.props.transactionRequest.result,
+            password: walletPassword,
+            publicDeriver: selected,
+          });
+          // if (this.generated.stores.wallets.sendMoneyRequest.error == null) {
+          //   this.props.onSubmit.trigger();
+          // }
+        },
+        onError: () => {},
+      });
+    }
+  };
+
+  getTotalBalance: (
+    recoveredBalance: MultiToken,
+    fee: MultiToken,
+    deregistrations: ?Array<{ +rewardAddress: string, +refund: MultiToken }>
+  ) => MultiToken = (recoveredBalance, fee, deregistrations) => {
+    const baseTotal = recoveredBalance.joinSubtractCopy(fee);
+    if (deregistrations === null) return baseTotal;
+
+    const refundSum = deregistrations.reduce(
+      (sum, curr) => (curr.refund == null ? sum : sum.joinAddCopy(curr.refund)),
+      new MultiToken([], recoveredBalance.defaults)
+    );
+
+    return baseTotal.joinAddCopy(refundSum);
   };
 
   render(): Node {
@@ -93,16 +189,48 @@ export default class WithdrawRewardsDialog extends Component<Props> {
     const defaultToken = this.generated.stores.tokenInfoStore.getDefaultTokenInfo(
       this.generated.stores.profile.selectedNetwork.NetworkId
     );
+    const ticker = getTokenName(defaultToken);
+    const formatValue = genFormatTokenAmount(
+      genLookupOrFail(this.generated.stores.tokenInfoStore.tokenInfo)
+    );
 
     const tentativeTx = createWithdrawalTx.result;
 
-    if (!tentativeTx) return 'no tx...';
+    if (!tentativeTx) return 'Loading...';
     const receivers = tentativeTx.receivers(true);
     const receiverAddress = addressToDisplayString(receivers[0], network);
+    const withdrawals = tentativeTx.withdrawals();
+    const deregistrations = tentativeTx.keyDeregistrations();
+
+    const recoveredBalance = withdrawals.reduce(
+      (sum, curr) => sum.joinAddCopy(curr.amount),
+      new MultiToken([], getDefaultEntryToken(defaultToken))
+    );
+    const txFee = tentativeTx.fee();
+    const finalRewards = this.getTotalBalance(recoveredBalance, txFee, deregistrations);
+
+    const withSigning = asGetSigningKey(publicDeriver);
+
+    const spendingPasswordForm =
+      withSigning == null ? undefined : (
+        <SpendingPasswordInput
+          setForm={form => this.setSpendingPasswordForm(form)}
+          classicTheme={false}
+          isSubmitting={this.generated.stores.wallets.sendMoneyRequest.isExecuting}
+        />
+      );
 
     return (
       <Dialog
         title={intl.formatMessage(messages.dialogTitle)}
+        actions={[
+          {
+            label: intl.formatMessage(globalMessages.withdrawLabel),
+            onClick: () => {},
+            primary: true,
+            disabled: true,
+          },
+        ]}
         closeOnOverlayClick={false}
         onClose={this.props.onClose}
         closeButton={<DialogCloseButton />}
@@ -154,15 +282,87 @@ export default class WithdrawRewardsDialog extends Component<Props> {
               <CopyAddress text={receiverAddress}>{truncateAddress(receiverAddress)}</CopyAddress>
             </Typography>
           </Box>
+
+          <Box
+            display="flex"
+            gap="16px"
+            justifyContent="space-between"
+            pt="24px"
+            mt="28px"
+            borderTop="1px solid"
+            borderColor="grayscale.200"
+          >
+            <Box minWidth="180px">
+              <Typography variant="body1" color="grayscale.600" mb="4px">
+                {intl.formatMessage(globalMessages.rewardsLabel)}
+              </Typography>
+              <Typography variant="body1" color="grayscale.900">
+                {formatValue(recoveredBalance.getDefaultEntry())} {ticker}
+              </Typography>
+            </Box>
+            <Box minWidth="180px">
+              <Typography variant="body1" color="grayscale.600" mb="4px">
+                {intl.formatMessage(globalMessages.feeLabel)}
+              </Typography>
+              <Typography variant="body1" color="grayscale.900">
+                {formatValue(txFee.getDefaultEntry())} {ticker}
+              </Typography>
+            </Box>
+            <Box minWidth="180px">
+              <Typography variant="body1" color="grayscale.600" mb="4px">
+                {intl.formatMessage(messages.finalRewards)}
+              </Typography>
+              <Typography variant="body1" color="grayscale.900">
+                {formatValue(finalRewards.getDefaultEntry())} {ticker}
+              </Typography>
+            </Box>
+          </Box>
+
+          <Box mt="24px">{spendingPasswordForm}</Box>
         </Box>
       </Dialog>
     );
   }
 
   @computed get generated(): {|
+    actions: {|
+      ada: {|
+        ledgerSend: {|
+          sendUsingLedgerWallet: {|
+            trigger: (params: {|
+              params: SendUsingLedgerParams,
+              publicDeriver: PublicDeriver<>,
+              onSuccess?: void => void,
+            |}) => Promise<void>,
+          |},
+          cancel: {| trigger: (params: void) => void |},
+        |},
+        trezorSend: {|
+          sendUsingTrezor: {|
+            trigger: (params: {|
+              params: SendUsingTrezorParams,
+              publicDeriver: PublicDeriver<>,
+              onSuccess?: void => void,
+            |}) => Promise<void>,
+          |},
+          cancel: {| trigger: (params: void) => void |},
+        |},
+        wallets: {|
+          sendMoney: {|
+            trigger: (params: {|
+              password: string,
+              publicDeriver: PublicDeriver<>,
+              signRequest: ISignRequest<any>,
+              onSuccess?: void => void,
+            |}) => Promise<void>,
+          |},
+        |},
+      |},
+    |},
     stores: {|
       tokenInfoStore: {|
         getDefaultTokenInfo: number => $ReadOnly<TokenRow>,
+        tokenInfo: TokenInfoMap,
       |},
       profile: {| selectedNetwork: void | $ReadOnly<NetworkRow> |},
       substores: {|
@@ -177,7 +377,14 @@ export default class WithdrawRewardsDialog extends Component<Props> {
           |},
         |},
       |},
-      wallets: {| selected: null | PublicDeriver<> |},
+      wallets: {|
+        selected: null | PublicDeriver<>,
+        sendMoneyRequest: {|
+          error: ?LocalizableError,
+          isExecuting: boolean,
+          reset: () => void,
+        |},
+      |},
       delegation: {|
         selectedPage: number,
         getLocalPoolInfo: ($ReadOnly<NetworkRow>, string) => void | PoolMeta,
@@ -194,12 +401,38 @@ export default class WithdrawRewardsDialog extends Component<Props> {
     const { stores, actions } = this.props;
 
     return Object.freeze({
+      actions: {
+        wallets: {
+          sendMoney: {
+            trigger: actions.wallets.sendMoney.trigger,
+          },
+        },
+        ada: {
+          ledgerSend: {
+            sendUsingLedgerWallet: {
+              trigger: actions.ada.ledgerSend.sendUsingLedgerWallet.trigger,
+            },
+            cancel: {
+              trigger: actions.ada.ledgerSend.cancel.trigger,
+            },
+          },
+          trezorSend: {
+            sendUsingTrezor: {
+              trigger: actions.ada.trezorSend.sendUsingTrezor.trigger,
+            },
+            cancel: {
+              trigger: actions.ada.trezorSend.cancel.trigger,
+            },
+          },
+        },
+      },
       stores: {
         profile: {
           selectedNetwork: stores.profile.selectedNetwork,
         },
         tokenInfoStore: {
           getDefaultTokenInfo: stores.tokenInfoStore.getDefaultTokenInfo,
+          tokenInfo: stores.tokenInfoStore.tokenInfo,
         },
         substores: {
           ada: {
@@ -215,6 +448,11 @@ export default class WithdrawRewardsDialog extends Component<Props> {
         },
         wallets: {
           selected: stores.wallets.selected,
+          sendMoneyRequest: {
+            reset: stores.wallets.sendMoneyRequest.reset,
+            error: stores.wallets.sendMoneyRequest.error,
+            isExecuting: stores.wallets.sendMoneyRequest.isExecuting,
+          },
         },
         delegation: {
           selectedPage: stores.delegation.selectedPage,
