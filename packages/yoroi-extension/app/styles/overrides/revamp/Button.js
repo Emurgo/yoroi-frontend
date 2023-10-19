1c4567541142d5ad93d73755b8353a19a3682ed9
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-10-19 10:27:10 +0300

    fix(send): fix the wallet send amount input

--- packages/yoroi-extension/app/styles/overrides/revamp/Button.js
+++ packages/yoroi-extension/app/styles/overrides/revamp/Button.js
@@ -17,7 +17,7 @@ const RevampButtonCommonProps: Object = {
       '&.MuiButton-sizeLarge': { height: '56px' },
       '&.MuiButton-sizeMedium': { padding: '10px' },
       '&.MuiButton-sizeSmall': { padding: '7px' },
-      '&.MuiButton-sizeFlat': { padding: '13px 24px', hight: 'unset' },
+      '&.MuiButton-sizeFlat': { padding: '13px 24px', height: 'unset' },
       '&:hover': { boxShadow: 'none' },
       '& span.MuiButton-startIcon': {
         marginLeft: '0px',
