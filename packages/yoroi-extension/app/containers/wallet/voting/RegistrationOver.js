c93e882dbfa4d49f0f8fb80b2018bb433a7e32e2
Ahmed Ibrahim <me@ahmedibrahim.dev>
2023-11-02 10:47:10 +0200

    feat(voting): revamp voting is not available screen

--- packages/yoroi-extension/app/containers/wallet/voting/RegistrationOver.js
+++ packages/yoroi-extension/app/containers/wallet/voting/RegistrationOver.js
@@ -1,24 +1,28 @@
 // @flow
-import type { Node } from 'react';
+import type { Node, ComponentType } from 'react';
 import { Component } from 'react';
 import FullscreenMessage from '../../../components/wallet/layouts/FullscreenMessage';
+import { ReactComponent as RegistrationNotAvailable } from '../../../assets/images/revamp/registration-is-not-available.inline.svg';
 import { observer } from 'mobx-react';
-
+import { withLayout } from '../../../styles/context/layout';
+import type { InjectedLayoutProps } from '../../../styles/context/layout';
 
 type Props = {|
-  title: string,
-  subtitle: string,
+  title: string | Node,
+  subtitle: string | Node,
 |};
 
 @observer
-export default class RegistrationOver extends Component<Props> {
-
+class RegistrationOver extends Component<Props & InjectedLayoutProps> {
   render(): Node {
     return (
       <FullscreenMessage
+        image={<RegistrationNotAvailable />}
         title={this.props.title}
         subtitle={this.props.subtitle}
       />
     );
   }
 }
+
+export default (withLayout(RegistrationOver): ComponentType<Props>);
