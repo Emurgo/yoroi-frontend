0e180617bfa573176e8b0b3290c63d21434f4e53
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2023-12-09 23:21:17 +0300

    removed ergo mentions

--- packages/yoroi-extension/chrome/extension/connector/index.js
+++ packages/yoroi-extension/chrome/extension/connector/index.js
@@ -1,13 +1,16 @@
 // @flow
 
 import { render } from 'react-dom';
+// eslint-disable-next-line no-unused-vars
 import { action, configure } from 'mobx';
 import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
 import { createHashHistory } from 'history';
 import { setupApi } from '../../../app/api/index';
 import createStores from '../../../app/connector/stores/index';
+// eslint-disable-next-line no-unused-vars
 import { translations } from '../../../app/i18n/translations';
 import actions from '../../../app/connector/actions/index';
+// eslint-disable-next-line no-unused-vars
 import { Action } from '../../../app/actions/lib/Action';
 import App from '../../../app/connector/App';
 import BigNumber from 'bignumber.js';
@@ -23,25 +26,26 @@ configure({ enforceActions: 'always' });
 BigNumber.DEBUG = true;
 
 // Entry point into our application
-const initializeErgoConnector: void => Promise<void> = async () => {
+const initializeDappConnector: void => Promise<void> = async () => {
   const api = await setupApi();
   const router = new RouterStore();
   const hashHistory = createHashHistory();
   const history = syncHistoryWithStore(hashHistory, router);
   const stores = createStores(api, actions);
 
-  window.ergo = {
-    api,
-    actions,
-    translations,
-    stores,
-    reset: action(() => {
-      Action.resetAllActions();
-      createStores(api, actions);
-    }),
-  };
+  // <TODO:PENDING_REMOVAL> ??? USAGE?
+  // window.ergo = {
+  //   api,
+  //   actions,
+  //   translations,
+  //   stores,
+  //   reset: action(() => {
+  //     Action.resetAllActions();
+  //     createStores(api, actions);
+  //   }),
+  // };
 
-  const root = document.querySelector('#root-ergo');
+  const root = document.querySelector('#root-yoroi-connector');
   if (root == null) {
     throw new Error('Root element not found.');
   }
@@ -64,6 +68,6 @@ const initializeErgoConnector: void => Promise<void> = async () => {
   render(<App stores={stores} actions={actions} history={history} />, root);
 };
 
-addCloseListener(TabIdKeys.ErgoConnector);
+addCloseListener(TabIdKeys.YoroiConnector);
 
-window.addEventListener('load', initializeErgoConnector);
+window.addEventListener('load', initializeDappConnector);
