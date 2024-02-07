e73d5dc14955ce3feaf1371b664a53bbe060a0fb
Nebytov Denis <nebytov.d@gmail.com>
2024-02-07 14:31:19 +0300

    removed outdated tests and environment for it

--- packages/yoroi-extension/features/pages/trezorConnectPage.js
+++
@@ -1,7 +0,0 @@
-// @flow
-
-import type { LocatorObject } from '../support/webdriver';
-
-export const dontAskAgainCheckbox: LocatorObject = { locator: '.custom-checkbox', method: 'css' };
-export const confirmUsingTrezorButton: LocatorObject = { locator: '.confirm', method: 'css' };
-export const exportTrezorButton: LocatorObject = { locator: '.confirm', method: 'css' };
