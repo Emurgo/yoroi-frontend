cc6b69d0619f2acd213b060b8589b4c7e7d2ef76
Sorin Chis <kish.sorin@yahoo.com>
2024-03-18 17:46:05 +0200

    fix checkbox color

--- packages/yoroi-extension/app/styles/overrides/revamp/Checkbox.js
+++ packages/yoroi-extension/app/styles/overrides/revamp/Checkbox.js
@@ -6,7 +6,7 @@ const RevampCheckbox: any = {
       padding: 0,
       marginRight: '18px',
       borderRadius: '2px',
-      color: theme.palette.primary[500],
+      color: theme.palette.grayscale[800],
       '&.Mui-checked': { color: theme.palette.primary[500] },
       '&.Mui-disabled': {
         color: theme.palette.grayscale[400],
