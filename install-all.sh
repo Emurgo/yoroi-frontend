ffdb4ac4dd6eb47aa284ba8ac8f88383ae952099
Nebytov Denis <nebytov.d@gmail.com>
2024-04-14 19:04:21 +0300

    update install-all script

--- install-all.sh
+++ install-all.sh
@@ -1,4 +1,4 @@
 npm i --legacy-peer-deps \
-&& npm i --legacy-peer-deps --prefix packages/yoroi-extension
+&& npm i --legacy-peer-deps --prefix packages/yoroi-extension \
 && npm i --legacy-peer-deps --prefix packages/e2e-tests
 
