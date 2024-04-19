342b93470ff1bb29771badabb1d8b8a9417e10bd
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2024-04-19 23:26:15 +0300

    lint fixes

--- packages/yoroi-extension/app/stores/ada/SwapStore.js
+++ packages/yoroi-extension/app/stores/ada/SwapStore.js
@@ -15,13 +15,14 @@ import { Quantities } from '../../utils/quantities';
 import BigNumber from 'bignumber.js';
 import { HaskellShelleyTxSignRequest } from '../../api/ada/transactions/shelley/HaskellShelleyTxSignRequest';
 import { cast, fail, hexToBytes, noop } from '../../coreUtils';
-import { asAddressedUtxo, cardanoUtxoHexFromRemoteFormat, signTransactionHex } from '../../api/ada/transactions/utils';
+import { asAddressedUtxo, cardanoUtxoHexFromRemoteFormat} from '../../api/ada/transactions/utils';
 import { genLookupOrFail, getTokenIdentifierIfExists, getTokenName } from '../stateless/tokenHelpers';
 import { splitAmount, truncateToken } from '../../utils/formatters';
 import adaLogo from '../../containers/swap/mockAssets/ada.inline.svg';
 import type { AssetAmount } from '../../components/swap/types';
 import type { QueriedUtxo } from '../../api/ada/lib/storage/models/PublicDeriver/interfaces';
 import { transactionHexToHash } from '../../api/ada/lib/cardanoCrypto/utils';
+import { signTransactionHex } from '../../api/ada/transactions/signTransactionHex';
 
 const FRONTEND_FEE_ADDRESS_MAINNET = 'addr1q9ry6jfdgm0lcrtfpgwrgxg7qfahv80jlghhrthy6w8hmyjuw9ngccy937pm7yw0jjnxasm7hzxjrf8rzkqcj26788lqws5fke';
 const FRONTEND_FEE_ADDRESS_PREPROD = 'addr_test1qrgpjmyy8zk9nuza24a0f4e7mgp9gd6h3uayp0rqnjnkl54v4dlyj0kwfs0x4e38a7047lymzp37tx0y42glslcdtzhqzp57km';
