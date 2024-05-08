9f9842dfd11547447735a66b4d56d42de83a23f8
Nebytov Denis <nebytov.d@gmail.com>
2024-05-08 14:18:24 +0300

    added error message

--- packages/e2e-tests/helpers/messages.js
+++ packages/e2e-tests/helpers/messages.js
@@ -3,3 +3,4 @@ export const PASSWORD_TOO_SHORT = 'Password requires at least 10 characters.';
 export const PASSWORDS_DONT_MATCH = 'Passwords do not match. Please retype.';
 export const MAX_ALLOWED_UNUSED_ADDRS =
   'You cannot generate more than 20 consecutive unused addresses.';
+export const INVALID_AMOUNT = 'Invalid amount. Please retype.';
