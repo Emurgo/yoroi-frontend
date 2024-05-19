5cdc63b176a471d45d64d4f534ee53283dcde086
Nebytov Denis <nebytov.d@gmail.com>
2024-05-20 00:59:12 +0300

    added blockchain page

---
+++ packages/e2e-tests/pages/wallet/settingsTab/blockchainSubTab.page.js
@@ -0,0 +1,34 @@
+import SettingsTab from './settingsTab.page.js';
+
+class BlockchainSubTab extends SettingsTab {
+  // locators
+  explorersDropDownLocator = {
+    locator: '//div[starts-with(@id, "explorerId--")]',
+    method: 'xpath',
+  };
+  getExplorerMenuItem = explorer => {
+    return {
+      locator: `selectExplorer-${explorer}-menuItem`,
+      method: 'id',
+    };
+  };
+  // methods
+  async openExplorerSelection() {
+    this.logger.info(`BlockchainSubTab::openExplorerSelection is called`);
+    await this.waitForElement(this.explorersDropDownLocator);
+    await this.click(this.explorersDropDownLocator);
+  }
+  async pickExplorer(explorer) {
+    this.logger.info(`BlockchainSubTab::pickExplorer is called. Explorer: "${explorer}"`);
+    const explorerLocator = this.getExplorerMenuItem(explorer);
+    await this.scrollIntoView(explorerLocator);
+    await this.click(explorerLocator);
+  }
+  async selectExplorer(explorer) {
+    this.logger.info(`BlockchainSubTab::selectExplorer is called. Explorer: "${explorer}"`);
+    await this.openExplorerSelection();
+    await this.pickExplorer(explorer);
+  }
+}
+
+export default BlockchainSubTab;
