549b2a191dcde8c4a1fd4bb7153ae523a73cabcc
Nebytov Denis <nebytov.d@gmail.com>
2024-05-28 12:58:52 +0300

    moved exportTransactionsModal to a separate file

--- packages/e2e-tests/pages/wallet/walletTab/walletTransactions.page.js
+++ packages/e2e-tests/pages/wallet/walletTab/walletTransactions.page.js
@@ -6,119 +6,8 @@ import {
 } from '../../../helpers/timeConstants.js';
 import BasePage from '../../basepage.js';
 import WalletTab from './walletTab.page.js';
+import ExportTransactionsModal from './transactionsModals/exportTransactionModal.page.js';
 import { convertPrettyDateToNormal, convertPrettyTimeToNormal } from '../../../utils/utils.js';
-
-class ExportTransactionsModal extends BasePage {
-  // locators
-  exportDialogWindowLocator = {
-    locator: 'exportTransactionsDialog-dialogWindow-modalWindow',
-    method: 'id',
-  };
-  exportDialogTitleLocator = {
-    locator: 'exportTransactionsDialog-dialogTitle-text',
-    method: 'id',
-  };
-  exportStartDateInputLocator = {
-    locator: '//div[contains(@class, "exportTransactionsDialog-startDate-datePicker")]/div/input',
-    method: 'xpath',
-  };
-  exportStartDateFieldsetLocator = {
-    locator:
-      '//div[contains(@class, "exportTransactionsDialog-startDate-datePicker")]/div/fieldset',
-    method: 'xpath',
-  };
-  exportEndDateInputLocator = {
-    locator: '//div[contains(@class, "exportTransactionsDialog-endDate-datePicker")]/div/input',
-    method: 'xpath',
-  };
-  exportEndDateFiedlsetInputLocator = {
-    locator: '//div[contains(@class, "exportTransactionsDialog-endDate-datePicker")]/div/fieldset',
-    method: 'xpath',
-  };
-  includeTxIdCheckboxLocator = {
-    locator: 'exportTransactionsDialog-includeTxIds-checkbox',
-    method: 'id',
-  };
-  exportTransactionsButtonLocator = {
-    locator: 'exportTransactionsDialog-export-button',
-    method: 'id',
-  };
-  exportErrorMessageLocator = {
-    locator: '.ErrorBlock_component',
-    method: 'css',
-  };
-  // methods
-  async isDisplayed() {
-    this.logger.info(`ExportTransactionsModal::isDisplayed is called`);
-    try {
-      await this.findElement(this.exportDialogWindowLocator);
-      await this.findElement(this.exportDialogTitleLocator);
-      await this.findElement(this.exportStartDateInputLocator);
-      await this.findElement(this.exportEndDateInputLocator);
-
-      return true;
-    } catch (error) {
-      this.logger.warn(
-        `ExportTransactionsModal::isDisplayed there is something wrong with Export Transaction Dialog`
-      );
-      return false;
-    }
-  }
-  async setStartDate(dateString) {
-    this.logger.info(`ExportTransactionsModal::setStartDate is called`);
-    await this.click(this.exportStartDateInputLocator);
-    await this.input(this.exportStartDateInputLocator, dateString);
-  }
-  async checkStartDateErrorMsg() {
-    this.logger.info(`ExportTransactionsModal::checkStartDateErrorMsg is called`);
-    throw new Error('The function is not implemented yet');
-  }
-  async setEndDate(dateString) {
-    this.logger.info(`ExportTransactionsModal::setEndDate is called`);
-    await this.click(this.exportEndDateInputLocator);
-    await this.input(this.exportEndDateInputLocator, dateString);
-  }
-  async checkEndDateErrorMsg() {
-    this.logger.info(`ExportTransactionsModal::checkEndDateErrorMsg is called`);
-    throw new Error('The function is not implemented yet');
-  }
-  async clickIncludeTxsIDs() {
-    this.logger.info(`ExportTransactionsModal::tickIncludeTxsIDs is called`);
-    await this.click(this.includeTxIdCheckboxLocator);
-  }
-  async exportButtonIsEnabled() {
-    const buttonIsEnabled = await this.customWaiter(
-      async () => {
-        const buttonlIsEnabled = await this.getAttribute(
-          this.exportTransactionsButtonLocator,
-          'disabled'
-        );
-        return buttonlIsEnabled === null;
-      },
-      twoSeconds,
-      quarterSecond
-    );
-
-    return buttonIsEnabled;
-  }
-  async exportTransactionsFile() {
-    this.logger.info(`ExportTransactionsModal::exportTransactionsFile is called`);
-    await this.click(this.exportTransactionsButtonLocator);
-    await this.sleep(twoSeconds + twoSeconds);
-  }
-  async getStartDateInputBorderColor() {
-    this.logger.info(`ExportTransactionsModal::getStartDateInputBorderColor is called`);
-    return await this.getCssValue(this.exportStartDateFieldsetLocator, 'border-color');
-  }
-  async getEndDateInputBorderColor() {
-    this.logger.info(`ExportTransactionsModal::getEndDateInputBorderColor is called`);
-    return await this.getCssValue(this.exportEndDateFiedlsetInputLocator, 'border-color');
-  }
-  async getErrorMessage() {
-    this.logger.info(`ExportTransactionsModal::getErrorMessage is called`);
-    return await this.getText(this.exportErrorMessageLocator);
-  }
-}
 
 export class TransactionsSubTab extends WalletTab {
   // locators
@@ -437,6 +326,7 @@ export class TransactionsSubTab extends WalletTab {
     }
   }
   async waitLoaderIsNotDisplayed(timeout, repearPeriod) {
+    this.logger.info(`TransactionsSubTab::waitLoaderIsNotDisplayed is called`);
     const loaderIsNotDisplayed = await this.customWaiter(
       async () => {
         const displayed = await this.loaderIsDisplayed();
@@ -445,10 +335,14 @@ export class TransactionsSubTab extends WalletTab {
       timeout,
       repearPeriod
     );
+    this.logger.info(
+      `TransactionsSubTab::waitLoaderIsNotDisplayed::loaderIsNotDisplayed ${loaderIsNotDisplayed}`
+    );
 
     return loaderIsNotDisplayed;
   }
   async downloadAllTxs() {
+    this.logger.info(`TransactionsSubTab::downloadAllTxs is called`);
     while (true) {
       const showMoreIsDisplayed = this.showMoreBtnIsDisplayed();
       const loaderIsDisplayed = this.loaderIsDisplayed();
@@ -471,6 +365,7 @@ export class TransactionsSubTab extends WalletTab {
     }
   }
   async __getAddrsLinks(groupIndex, txIndex, addrsAmount, getLocatorFunc) {
+    this.logger.info(`TransactionsSubTab::__getAddrsLinks is called`);
     const links = [];
 
     for (let addrIndex = 0; addrIndex < addrsAmount; addrIndex++) {
@@ -481,15 +376,24 @@ export class TransactionsSubTab extends WalletTab {
 
     return links;
   }
+  async clickOnTxRow(groupIndex, txIndex) {
+    this.logger.info(
+      `TransactionsSubTab::clickOnTxRow is called. Group index: ${groupIndex}, tx index: ${txIndex}`
+    );
+    const txRowLocator = this.txRowLocator(groupIndex, txIndex);
+    await this.click(txRowLocator);
+  }
   /**
    * Getting all links as strings from a selected tx
-   * @param {number} groupIndex 
-   * @param {number} txIndex 
+   * @param {number} groupIndex
+   * @param {number} txIndex
    * @returns {Promise<{fromAddrsLinks: string[], toAddrsLinks: string[], txLink: string[]}>}
    */
   async getTxURLs(groupIndex, txIndex) {
-    const txRowLocator = this.txRowLocator(groupIndex, txIndex);
-    await this.click(txRowLocator);
+    this.logger.info(
+      `TransactionsSubTab::getTxURLs is called. Group index: ${groupIndex}, tx index: ${txIndex}`
+    );
+    await this.clickOnTxRow(groupIndex, txIndex);
     // from addresses
     const amountFromAddrs = await this.__getAmountOfFromAddresses(groupIndex, txIndex);
     const fromAddrsLinks = await this.__getAddrsLinks(
@@ -516,6 +420,13 @@ export class TransactionsSubTab extends WalletTab {
       toAddrsLinks,
       txLink,
     };
+  }
+  async clickAddMemo(groupIndex, txIndex) {
+    this.logger.info(
+      `TransactionsSubTab::clickAddMemo is called. Group index: ${groupIndex}, tx index: ${txIndex}`
+    );
+    const addMemoBtnLocator = this.txAddMemoButtonLocator(groupIndex, txIndex);
+    await this.click(addMemoBtnLocator);
   }
 }
 
