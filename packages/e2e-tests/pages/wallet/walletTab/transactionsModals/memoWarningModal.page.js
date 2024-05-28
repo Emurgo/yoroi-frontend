7bda2daa45b383830ec22cf8817ac94a0b2f9abe
Nebytov Denis <nebytov.d@gmail.com>
2024-05-28 22:47:09 +0300

    prettified memoWarningModal page

--- packages/e2e-tests/pages/wallet/walletTab/transactionsModals/memoWarningModal.page.js
+++ packages/e2e-tests/pages/wallet/walletTab/transactionsModals/memoWarningModal.page.js
@@ -1,35 +1,36 @@
 import BasePage from '../../../basepage.js';
+import AddMemoDialog from './addMemoModal.page.js';
 
 class MemoWarningModal extends BasePage {
-    // locators
+  // locators
   // * modal window
   memoWarningDialogWindowLocator = {
     locator: 'memoNoExternalStorageDialog-dialogWindow-modalWindow',
     method: 'id',
-  }
+  };
   // * modal title
   memoWarningDialogTitleLocator = {
     locator: 'memoNoExternalStorageDialog-dialogTitle-text',
     method: 'id',
-  }
+  };
   // * modal close button
   memoWarningDialogCloseButtonLocator = {
     locator: 'memoNoExternalStorageDialog-closeModal-button',
     method: 'id',
-  }
+  };
   // * cancelBtn
   cancelButtonLocator = {
     locator: 'memoNoExternalStorageDialog-cancel-button',
     method: 'id',
-  }
+  };
   // * understandBtn
   undesrtandButtonLocator = {
     locator: 'memoNoExternalStorageDialog-iunderstand-button',
     method: 'id',
-  }
+  };
   // methods
   // * isDisplayed
-  async isDisplayed () {
+  async isDisplayed() {
     this.logger.info(`MemoWarningModal::isDisplayed is called`);
     try {
       await this.waitForElement(this.memoWarningDialogWindowLocator);
@@ -40,19 +41,20 @@ class MemoWarningModal extends BasePage {
     }
   }
   // * cancel
-  async pressCancel () {
+  async pressCancel() {
     this.logger.info(`MemoWarningModal::pressCancel is called`);
     await this.click(this.cancelButtonLocator);
   }
   // * close
-  async closeWarningDialog () {
+  async closeWarningDialog() {
     this.logger.info(`MemoWarningModal::closeWarningDialog is called`);
     await this.click(this.memoWarningDialogCloseButtonLocator);
   }
   // * confirm
-  async pressUnderstand () {
+  async pressUnderstand() {
     this.logger.info(`MemoWarningModal::pressUnderstand is called`);
     await this.click(this.undesrtandButtonLocator);
+    return new AddMemoDialog(this.driver, this.logger);
   }
 }
 
