6fde6cc26a7733a59135b4c9448ae9a29eeef460
ndnghiadn <ndnghiadn@gmail.com>
2024-06-10 10:31:09 +0700

    update module and tsconfig

--- packages/yoroi-extension/.d.ts
+++ packages/yoroi-extension/.d.ts
@@ -1,3 +1,10 @@
 declare module 'react-intl' {
   export const value: any;
+  export const defineMessages: any;
+  export const FormattedMessage: any;
 }
+
+declare module '*.png';
+declare module '*.svg';
+declare module '*.jpeg';
+declare module '*.jpg';
