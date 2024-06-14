7c397a8ba5454c695ae9186bb1ebd55c1e300061
yushi <yushihuang@gmail.com>
2024-06-14 12:02:30 +0800

    flow fix

--- packages/yoroi-extension/app/stores/toplevel/WalletStore.js
+++ packages/yoroi-extension/app/stores/toplevel/WalletStore.js
@@ -32,7 +32,7 @@ import type { ActionsMap } from '../../actions/index';
 import type { StoresMap } from '../index';
 import { getWalletChecksum } from '../../api/export/utils';
 import { MultiToken } from '../../api/common/lib/MultiToken';
-import { BigNumber, ROUND_UP } from 'bignumber.js';
+import { BigNumber } from 'bignumber.js';
 
 type GroupedWallets = {|
   publicDerivers: Array<PublicDeriver<>>,
@@ -360,7 +360,7 @@ export default class WalletStore extends Store<StoresMap, ActionsMap> {
             [{
               identifier: defaultToken.defaultIdentifier,
               networkId: defaultToken.defaultNetworkId,
-              amount: (new BigNumber(sellAdaParams.amount)).shiftedBy(6).decimalPlaces(0, ROUND_UP),
+              amount: (new BigNumber(sellAdaParams.amount)).shiftedBy(6).decimalPlaces(0, 0/* ROUND_UP */),
             }],
             defaultToken,
           ),
