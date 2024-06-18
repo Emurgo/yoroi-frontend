e7cd17c7b5bb9da22407bd26dbb1bf53fcf4041c
Nebytov Denis <nebytov.d@gmail.com>
2024-06-18 23:31:52 +0300

    minor fix

--- packages/e2e-tests/helpers/mock-dApp-webpage/dAppTxHelper.js
+++ packages/e2e-tests/helpers/mock-dApp-webpage/dAppTxHelper.js
@@ -1,6 +1,6 @@
 import BigNumber from 'bignumber.js';
 import * as CSL from '@emurgo/cardano-serialization-lib-nodejs';
-import { protocolParams } from './networkConfig';
+import { protocolParams } from './networkConfig.js';
 
 export function getTtl() {
   const fullConfig = [
