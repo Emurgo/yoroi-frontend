41a5a434ca91c4cc500bb930b6dbbc52d999674a
Denis Nebytov <nebytov.d@gmail.com>
2024-07-11 20:56:43 +0300

    added ids

--- packages/yoroi-extension/app/components/wallet/send/WalletSendFormRevamp.js
+++ packages/yoroi-extension/app/components/wallet/send/WalletSendFormRevamp.js
@@ -649,6 +649,7 @@ export default class WalletSendFormRevamp extends Component<Props, State> {
                   variant="caption1"
                   color={invalidMemo ? 'magenta.500' : 'grayscale.600'}
                   sx={{ position: 'absolute', bottom: '10px', right: '0' }}
+                  id='wallet:send:enterAddressStep-domainResolverAddress-text'
                 >
                   {intl.formatMessage(messages.receiverFieldLabelResolvedAddress)}:&nbsp;
                   {truncateAddressShort(domainResolverResult.address)}
