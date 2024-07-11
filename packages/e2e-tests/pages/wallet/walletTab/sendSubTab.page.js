0d4a787202294cf73860bbc75fd510505a11c16f
Denis Nebytov <nebytov.d@gmail.com>
2024-07-11 21:05:22 +0300

    added locators and methods required for tests

--- packages/e2e-tests/pages/wallet/walletTab/sendSubTab.page.js
+++ packages/e2e-tests/pages/wallet/walletTab/sendSubTab.page.js
@@ -1,13 +1,30 @@
 import WalletTab from './walletTab.page.js';
-import { twoSeconds, quarterSecond } from '../../../helpers/timeConstants.js';
+import { twoSeconds, quarterSecond, fiveSeconds } from '../../../helpers/timeConstants.js';
 
 class SendSubTab extends WalletTab {
   // locators
+  // ------
   // step 1
   // ------
   receiverAddressInputLocator = {
     locator: '//input[starts-with(@id, "receiver--")]',
     method: 'xpath',
+  };
+  receiverHelperTextLocator = {
+    locator: '//p[starts-with(@id, "receiver--") and contains(@id, "-helper-text")]',
+    method: 'xpath',
+  };
+  receiverDoneIcon = {
+    locator: 'input-done-icon',
+    method: 'id',
+  };
+  receiverErrorIcon = {
+    locator: 'input-error-icon',
+    method: 'id',
+  };
+  domainResolverAddressTextLocator = {
+    locator: 'wallet:send:enterAddressStep-domainResolverAddress-text',
+    method: 'id',
   };
   memoInputLocator = {
     locator: 'wallet:send:enterAddressStep-enterMemo-input',
@@ -45,6 +62,14 @@ class SendSubTab extends WalletTab {
   // ------
   // step 3
   // ------
+  receiverHandleBoxLocator = {
+    locator: 'wallet:send:confrimTransactionStep-receiverHandle-box',
+    method: 'id',
+  };
+  receiverHandleInfoTextLocator = {
+    locator: 'wallet:send:confrimTransactionStep-receiverHandleInfo-text',
+    method: 'id',
+  }
   receiverAddressTextLocator = {
     locator: 'wallet:send:confrimTransactionStep-receiverAddress-text',
     method: 'id',
@@ -77,8 +102,21 @@ class SendSubTab extends WalletTab {
     locator: 'wallet:send:confrimTransactionStep-confirmTransaction-button',
     method: 'id',
   };
-  // ------
   // functions
+  async stepOneIsDisplayed() {
+    this.logger.info(`SendSubTab::stepOneIsDisplayed is called.`);
+    const receiverState = await this.customWaitIsPresented(
+      this.receiverAddressInputLocator,
+      fiveSeconds,
+      quarterSecond
+    );
+    const memoState = await this.customWaitIsPresented(
+      this.memoInputLocator,
+      fiveSeconds,
+      quarterSecond
+    );
+    return receiverState && memoState;
+  }
   async buttonIsEnabled(locator) {
     const buttonIsEnabled = await this.customWaiter(
       async () => {
@@ -91,23 +129,49 @@ class SendSubTab extends WalletTab {
 
     return buttonIsEnabled;
   }
+  async enterReceiver(receiver) {
+    this.logger.info(`SendSubTab::enterReceiver is called. Receiver: ${receiver}`);
+    await this.click(this.receiverAddressInputLocator);
+    await this.clearInputAll(this.receiverAddressInputLocator);
+    await this.input(this.receiverAddressInputLocator, receiver);
+  }
+  async receiverIsGood() {
+    this.logger.info(`SendSubTab::receiverIsGood is called.`);
+    return await this.customWaitIsPresented(this.receiverDoneIcon, fiveSeconds, quarterSecond);
+  }
+  async getReceiverHelperText() {
+    this.logger.info(`SendSubTab::getReceiverHelperText is called.`);
+    return await this.getText(this.receiverHelperTextLocator);
+  }
+  async getReceiverHandlerAddress() {
+    this.logger.info(`SendSubTab::getReceiverHandlerAddress is called.`);
+    return await this.getText(this.domainResolverAddressTextLocator)
+  }
+  async enterMemo(memo) {
+    this.logger.info(`SendSubTab::enterMemo is called. Memo: ${memo}`);
+    await this.click(this.memoInputLocator);
+    await this.clearInputAll(this.memoInputLocator);
+    await this.input(this.memoInputLocator, memo);
+  }
+  async clickNextToStep2() {
+    this.logger.info(`SendSubTab::clickNextToStep2 is called.`);
+    const nextButtonIsEnabled = await this.buttonIsEnabled(this.nextToStep2ButtonLocator);
+    if (nextButtonIsEnabled) {
+      await this.click(this.nextToStep2ButtonLocator);
+    } else {
+      throw new Error('The Next button is disabled');
+    }
+  }
   async enterReceiverAndMemo(receiver, memo = '', confirm = true) {
     this.logger.info(
-      `SendSubTab::enterReceiver is called. ` + `Receiver: ${receiver}, memo: ${memo}`
+      `SendSubTab::enterReceiverAndMemo is called. ` + `Receiver: ${receiver}, memo: ${memo}`
     );
-    await this.click(this.receiverAddressInputLocator);
-    await this.input(this.receiverAddressInputLocator, receiver);
+    await this.enterReceiver(receiver);
     if (memo) {
-      await this.click(this.memoInputLocator);
-      await this.input(this.memoInputLocator, memo);
+      await this.enterMemo(memo);
     }
     if (confirm) {
-      const nextButtonIsEnabled = await this.buttonIsEnabled(this.nextToStep2ButtonLocator);
-      if (nextButtonIsEnabled) {
-        await this.click(this.nextToStep2ButtonLocator);
-      } else {
-        throw new Error('The Next button is disabled');
-      }
+      await this.clickNextToStep2();
     }
   }
   // It will require passing an object { <asset1Name>: <asset1Amount>, <asset2Name>: <asset2Amount> }
@@ -130,6 +194,10 @@ class SendSubTab extends WalletTab {
   async getInfoFromConfirmTxPage() {
     this.logger.info(`SendSubTab::getInfoFromConfirmTxPage is called.`);
     throw new Error('The function is in development');
+  }
+  async getHandlerInfoConfirmTxPage() {
+    this.logger.info(`SendSubTab::getHandlerInfoConfirmTxPage is called.`);
+    return await this.getText(this.receiverHandleInfoTextLocator);
   }
   async confirmTransaction(password, isHW = false) {
     this.logger.info(`SendSubTab::confirmTransaction is called. ` + `Password: ${password}`);
