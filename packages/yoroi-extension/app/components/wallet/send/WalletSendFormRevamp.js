c6ba1d6dfe12242d0d0b06bb2913c8ee90ed4c20
Denis Nebytov <nebytov.d@gmail.com>
2024-07-15 19:02:55 +0300

    added id

--- packages/yoroi-extension/app/components/wallet/send/WalletSendFormRevamp.js
+++ packages/yoroi-extension/app/components/wallet/send/WalletSendFormRevamp.js
@@ -867,6 +867,7 @@ export default class WalletSendFormRevamp extends Component<Props, State> {
                     color: 'magenta.500',
                     fontSize: '12px',
                   }}
+                  id='wallet:send:addAssetsStep-amountError-text'
                 >
                   {amountInputError}
                 </Typography>
