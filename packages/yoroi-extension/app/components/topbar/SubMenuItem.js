2013722219029d6f81605c0e7fb3b7f35825fb4c
Sorin Chis <kish.sorin@yahoo.com>
2024-08-14 12:26:33 +0300

    update tab

--- packages/yoroi-extension/app/components/topbar/SubMenuItem.js
+++ packages/yoroi-extension/app/components/topbar/SubMenuItem.js
@@ -37,18 +37,14 @@ class SubMenuItem extends Component<Props & InjectedProps> {
 
     return (
       <Box
-        sx={getStyles(active, isRevampLayout)}
+        sx={getStyles(active)}
         component="button"
         className={componentClasses}
         disabled={disabled}
         onClick={onClick}
         id={locationId + '-' + subMenuItemId + 'SubTab-button'}
       >
-        <Typography
-          variant="body1"
-          fontWeight="500"
-          color={active ? 'primary.500' : 'grayscale.600'}
-        >
+        <Typography variant="body1" fontWeight="500" color={active ? 'primary.500' : 'grayscale.600'}>
           {label}
         </Typography>
       </Box>
@@ -56,40 +52,22 @@ class SubMenuItem extends Component<Props & InjectedProps> {
   }
 }
 
-function getStyles(active: boolean, isRevampLayout: boolean): Object {
-  if (isRevampLayout && active)
+function getStyles(active: boolean): Object {
+  if (active)
     return {
-      borderBottomColor: 'primary.600',
-      color: 'primary.600',
+      borderBottomColor: 'ds.text_primary_medium',
+      color: 'ds.text_primary_medium',
       ':hover': {
-        borderBottomColor: 'primary.600',
-        color: 'primary.600',
+        borderBottomColor: 'ds.text_primary_max',
+        color: 'ds.text_primary_max',
       },
-      fontWeight: 500,
-      marginRight: '24px',
-    };
-
-  if (!isRevampLayout && active)
-    return {
-      borderBottomColor: 'var(--yoroi-palette-secondary-300)',
-      color: 'var(--yoroi-palette-secondary-300)',
-      ':hover': {
-        color: 'var(--yoroi-palette-secondary-300)',
-        borderBottomColor: 'var(--yoroi-palette-secondary-300)',
-      },
-      fontWeight: 500,
-      marginRight: '43px',
-    };
-
-  if (isRevampLayout)
-    return {
       fontWeight: 500,
       marginRight: '24px',
     };
 
   return {
-    marginRight: '43px',
     fontWeight: 500,
+    marginRight: '24px',
   };
 }
 
