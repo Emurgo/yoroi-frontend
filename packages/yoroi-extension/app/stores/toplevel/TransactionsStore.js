189c71540fa6917606abcbdf20c9374d34d63a3e
Sorin Chis <kish.sorin@yahoo.com>
2024-08-21 16:41:14 +0300

    fix: add return

--- packages/yoroi-extension/app/stores/toplevel/TransactionsStore.js
+++ packages/yoroi-extension/app/stores/toplevel/TransactionsStore.js
@@ -183,7 +183,7 @@ export default class TransactionsStore extends Store<StoresMap, ActionsMap> {
   }
 
   @computed get submitted(): Array<SubmittedTransactionEntry> {
-   this._submittedTransactions
+    return this._submittedTransactions;
   }
 
   @computed get hasAny(): boolean {
