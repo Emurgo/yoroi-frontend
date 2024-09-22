f7b8f06401e78b567e7f50b8cecac80d0832619a
Sorin Chis <kish.sorin@yahoo.com>
2024-09-22 13:26:31 +0300

    update warning banner design

--- packages/yoroi-extension/app/components/wallet/send/MaxAssetsError.js
+++ packages/yoroi-extension/app/components/wallet/send/MaxAssetsError.js
@@ -2,16 +2,16 @@
 import { Component } from 'react';
 import type { Node } from 'react';
 import { Typography } from '@mui/material';
-import { Box } from '@mui/system';
+import { Box, Stack } from '@mui/system';
 import { defineMessages, intlShape, FormattedHTMLMessage } from 'react-intl';
 import type { $npm$ReactIntl$IntlFormat } from 'react-intl';
 import globalMessages from '../../../i18n/global-messages';
+import { ReactComponent as AttentionIcon } from '../../../assets/images/attention-modern.inline.svg';
 
 const messages = defineMessages({
   maxNumberAllowed: {
     id: 'wallet.send.form.dialog.maxNumberAllowed',
-    defaultMessage:
-      '!!!<strong>{number} Assets</strong> is maximum number allowed to be send in one transaction',
+    defaultMessage: '!!!<strong>{number} Assets</strong> is maximum number allowed to be send in one transaction',
   },
 });
 
@@ -28,15 +28,15 @@ export default class MaxAssetsError extends Component<Props> {
     const { intl } = this.context;
 
     return (
-      <Box sx={{ backgroundColor: 'magenta.100', padding: '12px 16px', borderRadius: '8px' }}>
-        <Typography component="div" fontWeight={500} variant="body1" color="magenta.500" marginBottom="8px">
-          {intl.formatMessage(globalMessages.errorLabel)}
-        </Typography>
+      <Box sx={{ backgroundColor: 'ds.sys_magenta_100', padding: '12px 16px', borderRadius: '8px' }}>
+        <Stack direction="row" gap="8px">
+          <AttentionIcon />
+          <Typography component="div" fontWeight={500} variant="body1" color="ds.sys_magenta_500" marginBottom="8px">
+            {intl.formatMessage(globalMessages.errorLabel)}
+          </Typography>
+        </Stack>
         <Typography component="div" variant="body1" color="grayscale.900">
-          <FormattedHTMLMessage
-            {...messages.maxNumberAllowed}
-            values={{ number: this.props.maxAssetsAllowed }}
-          />
+          <FormattedHTMLMessage {...messages.maxNumberAllowed} values={{ number: this.props.maxAssetsAllowed }} />
         </Typography>
       </Box>
     );
