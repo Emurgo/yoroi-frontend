04af0cb6835adb7b5397bd19e6018575ed634961
Sorin Chis <kish.sorin@yahoo.com>
2024-10-01 17:14:36 +0300

    fix lint

--- packages/yoroi-extension/app/styles/context/layout.js
+++ packages/yoroi-extension/app/styles/context/layout.js
@@ -1,7 +1,6 @@
 // @flow
 import React from 'react';
 import type { Node } from 'react';
-import { THEMES } from '../themes';
 
 export type Layouts = 'CLASSIC' | 'REVAMP';
 export type LayoutComponentMap = {|
