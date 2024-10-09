9f017d14e127d05860bac4b68401656f7b7f7aff
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2024-10-09 16:14:03 +0300

    small rollback

--- scripts/compress.js
+++ scripts/compress.js
@@ -6,7 +6,6 @@ module.paths.unshift(`${process.cwd()}/node_modules`);
 const fs = require('fs');
 const path = require('path');
 const ChromeExtension = require('crx');
-const { utfToBytes } = require('../packages/yoroi-extension/app/coreUtils');
 const argv = require('minimist')(process.argv.slice(2));
 /* eslint import/no-unresolved: 0 */
 
@@ -26,7 +25,7 @@ function getPrivateKey() {
   if (argv.key.startsWith('./')) {
     return readKeyFromFile(argv.key);
   }
-  return utfToBytes(argv.key);
+  return Buffer.from(argv.key, 'utf-8');
 }
 
 const privateKey = getPrivateKey();
