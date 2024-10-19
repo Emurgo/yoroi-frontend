38f6e0a45bbdcb59f001af91138e04e60f12fe41
Denis Nebytov <nebytov.d@gmail.com>
2024-10-19 12:33:15 +0300

    added special links

--- packages/e2e-tests/helpers/windowManager.js
+++ packages/e2e-tests/helpers/windowManager.js
@@ -9,6 +9,12 @@ export const extensionTabName = 'Yoroi';
 export const faqTabName = 'Yoroi - EMURGO';
 export const trezorConnectTabName = 'Trezor';
 export const backgroungTabName = 'background';
+export const serviceWorkersTabName = 'chrome://serviceworker-internals';
+export const serviceWorkersLink = 'chrome://serviceworker-internals';
+export const browserWindowType = Object.freeze({
+  tab: 'tab',
+  window: 'window',
+});
 
 export class WindowManager {
   constructor(driver, logger) {
@@ -130,11 +136,11 @@ export class WindowManager {
   }
 
   async openNewTab(tabTitle, url) {
-    return await this._openNewWithCheck('tab', tabTitle, url);
+    return await this._openNewWithCheck(browserWindowType.tab, tabTitle, url);
   }
 
   async openNewWindow(windowTitle, url) {
-    return await this._openNewWithCheck('window', windowTitle, url);
+    return await this._openNewWithCheck(browserWindowType.window, windowTitle, url);
   }
 
   async closeTabWindow(titleToClose, switchToTitle) {
@@ -259,11 +265,5 @@ export class WindowManager {
     await this.switchTo(titleSwitchTo);
 
     return this._getHandleByTitle(titleSwitchTo)[0];
-  }
-
-  async reopenExtensionTab() {
-    this.logger.info(`WindowManager::reopenExtensionTab Reopening the extension tab`);
-    const extURL = '';
-    await openNewTab(extensionTabName, extURL);
   }
 }
