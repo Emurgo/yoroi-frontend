907d4fdd8e7be5bd8d07f3eced5edce8d36fef52
yushi <yushihuang@gmail.com>
2024-11-06 23:18:26 +0800

    handle race condition when loading db

--- packages/yoroi-extension/chrome/extension/background/state/databaseManager.js
+++ packages/yoroi-extension/chrome/extension/background/state/databaseManager.js
@@ -6,24 +6,26 @@ import { migrateNoRefresh } from '../../../../app/api/common/migration';
 import LocalStorageApi from '../../../../app/api/localStorage/index';
 import type { lf$Database, } from 'lovefield';
 
-let dbCache = null;
+let loadDbPromiseCache = null;
 let migratePromiseCache = null;
 
 export async function getDb(): Promise<lf$Database> {
   const localStorageApi = new LocalStorageApi();
 
-  if (!dbCache) {
-    dbCache = await loadLovefieldDB(schema.DataStoreType.INDEXED_DB);
+  if (!loadDbPromiseCache) {
+    loadDbPromiseCache = loadLovefieldDB(schema.DataStoreType.INDEXED_DB);
   }
+
+  const db = await loadDbPromiseCache;
 
   if (!migratePromiseCache) {
     migratePromiseCache =  migrateNoRefresh({
       localStorageApi,
-      persistentDb: dbCache,
+      persistentDb: db,
       currVersion: environment.getVersion(),
     });
   }
   await migratePromiseCache;
 
-  return dbCache;
+  return db;
 }
