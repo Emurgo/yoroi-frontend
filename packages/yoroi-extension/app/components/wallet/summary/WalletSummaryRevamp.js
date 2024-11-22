abf74fc41bb34d56ce34013d46d2dadef42c91e6
Sorin Chis <kish.sorin@yahoo.com>
2024-11-22 12:08:12 +0200

    Update packages/yoroi-extension/app/components/wallet/summary/WalletSummaryRevamp.js
    Co-authored-by: Denis Nebytov <denis@emurgo.io>

--- packages/yoroi-extension/app/components/wallet/summary/WalletSummaryRevamp.js
+++ packages/yoroi-extension/app/components/wallet/summary/WalletSummaryRevamp.js
@@ -237,7 +237,7 @@ export default class WalletSummaryRevamp extends Component<Props> {
             <Grid item xs={2} sx={{ textAlign: 'right' }} color="ds.text_gray_low">
               <Typography variant="body2">{intl.formatMessage(globalMessages.feeLabel)}</Typography>
             </Grid>
-            <Grid item xs={4} sx={{ textAlign: 'right', pr: '30px' }} color="ds.text_gray_medium">
+            <Grid item xs={4} sx={{ textAlign: 'right', pr: '30px' }} color="ds.text_gray_low">
               <Typography component="div" variant="body2">
                 {intl.formatMessage(globalMessages.amountLabel)}
               </Typography>
