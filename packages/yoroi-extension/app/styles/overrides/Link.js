b1505cea5a31555408d1adbccb0659d9c6c0b3a7
Denis Nebytov <nebytov.d@gmail.com>
2024-12-13 16:53:05 +0300

    correct colors for the link component

--- packages/yoroi-extension/app/styles/overrides/Link.js
+++ packages/yoroi-extension/app/styles/overrides/Link.js
@@ -1,13 +1,33 @@
 // @flow
 
-const Link = {
+import { darkThemeBase } from '../themes/dark-theme-base';
+import { lightThemeBase } from '../themes/light-theme-base';
+
+const { palette: darkThemePalette } = darkThemeBase;
+const { palette: lightThemePalette } = lightThemeBase;
+const ltDs = lightThemePalette.ds;
+const dtDs = darkThemePalette.ds;
+
+export const LightLink = {
   styleOverrides: {
     root: {
-      color: 'ds.text_primary_medium',
+      color: ltDs.text_primary_medium,
       '&:hover': {
         textDecoration: 'underline',
+        color: ltDs.text_primary_max,
       },
     },
   },
-};
-export { Link };
+}
+
+export const DarkLink = {
+  styleOverrides: {
+    root: {
+      color: dtDs.text_primary_medium,
+      '&:hover': {
+        textDecoration: 'underline',
+        color: dtDs.text_primary_max,
+      },
+    },
+  },
+}
