5712bd9ba66714246d76dc64e707e9d4ffeafb78
Denis Nebytov <nebytov.d@gmail.com>
2025-01-24 16:10:07 +0300

    Staking dark theme issues fix

--- packages/yoroi-extension/app/components/widgets/ErrorBlock.js
+++ packages/yoroi-extension/app/components/widgets/ErrorBlock.js
@@ -7,8 +7,7 @@ import { intlShape } from 'react-intl';
 import LocalizableError from '../../i18n/LocalizableError';
 import { Logger, stringifyError } from '../../utils/logging';
 import type { $npm$ReactIntl$IntlFormat } from 'react-intl';
-
-import styles from './ErrorBlock.scss';
+import { Box, Typography } from '@mui/material';
 
 type Props = {|
   +error: ?LocalizableError,
@@ -16,8 +15,8 @@ type Props = {|
 
 @observer
 export default class ErrorBlock extends Component<Props> {
-  static contextTypes: {|intl: $npm$ReactIntl$IntlFormat|} = {
-    intl: intlShape.isRequired
+  static contextTypes: {| intl: $npm$ReactIntl$IntlFormat |} = {
+    intl: intlShape.isRequired,
   };
 
   render(): Node {
@@ -26,14 +25,15 @@ export default class ErrorBlock extends Component<Props> {
 
     let errorText = '';
     try {
-      errorText = (error) ? intl.formatMessage(error, error.values) : '';
+      errorText = error ? intl.formatMessage(error, error.values) : '';
     } catch (e) {
       Logger.error(`${nameof(ErrorBlock)}:${nameof(this.render)} ${stringifyError(e)}`);
     }
 
     return (
-      <div className={styles.component}>
-        <span>{errorText}</span>
-      </div>);
+      <Box mt="10px" textAlign="center">
+        <Typography variant="body2" color="ds.text_error">{errorText}</Typography>
+      </Box>
+    );
   }
 }
