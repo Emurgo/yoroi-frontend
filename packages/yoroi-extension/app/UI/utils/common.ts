d4e69c3bd01273944a3c0fe754ae0da60b51dbad
Sorin Chis <kish.sorin@yahoo.com>
2025-02-12 10:17:27 +0200

    fix

--- packages/yoroi-extension/app/UI/utils/common.ts
+++ packages/yoroi-extension/app/UI/utils/common.ts
@@ -5,7 +5,7 @@ export const deriveRewardAddressFromAddress = async (address: string, chainId: n
     const from_bech32 = await RustModule.WalletV4.Address.from_bech32(address);
     const baseAddress = RustModule.WalletV4.BaseAddress.from_address(from_bech32);
     const stakeCred = baseAddress?.stake_cred();
-    const stakeCredential = RustModule.WalletV4.RewardAddress.new(1, stakeCred);
+    const stakeCredential = RustModule.WalletV4.RewardAddress.new(chainId, stakeCred);
     const rewardAddress = stakeCredential.to_address();
     const rewardAddrAsAddress = rewardAddress.to_bech32();
 
