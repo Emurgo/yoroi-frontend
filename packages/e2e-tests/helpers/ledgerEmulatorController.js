96f1c2712a11ef37e1e0e80a9d3a6bcb4fbce139
Denis Nebytov <nebytov.d@gmail.com>
2025-03-11 21:42:14 +0300

    added logs and method isReadyForAction

--- packages/e2e-tests/helpers/ledgerEmulatorController.js
+++ packages/e2e-tests/helpers/ledgerEmulatorController.js
@@ -2,12 +2,15 @@ const SPECULOS_ENDPOINT = 'http://localhost:5001';
 
 class LedgerEmulatorControllerError extends Error {}
 
+export const CARDANO_IS_READY = 'Cardano is ready';
+
 export class LedgerEmulatorController {
   constructor(logger) {
     this.logger = logger;
   }
 
   async _click(button) {
+    this.logger.info(`LedgerEmulator::_click is called. Button: ${button}`);
     await fetch(
       `${SPECULOS_ENDPOINT}/button/${button}`,
       {
@@ -32,8 +35,12 @@ export class LedgerEmulatorController {
     await this._click('right');
   }
 
-  // todo
+  /**
+   * The function reads a text from the current ledger screen
+   * @returns {string}
+   */
   async readScreen() {
+    this.logger.info(`LedgerEmulator::readScreen is called`);
     try {
       const eventsResponse = await fetch(
         `${SPECULOS_ENDPOINT}/events?currentscreenonly=true`
@@ -43,11 +50,27 @@ export class LedgerEmulatorController {
       }
       const eventsObj = await eventsResponse.json();
       const eventsText = eventsObj.events.map(evt => evt.text).join(' ');
+      this.logger.info(`LedgerEmulator::readScreen The current screen text: "${eventsText}"`);
 
       return eventsText;
     } catch (error) {
       console.error(error);
       throw new LedgerEmulatorController('Some error happen: ', error);
     }
+  }
+
+  async isReadyForAction(timeoutMilliSec, repeatPeriodMilliSec) {
+    this.logger.info(`LedgerEmulator::isReadyForSigning is called`);
+    const endTime = Date.now() + timeoutMilliSec;
+    while(Date.now() < endTime) {
+      const currentText = await this.readScreen();
+      if (currentText !== CARDANO_IS_READY) {
+        this.logger.info(`LedgerEmulator::isReadyForSigning Ledger is ready.`);
+        return true;
+      }
+      this.logger.info(`LedgerEmulator::isReadyForSigning Ledger is not ready. Waiting for ${repeatPeriodMilliSec} milliseconds`);
+      await new Promise(resolve => setTimeout(resolve, repeatPeriodMilliSec));
+    }
+    return false;
   }
 }
