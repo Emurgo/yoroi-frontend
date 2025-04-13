5b3a3234d21457b16d27df64b6fd1f310f184a80
Denis Nebytov <nebytov.d@gmail.com>
2025-04-13 14:44:28 +0300

    added function

--- packages/e2e-tests/helpers/ledgerHelper.js
+++ packages/e2e-tests/helpers/ledgerHelper.js
@@ -1,5 +1,32 @@
+import { expect } from 'chai';
+
 export const LedgerModels = Object.freeze({
   NanoS: 'nanos',
   NanoSPlus: 'nanosp',
   NanoX: 'nanox',
 });
+
+/**
+ * Getting cleaned info from screens
+ * @param {Array<string>} ledgerScreensContent
+ * @returns {{addressDerivationPath: string, addressFull: string, stakeKeyPath: string}}
+ */
+export const convertVerifiedAddressesInfo = ledgerScreensContent => {
+  expect(
+    ledgerScreensContent.length,
+    'Ledger screens contents amount is different from expected'
+  ).to.equal(3);
+
+  const addrDerPathSplit = ledgerScreensContent[0].split(' ');
+  const addressDerivationPath = addrDerPathSplit[addrDerPathSplit.length - 1];
+
+  const stakeKeyPathSplit = ledgerScreensContent[1].split(' ');
+  const stakeKeyPath = stakeKeyPathSplit[stakeKeyPathSplit.length - 1];
+
+  const addressFull = ledgerScreensContent[2].trim().replace(/ /g, '');
+  return {
+    addressDerivationPath,
+    addressFull,
+    stakeKeyPath,
+  };
+};
