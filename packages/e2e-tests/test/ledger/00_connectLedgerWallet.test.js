eb1f51555b5b89e42d997210d39d3d850a25211c
Denis Nebytov <nebytov.d@gmail.com>
2025-04-14 09:50:00 +0300

    checking connection for nanos, nanosp, nanox

--- packages/e2e-tests/test/ledger/00_connectLedgerWallet.test.js
+++ packages/e2e-tests/test/ledger/00_connectLedgerWallet.test.js
@@ -19,90 +19,102 @@ import { SpeculosDockerController } from '../../helpers/speculos/speculosDockerC
 import { LedgerModels } from '../../helpers/ledgerHelper.js';
 import { testWalletLedger } from '../../utils/testWallets.js';
 
-describe('Connect Ledger HW wallet', function () {
-  this.timeout(2 * oneMinute);
-  /** @type {SpeculosDockerController} */
-  let speculosDockerController = null;
-  /** @type {WebDriver} */
-  let webdriver = null;
-  /** @type {Logger} */
-  let logger = null;
-  /** @type {LedgerEmulatorController} */
-  let ledgerController = null;
-  /** @type {WindowManager} */
-  let windowManager = null;
+for (const model in LedgerModels) {
+  describe(`Connect Ledger HW wallet ${model}`, function () {
+    this.timeout(2 * oneMinute);
+    /** @type {SpeculosDockerController} */
+    let speculosDockerController = null;
+    /** @type {WebDriver} */
+    let webdriver = null;
+    /** @type {Logger} */
+    let logger = null;
+    /** @type {LedgerEmulatorController} */
+    let ledgerController = null;
+    /** @type {WindowManager} */
+    let windowManager = null;
 
-  before(async function () {
-    const speculosDockerLogger = getTestLogger('speculosDocker', this.test.parent.title);
-    speculosDockerController = new SpeculosDockerController(
-      speculosDockerLogger,
-      LedgerModels.NanoSPlus,
-      testWalletLedger.mnemonic
-    );
-    await speculosDockerController.runContainer();
+    before(async function () {
+      const speculosDockerLogger = getTestLogger('speculosDocker', this.test.parent.title);
+      speculosDockerController = new SpeculosDockerController(
+        speculosDockerLogger,
+        LedgerModels[model],
+        testWalletLedger.mnemonic
+      );
+      await speculosDockerController.runContainer();
 
-    webdriver = await driversPoolsManager.getDriverFromPool();
-    logger = getTestLogger(this.test.parent.title);
+      webdriver = await driversPoolsManager.getDriverFromPool();
+      logger = getTestLogger(this.test.parent.title);
 
-    const ledgerLogger = getTestLogger('ledger', this.test.parent.title);
-    ledgerController = new LedgerEmulatorController(ledgerLogger);
+      const ledgerLogger = getTestLogger('ledger', this.test.parent.title);
+      ledgerController = new LedgerEmulatorController(ledgerLogger);
 
-    const wmLogger = getTestLogger('windowManager', this.test.parent.title);
-    windowManager = new WindowManager(webdriver, wmLogger);
-    await windowManager.init();
+      const wmLogger = getTestLogger('windowManager', this.test.parent.title);
+      windowManager = new WindowManager(webdriver, wmLogger);
+      await windowManager.init();
+    });
+
+    it('Ledger is ready', async function () {
+      const ledgerState = await ledgerController.readScreen();
+      expect(ledgerState, 'Cardano app is not ready').to.equal(LedgerStates.cardanoIsReady);
+    });
+
+    it('Selecting Connect HW wallet', async function () {
+      const addNewWalletPage = new AddNewWallet(webdriver, logger);
+      await addNewWalletPage.selectConnectHW();
+      await addNewWalletPage.selectLedgerHW();
+      await addNewWalletPage.confirmChecking();
+      await addNewWalletPage.connectHardwareWallet();
+    });
+
+    it('Approve connection', async function () {
+      await windowManager.findNewWindowAndSwitchTo(ledgerConnectTabName);
+      const ledgerConnectPage = new LedgerConnect(webdriver, logger);
+      if (LedgerModels[model] === LedgerModels.NanoX) {
+        await ledgerConnectPage.selectNanoX();
+      } else {
+        await ledgerConnectPage.selectNanoS();
+      }
+      const ledgerIsReady = await ledgerController.isReadyForAction(threeSeconds, quarterSecond);
+      expect(ledgerIsReady, `Ledger isn't ready after ${threeSeconds / 1000} seconds`).to.be.true;
+      if (LedgerModels[model] === LedgerModels.NanoS){
+        await ledgerController.clickRight();
+      } else {
+        await ledgerController.clickBoth();
+      }
+      await windowManager.waitForClosingAndSwitchTo(ledgerConnectTabName, extensionTabName);
+    });
+
+    it('Enter wallet details', async function () {
+      const addNewWalletPage = new AddNewWallet(webdriver, logger);
+      await addNewWalletPage.enterHWWalletName(testWalletLedger.name);
+      await addNewWalletPage.saveHWInfo();
+    });
+
+    it('Check new wallet', async function () {
+      const transactionsPage = new TransactionsSubTab(webdriver, logger);
+      await transactionsPage.waitPrepareWalletBannerIsClosed();
+      await transactionsPage.closeUpdatesModalWindow();
+      const txPageIsDisplayed = await transactionsPage.isDisplayed();
+      expect(txPageIsDisplayed, 'The transactions page is not displayed').to.be.true;
+      const walletInfo = await transactionsPage.getSelectedWalletInfo();
+      expect(walletInfo.balance, 'The wallet balance is different').to.equal(
+        testWalletLedger.balance
+      );
+      expect(walletInfo.name, `The wallet name should be Speculos.`).to.equal(
+        testWalletLedger.name
+      );
+      expect(walletInfo.plate, `The wallet plate is different`).to.equal(testWalletLedger.plate);
+    });
+
+    afterEach(function (done) {
+      customAfterEach(this, webdriver, logger);
+      done();
+    });
+
+    after(async function () {
+      const basePage = new BasePage(webdriver, logger);
+      basePage.closeBrowser();
+      await speculosDockerController.killAndRemove();
+    });
   });
-
-  it('Ledger is ready', async function () {
-    const ledgerState = await ledgerController.readScreen();
-    expect(ledgerState, 'Cardano app is not ready').to.equal(LedgerStates.cardanoIsReady);
-  });
-
-  it('Selecting Connect HW wallet', async function () {
-    const addNewWalletPage = new AddNewWallet(webdriver, logger);
-    await addNewWalletPage.selectConnectHW();
-    await addNewWalletPage.selectLedgerHW();
-    await addNewWalletPage.confirmChecking();
-    await addNewWalletPage.connectHardwareWallet();
-  });
-
-  it('Approve connection', async function () {
-    await windowManager.findNewWindowAndSwitchTo(ledgerConnectTabName);
-    const ledgerConnectPage = new LedgerConnect(webdriver, logger);
-    await ledgerConnectPage.selectNanoS();
-    const ledgerIsReady = await ledgerController.isReadyForAction(threeSeconds, quarterSecond);
-    expect(ledgerIsReady, `Ledger isn't ready after ${threeSeconds / 1000} seconds`).to.be.true;
-    await ledgerController.clickBoth();
-    await windowManager.waitForClosingAndSwitchTo(ledgerConnectTabName, extensionTabName);
-  });
-
-  it('Enter wallet details', async function () {
-    const addNewWalletPage = new AddNewWallet(webdriver, logger);
-    await addNewWalletPage.enterHWWalletName(testWalletLedger.name);
-    await addNewWalletPage.saveHWInfo();
-  });
-
-  it('Check new wallet', async function () {
-    const transactionsPage = new TransactionsSubTab(webdriver, logger);
-    await transactionsPage.waitPrepareWalletBannerIsClosed();
-    await transactionsPage.closeUpdatesModalWindow();
-    const txPageIsDisplayed = await transactionsPage.isDisplayed();
-    expect(txPageIsDisplayed, 'The transactions page is not displayed').to.be.true;
-    const walletInfo = await transactionsPage.getSelectedWalletInfo();
-    expect(walletInfo.balance, 'The wallet balance is different').to.equal(
-      testWalletLedger.balance
-    );
-    expect(walletInfo.name, `The wallet name should be Speculos.`).to.equal(testWalletLedger.name);
-    expect(walletInfo.plate, `The wallet plate is different`).to.equal(testWalletLedger.plate);
-  });
-
-  afterEach(function (done) {
-    customAfterEach(this, webdriver, logger);
-    done();
-  });
-
-  after(async function () {
-    const basePage = new BasePage(webdriver, logger);
-    basePage.closeBrowser();
-    await speculosDockerController.killAndRemove();
-  });
-});
+}
