867131f11ad6502cd5e92817fb76d60db56a3e12
Denis Nebytov <nebytov.d@gmail.com>
2025-04-16 17:55:13 +0300

    added TxReviewSubmit page

---
+++ packages/e2e-tests/pages/transactionReviewPages/txReviewSubmit.page.js
@@ -0,0 +1,17 @@
+import TxReviewCommon from './txReviewCommon.page';
+
+class TxReviewSubmit extends TxReviewCommon {
+  // locators
+  passwordInputLocator = {
+    locator: 'txReview:submitTransaction-password-input',
+    method: 'id',
+  };
+
+  // methods
+  async enterPassword(password) {
+    this.logger.info(`TxReviewSubmit::enterPassword is called. Password: ${password}`);
+    await this.input(this.passwordInputLocator, password);
+  }
+}
+
+export default TxReviewSubmit;
