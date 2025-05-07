44df50628ffb578ac3610466c6165631e7952fde
Denis Nebytov <nebytov.d@gmail.com>
2025-05-07 13:00:55 +0300

    added password error locator and method

--- packages/e2e-tests/pages/transactionReviewPages/txReviewSubmit.page.js
+++ packages/e2e-tests/pages/transactionReviewPages/txReviewSubmit.page.js
@@ -1,3 +1,4 @@
+import { fiveSeconds, quarterSecond } from '../../helpers/timeConstants.js';
 import TxReviewCommon from './txReviewCommon.page.js';
 
 class TxReviewSubmit extends TxReviewCommon {
@@ -6,11 +7,28 @@ class TxReviewSubmit extends TxReviewCommon {
     locator: 'txReview:submitTransaction-password-input',
     method: 'id',
   };
+  passwordErrorLocator = {
+    locator: 'txReview:submitTransaction-passwordError-text',
+    method: 'id',
+  };
 
   // methods
   async enterPassword(password) {
     this.logger.info(`TxReviewSubmit::enterPassword is called. Password: ${password}`);
     await this.input(this.passwordInputLocator, password);
+  }
+
+  async waitAndGetPasswordError() {
+    this.logger.info(`TxReviewSubmit::waitAndGetPasswordError is called.`);
+    const isPresented = await this.customWaitIsPresented(
+      this.passwordErrorLocator,
+      fiveSeconds,
+      quarterSecond
+    );
+    if (isPresented) {
+      return await this.getText(this.passwordErrorLocator);
+    }
+    throw new Error('The password error message is not displayed');
   }
 }
 
