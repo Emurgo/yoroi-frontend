ac9e0e62942e986b713b907071f478fe9fe69bd9
banklesss <javierbueno@emurgo.io>
2025-05-14 12:29:38 +0200

    fix: confirm swap screen layout

--- packages/yoroi-extension/app/containers/swap/asset-swap/ConfirmSwapTransaction.js
+++ packages/yoroi-extension/app/containers/swap/asset-swap/ConfirmSwapTransaction.js
@@ -4,8 +4,6 @@ import { makeLimitOrder, makePossibleMarketOrder, useSwap, useSwapCreateOrder } 
 import { useEffect } from 'react';
 import { useSwapForm } from '../context/swap-form';
 import { useSwapFeeDisplay } from '../hooks';
-import type { RemoteTokenInfo } from '../../../api/ada/lib/state-fetch/types';
-import type { PriceImpact } from '../../../components/swap/types';
 import { ampli } from '../../../../ampli/index';
 import { tokenInfoToAnalyticsFromAndToAssets } from '../swapAnalytics';
 import { useStrings } from '../common/useStrings';
@@ -100,30 +98,6 @@ export default function ConfirmSwapTransaction({
         priceImpactState={priceImpactState}
         slippageValue={slippageValue}
       />
-      <GradientBox p="16px" borderRadius="8px" color="common.white">
-        <Box display="flex" justifyContent="space-between">
-          <Typography color="ds.white_static">{strings.total}</Typography>
-          <Box>
-            <Typography component="div" fontSize="20px" fontWeight="500" color="ds.white_static">
-              {formattedNonPtAmount ?? formattedPtAmount}
-            </Typography>
-          </Box>
-        </Box>
-        {formattedNonPtAmount && (
-          <Box display="flex" justifyContent="right">
-            <Box>
-              <Typography component="div" fontSize="20px" fontWeight="500" color="ds.white_static">
-                {formattedPtAmount}
-              </Typography>
-            </Box>
-          </Box>
-        )}
-        <Box display="flex" justifyContent="right">
-          <Typography component="div" variant="body1" color="ds.white_static" sx={{ opacity: '0.5' }}>
-            {getFormattedPairingValue(ptAmount)}
-          </Typography>
-        </Box>
-      </GradientBox>
     </Box>
   );
 }
