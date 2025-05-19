ae5e4e6e9f34cf6d0d44c26ed69512350877288e
banklesss <105349292+banklesss@users.noreply.github.com>
2025-05-19 11:32:43 +0200

    Update packages/yoroi-extension/app/UI/features/transaction-review/common/operations.tsx

--- packages/yoroi-extension/app/UI/features/transaction-review/common/operations.tsx
+++ packages/yoroi-extension/app/UI/features/transaction-review/common/operations.tsx
@@ -157,7 +157,7 @@ export const NoConfidenceOperation = ({ label, fee }: { label: string; fee: stri
 };
 export const VoteDelegationOperation = ({ label, hash, fee }: { label: string; hash?: string; fee: string | null }) => {
   const strings = useStrings();
-  const drepTitle = hash ? drepNames[hash] ?? hash : '-'
+  const drepTitle = hash ? drepNames[hash] ?? hash : '-';
 
   return (
     <Stack direction="column" spacing={2}>
