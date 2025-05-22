3b159509f301b49ca461b88e1ad12f9c560335a3
Denis Nebytov <nebytov.d@gmail.com>
2025-05-22 20:12:57 +0300

    added template for nft details pageObject

---
+++ packages/e2e-tests/pages/wallet/nftGallery/nftDetails.page.js
@@ -0,0 +1,24 @@
+import WalletCommonBase from '../../walletCommonBase.page.js';
+
+class NftDetails extends WalletCommonBase {
+    // locators
+    // * back to gallery btn
+    // * nft name
+    // * nft image
+    // * switch to the next nft btn
+    // * switch to the previous nft btn
+    // * overview tab btn
+    // * metadata tab btn
+    // * overview:description
+    // * overview:author
+    // * overview:fingerprint
+    // * overview:fingerprint:copy btn
+    // * overview:policyId
+    // * overview:policyId:copy btn
+    // * overview:detailsOn
+    // * metadata:copy btn
+    // * metadata:metadataInfo
+    // methods
+}
+
+export default NftDetails;
