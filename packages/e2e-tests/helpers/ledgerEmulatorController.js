8d6aa28e609a4dcf97f3c89f281c3a98078eedf3
Denis Nebytov <nebytov.d@gmail.com>
2025-05-23 08:16:01 +0300

    using ip address

--- packages/e2e-tests/helpers/ledgerEmulatorController.js
+++ packages/e2e-tests/helpers/ledgerEmulatorController.js
@@ -1,5 +1,5 @@
 import { sleep } from '../utils/utils.js';
-import { hostname } from 'os';
+import { networkInterfaces } from 'os';
 import { LedgerModels } from './ledgerHelper.js';
 import { quarterSecond, threeSeconds } from './timeConstants.js';
 
@@ -15,7 +15,7 @@ export class LedgerEmulatorController {
   constructor(logger, model) {
     this.logger = logger;
     this.model = model;
-    this.speculosEndpoint = `http://127.0.0.1:5001`;
+    this.speculosEndpoint = `http://${networkInterfaces().eth0[0].address}:5001`;
     this.logger.info(`LedgerEmulator::constructor speculos endpoint: ${this.speculosEndpoint}`);
   }
 
