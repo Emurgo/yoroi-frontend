1d56f999360245b412988ae54ccc23fa1c033983
Denis Nebytov <nebytov.d@gmail.com>
2025-05-26 16:43:23 +0300

    added locators

--- packages/e2e-tests/pages/wallet/nftGallery/nftDetails.page.js
+++ packages/e2e-tests/pages/wallet/nftGallery/nftDetails.page.js
@@ -1,24 +1,98 @@
+import { ElementLocator } from '../../locator.js';
 import WalletCommonBase from '../../walletCommonBase.page.js';
 
 class NftDetails extends WalletCommonBase {
-    // locators
-    // * back to gallery btn
-    // * nft name
-    // * nft image
-    // * switch to the next nft btn
-    // * switch to the previous nft btn
-    // * overview tab btn
-    // * metadata tab btn
-    // * overview:description
-    // * overview:author
-    // * overview:fingerprint
-    // * overview:fingerprint:copy btn
-    // * overview:policyId
-    // * overview:policyId:copy btn
-    // * overview:detailsOn
-    // * metadata:copy btn
-    // * metadata:metadataInfo
-    // methods
+  // locators
+  /** @type {ElementLocator} */
+  backNFTsGalleryBtnLocator = {
+    locator: 'nftDetails-backToGallery-button',
+    method: 'id',
+  };
+  /**@type {ElementLocator} */
+  nftNameLocator = {
+    locator: 'nftDetails-nftName-text',
+    method: 'id',
+  };
+  /**@type {ElementLocator} */
+  nftImageLocator = {
+    locator: 'nftDetails-image-component',
+    method: 'id',
+  };
+  /**@type {ElementLocator} */
+  nftTemplateImageLocator = {
+    locator: '#nftDetails-image-component > svg',
+    method: 'css',
+  };
+  /**@type {ElementLocator} */
+  nextNftBtnLocator = {
+    locator: 'nftDetails-nextNFT-button',
+    method: 'id',
+  };
+  /**@type {ElementLocator} */
+  prevNftBtnLocator = {
+    locator: 'nftDetails-previousNFT-button',
+    method: 'id',
+  };
+  /**@type {ElementLocator} */
+  // probably will need to do it by document.querySelector
+  // document.querySelector("button:has(#nftDetails-overviewTab-text)")
+  overviewTabBtnLocator = {
+    locator: 'button:has(#nftDetails-overviewTab-text)',
+    method: 'css',
+  };
+  /**@type {ElementLocator} */
+  // probably will need to do it by document.querySelector
+  // document.querySelector("button:has(#nftDetails-metadataTab-text)")
+  metadataTabBtnLocator = {
+    locator: 'button:has(#nftDetails-metadataTab-text)',
+    method: 'css',
+  };
+  /**@type {ElementLocator} */
+  overviewDescriptionTextLocator = {
+    locator: 'nftDetails:overview-description-text',
+    method: 'id',
+  };
+  /**@type {ElementLocator} */
+  overviewAuthorTextLocator = {
+    locator: 'nftDetails:overview-author-text',
+    method: 'id',
+  };
+  /**@type {ElementLocator} */
+  overviewFingerPrintTextLocator = {
+    locator: 'nftDetails:overview:fingerprint-info-text',
+    method: 'id',
+  };
+  /**@type {ElementLocator} */
+  overviewFingerPrintCopyBtnLocator = {
+    locator: 'nftDetails:overview:fingerprint-copy-button',
+    method: 'id',
+  };
+  /**@type {ElementLocator} */
+  overviewPolicyIdTextLocator = {
+    locator: 'nftDetails:overview:policyId-info-text',
+    method: 'id',
+  };
+  /**@type {ElementLocator} */
+  overviewPolicyIdCopyBtnLocator = {
+    locator: 'nftDetails:overview:policyId-copy-button',
+    method: 'id',
+  };
+  /**@type {ElementLocator} */
+  overviewExplorerLinkLocator = {
+    locator: '#nftDetails:overview-explorer-link > a',
+    method: 'css',
+  };
+  /**@type {ElementLocator} */
+  metadataCopyBtnLocator = {
+    locator: 'nftDetails:metadata-copy-button',
+    method: 'id',
+  };
+  /**@type {ElementLocator} */
+  metadataInfoTextLocator = {
+    locator: 'nftDetails:metadata-info-text',
+    method: 'id',
+  };
+  // methods
 }
 
 export default NftDetails;
