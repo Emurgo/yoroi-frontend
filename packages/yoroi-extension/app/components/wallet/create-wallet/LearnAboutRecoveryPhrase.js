0466dcd20ca5ba2784b3d5834ad6eeab32892625
banklesss <105349292+banklesss@users.noreply.github.com>
2025-06-10 18:56:17 +0200

    Revert "fix: wallet creation notes formatting"
    This reverts commit 4a358e13f119e936fec12b9df7fbbc7c41a73dc1.

--- packages/yoroi-extension/app/components/wallet/create-wallet/LearnAboutRecoveryPhrase.js
+++ packages/yoroi-extension/app/components/wallet/create-wallet/LearnAboutRecoveryPhrase.js
@@ -1,7 +1,7 @@
 // @flow
 import { useEffect } from 'react';
 import type { Node, ComponentType } from 'react';
-import { defineMessages, injectIntl } from 'react-intl';
+import { defineMessages, injectIntl, FormattedMessage } from 'react-intl';
 import { observer } from 'mobx-react';
 import type { $npm$ReactIntl$IntlShape } from 'react-intl';
 import { Stack, Typography, Box, Link } from '@mui/material';
@@ -49,7 +49,7 @@ function _LearnAboutRecoveryPhrase(props: Props & Intl): Node {
     <Stack alignItems="center" justifyContent="center">
       <Stack direction="column" alignItems="center" justifyContent="center" maxWidth="648px">
         <Typography component="div" mb="16px" variant="body1">
-          {intl.formatMessage(text.description, {strong: chunks => <strong>{chunks}</strong>})}
+          <FormattedMessage {...messages.description} values={{ strong }}/>
           <Box
             component="span"
             sx={{
@@ -136,7 +136,7 @@ function LearnAboutRecoveryPhrase(props: Props & Intl): Node {
     <Stack alignItems="center" justifyContent="center" id="learnAboutRecoveryPhraseComponent">
       <Stack maxWidth="648px">
         <Typography component="div" mb="16px" variant="body1" color="ds.text_gray_medium">
-           {intl.formatMessage(text.shortDescription, {strong: chunks => <strong>{chunks}</strong>})}
+          <FormattedMessage {...text.shortDescription} />
         </Typography>
 
         <Stack
@@ -157,7 +157,7 @@ function LearnAboutRecoveryPhrase(props: Props & Intl): Node {
             {tips.map(tip => (
               <Box component="li" key={tip.id}>
                 <Typography component="div" variant="body1" color="ds.text_gray_medium" mb="4px">
-                   {intl.formatMessage(tip, {strong: chunks => <strong>{chunks}</strong>})}
+                  <FormattedMessage {...tip} />
                 </Typography>
               </Box>
             ))}
