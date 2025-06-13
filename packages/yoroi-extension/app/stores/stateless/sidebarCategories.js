b24d3d5f842e44bd86c768857f72fe89c57609c0
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2025-06-13 19:00:12 +0300

    small update to sidebar categories

--- packages/yoroi-extension/app/stores/stateless/sidebarCategories.js
+++ packages/yoroi-extension/app/stores/stateless/sidebarCategories.js
@@ -33,8 +33,10 @@ export type SidebarCategoryRevamp = {|
   +featureFlagName?: string,
 |};
 
+const always = () => true;
 const existsSelectedWallet = ({ selected }) => selected != null;
 const isOnMainnet = ({ selected }): boolean => selected != null && !selected.isTestnet;
+const isDev = (): boolean => environment.isDev();
 const nonTrezorWallet = ({ selected }): boolean => selected?.type !== 'trezor';
 
 // TODO: Fix routes and isVisible prop
@@ -69,8 +71,8 @@ export const allCategoriesRevamp: Array<SidebarCategoryRevamp> = [
     className: 'swap',
     route: ROUTES.SWAP_REVAMP.ASSET_SWAP,
     icon: swapIcon,
-    label: globalMessages.sidebarSwap,
-    isVisible: () => environment.isDev(),
+    label: { id: 'sidebar.swap2', defaultMessage: '!!!Swap2' },
+    isVisible: isDev,
   },
   {
     className: 'portfolio',
@@ -91,7 +93,7 @@ export const allCategoriesRevamp: Array<SidebarCategoryRevamp> = [
     route: ROUTES.NFT_GALLERY.ROOT,
     icon: nftsIcon,
     label: globalMessages.sidebarNfts,
-    isVisible: () => environment.isDev(),
+    isVisible: isDev,
   },
   {
     className: 'voting',
@@ -112,7 +114,7 @@ export const allCategoriesRevamp: Array<SidebarCategoryRevamp> = [
     route: ROUTES.DAPP_CONNECTOR.CONNECTED_WEBSITES,
     icon: dappConnectorIcon,
     label: connectorMessages.connector,
-    isVisible: () => true,
+    isVisible: always,
   },
   {
     className: 'governance',
@@ -126,7 +128,7 @@ export const allCategoriesRevamp: Array<SidebarCategoryRevamp> = [
     route: '/settings',
     icon: settingIcon,
     label: globalMessages.sidebarSettings,
-    isVisible: () => true,
+    isVisible: always,
   },
 ];
 
