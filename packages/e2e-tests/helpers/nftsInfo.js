903651b0afaf1d29654ff7412af3f81789bc966f
Denis Nebytov <nebytov.d@gmail.com>
2025-06-24 14:43:01 +0300

    add nft names in HEX

--- packages/e2e-tests/helpers/nftsInfo.js
+++ packages/e2e-tests/helpers/nftsInfo.js
@@ -1,5 +1,6 @@
 export const NFT1 = Object.freeze({
   name: 'Baked_Nation',
+  nameHex: '42616b65645f4e6174696f6e',
   description: 'Unrevealed Baked Nation Pack',
   fingerprint: 'asset1mhezn55rcuc7gvlrdn4tssyf8m7t0z2a6mzc0v',
   policyId: '035ce798840c76700f8c57d99766956322138cd7b29d3339dc189861',
@@ -7,6 +8,7 @@ export const NFT1 = Object.freeze({
 });
 export const NFT2 = Object.freeze({
   name: 'Ub2AdaGang180',
+  nameHex: '55623241646147616e67313830',
   description:
     '{\\n Artist: "Ugly Bros",\\n Collection: "Ugly Bros 2 ADA Gang",\\n ID: "180",\\n Type: "Atlantic",\\n Variation: "Astronaut",\\n character: "Roll"}',
   fingerprint: 'asset1htswaqwf4077hff5p0xdtxf5zjjf6rzwuzn50s',
@@ -15,6 +17,7 @@ export const NFT2 = Object.freeze({
 });
 export const NFT3 = Object.freeze({
   name: '20022024_testNFT',
+  nameHex: '32303032323032345f746573744e4654',
   description:
     'very long description 123 very long description 123 very long description 123 very long description 123 very long description 123 very long description 123 very long description 123 very long description 123 very long description 123',
   fingerprint: 'asset188zn97aa5tcyd3g9mc0udxw4z9f4r6x709hngv',
@@ -23,6 +26,7 @@ export const NFT3 = Object.freeze({
 });
 export const NFT4 = Object.freeze({
   name: 'Mocossi-Ito-8724',
+  nameHex: '4d6f636f7373692d49746f2d38373234',
   description: 'This is an NFT from project Mocossi.',
   fingerprint: 'asset1k3qdm2vwv4r6h7h9gt08jcndr0pxpueylx3s5d',
   policyId: '6af53d37a0fc0e8cf39f481d8edc014f779493d9b0b91c38afc3a39e',
@@ -30,6 +34,7 @@ export const NFT4 = Object.freeze({
 });
 export const NFT5 = Object.freeze({
   name: 'BossCatRocketClub#3679',
+  nameHex: '426f7373436174526f636b6574436c75622333363739',
   description: 'Boss Cat Rocket Club #3679',
   fingerprint: 'asset1l5kejs5c09jjv4kh3n8ulahq4zutm9hymczxkm',
   policyId: '6af53d37a0fc0e8cf39f481d8edc014f779493d9b0b91c38afc3a39e',
@@ -37,6 +42,7 @@ export const NFT5 = Object.freeze({
 });
 export const NFT6 = Object.freeze({
   name: 'TestYoroiNFTKurt',
+  nameHex: '54657374596f726f694e46544b757274',
   description: 'Kurt the Bear',
   fingerprint: 'asset17nlv9g6jva4v3uzqazheu5m9sqxfe5e5g7ckdl',
   policyId: '5449dbad479b09de066bdf7934799c8a5aa2b66cf4a11eb759aa76c6',
@@ -44,6 +50,7 @@ export const NFT6 = Object.freeze({
 });
 export const NFT7 = Object.freeze({
   name: 'TestYoroiNFTDaVinci',
+  nameHex: '54657374596f726f694e4654446156696e6369',
   description: 'Upside-down DaVinci',
   fingerprint: 'asset1jlxwnwgj0vj88m4ks8zhs4p78mg4vje69ad623',
   policyId: '5449dbad479b09de066bdf7934799c8a5aa2b66cf4a11eb759aa76c6',
@@ -51,6 +58,7 @@ export const NFT7 = Object.freeze({
 });
 export const NFT8 = Object.freeze({
   name: 'nftTest_51_2',
+  nameHex: '6e6674546573745f35315f32',
   description: 'a test nft for regression testing the version 5.1 _nftTest_51_2',
   fingerprint: 'asset1xu3d8eu9xdq5xjgy88d5jqxzuqm7cs8av8nqa3',
   policyId: '6af53d37a0fc0e8cf39f481d8edc014f779493d9b0b91c38afc3a39e',
