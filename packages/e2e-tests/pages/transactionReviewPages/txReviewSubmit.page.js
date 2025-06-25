62b569285ebd3f32421b923fe2944b51df1fa6e0
Denis Nebytov <nebytov.d@gmail.com>
2025-06-25 15:50:21 +0300

    add id and method in page object

--- packages/e2e-tests/pages/transactionReviewPages/txReviewSubmit.page.js
+++ packages/e2e-tests/pages/transactionReviewPages/txReviewSubmit.page.js
@@ -1,9 +1,16 @@
+import { ElementLocator } from '../locator.js';
 import TxReviewCommon from './txReviewCommon.page.js';
 
 class TxReviewSubmit extends TxReviewCommon {
   // locators
+  /** @type {ElementLocator} */
   passwordInputLocator = {
     locator: 'txReview:submitTransaction-password-input',
+    method: 'id',
+  };
+  /** @type {ElementLocator} */
+  passwordErrorMessageLocator = {
+    locator: 'password-errorMessage-text',
     method: 'id',
   };
 
@@ -11,6 +18,14 @@ class TxReviewSubmit extends TxReviewCommon {
   async enterPassword(password) {
     this.logger.info(`TxReviewSubmit::enterPassword is called. Password: ${password}`);
     await this.input(this.passwordInputLocator, password);
+  }
+  async getPasswordErrorMessage() {
+    this.logger.info(`TxReviewSubmit::getPasswordErrorMessage is called.`);
+    const result = await this.waitPresentedAndAct(this.passwordErrorMessageLocator, async () => {
+      return await this.getText(this.passwordErrorMessageLocator);
+    });
+    this.logger.info(`TxReviewSubmit::getPasswordErrorMessage. Result: ${result}`);
+    return result;
   }
 }
 
