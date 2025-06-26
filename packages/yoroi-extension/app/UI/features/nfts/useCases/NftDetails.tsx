9b404263cb39937119f90107bf35f6d0694b369d
Sorin Chis <kish.sorin@yahoo.com>
2025-06-26 10:57:19 +0300

    fix spacing

--- packages/yoroi-extension/app/UI/features/nfts/useCases/NftDetails.tsx
+++ packages/yoroi-extension/app/UI/features/nfts/useCases/NftDetails.tsx
@@ -72,7 +72,11 @@ export default function NftDetails() {
     <>
       <Box sx={{ mb: '24px', width: '100%' }}>
         <Link to={ROUTES.NFT_GALLERY.ROOT}>
-          <Button variant="text" sx={{ px: '16px !important' }} startIcon={<IconWrapper icon={Icons.ChevronLeft} id={`${nftPathId}-backToGallery-button`} />}>
+          <Button
+            variant="text"
+            sx={{ px: '16px !important' }}
+            startIcon={<IconWrapper icon={Icons.ChevronLeft} id={`${nftPathId}-backToGallery-button`} />}
+          >
             <Typography color="ds.el_gray_medium" fontWeight="500" fontSize="14px">
               {strings.back}
             </Typography>
@@ -84,15 +88,24 @@ export default function NftDetails() {
           <ImageItem onClick={openModal} nftInfo={currentNft} />
         </Grid>
         <Grid item xs={6}>
-          <Stack direction="row" mx="24px" justifyContent="space-between" spacing={2}>
-            <Typography variant="h2" id={`${nftPathId}-nftName-text`}>{currentNft?.name}</Typography>
+          <Stack direction="row" mx="24px" justifyContent="space-between" spacing={16}>
+            <Typography variant="h2" id={`${nftPathId}-nftName-text`}>
+              {currentNft?.name}
+            </Typography>
             <NftsNavigation
               prevNftId={nftsList[currentNftIndex === 0 ? nftsList.length - 1 : currentNftIndex - 1]?.id || ''}
               nextNftId={nftsList[currentNftIndex === nftsList.length - 1 ? 0 : currentNftIndex + 1]?.id || ''}
               activeTab={currentTab}
             />
           </Stack>
-          <Tabs initialTabId={tabSearchParam} onTabChange={handleTabChange} headerSx={{ mx: '24px' }} tabs={getTabs()} pathId={nftPathId} />
+          <Tabs
+            initialTabId={tabSearchParam}
+            onTabChange={handleTabChange}
+            headerSx={{ mx: '24px' }}
+            tabs={getTabs()}
+            pathId={nftPathId}
+            contentSx={{ mx: '24px', mt: '16px' }}
+          />
         </Grid>
       </Grid>
       <Modal
