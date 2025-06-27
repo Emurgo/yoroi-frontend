efd3d63e351f809fbf017624d9d2db7b7f455302
Denis Nebytov <nebytov.d@gmail.com>
2025-06-27 17:02:37 +0300

    add skip for ADA handle in case of error from service

--- packages/e2e-tests/helpers/messages.js
+++ packages/e2e-tests/helpers/messages.js
@@ -11,3 +11,4 @@ export const NOT_ENOUGH_BALANCE = 'Not enough balance';
 export const NO_NFTS_ADDED = 'No NFTs added to your wallet';
 export const NO_NFTS_FOUND = 'No NFTs found';
 export const WRONG_PASSWORD_TX_REVIEW = 'Wrong password';
+export const ADA_HANDLE_UNEXPECTED_ERROR = 'ADA Handle: unexpected error';
