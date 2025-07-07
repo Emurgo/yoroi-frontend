2fb47f07dc1b7bc667565b71612db5dbe7020423
Sorin Chis <kish.sorin@yahoo.com>
2025-07-07 17:32:00 +0300

    add new constant for prod and dev

--- packages/yoroi-extension/app/UI/common/constants.ts
+++ packages/yoroi-extension/app/UI/common/constants.ts
@@ -1,3 +1,4 @@
 export const MIDNIGHT_DISTRIBUTION_URL = 'https://www.midnight.gd/';
 
-export const YOROI_REMOTE_CONFIG_URL = 'https://raw.githubusercontent.com/Emurgo/yoroi-config/refs/heads/main/prod.json';
+export const YOROI_DEV_REMOTE_CONFIG_URL = 'https://raw.githubusercontent.com/Emurgo/yoroi-config/refs/heads/main/dev.json';
+export const YOROI_PROD_REMOTE_CONFIG_URL = 'https://raw.githubusercontent.com/Emurgo/yoroi-config/refs/heads/main/prod.json';
