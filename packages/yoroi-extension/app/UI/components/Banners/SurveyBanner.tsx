818780fd6dd9fcce8aadbb71f6018a3c569821e2
yushi <yushihuang@gmail.com>
2025-07-09 12:26:40 +0800

    add a survey banner

---
+++ packages/yoroi-extension/app/UI/components/Banners/SurveyBanner.tsx
@@ -0,0 +1,45 @@
+import { useStrings } from '../../common/hooks/useStrings';
+import { BaseBanner } from './BaseBanner';
+import { SurveyIllustration } from './SurveyIllustration';
+
+export const SurveyBanner = (props: { onClose: () => void, }) => {
+  const { surveyTitle, surveyDescription, surveyButton } = useStrings();
+
+  const handleClick = () => {
+    props.onClose();
+    window.open(
+      'https://emurgohelpdesk.zendesk.com/hc/en-us/articles/9634494298895-Yoroi-Wallet-Experience-Survey',
+      '_blank'
+    );
+  };
+
+  return (
+    <BaseBanner
+      onClose={props.onClose}
+      title={surveyTitle}
+      description={surveyDescription}
+      buttonText={surveyButton}
+      buttonProps={{
+        onClick: handleClick,
+        //  @ts-ignore
+        variant: 'primary',
+        sx: {
+          width: 'fit-content',
+          height: '40px',
+          '&.MuiButton-sizeMedium': {
+            p: '9px 20px',
+          },
+        },
+      }}
+      displayIllustration
+      illustration={<SurveyIllustration />}
+      illustrationProps={{
+        sx: {
+          height: '154px',
+          zIndex: 20,
+          marginRight: '100px',
+        },
+      }}
+    />
+  );
+};
