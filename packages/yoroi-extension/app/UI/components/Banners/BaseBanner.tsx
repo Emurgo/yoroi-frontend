e2ad303d1283328d327b2003653e4715452ff553
Denis Nebytov <nebytov.d@gmail.com>
2025-07-17 13:40:36 +0300

    update midnight banner

--- packages/yoroi-extension/app/UI/components/Banners/BaseBanner.tsx
+++ packages/yoroi-extension/app/UI/components/Banners/BaseBanner.tsx
@@ -46,7 +46,6 @@ export const BaseBanner = ({
           iconProps={{ fill: theme.palette.ds.el_gray_max }}
           color="ds.el_gray_max"
           borderColor="ds.el_gray_max"
-          iconProps={{fill: theme.palette.ds.el_gray_max}}
           asButton
         />
       </Stack>
