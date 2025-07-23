19e45cf15cd82133d5af20a998af5150358f9de0
Denis Nebytov <nebytov.d@gmail.com>
2025-07-23 16:54:34 +0300

    prettify after develop merge

--- packages/yoroi-extension/app/containers/wallet/WalletReceivePage.js
+++ packages/yoroi-extension/app/containers/wallet/WalletReceivePage.js
@@ -334,7 +334,8 @@ export default class WalletReceivePage extends Component<StoresProps> {
     );
   }
 
-  getTypeStore: any => void | {| // unused for now
+  getTypeStore: any => void | {|
+    // unused for now
     +request: IAddressTypeUiSubset,
     +meta: AddressSubgroupMeta<IAddressTypeStore>,
   |} = _publicDeriver => {
