b9ea021e5e11b546ba9d47b41de011ef2340ae38
yushi <yushihuang@gmail.com>
2025-07-28 16:50:54 +0800

    refactor as per code review

---
+++ packages/yoroi-extension/app/UI/features/airdrop/useCases/ClaimDialog.tsx
@@ -0,0 +1,98 @@
+import Dialog from '../../../../components/widgets/Dialog';
+import { useIntl, defineMessages } from 'react-intl';
+import { Typography } from '@mui/material';
+import { useState } from 'react';
+import { WrongPassphraseError } from '../../../../api/ada/lib/cardanoCrypto/cryptoErrors';
+import TextField from '../../../../components/common/TextField';
+import globalMessages from '../../../../i18n/global-messages';
+
+const messages = defineMessages({
+  claimDialogTitle: {
+    id: 'airdrop.claimDialogTitle',
+    defaultMessage: '!!!sign message',
+  },
+  messageLabel: {
+    id: 'airdrop.messageLabel',
+    defaultMessage: '!!!Message',
+  },
+  wrongPassword: {
+    id: 'airdrop.wrongPassword',
+    defaultMessage: '!!!Wrong password',
+  },
+  mnemonicClaimDialogText: {
+    id: 'airdrop.mnemonicClaimDialogText',
+    defaultMessage: '!!!Please sign message to prove ownership of your assets. Signing this message will not affect your wallet’s balance in any way and does not require you to pay any fees.',
+  },
+});
+
+
+export default function ClaimDialog(props: { onClose: () => void, onClaim: (password: string) => Promise<void>, message: string }) {
+  const intl = useIntl();
+  const wrongPasswordErrorMessage = intl.formatMessage(messages.wrongPassword);
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState<string | null>(null);
+  const [passwordError, setPasswordError] = useState<string | null>(null);
+  const [isClaiming, setClaiming] = useState(false);
+
+  const onClaim = async () => {
+    setClaiming(true);
+    setError(null);
+    try {
+      await props.onClaim(password);
+    } catch (error) {
+      if (error instanceof WrongPassphraseError) {
+        setPasswordError(wrongPasswordErrorMessage);
+      } else if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError(String(error));
+      }
+    } finally {
+      setClaiming(false);
+    }
+  };
+
+  return (
+    <Dialog
+      withCloseButton
+      onClose={props.onClose}
+      title={intl.formatMessage(messages.claimDialogTitle)}
+      dialogActions={[
+        {
+          label: intl.formatMessage(messages.claimDialogTitle),
+          primary: true,
+          disabled: (password.length === 0) || isClaiming,
+          onClick: onClaim,
+        },
+      ]}
+    >
+      <Typography variant="body1" color="ds.text_gray_medium">
+        {intl.formatMessage(messages.mnemonicClaimDialogText)}
+      </Typography>
+      <Typography variant="body1" color="ds.text_gray_low" sx={{ marginTop: '16px' }}>
+        {intl.formatMessage(messages.messageLabel)}
+      </Typography>
+      <Typography variant="body1" color="ds.text_gray_medium" sx={{ marginBottom: '16px' }}>
+        {props.message}
+      </Typography>
+      <TextField
+        error={passwordError}
+        type="password"
+        className="walletPassword"
+        value={password}
+        label={intl.formatMessage(globalMessages.passwordLabel)}
+        isLoading={isClaiming}
+        onChange={e => {
+          if (error === wrongPasswordErrorMessage) {
+            setPasswordError(null);
+          }
+          setPassword(e.target.value);
+        }}
+        autoFocus
+      />
+      <Typography component="div" variant="body2" color="ds.text_error">
+        {error}
+      </Typography>
+    </Dialog>
+  );
+}
---
+++ packages/yoroi-extension/app/UI/features/airdrop/useCases/Terms.tsx
@@ -0,0 +1,36 @@
+import { Typography } from '@mui/material';
+import { useIntl, defineMessages } from 'react-intl';
+
+const messages = defineMessages({
+  terms: {
+    id: 'airdrop.terms',
+    defaultMessage: '!!!Terms & Conditions',
+  },
+});
+
+export default function Terms() {
+  const intl = useIntl();
+  return (<>
+    <Typography variant="body1" color="ds.text_gray_min" sx={{ marginTop: '32px', marginBottom: '16px' }}>
+      {intl.formatMessage(messages.terms)}
+    </Typography>
+    <Typography variant="body1">
+      <Typography fontWeight={500}>1. Acceptance of Terms</Typography>
+
+      <p>By participating in the [Project Name] Airdrop ("Airdrop"), you ("Participant") agree to be bound by these Terms of Use ("Terms"). If you do not agree with these Terms, do not participate in the Airdrop.</p>
+
+       <p>&nbsp;</p>
+       <Typography fontWeight={500}>2. Eligibility</Typography>
+
+       <p>2.1 Age Requirement: Participants must be at least 18 years old or the age of majority in their jurisdiction, whichever is higher.</p>
+
+       <p>2.2 Jurisdiction: The Airdrop is not available to residents or citizens of countries where participation in cryptocurrency activities is restricted or illegal. It is your responsibility that you comply with your local laws.</p>
+       <p>2.3 Verification: Participants may be required to undergo identity</p>
+
+       <p>&nbsp;</p>
+       <Typography fontWeight={500}>3. Heading</Typography>
+
+       <p>3.1 Age Requirement: Participants must be at least 18 years old or the age of majority in their jurisdiction, whichever is higher.</p>
+    </Typography>
+  </>);
+}
