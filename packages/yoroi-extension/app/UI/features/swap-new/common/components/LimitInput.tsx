66a024d01ac0e0a54bd87b278546827f9677705c
Sorin Chis <kish.sorin@yahoo.com>
2025-07-31 12:12:32 +0300

    refactor limit order component

--- packages/yoroi-extension/app/UI/features/swap-new/common/components/LimitInput.tsx
+++ packages/yoroi-extension/app/UI/features/swap-new/common/components/LimitInput.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from '@mui/material';
+import { Box, Stack, Typography, styled } from '@mui/material';
 import { useSwapRevamp } from '../../module/SwapContextProvider';
 
 export const LimitInput = () => {
@@ -10,64 +10,77 @@ export const LimitInput = () => {
   }
 
   return (
-    <Stack sx={{ my: 16 }}>
-      <Box
-        component="fieldset"
-        sx={{
-          border: '1px solid',
-          borderColor: 'grayscale.400',
-          borderRadius: '8px',
-          p: '16px',
-          display: 'grid',
-          gridTemplateColumns: '1fr auto',
-          justifyContent: 'start',
-          position: 'relative',
-          bgcolor: 'ds.bg_color_max',
-          columnGap: '6px',
-          rowGap: '8px',
-        }}
-        height="56px"
-      >
-        <Box
-          component="legend"
-          sx={{
-            top: '-9px',
-            left: '16px',
-            position: 'absolute',
-            px: '4px',
-            bgcolor: 'ds.bg_color_max',
-            borderRadius: '10px',
-            color: 'ds.text_gray_medium',
-          }}
-        >
-          Buy At
-        </Box>
+    <Wrapper>
+      <Fieldset component="fieldset">
+        <Legend component="legend">Buy At</Legend>
 
-        <Typography
-          sx={{
-            appearance: 'none',
-            border: '0',
-            outline: 'none',
-            '::placeholder': { color: 'ds.gray_600' },
-          }}
+        <StyledInput
+          // @ts-ignore
           component="input"
           type="text"
           variant="body1"
           color="ds.text_gray_medium"
           placeholder="0"
-          bgcolor={'ds.bg_color_max'}
           value={swapForm.wantedPrice}
           onChange={event => {
             const val = (event.target as HTMLInputElement).value;
             swapForm.action({ type: 'WantedPriceInputChanged', value: val });
           }}
         />
-        <Box sx={{ justifySelf: 'end' }}>
-          <Box height="100%" width="max-content" display="flex" alignItems="center">
-            <Typography color="ds.text_gray_max">{tokenInputInfo?.ticker || '-'}</Typography>
-          </Box>
-        </Box>
-      </Box>
-    </Stack>
+
+        <EndAdornment>
+          <Typography color="ds.text_gray_max">{tokenInputInfo?.ticker || '-'}</Typography>
+        </EndAdornment>
+      </Fieldset>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled(Stack)(({ theme }) => ({
+  marginTop: theme.spacing(16),
+  marginBottom: theme.spacing(16),
+}));
+
+const Fieldset = styled(Box)(({ theme }: any) => ({
+  border: '1px solid',
+  borderColor: theme.palette.ds.gray_400,
+  borderRadius: 8,
+  padding: 16,
+  display: 'grid',
+  gridTemplateColumns: '1fr auto',
+  justifyContent: 'start',
+  position: 'relative',
+  backgroundColor: theme.palette.ds.bg_color_max,
+  columnGap: '6px',
+  rowGap: '8px',
+  height: 56,
+}));
+
+const Legend = styled(Box)(({ theme }: any) => ({
+  position: 'absolute',
+  top: -9,
+  left: 16,
+  paddingLeft: 4,
+  paddingRight: 4,
+  backgroundColor: theme.palette.ds?.bg_color_max,
+  borderRadius: 10,
+  color: theme.palette.ds?.text_gray_medium,
+}));
+
+const StyledInput = styled(Typography)(({ theme }: any) => ({
+  appearance: 'none',
+  border: 0,
+  outline: 0,
+  backgroundColor: theme.palette.ds?.bg_color_max,
+  '::placeholder': {
+    color: theme.palette.ds?.gray_600,
+  },
+}));
+
+const EndAdornment = styled(Box)({
+  justifySelf: 'end',
+  display: 'flex',
+  alignItems: 'center',
+  height: '100%',
+  width: 'max-content',
+});
