5c76a606944739bd5a0c3d23a35a1d045997cc0c
vantuz-subhuman <vantuz-subhuman@yandex.ru>
2025-08-07 16:11:01 +0300

    flow fix

--- packages/yoroi-extension/app/containers/wallet/WalletDelegationBanner.js
+++ packages/yoroi-extension/app/containers/wallet/WalletDelegationBanner.js
@@ -170,7 +170,7 @@ function WalletDelegationBanner({ isOpen, isWalletWithNoFunds, isTestnet, intl, 
             label={intl.formatMessage(globalMessages.delegateLabel)}
             disabled={isWalletWithNoFunds}
             isTestnet={isTestnet}
-            socialMediaInfo={{ socialLinks, websiteUrl: socialLinks.web ?? websiteUrl }}
+            socialMediaInfo={{ socialLinks, websiteUrl: socialLinks?.web ?? websiteUrl }}
           />
         </Box>
       </Box>
