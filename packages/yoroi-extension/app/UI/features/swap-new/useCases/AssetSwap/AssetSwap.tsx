a230e090824251135d79b6bdb1c3a56a40a92153
Sorin Chis <kish.sorin@yahoo.com>
2025-08-13 11:11:22 +0300

    uncoment function

--- packages/yoroi-extension/app/UI/features/swap-new/useCases/AssetSwap/AssetSwap.tsx
+++ packages/yoroi-extension/app/UI/features/swap-new/useCases/AssetSwap/AssetSwap.tsx
@@ -44,13 +44,12 @@ export const AssetSwap = () => {
     });
 
     try {
-      // Uncomment the following line to actually send the transaction as is not woking in the current context
-      // await stores.transactionProcessingStore.adaSendAndRefresh({
-      //   wallet,
-      //   signRequest: unisgnedTxRequest,
-      //   password,
-      //   callback: () => stores.wallets.refreshWalletFromRemote(wallet.publicDeriverId),
-      // });
+      await stores.transactionProcessingStore.adaSendAndRefresh({
+        wallet,
+        signRequest: unisgnedTxRequest,
+        password,
+        callback: () => stores.wallets.refreshWalletFromRemote(wallet.publicDeriverId),
+      });
       showTxResultModal(TransactionResult.SUCCESS);
     } catch (e) {
       showTxResultModal(TransactionResult.FAIL);
