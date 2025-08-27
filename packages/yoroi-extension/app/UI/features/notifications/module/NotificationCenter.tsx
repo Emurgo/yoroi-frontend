84b6250a6131bcc281bd0ff75badda0a039333fd
yushi <yushihuang@gmail.com>
2025-08-27 18:20:15 +0800

    refactor

--- packages/yoroi-extension/app/UI/features/notifications/module/NotificationCenter.tsx
+++ packages/yoroi-extension/app/UI/features/notifications/module/NotificationCenter.tsx
@@ -24,10 +24,87 @@ const StyledButton = styled(IconButton)(({ theme }: any) => ({
   },
 }));
 
+const NotificationList = () => {
+  const strings = useStrings();
+  const notifications = useModelValue(appState.notifications.all).value;
+
+  if (notifications && notifications.length) {
+    const hasUnread = notifications.find(n => !n.read);
+
+    return (
+      <>
+        <Stack spacing="16px" sx={{ padding: '16px' }}>
+          {notifications?.map(notification => (
+            <Box sx={{ display: 'flex', flexDirection: 'row', gap: '16px' }}>
+              <Box>
+                <Icon.Notification />
+              </Box>
+              <Box>
+                <Typography variant="body1" color="ds.text_gray_medium">
+                  {notification.title}
+                </Typography>
+                {notification.body !== notification.title && (
+                  <Typography variant="body1" color="ds.text_gray_medium">
+                    {notification.body}
+                  </Typography>
+                )}
+                {/* @ts-ignore */}
+                <Typography variant="caption1" color="ds.text_gray_low">
+                  {new Date(notification.time).toLocaleString()}
+                </Typography>
+              </Box>
+              {!notification.read && (
+                <Box
+                  sx={{
+                    height: '6px',
+                    width: '6px',
+                    borderRadius: '50%',
+                    backgroundColor: 'var(--static-red, rgba(255, 19, 81, 1))',
+                    marginLeft: 'auto',
+                    marginBottom: 'auto',
+                  }}
+                />
+              )}
+            </Box>
+          ))}
+        </Stack>
+        {hasUnread && (
+          <Box sx={{ marginTop: 'auto', display: 'flex', flexDirection: 'column' }}>
+            <Divider />
+            <Button
+              // @ts-ignore
+              variant="secondary"
+              onClick={() => {
+                call(appState.notifications.markAllRead);
+              }}
+              sx={{ margin: 'auto', marginTop: '16px', marginBottom: '16px' }}
+            >
+              {strings.readAll}
+            </Button>
+          </Box>
+        )}
+      </>
+    );
+  } else if (notifications) {
+    // empty
+    return (
+      <Stack spacing="16px" sx={{ margin: 'auto' }}>
+        <NoNotificationIllustration />
+        {/* @ts-ignore */}
+        <Typography variant="heading-3-regular" color="ds.text_gray_medium" sx={{ textAlign: 'center' }}>
+          {strings.noNotification}
+        </Typography>
+      </Stack>
+    );
+  } else {
+    // loading
+    return null;
+  }
+};
+
 export const NotificationCenter = () => {
   const strings = useStrings();
   const { isNotificationCenterOpen, setIsNotificationCenterOpen } = useNotifications();
-  const notifications = useModelValue(appState.notifications.all).value;
 
   if (!isNotificationCenterOpen) {
     return null;
@@ -56,70 +133,7 @@ export const NotificationCenter = () => {
             <Icon.CloseIcon />
           </StyledButton>
         </Stack>
-        {notifications && notifications.length ? (
-          <>
-            <Stack spacing="16px" sx={{ padding: '16px' }}>
-              {notifications?.map(notification => (
-                <Box sx={{ display: 'flex', flexDirection: 'row', gap: '16px' }}>
-                  <Box>
-                    <Icon.Notification />
-                  </Box>
-                  <Box>
-                    <Typography variant="body1" color="ds.text_gray_medium">
-                      {notification.title}
-                    </Typography>
-                    {notification.body !== notification.title && (
-                      <Typography variant="body1" color="ds.text_gray_medium">
-                        {notification.body}
-                      </Typography>
-                    )}
-                    {/* @ts-ignore */}
-                    <Typography variant="caption1" color="ds.text_gray_low">
-                      {new Date(notification.time).toLocaleString()}
-                    </Typography>
-                  </Box>
-                  {!notification.read && (
-                    <Box
-                      sx={{
-                        height: '6px',
-                        width: '6px',
-                        borderRadius: '50%',
-                        backgroundColor: 'var(--static-red, rgba(255, 19, 81, 1))',
-                        marginLeft: 'auto',
-                        marginBottom: 'auto',
-                      }}
-                    />
-                  )}
-                </Box>
-              ))}
-            </Stack>
-            {notifications?.find(n => !n.read) && (
-              <Box sx={{ marginTop: 'auto', display: 'flex', flexDirection: 'column' }}>
-                <Divider />
-                <Button
-                  // @ts-ignore
-                  variant="secondary"
-                  onClick={() => {
-                    call(appState.notifications.markAllRead);
-                  }}
-                  sx={{ margin: 'auto', marginTop: '16px', marginBottom: '16px' }}
-                >
-                  {strings.readAll}
-                </Button>
-              </Box>
-            )}
-          </>
-        ) : notifications ? (
-          // empty
-          <Stack spacing="16px" sx={{ margin: 'auto' }}>
-            <NoNotificationIllustration />
-            {/* @ts-ignore */}
-            <Typography variant="heading-3-regular" color="ds.text_gray_medium" sx={{ textAlign: 'center' }}>
-              {strings.noNotification}
-            </Typography>
-          </Stack>
-        ) : // loading
-        null}
+        <NotificationList />
       </StyledDrawer>
     </>
   );
