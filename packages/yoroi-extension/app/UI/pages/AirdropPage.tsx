7cada07581e89f5cc597206de6d7f22fb8847241
yushi <yushihuang@gmail.com>
2025-08-28 19:56:09 +0800

    expand search for airdrop destination address

--- packages/yoroi-extension/app/UI/pages/AirdropPage.tsx
+++ packages/yoroi-extension/app/UI/pages/AirdropPage.tsx
@@ -91,7 +91,16 @@ export default function AirdropPage({ stores }: Readonly<Props>) {
       wallet.allAddresses.utxoAddresses.find(a => a.address.Type === CoreAddressTypes.CARDANO_BASE && !a.address.IsUsed)
     ).address.Hash
   );
-
+  console.log(
+    '>>>',
+    JSON.stringify(
+      wallet.allAddresses.utxoAddresses
+        .filter(a => a.address.Type === CoreAddressTypes.CARDANO_BASE)
+        .map(a => [a.path.slice(-2).join(','), addressHexToBech32(a.address.Hash), a.address.IsUsed]),
+      null,
+      2
+    )
+  );
   const [originalDestAddrBech32, setOriginalDestAddrBech32] = useState('');
 
   useEffect(() => {
@@ -113,13 +122,16 @@ export default function AirdropPage({ stores }: Readonly<Props>) {
         if (currentWalletClaim) {
           setOriginalDestAddrBech32(currentWalletClaim.destAddr);
         } else {
+          const usedAddresses = wallet.allAddresses.utxoAddresses
+            .filter(a => a.address.Type === CoreAddressTypes.CARDANO_BASE && a.address.IsUsed)
+            .sort((addr1, addr2) => addr2.path[4] - addr1.path[4]);
+          const unusedAddresses = wallet.allAddresses.utxoAddresses.filter(
+            a => a.address.Type === CoreAddressTypes.CARDANO_BASE && !a.address.IsUsed
+          );
           const result = await scanForOriginalDestAddress(
             claimEndpoint,
             destAddrBech32,
-            wallet.allAddresses.utxoAddresses
-              .filter(a => a.address.Type === CoreAddressTypes.CARDANO_BASE && a.address.IsUsed)
-              .sort((addr1, addr2) => addr2.path[4] - addr1.path[4])
-              .map(addr => addressHexToBech32(addr.address.Hash))
+            [...usedAddresses, ...unusedAddresses].map(addr => addressHexToBech32(addr.address.Hash))
           );
           if (result) {
             setOriginalDestAddrBech32(result.destAddr);
