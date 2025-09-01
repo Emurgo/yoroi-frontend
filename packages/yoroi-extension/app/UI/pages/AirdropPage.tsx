8554973b56a0472c0257d4f3340dd65e07c468b1
yushi <yushihuang@gmail.com>
2025-09-01 19:05:28 +0800

    explicitly show scan api call error

--- packages/yoroi-extension/app/UI/pages/AirdropPage.tsx
+++ packages/yoroi-extension/app/UI/pages/AirdropPage.tsx
@@ -129,7 +129,7 @@ export default function AirdropPage({ stores }: Readonly<Props>) {
             destAddrBech32,
             [...usedAddresses, ...unusedAddresses].map(addr => addressHexToBech32(addr.address.Hash))
           );
-          if (result) {
+          if (result && result.success) {
             setOriginalDestAddrBech32(result.destAddr);
             airdropClaims.push({
               publicDeriverId: wallet.publicDeriverId,
@@ -138,6 +138,8 @@ export default function AirdropPage({ stores }: Readonly<Props>) {
               amount: result.amount,
             });
             await localStorageApi.saveAirdropClaimResults(airdropClaims);
+          } else if (result) {
+            setOriginalDestAddrBech32(result.error);
           }
         }
       }
