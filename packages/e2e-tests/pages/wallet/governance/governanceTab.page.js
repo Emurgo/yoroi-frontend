21c86ad258acb42104932ad70562743a658db96a
Denis Nebytov <nebytov.d@gmail.com>
2025-09-09 15:26:33 +0300

    create Governance page object

---
+++ packages/e2e-tests/pages/wallet/governance/governanceTab.page.js
@@ -0,0 +1,104 @@
+import WalletCommonBase from '../../walletCommonBase.page';
+import { ElementLocator } from '../../locator.js';
+import { defaultWaitTimeout, fiveSeconds, halfSecond, quarterSecond } from '../../../helpers/timeConstants.js';
+import { pageTitle } from '../../../helpers/pageTitles.js';
+
+export default class GovernanceTab extends WalletCommonBase {
+  // locators
+  /** @type {ElementLocator} */
+  titleLocator = {
+    locator: 'governance-title-text',
+    method: 'id',
+  };
+  /** @type {ElementLocator} */
+  pageDescriptionLocator = {
+    locator: 'governance-status-text',
+    method: 'id',
+  };
+  /** @type {ElementLocator} */
+  delegateToYoroiBtnLocator = {
+    locator: 'governance-delegateToYoroiDRep-button',
+    method: 'id',
+  };
+  /** @type {ElementLocator} */
+  delegateToYoroiSkeletonLocator = {
+    locator: 'governance-delegateToYoroiDRepSkeleton-component',
+    method: 'id',
+  };
+  /** @type {ElementLocator} */
+  delegateToDrepBtnLocator = {
+    locator: 'governance-delegateToADRep-button',
+    method: 'id',
+  };
+  /** @type {ElementLocator} */
+  delegateToDrepSkeletonLocator = {
+    locator: 'governance-delegateToADRepSkeleton-component',
+    method: 'id',
+  };
+  /** @type {ElementLocator} */
+  abstainBtnLocator = {
+    locator: 'governance-abstain-button',
+    method: 'id',
+  };
+  /** @type {ElementLocator} */
+  abstainSkeletonLocator = {
+    locator: 'governance-abstainSkeleton-component',
+    method: 'id',
+  };
+  /** @type {ElementLocator} */
+  noConfidenceBtnLocator = {
+    locator: 'governance-noConfidence-button',
+    method: 'id',
+  };
+  /** @type {ElementLocator} */
+  noConfidenceSkeletonLocator = {
+    locator: 'governance-noConfidenceSkeleton-component',
+    method: 'id',
+  };
+  /** @type {ElementLocator} */
+  learnMoreLinkLocator = {
+    locator: 'governance-learnMore-link',
+    method: 'id',
+  };
+
+  // methods
+  async isDisplayed() {
+    const curPageTitle = await this.getPageTitle();
+    const discriptionDisplayedPromise = this.customWaitIsPresented(this.pageDescriptionLocator, fiveSeconds, quarterSecond);
+    const linkDisplayedPromise = this.customWaitIsPresented(this.learnMoreLinkLocator, fiveSeconds, quarterSecond);
+    const [discriptionDisplayed, linkDisplayed] = await Promise.all([discriptionDisplayedPromise, linkDisplayedPromise]);
+    return curPageTitle === pageTitle.governance && discriptionDisplayed && linkDisplayed;
+  }
+
+  async votingCardsAreDisplayed() {
+    const yoroiCardPromise = this.customWaitIsPresented(this.delegateToYoroiBtnLocator, fiveSeconds, quarterSecond);
+    const drepCardPromise = this.customWaitIsPresented(this.delegateToDrepBtnLocator, fiveSeconds, quarterSecond);
+    const abstainCardPromise = this.customWaitIsPresented(this.abstainBtnLocator, fiveSeconds, quarterSecond);
+    const noConfidenceCardPromise = this.customWaitIsPresented(this.noConfidenceBtnLocator, fiveSeconds, quarterSecond);
+
+    const allDisplayed = await Promise.all([yoroiCardPromise, drepCardPromise, abstainCardPromise, noConfidenceCardPromise]);
+
+    return allDisplayed.every(result => result === true);
+  }
+
+  async isLoaded() {
+    const yoroiSkeletonDisplayedPromise = this.customWaitIsNotPresented(this.delegateToYoroiSkeletonLocator);
+    const drepSkeletonDisplayedPromise = this.customWaitIsNotPresented(this.delegateToDrepSkeletonLocator);
+    const abstainSkeletonDisplayedPromise = this.customWaitIsNotPresented(this.abstainSkeletonLocator);
+    const noConfidenceSkeletonDisplayedPromise = this.customWaitIsNotPresented(this.noConfidenceSkeletonLocator);
+    const allResults = await Promise.all([
+      yoroiSkeletonDisplayedPromise,
+      drepSkeletonDisplayedPromise,
+      abstainSkeletonDisplayedPromise,
+      noConfidenceSkeletonDisplayedPromise,
+    ]);
+
+    const allLoaded = allResults.every(result => result === true);
+
+    if (allLoaded) {
+      return await this.votingCardsAreDisplayed();
+    } else {
+      throw new Error(`The governance voting cards are still loading after ${defaultWaitTimeout / 1000} seconds`);
+    }
+  }
+}
