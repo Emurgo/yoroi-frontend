ecddbc6c2f7e51aa8ae3fdeda00943d0d3674c8d
Denis Nebytov <nebytov.d@gmail.com>
2025-09-15 15:05:42 +0300

    fix ids for nft image

--- packages/e2e-tests/pages/wallet/nftGallery/nftDetails.page.js
+++ packages/e2e-tests/pages/wallet/nftGallery/nftDetails.page.js
@@ -31,7 +31,7 @@ export default class NftDetails extends WalletCommonBase {
   };
   /**@type {ElementLocator} */
   nftImageZoomedLocator = {
-    locator: 'nftDetails:zoomed-image-component',
+    locator: 'nftDetails:zoomedImage-image-component',
     method: 'id',
   };
   /**@type {ElementLocator} */
@@ -268,7 +268,7 @@ export default class NftDetails extends WalletCommonBase {
    */
   async zoomImage() {
     this.logger.info(`NftDetails::zoomImage is called`);
-    await this.click(this.nftImageBoxLocator);
+    await this.click(this.nftImageLocator);
   }
   /**
    * Getting the zoomed NFT image link
