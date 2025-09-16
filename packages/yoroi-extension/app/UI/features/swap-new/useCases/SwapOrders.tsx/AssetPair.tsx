0925ff38239db482609c6200d53df2ddd74fc75d
Sorin Chis <kish.sorin@yahoo.com>
2025-09-16 13:32:09 +0300

    remove unused comp

--- packages/yoroi-extension/app/UI/features/swap-new/useCases/SwapOrders.tsx/AssetPair.tsx
+++ packages/yoroi-extension/app/UI/features/swap-new/useCases/SwapOrders.tsx/AssetPair.tsx
@@ -1,10 +1,6 @@
-import { Box, useTheme } from '@mui/material';
+import { Box } from '@mui/material';
 import type { SxProps, Theme } from '@mui/system';
 
-import adaTokenImage from '../../../../../assets/images/ada.inline.svg';
-import defaultTokenImage from '../../../../../assets/images/revamp/token-default.inline.svg';
-import defaultTokenDarkImage from '../../../../../assets/images/revamp/asset-default-dark.inline.svg';
-import { urlResolveForIpfsAndCorsproxy } from '../../../../../coreUtils';
 import { Portfolio } from '@yoroi/types';
 import { TokenInfoIcon } from '../../../portfolio/common/components/TokenInfoIcon';
 import { normalizeTokenId } from '../../common/helpers';
@@ -15,43 +11,6 @@ type Props = {
   tokenOutID: Portfolio.Token.Id;
   sx?: SxProps<Theme>;
   defaultTokenInfo: Portfolio.Token.Info;
-};
-
-function tokenImgSrc(token: Portfolio.Token.Info, defaultTokenInfo: Portfolio.Token.Info): string {
-  return token.ticker === defaultTokenInfo.ticker
-    ? adaTokenImage
-    : (urlResolveForIpfsAndCorsproxy(token.originalImage) ?? defaultTokenImage);
-}
-
-export const tokenImg = (
-  token: Portfolio.Token.Info,
-  defaultTokenInfo: Portfolio.Token.Info,
-  width: number | string = '24px',
-  height: number | string = '24px'
-): React.ReactNode => {
-  const theme = useTheme() as Theme & { name?: string };
-  const fallbackImage = theme.name === 'dark-theme' ? defaultTokenDarkImage : defaultTokenImage;
-
-  return (
-    <Box
-      width={width}
-      height={height}
-      sx={{
-        overflowY: 'hidden',
-        '& > svg': { width: '100%', height: '100%' },
-        borderRadius: '4px',
-      }}
-    >
-      <img
-        width="100%"
-        src={tokenImgSrc(token, defaultTokenInfo)}
-        alt=""
-        onError={(e): void => {
-          (e.currentTarget as HTMLImageElement).src = fallbackImage;
-        }}
-      />
-    </Box>
-  );
 };
 
 const AssetPair = ({ tokenInID, tokenOutID, defaultTokenInfo, sx = {} }: Props): React.ReactNode => {
