f509ca3aa149e904f22487865e6bda0277381a71
Denis Nebytov <nebytov.d@gmail.com>
2025-09-22 15:15:31 +0300

    add portfolioDetails template page

---
+++ packages/e2e-tests/pages/wallet/portfolio/protfolioDetails.page.js
@@ -0,0 +1,7 @@
+import WalletCommonBase from '../../walletCommonBase.page.js';
+import { ElementLocator } from '../../locator.js';
+
+export default class PortfolioTab extends WalletCommonBase {
+    // locators
+    // methods
+}
