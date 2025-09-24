839e966afc5d8296e9d9286516300dd45fa82c61
Denis Nebytov <denis@emurgo.io>
2025-09-24 10:35:54 +0300

    Apply suggestions from code review
    Co-authored-by: Amin Limbada <aminlimbada94@gmail.com>

--- packages/yoroi-extension/app/UI/components/BackButton.tsx
+++ packages/yoroi-extension/app/UI/components/BackButton.tsx
@@ -14,9 +14,9 @@ const SButton = styled(Button)(({ theme }: any) => ({
   },
 }));
 
-export const BackButton = ({ label, onAction, pathId }: { label: string; onAction: () => void; pathId?: string }) => {
+export const BackButton = ({ label, onAction, pathId=''}: { label: string; onAction: () => void; pathId?: string }) => {
   return (
-    <SButton onClick={onAction} startIcon={<BackIcon />} id={`${pathId || ''}-back-button`}>
+    <SButton onClick={onAction} startIcon={<BackIcon />} id={`${pathId}-back-button`}>
       <Typography fontWeight="500" fontSize="14px">
         {label}
       </Typography>
