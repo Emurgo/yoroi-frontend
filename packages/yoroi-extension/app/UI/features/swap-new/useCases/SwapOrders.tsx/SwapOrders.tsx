64d63f35a9a6d4fc5f5d060e10601c2205f1ef5f
Sorin Chis <kish.sorin@yahoo.com>
2025-09-30 12:25:38 +0300

    update cancel order

--- packages/yoroi-extension/app/UI/features/swap-new/useCases/SwapOrders.tsx/SwapOrders.tsx
+++ packages/yoroi-extension/app/UI/features/swap-new/useCases/SwapOrders.tsx/SwapOrders.tsx
@@ -241,11 +241,17 @@ const OrderCancelation = ({ order }: { order: Swap.Order }) => {
     try {
       startLoadingTxReview();
       try {
-        await stores.transactionProcessingStore.adaSignTransactionHexFromWallet({
-          wallet,
-          transactionHex: cancelTxCbor,
-          password: passswordInput,
-        });
+      const { signedTxHex: signedCancelTx } = await stores.transactionProcessingStore.adaSignTransactionHexFromWallet({
+        wallet,
+        transactionHex: cancelTxCbor,
+        password: passswordInput,
+      });
+
+      const signedTransactionHexes: any = [signedCancelTx];
+      await stores.substores.ada.swapStore.executeTransactionHexes({
+        wallet,
+        signedTransactionHexes,
+      });
 
         showTxResultModal(TransactionResult.SUCCESS);
       } catch (error) {
