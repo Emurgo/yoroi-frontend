981afffc5ee9b2894b0f866cac531eac6035ff9e
Sorin Chis <kish.sorin@yahoo.com>
2025-10-08 13:42:34 +0300

    Update packages/yoroi-extension/app/UI/features/swap-new/useCases/SwapOrders.tsx/NoOpenOrders.tsx
    Co-authored-by: Denis Nebytov <denis@emurgo.io>

--- packages/yoroi-extension/app/UI/features/swap-new/useCases/SwapOrders.tsx/NoOpenOrders.tsx
+++ packages/yoroi-extension/app/UI/features/swap-new/useCases/SwapOrders.tsx/NoOpenOrders.tsx
@@ -1,5 +1,5 @@
 import { Stack, Typography } from '@mui/material';
-import { ReactComponent as NoOpenOders } from '../../../../../assets/images/revamp/no-complete-orders.inline.svg';
+import { ReactComponent as NoOpenOders } from '../../../../../assets/images/revamp/no-open-orders.inline.svg';
 import { useStrings } from '../../common/hooks/useStrings';
 
 const NoOpenOrders = (): React.ReactNode => {
