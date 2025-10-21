1d1e0c3b94999e0ba5513a4224c40bf4c207660e
Denis Nebytov <denis@emurgo.io>
2025-10-21 17:32:06 +0300

    stabilize test (#4380)
    Co-authored-by: Amin Limbada <aminlimbada94@gmail.com>

--- packages/e2e-tests/test/Portfolio_03_switch_currencies.test.js
+++ packages/e2e-tests/test/Portfolio_03_switch_currencies.test.js
@@ -7,7 +7,6 @@ import { Logger } from 'simple-node-logger';
 import { oneMinute } from '../helpers/timeConstants.js';
 import { prepareWallet } from '../helpers/restoreWalletHelper.js';
 import BasePage from '../pages/basepage.js';
-import WalletTab from '../pages/wallet/walletTab/walletTab.page.js';
 import PortfolioTab from '../pages/wallet/portfolio/porfolioMain.page.js';
 import PortfolioTokenDetails from '../pages/wallet/portfolio/portfolioDetails.page.js';
 import TransactionsSubTab from '../pages/wallet/walletTab/walletTransactions.page.js';
@@ -21,43 +20,54 @@ describe('Portfolio switching currencies', function () {
   let webdriver = null;
   /** @type {Logger} */
   let logger = null;
+  /** @type {TransactionsSubTab} */
+  let transactionsPage = null;
+  /** @type {SettingsTab} */
+  let settingsPage = null;
+  /** @type {GeneralSubTab} */
+  let generalSubTab = null;
+  /** @type {PortfolioTab} */
+  let portfolioMainPage = null;
+  /** @type {PortfolioTokenDetails} */
+  let detailsPage = null;
 
   before(async function () {
     logger = getTestLogger(this.test.parent.title);
     webdriver = await driversPoolsManager.getDriverFromPool();
     await prepareWallet(webdriver, logger, 'testWallet1Mainnet', this, false);
+    transactionsPage = new TransactionsSubTab(webdriver, logger);
+    settingsPage = new SettingsTab(webdriver, logger);
+    generalSubTab = new GeneralSubTab(webdriver, logger);
+    portfolioMainPage = new PortfolioTab(webdriver, logger);
+    detailsPage = new PortfolioTokenDetails(webdriver, logger);
   });
 
   const testData = ['BRL', 'ETH', 'BTC', 'KRW', 'CNY', 'EUR', 'JPY', 'USD'];
 
   for (const testDatum of testData) {
     describe(`Change currency to ${testDatum}`, function () {
-      it('Open General settings', async function () {
-        const transactionsPage = new TransactionsSubTab(webdriver, logger);
+      it(`${testDatum} Open General settings`, async function () {
         await transactionsPage.goToSettingsTab();
-        const settingsPage = new SettingsTab(webdriver, logger);
         await settingsPage.goToGeneralSubMenu();
       });
 
       it(`Select currency ${testDatum}`, async function () {
-        const generalSubTab = new GeneralSubTab(webdriver, logger);
         await generalSubTab.selectFiat(testDatum);
       });
 
-      it('Open Portfolio page', async function () {
-        const walletCommonPage = new WalletTab(webdriver, logger);
-        await walletCommonPage.goToPortfolioTab();
-        const portfolioMainPage = new PortfolioTab(webdriver, logger);
+      it(`${testDatum} Open Portfolio page`, async function () {
+        await generalSubTab.goToPortfolioTab();
         const pageIsDisplayed = await portfolioMainPage.isDisplayed();
         expect(pageIsDisplayed, 'Portfolio page is not displayed').to.be.true;
         const isLoaded = await portfolioMainPage.waitIsLoaded();
         expect(isLoaded, 'Portfolio page is not loaded').to.be.true;
       });
 
-      it('Switch to second currency', async function () {
-        const portfolioMainPage = new PortfolioTab(webdriver, logger);
+      it(`${testDatum} Switch to second currency`, async function () {
         const pageIsLoaded = await portfolioMainPage.waitIsLoaded();
         expect(pageIsLoaded, 'Portfolio is not loaded').to.be.true;
+        const pricesAreLoaded = await portfolioMainPage.waitPriceIsLoaded();
+        expect(pricesAreLoaded, 'Tokens prices are not loaded').to.be.true;
         const balanceBeforeSwitch = await portfolioMainPage.getPortfolioBalance();
         await portfolioMainPage.switchCurrencies();
         const balanceAfterSwitch = await portfolioMainPage.getPortfolioBalance();
@@ -71,8 +81,7 @@ describe('Portfolio switching currencies', function () {
         expect(balanceAfterSwitch.secondary.fiat, 'Token is not ada after switching').to.be.equal(balanceBeforeSwitch.main.fiat);
       });
 
-      it('Check fiat in price column', async function () {
-        const portfolioMainPage = new PortfolioTab(webdriver, logger);
+      it(`${testDatum} Check fiat in price column`, async function () {
         const tokensPrices = await portfolioMainPage.getColumnValues(Columns.Price);
         for (const tokenPrice of tokensPrices) {
           if (tokenPrice.value) {
@@ -81,20 +90,17 @@ describe('Portfolio switching currencies', function () {
         }
       });
 
-      it('Check fiat in total column', async function () {
-        const portfolioMainPage = new PortfolioTab(webdriver, logger);
+      it(`${testDatum} Check fiat in total column`, async function () {
         const totalValues = await portfolioMainPage.getColumnValues(Columns.Total);
         for (const totalValue of totalValues) {
           expect(totalValue.currency.fiat, 'Different fiat is in the total column').to.be.equal(testDatum);
         }
       });
 
-      it('Check info on token details', async function () {
-        const portfolioMainPage = new PortfolioTab(webdriver, logger);
+      it(`${testDatum} Check info on token details`, async function () {
         await portfolioMainPage.switchCurrencies();
         const balanceMainPage = await portfolioMainPage.getPortfolioBalance();
         await portfolioMainPage.selectTokenByIndex(0);
-        const detailsPage = new PortfolioTokenDetails(webdriver, logger);
         const mainBalanceDetailsPage = await detailsPage.getMainBalance();
         const secondsBalanceDetailsPage = await detailsPage.getSecondBalance();
 
