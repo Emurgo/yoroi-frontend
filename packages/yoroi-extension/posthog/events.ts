a4c1a64dedb4a9410db0ea7cab19da6409c34fda
Sorin Chis <kish.sorin@yahoo.com>
2025-11-13 15:08:23 +0200

    add missing event

--- packages/yoroi-extension/posthog/events.ts
+++ packages/yoroi-extension/posthog/events.ts
@@ -31,6 +31,7 @@ export interface EventDefinitions {
   'Cashback Dashboard Viewed': [];
   'Settings Page Viewed': [];
   'Transactions Page Viewed': [];
+  'Transaction Review Submit Modal Viewed': [];
   'Midnight Airdrop Page Viewed': [];
   'Swap Initiated': [];
   'Swap Review Page Viewed': [];
