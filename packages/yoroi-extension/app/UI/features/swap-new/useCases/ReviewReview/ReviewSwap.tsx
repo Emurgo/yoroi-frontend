f31d9db00af51b342ed7321df9d6082d9775ada8
Sorin Chis <kish.sorin@yahoo.com>
2025-11-14 13:10:16 +0200

    add scroll in review screen

--- packages/yoroi-extension/app/UI/features/swap-new/useCases/ReviewReview/ReviewSwap.tsx
+++ packages/yoroi-extension/app/UI/features/swap-new/useCases/ReviewReview/ReviewSwap.tsx
@@ -1,24 +1,31 @@
 import { Button, Stack, Typography, styled, useTheme } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import { useEffect } from 'react';
+
 import { PriceImpactBanner } from './PriceImpactBanner';
 import { EstimateSummary } from '../AssetSwap/EstimateSummary';
+import { AssetSummary } from '../../common/components/AssetSummary';
+
 import { useTxReviewModal } from '../../../transaction-review/module/ReviewTxProvider';
 import { SwapActionType, useSwapRevamp } from '../../module/SwapContextProvider';
 import { TransactionResult } from '../../../transaction-review/common/types';
-import { LoadingButton } from '@mui/lab';
-import { useNavigateTo } from '../../common/hooks/useNavigateTo';
-import { useEffect } from 'react';
-import { AssetSummary } from '../../common/components/AssetSummary';
-import { ASSET_DIRECTION_OUT } from '../../common/constants';
-import { useStrings } from '../../common/hooks/useStrings';
 
-const ReviewSwap = ({ stores }) => {
+import { useNavigateTo } from '../../common/hooks/useNavigateTo';
+import { useStrings } from '../../common/hooks/useStrings';
+import { ASSET_DIRECTION_OUT } from '../../common/constants';
+
+type ReviewSwapProps = {
+  stores: any;
+};
+
+const ReviewSwap = ({ stores }: ReviewSwapProps) => {
   const { swapForm } = useSwapRevamp();
   const wallet = stores.wallets.selectedOrFail;
   const strings = useStrings();
   const navigateTo = useNavigateTo();
   const { palette }: any = useTheme();
   const { openTxReviewModal, closeTxReviewModal, showTxResultModal } = useTxReviewModal();
-  
+
   const tokenInInfo = swapForm['tokenInInput'];
   const tokenOutInfo = swapForm['tokenOutInput'];
 
@@ -65,25 +72,32 @@ const ReviewSwap = ({ stores }) => {
   };
 
   return (
-    <Content direction="column" justifyContent="space-between">
-      <Stack>
+    <Content direction="column">
+      <ScrollArea>
         <PriceImpactBanner />
+
         <Typography variant="body1" mb={12}>
           {strings.swapFromLabel}
         </Typography>
         <AssetSummary tokenId={tokenInInfo.tokenId} value={tokenInInfo.value} />
+
         <Stack pb={20} />
+
         <Typography variant="body1" mb={12}>
           {strings.swapToLabel}
         </Typography>
         <AssetSummary tokenId={tokenOutInfo.tokenId} value={tokenOutInfo.value} direction={ASSET_DIRECTION_OUT} />
+
         <Stack pb={24} mb={24} borderBottom={`1px solid ${palette.ds.gray_200}`} />
+
         <Stack>
           <Typography fontWeight={500}>{strings.swapDetails}</Typography>
           <EstimateSummary showPriceImpact />
         </Stack>
-      </Stack>
-      <Stack direction="row" justifyContent="center" spacing={24} mt={50}>
+      </ScrollArea>
+
+      {/* Fixed bottom buttons */}
+      <Footer direction="row" justifyContent="center" spacing={24}>
         <Button
           //  @ts-ignore
           variant="secondary"
@@ -94,11 +108,12 @@ const ReviewSwap = ({ stores }) => {
         >
           {strings.backLabel}
         </Button>
+
         {/* @ts-ignore */}
         <LoadingButton variant="primary" onClick={openTxReview}>
           {strings.confirmLabel}
         </LoadingButton>
-      </Stack>
+      </Footer>
     </Content>
   );
 };
@@ -110,4 +125,27 @@ const Content = styled(Stack)(({ theme }: any) => ({
   width: '503px',
   margin: '0 auto',
   height: '100%',
+  maxHeight: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+}));
+
+const ScrollArea = styled(Stack)(() => ({
+  flex: 1,
+  overflowY: 'auto',
+  paddingBottom: '24px',
+  msOverflowStyle: 'none',
+  scrollbarWidth: 'none',
+  '&::-webkit-scrollbar': {
+    display: 'none',
+  },
+}));
+
+/**
+ * Fixed footer bar with buttons
+ */
+const Footer = styled(Stack)(({ theme }: any) => ({
+  paddingTop: '24px',
+  borderTop: `1px solid ${theme.palette?.ds?.gray_200}`,
+  backgroundColor: theme.palette.background.paper,
 }));
