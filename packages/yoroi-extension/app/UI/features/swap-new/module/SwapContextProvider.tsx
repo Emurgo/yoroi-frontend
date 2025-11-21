ce213c31f0de44abd33daae3f0648aa22a4d4238
Denis Nebytov <nebytov.d@gmail.com>
2025-11-21 16:29:58 +0300

    fix orders after switching wallet

--- packages/yoroi-extension/app/UI/features/swap-new/module/SwapContextProvider.tsx
+++ packages/yoroi-extension/app/UI/features/swap-new/module/SwapContextProvider.tsx
@@ -54,13 +54,14 @@ export const SwapContextProvider = ({ children, currentWallet, stores }: any) =>
   const strings = useStrings();
 
   useEffect(() => {
-    const stakignAddr = stores.wallets.selected.stakingAddress;
+    if (!selectedWallet) return;
+    const stakignAddr = selectedWallet.stakingAddress;
     const skey = unwrapStakingKey(stakignAddr).to_keyhash()?.to_hex();
     if (skey == null) {
       throw new Error('Cannot get staking key from the wallet!');
     }
     setStakingKey(skey);
-  }, []);
+  }, [selectedWallet?.stakingAddress, selectedWallet?.publicDeriverId]);
 
   const swapManager = useMemo(() => {
     const storage = swapStorageMaker();
@@ -75,7 +76,7 @@ export const SwapContextProvider = ({ children, currentWallet, stores }: any) =>
       isPrimaryToken,
       partners,
     });
-  }, [stakingKey, primaryTokenInfo, partners]);
+  }, [stakingKey, walletAddresses, primaryTokenInfo, partners]);
 
   const { data: orders = [], refetch: refetchOrders } = useQuery({
     queryKey: ['useSwapOrders', stakingKey, swapManager.settings.routingPreference],
