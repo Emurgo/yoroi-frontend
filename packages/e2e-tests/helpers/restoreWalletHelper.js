87454202d969bb376d67acf0a71e1e15db32b9d1
Denis Nebytov <nebytov.d@gmail.com>
2025-12-12 19:52:15 +0300

    improve service worker restarting

--- packages/e2e-tests/helpers/restoreWalletHelper.js
+++ packages/e2e-tests/helpers/restoreWalletHelper.js
@@ -14,7 +14,7 @@ import CreateWalletStepTwo from '../pages/newWalletPages/createWalletSteps/creat
 import CreateWalletStepThree from '../pages/newWalletPages/createWalletSteps/createWalletStepThree.page.js';
 import { walletNameShortener } from '../utils/utils.js';
 import { extensionTabName, serviceWorkersLink, serviceWorkersTabName, WindowManager } from './windowManager.js';
-import { quarterSecond } from './timeConstants.js';
+import { fiveSeconds, quarterSecond } from './timeConstants.js';
 import NetworksInfoModal from '../pages/wallet/settingsTab/modals/networksInfoModal.page.js';
 import { WebDriver } from 'selenium-webdriver';
 import { Logger } from 'simple-node-logger';
@@ -169,22 +169,58 @@ export const restartServiceWorker = async (webdriver, logger) => {
   await basepage.sleep(quarterSecond);
 
   const stopBtnLocator = {
-    locator: 'div.worker-controls > cr-button:nth-child(1)',
+    locator: 'cr-button[data-command="stop"]',
     method: 'css',
   };
   const startBtnLocator = {
-    locator: 'div.registration-controls > cr-button:nth-child(2)',
+    locator: 'cr-button[data-command="start"]',
+    method: 'css',
+  };
+  const runningStatusLocator = {
+    locator: 'div.serviceworker-running-status > span.value',
     method: 'css',
   };
 
   await basepage.waitPresentedAndAct(stopBtnLocator, async () => {
     await basepage.click(stopBtnLocator);
-    await basepage.sleep(500);
+    const stopped = await basepage.customWaiter(
+      async () => {
+        try {
+          const runningStatus = await basepage.getText(runningStatusLocator);
+          return runningStatus === 'STOPPED';
+        } catch (error) {
+          return false;
+        }
+      },
+      fiveSeconds,
+      quarterSecond
+    );
+    if (!stopped) {
+      const currentStatus = await basepage.getText(runningStatusLocator).catch(() => 'unknown');
+      throw new Error(`Service worker did not stop. Current status: ${currentStatus}`);
+    }
+    logger.info(`Service worker stopped successfully. Status: STOPPED`);
   });
 
   await basepage.waitPresentedAndAct(startBtnLocator, async () => {
     await basepage.click(startBtnLocator);
-    await basepage.sleep(500);
+    const started = await basepage.customWaiter(
+      async () => {
+        try {
+          const runningStatus = await basepage.getText(runningStatusLocator);
+          return runningStatus === 'RUNNING';
+        } catch (error) {
+          return false;
+        }
+      },
+      fiveSeconds,
+      quarterSecond
+    );
+    if (!started) {
+      const currentStatus = await basepage.getText(runningStatusLocator).catch(() => 'unknown');
+      throw new Error(`Service worker did not start. Current status: ${currentStatus}`);
+    }
+    logger.info(`Service worker started successfully. Status: RUNNING`);
   });
 
   await windowManager.closeTabWindow(serviceWorkersTabName, extensionTabName);
