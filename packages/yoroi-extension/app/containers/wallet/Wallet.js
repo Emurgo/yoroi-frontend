f2815c0bfb59abc5b8e34c1685a19325b102e62c
Amin Limbada <aminlimbada94@gmail.com>
2025-12-30 13:11:10 +0400

    fix: ignore flow lint

--- packages/yoroi-extension/app/containers/wallet/Wallet.js
+++ packages/yoroi-extension/app/containers/wallet/Wallet.js
@@ -34,7 +34,9 @@ import { useYoroiRemoteConfig } from '../../UI/common/hooks/useYoroiRemoteConfig
 import { withYoroiRemoteConfig } from '../../UI/common/helpers/withYoroiRemoteConfig';
 // $FlowIgnore: suppressing this error
 import { StakepoolFeeIncrease } from '../../UI/components/Dialogs/StakepoolFeeIncrease';
+// $FlowIgnore: suppressing this error
 import { CardanoCardDialog } from '../../UI/components/Dialogs/CardanoCardDialog';
+
 type Props = {|
   +children: Node,
   +stores?: any,
