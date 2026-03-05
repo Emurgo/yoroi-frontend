ac711c7de309ec492c5c874eb51bfc7bff1c4973
Cerkoryn <30681834+Cerkoryn@users.noreply.github.com>
2026-03-05 09:07:25 -0500

    Remove unused constants

--- packages/yoroi-extension/app/UI/features/governace/common/constants.ts
+++ packages/yoroi-extension/app/UI/features/governace/common/constants.ts
@@ -7,8 +7,6 @@ export const LEARN_MORE_LINK =
   'https://help.yoroi-wallet.com/en/article/how-can-i-participate-in-governance-through-yoroi-155o8l3/ ';
 export const FIND_DREPS_LINK = 'https://beta.cexplorer.io/drep?tab=list';
 export const FIND_DREPS_LINK_TESTNET = 'https://preprod.cexplorer.io/drep';
-export const YOROI_DREP_ID = 'drep1ygr9tuapcanc3kpeyy4dc3vmrz9cfe5q7v9wj3x9j0ap3tswtre9j';
-export const YOROI_DREP_ID_TESTNET = 'drep1y23nc498g205wtvp9esysyxam0n7msusm5d734xqlzhvkgq3pn5r7';
 
 export const DREP_ALWAYS_ABSTAIN = API_ABSTAIN;
 export const DREP_ALWAYS_NO_CONFIDENCE = API_NO_CONFIDENCE;
