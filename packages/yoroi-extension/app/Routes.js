5d6a508a1af1b23f8c9a3687e2324929ec9517d6
zuzunker <zuzunker@ya.ru>
2026-03-16 23:58:24 +0300

    governance status page removed, straight to governance options

--- packages/yoroi-extension/app/Routes.js
+++ packages/yoroi-extension/app/Routes.js
@@ -47,8 +47,6 @@ import { DappCenterContextProvider } from './UI/features/dapp-center/module/Dapp
 // $FlowIgnore: suppressing this error
 import GovernanceOptionsPage from './UI/pages/Governance/GovernanceOptionsPage';
 // $FlowIgnore: suppressing this error
-import GovernanceStatusPage from './UI/pages/Governance/GovernanceStatusPage';
-// $FlowIgnore: suppressing this error
 import PortfolioDappsPage from './UI/pages/portfolio/PortfolioDappsPage';
 // $FlowIgnore: suppressing this error
 import NftsPage from './UI/pages/nfts/NftsPage';
@@ -253,8 +251,7 @@ export const YoroiRoutes = (stores: StoresMap): Node => {
           </Route>
 
           <Route element={<GovernanceSubpages stores={stores} />}>
-            <Route path={ROUTES.GOVERNANCE.ROOT} element={<GovernanceStatusPage stores={stores} />} />
-            <Route path={ROUTES.GOVERNANCE.OPTIONS} element={<GovernanceOptionsPage stores={stores} />} />
+            <Route path={ROUTES.GOVERNANCE.ROOT} element={<GovernanceOptionsPage stores={stores} />} />
           </Route>
           <Route element={<StakingSubpages stores={stores} />}>
             <Route path={ROUTES.STAKING_REVAMP.ROOT} element={<StakingPageRevamp stores={stores} />} />
--- packages/yoroi-extension/app/UI/features/governace/useCases/GovernanceOptions/DRepOptions.tsx
+++ packages/yoroi-extension/app/UI/features/governace/useCases/GovernanceOptions/DRepOptions.tsx
@@ -1,9 +1,7 @@
-import { Box, Typography, Stack } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { styled } from '@mui/system';
 import { Icon } from '../../../../components';
-import { useNavigateTo } from '../../common/useNavigateTo';
 import { useStrings } from '../../common/hooks/useStrings';
-import { useTheme } from '@mui/material/styles';
 import { DrepOptionsCard } from './DrepOptionsCard';
 import { useGovernance } from '../../module/GovernanceContextProvider';
 import { useGovernanceDelegation } from '../../common/hooks/useGovernanceDelegation';
@@ -15,9 +13,7 @@ import { OptionsSkeletonScreen } from '../../common/SkeletonCardLoaders';
 interface DRepOptionsScreenProps {}
 
 export const DRepOptions: React.FC<DRepOptionsScreenProps> = () => {
-  const navigateTo = useNavigateTo();
   const strings = useStrings();
-  const theme: any = useTheme();
 
   const { submitedTransactions } = useGovernance();
   const { loadingUnsignTx, openDelegateModalForCustomDrep, delegateToAbstain, delegateToNoConfidence } =
@@ -30,10 +26,6 @@ export const DRepOptions: React.FC<DRepOptionsScreenProps> = () => {
     governanceStatus,
     isDelegated,
   });
-
-  const onBack = () => {
-    navigateTo.selectRevampStatus();
-  };
 
   const drepOptionsConfig = [
     {
@@ -73,13 +65,6 @@ export const DRepOptions: React.FC<DRepOptionsScreenProps> = () => {
 
   return (
     <Container>
-      <Stack direction="row" alignSelf="flex-start" spacing={6} sx={{ cursor: 'pointer' }} onClick={onBack}>
-        <Icon.LeftArrow fill={theme.palette.ds.el_gray_medium} />
-        <Typography variant="body1" fontWeight={500} textTransform="uppercase">
-          {strings.backToDashboard}
-        </Typography>
-      </Stack>
-
       <TitleSection>
         <Typography variant="h5">{strings.chooseVotingPower}</Typography>
         <Typography variant="body1" color="ds.text_gray_low">
