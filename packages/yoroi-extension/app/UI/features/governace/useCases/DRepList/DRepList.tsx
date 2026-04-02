79104494327f3858eb7932b95efbd5dddd606e40
Yushi Huang <yushihuang@EMG-147.local>
2026-04-03 01:15:14 +0800

    format

--- packages/yoroi-extension/app/UI/features/governace/useCases/DRepList/DRepList.tsx
+++ packages/yoroi-extension/app/UI/features/governace/useCases/DRepList/DRepList.tsx
@@ -183,9 +183,7 @@ export const DRepList = () => {
       const accumulated: any[] = [];
       let page = 1;
       while (true) {
-        const res = await fetch(
-          `${backendServiceZero}/dreps/active?page=${page}&pageSize=${PAGE_SIZE}`
-        );
+        const res = await fetch(`${backendServiceZero}/dreps/active?page=${page}&pageSize=${PAGE_SIZE}`);
         if (!res.ok) throw new Error(`HTTP ${res.status}`);
         const data: any[] = await res.json();
         accumulated.push(...data);
