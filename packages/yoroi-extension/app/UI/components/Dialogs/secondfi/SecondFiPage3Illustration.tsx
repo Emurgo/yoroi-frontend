3faedf30751ac882699d462dbe38a31073f7d13b
Yushi Huang <yushihuang@EMG-147.local>
2026-04-09 23:24:09 +0800

    implement SecondFi teaser

---
+++ packages/yoroi-extension/app/UI/components/Dialogs/secondfi/SecondFiPage3Illustration.tsx
@@ -0,0 +1,5 @@
+import SecondFiIllustration3 from './SecondFiIllustration3.svg';
+
+export const SecondFiPage3Illustration = () => (
+  <img src={SecondFiIllustration3} alt="" width={280} height={280} style={{ flexShrink: 0 }} />
+);
